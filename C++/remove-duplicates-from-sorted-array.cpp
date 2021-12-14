bbd1205b85a3479f41a53cbe1feca0906d7e7acd
milochen0418 <milo_chen@mail2000.com.tw>
2021-12-14 11:03:02 +0800

    update code for readable reason

--- C++/remove-duplicates-from-sorted-array.cpp
+++ C++/remove-duplicates-from-sorted-array.cpp
@@ -1,15 +1,14 @@
 class Solution {
+    //https://leetcode.com/problems/remove-duplicates-from-sorted-array
 public:
     int removeDuplicates(vector<int>& nums) {
-        if(nums.empty()) return 0;
-        
-        int i = 0; 
+        if(nums.empty()) return 0;        
+        int k_idx = 0; 
         for (int j = 1; j < nums.size(); j++) {
-            if (nums[j] != nums[i]) {
-                i++;
-                nums[i] = nums[j];
+            if (nums[k_idx] != nums[j] ) {
+                nums[++k_idx] = nums[j];
             }
         }     
-        return i + 1;
+        return k_idx + 1; //number of k element in-placed
     }
 };
