1f8aacdcb0ff141bd1b5b1f2d5862d4db20de3ec
milochen0418 <milo_chen@mail2000.com.tw>
2021-12-14 13:39:46 +0800

    Add Question of Rotate Array in C++

---
+++ C++/rorate-array.cpp
@@ -0,0 +1,22 @@
+//https://leetcode.com/problems/rotate-array/
+//189. Rotate Array
+class Solution {
+public:
+    
+    //[0,1,2,3,4,5,6] steps 3
+    //0,3,6,2,5,1,4,0
+    
+    //[0,1,2,3,4,5,6,7] steps 4
+    //0,4,0
+    //1,5,1
+    //2,6,2
+    //3,7,3
+    
+    //[0,1,2,3,4,5,6,7] steps 2
+    //0,2,4,6,0
+    //1,3,5,7,1
+    
+    void rotate(vector<int>& nums, int k) {
+        
+    }
+};
