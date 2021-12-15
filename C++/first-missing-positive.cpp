537f854f06ffed83b86717cfd44e09733d1d3760
milochen0418 <milo_chen@mail2000.com.tw>
2021-12-15 20:54:05 +0800

    Add Plover version for C++ first-missing-positive.cpp

--- C++/first-missing-positive.cpp
+++ C++/first-missing-positive.cpp
@@ -2,6 +2,20 @@ class Solution {
     //https://leetcode.com/problems/first-missing-positive
 public:
     int firstMissingPositive(vector<int>& nums) {
+        //The passed answser is rewrite from Plover's Java answer
+        int n = nums.size();
+        for(int i = 0; i < n; i++) {
+            while(nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+                swap(nums[i], nums[nums[i]-1]);
+            }
+        }
+        for(int i = 0; i < n; i++) {
+            if(nums[i] != i + 1 ) {
+                return i+1;
+            }
+        }
+        return n+1;
+        /*
         unordered_set<int> uset;
         int max = 0;
         for(int num:nums){
@@ -26,5 +40,6 @@ public:
             }
         }
         return min;
+        */
     }
 };
