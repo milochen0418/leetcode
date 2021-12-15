403344af3f1a7041b21a738701253a1064132746
milochen0418 <milo_chen@mail2000.com.tw>
2021-12-15 20:39:32 +0800

    Add V02 for C++ find-all-numbers-disappeared-in-the-array without unordered set

--- C++/find-all-numbers-disappeared-in-an-array.cpp
+++ C++/find-all-numbers-disappeared-in-an-array.cpp
@@ -7,6 +7,24 @@ class Solution {
     //https://www.geeksforgeeks.org/unordered_set-in-cpp-stl/?ref=lbp
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
+        return findDisappearedNumbersV02(nums);    
+    }
+    
+    //The answser without uset is refer from here
+    //https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/discuss/1583652/C%2B%2B-Short-Solution-%2B-Python-One-Liner-Explained-No-Extra-Space
+    vector<int> findDisappearedNumbersV02(vector<int>& nums) {
+        for (int i = 0; i < nums.size(); i++) 
+            nums[abs(nums[i])-1] = -abs(nums[abs(nums[i])-1]);
+        
+        vector<int> res;
+        for (int i = 0; i < nums.size(); i++)
+            if (nums[i] > 0) res.push_back(i+1);
+        
+        return res;
+        
+    }
+    
+    vector<int> findDisappearedNumbersV01(vector<int>& nums) {
         unordered_set<int> uset; 
         for(int num: nums) {
             uset.insert(num);
@@ -19,5 +37,7 @@ public:
             }
         }
         return v;
+
     }
+
 };
