905fb0386fc58580a4c8d8180abb767a74a43e18
milochen0418 <milo_chen@mail2000.com.tw>
2021-12-16 19:00:13 +0800

    Add case thinking of first missing positive

--- C++/first-missing-positive.cpp
+++ C++/first-missing-positive.cpp
@@ -2,46 +2,33 @@ class Solution {
     //https://leetcode.com/problems/first-missing-positive
 public:
     int firstMissingPositive(vector<int>& nums) {
-        //The passed answser is rewrite from Plover's Java answer
+        //Interesting of the scope of solution ?
+        //The largest solution of missing positive will be n since size of nums[] is n.
+        //And the case will be like
+        //it mean nums[0]=1, nums[1]=2, ..., nums[n-1] = n or any arrangement of this sequence. 
+        //
+        //What about that one number is missing in this case? 
+        //for example,there is only one k in 1<=k<=n s.t. nums[k-1] != k 
+        //Then k is missing positive. 
+        
+        
         int n = nums.size();
         for(int i = 0; i < n; i++) {
             while(nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
                 swap(nums,i, nums[i]-1);
             }
         }
+
+
+        
+
+
         for(int i = 0; i < n; i++) {
             if(nums[i] != i + 1 ) {
                 return i+1;
             }
         }
         return n+1;
-
-        /*
-        unordered_set<int> uset;
-        int max = 0;
-        for(int num:nums){
-            uset.insert(num);
-            if(num > max) {
-                max = num;
-            }
-        }
-        
-        vector<int> v;
-        int upmax = max==INT_MAX ? max:max+1;
-        for(int i=1; i<=upmax;i++){
-            if(uset.find(i) == uset.end()) {
-                v.push_back(i);
-            }
-        }
-        
-        int min = max+1;
-        for(int val: v) {
-            if(val<min) {
-                min = val; 
-            }
-        }
-        return min;
-        */
     }
 
 private:
