e20439312ece861e19f753bfdbed2f81d5b32522
milochen0418 <milo_chen@mail2000.com.tw>
2022-01-05 21:03:38 +0800

    C++/prefix-and-suffix-search.cpp Solution 2 - Still Time Limit Exceeded

--- C++/prefix-and-suffix-search.cpp
+++ C++/prefix-and-suffix-search.cpp
@@ -1,23 +1,40 @@
 class WordFilter {
     //https://leetcode.com/problems/prefix-and-suffix-search
+    //refer Solution 2 in this address https://www.cnblogs.com/grandyang/p/8331660.html 
+private:
+    unordered_map<string, vector<int>> pre_map;
+    unordered_map<string, vector<int>> suf_map;    
 public:
-    vector<string> *w;
-    WordFilter(vector<string>& words) {
-        w = &words;
-    }
-    
-    int f(string prefix, string suffix) {
-        int i = w->size()-1;
-        while(i>=0) {
-            string str = (*w)[i--];
-            if(str.find(prefix) == 0 ) {
-                int rfind_expected_idx = str.size() - suffix.size() ;    
-                if(str.rfind(suffix) == rfind_expected_idx) {
-                    return i+1;
-                }
+    WordFilter(vector<string> words) {
+        for (int k = 0; k < words.size(); ++k) {
+            for (int i = 0; i <= words[k].size(); ++i) {
+                pre_map[words[k].substr(0, i)].push_back(k);
+            }
+            for (int i = 0; i <= words[k].size(); ++i) {
+                suf_map[words[k].substr(words[k].size() - i)].push_back(k);
             }
         }
-        return -1;   
+    }
+
+    int f(string prefix, string suffix) {
+        if (!pre_map.count(prefix) || !suf_map.count(suffix)) {
+            return -1;
+        }
+        vector<int> pre = pre_map[prefix];
+        vector<int> suf = suf_map[suffix];
+        int i = pre.size() - 1, j = suf.size() - 1;
+        while (i >= 0 && j >= 0) {
+            if (pre[i] < suf[j]) { 
+                --j;
+            }
+            else if (pre[i] > suf[j]) {    
+                --i;
+            }
+            else {
+                return pre[i];
+            }
+        }
+        return -1;
     }
 };
 
