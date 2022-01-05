cab3f81442af64d99de19ce06daa3ebff3999e31
milochen0418 <milo_chen@mail2000.com.tw>
2022-01-05 21:26:20 +0800

    time O(1) for prefix-and-suffix-search C++

--- C++/prefix-and-suffix-search.cpp
+++ C++/prefix-and-suffix-search.cpp
@@ -1,42 +1,29 @@
 class WordFilter {
     //https://leetcode.com/problems/prefix-and-suffix-search
-    //refer Solution 2 in this address https://www.cnblogs.com/grandyang/p/8331660.html 
+    //Idea: We want to use prefix_suffix  to search data in hash map  (unordered_map). 
+    //
+    //for each string in words, there are many kind of pattern prefix_suffix for this one string.
+    // So we just need to put all kind of case prefix_suffix into the unordered map. 
+    //Then search it by prefix_suffix
 private:
-    unordered_map<string, vector<int>> pre_map;
-    unordered_map<string, vector<int>> suf_map;    
+    unordered_map<string, int> m;    
 public:
     WordFilter(vector<string> words) {
         for (int k = 0; k < words.size(); ++k) {
             for (int i = 0; i <= words[k].size(); ++i) {
-                pre_map[words[k].substr(0, i)].push_back(k);
-            }
-            for (int i = 0; i <= words[k].size(); ++i) {
-                suf_map[words[k].substr(words[k].size() - i)].push_back(k);
+                for (int j = 0; j <= words[k].size(); ++j) {
+                    m[words[k].substr(0, i) + "_" + words[k].substr(words[k].size() - j)] = k;
+                }
             }
         }
     }
 
     int f(string prefix, string suffix) {
-        if (!pre_map.count(prefix) || !suf_map.count(suffix)) {
-            return -1;
-        }
-        vector<int> pre = pre_map[prefix];
-        vector<int> suf = suf_map[suffix];
-        int i = pre.size() - 1, j = suf.size() - 1;
-        while (i >= 0 && j >= 0) {
-            if (pre[i] < suf[j]) { 
-                --j;
-            }
-            else if (pre[i] > suf[j]) {    
-                --i;
-            }
-            else {
-                return pre[i];
-            }
-        }
-        return -1;
+        return (m.count(prefix + "_" + suffix)) ? m[prefix + "_" + suffix] : -1;
     }
 };
+
+
 
 /**
  * Your WordFilter object will be instantiated and called as such:
