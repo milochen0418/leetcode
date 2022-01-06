59cb4321df855cd4946a830c1ee5a50a6c82a227
milochen0418 <milo_chen@mail2000.com.tw>
2022-01-06 20:33:13 +0800

    Add c_str strncpy solution for insteading substr

--- C++/prefix-and-suffix-search.cpp
+++ C++/prefix-and-suffix-search.cpp
@@ -5,10 +5,12 @@ class WordFilter {
     //for each string in words, there are many kind of pattern prefix_suffix for this one string.
     // So we just need to put all kind of case prefix_suffix into the unordered map. 
     //Then search it by prefix_suffix
+
 private:
     unordered_map<string, int> m;    
 public:
     WordFilter(vector<string> words) {
+        /*
         for (int k = 0; k < words.size(); ++k) {
             for (int i = 0; i <= words[k].size(); ++i) {
                 for (int j = 0; j <= words[k].size(); ++j) {
@@ -16,11 +18,33 @@ public:
                 }
             }
         }
+        */
+        
+        for(int k = 0; k < words.size(); k++) {
+            string& word = words[k];
+            int word_len = word.length();            
+            const char * word_array =  (const char*)word.c_str();
+
+            for (int i = 0; i <= word_len; i++) {
+                for (int j = 0; j <= word_len; j++) {
+                    char str_array[(i+j+1)+1];
+                    str_array[(i+j+1)] = '\0';
+                    strncpy(str_array, word_array,i);
+                    str_array[i] = '_';
+                    strncpy(&str_array[i+1],&word_array[word_len - j], j );
+                    m[str_array] = k;
+                    
+                }
+            }
+        }
+        
+        
     }
 
     int f(string prefix, string suffix) {
         return (m.count(prefix + "_" + suffix)) ? m[prefix + "_" + suffix] : -1;
     }
+
 };
 
 
