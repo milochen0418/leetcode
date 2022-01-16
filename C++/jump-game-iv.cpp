8b579d60627b31c37fc9ce009b96b5f5d9c13bc2
milochen0418 <milo_chen@mail2000.com.tw>
2022-01-16 12:49:31 +0800

    Update to unordered_multimap

--- C++/jump-game-iv.cpp
+++ C++/jump-game-iv.cpp
@@ -1,138 +1,54 @@
+
 class Solution {
     //https://leetcode.com/problems/jump-game-iv
 public:
-    int someone(vector<int>& arr) {
-        int n = arr.size();
-        if(n == 1) return 0;
-        
-        unordered_map<int, vector<int>> mp;
-        
-        int step = 0;
-        
-        //fill map
-        for(int i = 0; i<n; i++){
-            mp[arr[i]].push_back(i);
-        }
-        
-        //queue
-        queue<int> q;
-        q.push(0);
-        
-        while(!q.empty()){
-            step++;
-            int size = q.size();
-            
-            for(int i = 0; i<size; i++){
-                int j = q.front();
-                q.pop();
-                
-                //jump to j-1
-                if(j-1 >= 0 && mp.find(arr[j-1]) != mp.end()){
-                //if(j-1 >= 0 ){                
-                    q.push(j-1);
-                }
-                
-                //jump to j+1
-                if(j+1 < n && mp.find(arr[j+1]) != mp.end()){
-                //if(j+1 < n){
-                    if(j+1 == n-1) return step;
-                    q.push(j+1);
-                }
-                
-                // jump to equal
-                if(mp.find(arr[j]) != mp.end()){
-                    for(auto k: mp[arr[j]]){
-                        if(k != j){
-                            if(k == n-1) return step;
-                            q.push(k);
-                        }
-                    }
-                }
-                mp.erase(arr[j]);
-            }
-        }
-        return step;        
-    }
-    int minJumps(vector<int>& arr) {
-        
-        //return someone(arr);
-        //unordered_multimap<int,int> mmap;
+    int minJumps(vector<int>& arr) {        
         unordered_multimap<int,int> mmap;
-        unordered_map<int, vector<int>> vmap;
-        
-        
         for(int i = 0; i < arr.size(); i++) {
             mmap.insert({arr[i],i});
-            vmap[arr[i]].push_back(i);
         }
     
         int end = arr.size() - 1;
         vector<bool> visited(arr.size(), false);
         vector<int> lens(arr.size(), 0);
         queue<int> q; 
-        visited[end] = true;
-        lens[end] = 0;
-        q.push(end);
+        visited[0] = true;
+        lens[0] = 0;
+        q.push(0);
         
         while(!q.empty()) {
             int i = q.front();            
             q.pop();
             
-            if(i == 0) {
+            if(i == end) {
                return lens[i]; 
             }
             
-            if( i+1 < arr.size()) {
-                int candidate = i + 1;
-                if(visited[candidate] == false) {
-                    visited[candidate] = true;
-                    lens[candidate] = lens[i] + 1;
-                    q.push(candidate);
-                }
+            if( i+1 < arr.size() && visited[i+1] == false) {
+                visited[i+1] = true;
+                lens[i+1] = lens[i] + 1;
+                q.push(i+1);
             } 
-            if( i-1 >= 0) {
-                int candidate = i - 1;
-                if(visited[candidate] == false) {
-                    visited[candidate] = true;
-                    lens[candidate] = lens[i] + 1;
-                    q.push(candidate);                    
-                }                
+            if( i-1 >= 0 && visited[i-1] == false) {
+                visited[i-1] = true;
+                lens[i-1] = lens[i] + 1;
+                q.push(i-1);                    
             }
-            
             
             if(mmap.find(arr[i]) != mmap.end()) {
                 auto p = mmap.equal_range(arr[i]);
                 for (auto it = p.first; it != p.second; ++it) {
-                    //std::cout << " " << it->first << ":" << it->second;
                     int j = it->second;
-                    if(i!=j ) {
-                        int candidate = j;
-                        if(visited[candidate] == false) {
-                            visited[candidate] = true;
-                            lens[candidate] = lens[i] + 1;
-                            q.push(candidate);                    
-                        }
+                    if(i!=j && visited[j] == false) {
+                        visited[j] = true;
+                        lens[j] = lens[i] + 1;
+                        q.push(j);
                     }
                 }
-                mmap.erase(arr[i]);
-            }
-            /*
-            if(vmap.find(arr[i]) != vmap.end()) {
-                auto v = vmap[arr[i]];
-                for(auto j:v) {
-                    if(i!=j ) {
-                        int candidate = j;
-                        if(visited[candidate] == false) {
-                            visited[candidate] = true;
-                            lens[candidate] = lens[i] + 1;
-                            q.push(candidate);                    
-                        }
-                    }                
-                }
-                vmap.erase(arr[i]);
-            }
-            */
-            
+                mmap.erase(arr[i]); 
+                //It can avoid redundant processing case, like this
+                //[7,7,7,7,....7,11]
+            }           
         }
         return 0;
     }
