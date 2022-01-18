9c55f4faf22d3c8cf17bb3a87d9c099184a95196
milochen0418 <milo_chen@mail2000.com.tw>
2022-01-18 10:15:45 +0800

    Process as like dummy

--- C++/can-place-flowers.cpp
+++ C++/can-place-flowers.cpp
@@ -1,6 +1,7 @@
 class Solution {
     //https://leetcode.com/problems/can-place-flowers
 public:
+    
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
         //0 0
         //00 0
@@ -19,17 +20,12 @@ public:
         int z_cnt = 1;
         int i = 0; 
         int planted_cnt = 0; 
-        for(int i = 0; i < flowerbed.size(); i++) {
-            if(flowerbed[i] != 0 || i + 1 >= flowerbed.size() ) {
-                if(flowerbed[i]  == 0 && i + 1 >= flowerbed.size() ) {
-                    z_cnt+=2;
-                }
-                if(z_cnt != 0) {
-                    planted_cnt += (z_cnt-1) / 2; 
-                    z_cnt = 0;   
-                }               
+        for(int i = 0; i < flowerbed.size()+2; i++) {
+            if(i==flowerbed.size() || (i<flowerbed.size() && flowerbed[i]==0) ){
+                z_cnt++;
             } else {
-                z_cnt++;  
+                planted_cnt += (z_cnt-1) / 2; 
+                z_cnt = 0;                
             }
         }
         if(n > planted_cnt) {
