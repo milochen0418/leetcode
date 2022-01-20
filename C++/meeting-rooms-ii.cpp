7be363d90ff429dd88047146478f61927e2d1f61
milochen0418 <milo_chen@mail2000.com.tw>
2022-01-20 11:12:55 +0800

    use default greater<int>

--- C++/meeting-rooms-ii.cpp
+++ C++/meeting-rooms-ii.cpp
@@ -1,16 +1,9 @@
 class Solution {
     //https://leetcode.com/problems/meeting-rooms-ii
 public:
-    struct compare  {  
-        bool operator()(const int& l, const int& r)  {  
-            return l > r;   
-        }  
-    };  
-        
     int minMeetingRooms(vector<vector<int>>& intervals) {
-        //default priority_queue is max queue. So following make min queue
-        priority_queue<int,vector<int>, compare > start_q;  
-        priority_queue<int,vector<int>, compare > end_q;  
+        priority_queue<int,vector<int>, greater<int> > start_q;  
+        priority_queue<int,vector<int>, greater<int> > end_q;  
         for(auto v:intervals) {
             start_q.push(v[0]);
             end_q.push(v[1]);
