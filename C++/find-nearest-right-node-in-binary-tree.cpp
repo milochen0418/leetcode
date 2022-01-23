ef417018f5912cfada953be45a5336f98641a655
milochen0418 <milo_chen@mail2000.com.tw>
2022-01-23 14:56:02 +0800

    C++ find-nearest-right-note-in-binary-tree (medium)refactor

--- C++/find-nearest-right-node-in-binary-tree.cpp
+++ C++/find-nearest-right-node-in-binary-tree.cpp
@@ -10,8 +10,9 @@
  * };
  */
 class Solution {
-    //https://leetcode.com/problems/find-nearest-right-node-in-binary-tree/
+    //https://leetcode.com/problems/find-nearest-right-node-in-binary-tree
 public:
+    
     TreeNode* findNearestRightNode(TreeNode* root, TreeNode* u) {
         
         queue<TreeNode*> qs[2]; 
@@ -19,50 +20,38 @@ public:
         qs[0] = queue<TreeNode*>();
         qs[1] = queue<TreeNode*>();
         
-        if(u==root)return nullptr;
+        if(u==root) return nullptr;
         
         qs[qidx].push(root);
         bool startToGetNextNode  = false;
         int step = 0;
         int saved_step = -1;
+        
         while(!qs[qidx].empty()) {
             int nidx = (qidx + 1) % 2;
             TreeNode* p = qs[qidx].front();
-            
-            if(p->left != nullptr) {
-                TreeNode* c = p->left;
-                if(startToGetNextNode==true) {
-                    if(saved_step == step) {
-                        return c;
-                    } else {
-                        return nullptr;
-                    }
-                } else {
-                    if(c == u) {
-                        saved_step = step;
-                        startToGetNextNode = true; 
-                        qs[nidx].push(c);
-                    }                    
-                }
-            }
-            if(p->right != nullptr) {
-                TreeNode* c = p->right;
-                if(startToGetNextNode==true) {
-                    if(saved_step == step) {
-                        return c;
-                    } else {
-                        return nullptr;
-                    }
-                } else {
-                    if(c == u) {
-                        saved_step = step;
-                        startToGetNextNode = true; 
-                        qs[nidx].push(c);
-                    }                    
-                }
-            }
-            
             qs[qidx].pop();
+            
+            TreeNode* childs[] = {p->left, p->right};
+            for(int i=0; i<2; i++) {
+                TreeNode*c = childs[i];
+                if(c!=nullptr) {
+                    if(startToGetNextNode==true) {
+                        if(saved_step == step) {
+                            return c;
+                        } else {
+                            return nullptr;
+                        }
+                    } else {
+                        if(c == u) {
+                            saved_step = step;
+                            startToGetNextNode = true; 
+                        }
+                    }
+                    qs[nidx].push(c);                    
+                }
+            }
+            
             if(qs[qidx].empty()) {
                 qidx=(qidx+1)%2;
                 step++;
