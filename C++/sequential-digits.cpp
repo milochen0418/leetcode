92808e5697be69b0cb6d05694792b34434adb7f2
milochen0418 <milo_chen@mail2000.com.tw>
2022-01-23 19:44:52 +0800

    C++ sequential-digits(medium)  answer

--- C++/sequential-digits.cpp
+++ C++/sequential-digits.cpp
@@ -2,13 +2,52 @@ class Solution {
     //https://leetcode.com/problems/sequential-digits/
 public:
     vector<int> sequentialDigits(int low, int high) {
-        vector<int> v; 
-        int test_val = 93241;
+        //return step1(low,high);
+        //Two case for fit overflow issue
+        //10
+        //1000000000
+        //372591426
+        //841554424
+        
+        vector<int> v;        
+        int temp_val = low;
+        while(true) {
+            int maxd = 1; 
+            int inc = 1;
+            int base = 1;
+            int i=2;
+            while((temp_val/10)/(maxd)>0) {
+                maxd*=10;
+                inc=(inc*10)+1;
+                base = base*10+i;
+                i++;
+                if(i>10) return v; ////it's enough to return answer v when i>10
+                
+            }
+            
+            for (int i = 0; i<(10-base%10); i++) {
+                int val = inc*i+base;
+                if(low<=val && val<=high) {
+                    v.push_back(val);    
+                }
+            }
+            
+            
+            if(temp_val/high>0) break;
+            if(temp_val > INT_MAX/10) return v; //it's enough to return answer when temp_val*10 can cause overflow
+            temp_val*=10;
+        }
+        return v;
+    }
+    
+    vector<int> step1(int low, int high) {
+        vector<int> v;
+        int temp_val = 100000000;
         int maxd = 1; 
         int inc = 1;
         int base = 1;
         int i=2;
-        while(test_val/(maxd*10)>0) {
+        while((temp_val/10)/(maxd)>0) {            
             maxd*=10;
             inc=(inc*10)+1;
             base = base*10+i;
