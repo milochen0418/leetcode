df0b4e8b312de7dc53d1df0068676640a630fc2f
milochen0418 <milo_chen@mail2000.com.tw>
2022-01-24 22:37:02 +0800

    beleive call by reference optimal

--- C++/stone-game-ii.cpp
+++ C++/stone-game-ii.cpp
@@ -1,25 +1,24 @@
 class Solution {
     vector<vector<int>> dp;
-    vector<int>* piles=nullptr;
 public:
-    int stoneGameII(vector<int> &ps) {
-        piles = &ps;
-        int n = piles->size();
+    int stoneGameII(vector<int> &piles) {
+        
+        int n = piles.size();
         dp.resize(n+1, vector<int>(n+1)); 
-        int total = accumulate(piles->begin(), piles->end(), 0);
-        int diff = more_than_yours(0, 1); 
+        int total = accumulate(piles.begin(), piles.end(), 0);
+        int diff = more_than_yours(piles, 0, 1); 
         return (total + diff)/2; 
     }
     
-    int more_than_yours(int i, int M){
-        if(i >= piles->size()) return 0; 
+    int more_than_yours(vector<int> &piles, int i, int M){
+        if(i >= piles.size()) return 0; 
         if(dp[i][M] != 0) return dp[i][M]; 
         int my_taken_in_turn = 0;
         int max_more_than_yours = INT_MIN; 
         
         for(int j=0; j<2*M; j++){   //1<=X<=2*M
-            if(i+j < piles->size()) my_taken_in_turn += (*piles)[i+j]; 
-            int competitor_more_than_mine = more_than_yours( i+j+1,  max(M, min(j+1, (int)piles->size())) );
+            if(i+j < piles.size()) my_taken_in_turn += piles[i+j]; 
+            int competitor_more_than_mine = more_than_yours(piles, i+j+1,  max(M, min(j+1, (int)piles.size())) );
             max_more_than_yours = max(max_more_than_yours, my_taken_in_turn - competitor_more_than_mine);
         }     
         return dp[i][M] = max_more_than_yours;
