4c4f93cf646289eb064ed8d6d26cef7a69d30243
milochen0418 <milo_chen@mail2000.com.tw>
2022-01-24 22:14:00 +0800

    C++ stone-game-ii refactor memory usage

--- C++/stone-game-ii.cpp
+++ C++/stone-game-ii.cpp
@@ -1,19 +1,21 @@
 class Solution {
     vector<vector<int>> dp;
+    vector<int>* piles=nullptr;
     //refer from https://leetcode.com/problems/stone-game-ii/discuss/1708282/C%2B%2B-Simple-Intuitive-Solution-W-Explanation-or-DP-or-Memorization
 public:
-    int stoneGameII(vector<int>& piles) {
-        int n = piles.size();
+    int stoneGameII(vector<int> &ps) {
+        piles = &ps;
+        int n = piles->size();
             
-        dp.resize(101, vector<int>(202)); 
+        dp.resize(n+1, vector<int>(n+1)); 
 		
-        int sum = accumulate(piles.begin(), piles.end(), 0), diff = solve(piles, 0, 1); //sum of stones in all piles
+        int sum = accumulate(piles->begin(), piles->end(), 0), diff = solve(0, 1); //sum of stones in all piles
         
         return (sum + diff)/2; //Max stones won by Alice - We need to maximize the diff so this can be maximum
     }
     
-    int solve(vector<int> piles, int i, int M){
-        if(i >= piles.size()) return 0; //If i>n-1 return 0
+    int solve(int i, int M){
+        if(i >= piles->size()) return 0; //If i>n-1 return 0
         
         if(dp[i][M] != 0) return dp[i][M]; //memorized value
         
@@ -21,9 +23,9 @@ public:
 		//ans - stores the maximum difference between Alice & Bob's stones
         
         for(int j=0; j<2*M; j++){   //1<=X<=2*M
-            if(i+j < piles.size()) total += piles[i+j]; //Each time we're incrementing X, add that index's stones to total
+            if(i+j < piles->size()) total += (*piles)[i+j]; //Each time we're incrementing X, add that index's stones to total
             
-            ans = max(ans, total-solve(piles, i+j+1, max(M, j+1))); //total-solve() is to check if we can maximize the score diff by incrementing X at any time.
+            ans = max(ans, total-solve(i+j+1, max(M, min(j+1,(int)piles->size()) )) ); //total-solve() is to check if we can maximize the score diff by incrementing X at any time.
         }     
         return dp[i][M] = ans; //return the maximum diff
     }    
