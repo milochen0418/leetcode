498b8bd890a9463954fe018c723b2b048f73d237
milochen0418 <milo_chen@mail2000.com.tw>
2022-01-24 22:33:32 +0800

    give good name

--- C++/stone-game-ii.cpp
+++ C++/stone-game-ii.cpp
@@ -1,32 +1,27 @@
 class Solution {
     vector<vector<int>> dp;
     vector<int>* piles=nullptr;
-    //refer from https://leetcode.com/problems/stone-game-ii/discuss/1708282/C%2B%2B-Simple-Intuitive-Solution-W-Explanation-or-DP-or-Memorization
 public:
     int stoneGameII(vector<int> &ps) {
         piles = &ps;
         int n = piles->size();
-            
         dp.resize(n+1, vector<int>(n+1)); 
-		
-        int sum = accumulate(piles->begin(), piles->end(), 0), diff = solve(0, 1); //sum of stones in all piles
-        
-        return (sum + diff)/2; //Max stones won by Alice - We need to maximize the diff so this can be maximum
+        int total = accumulate(piles->begin(), piles->end(), 0);
+        int diff = more_than_yours(0, 1); 
+        return (total + diff)/2; 
     }
     
-    int solve(int i, int M){
-        if(i >= piles->size()) return 0; //If i>n-1 return 0
-        
-        if(dp[i][M] != 0) return dp[i][M]; //memorized value
-        
-        int my_taken_in_turn = 0, ans = INT_MIN; //total stores the total no. of stones won by Alice so far
-		//ans - stores the maximum difference between Alice & Bob's stones
+    int more_than_yours(int i, int M){
+        if(i >= piles->size()) return 0; 
+        if(dp[i][M] != 0) return dp[i][M]; 
+        int my_taken_in_turn = 0;
+        int max_more_than_yours = INT_MIN; 
         
         for(int j=0; j<2*M; j++){   //1<=X<=2*M
-            if(i+j < piles->size()) my_taken_in_turn += (*piles)[i+j]; //Each time we're incrementing X, add that index's stones to total
-            int your_optimal_number_more_than_mine = solve(i+j+1, max(M, min(j+1,(int)piles->size()) ));
-            ans = max(ans, my_taken_in_turn-your_optimal_number_more_than_mine); //total-solve() is to check if we can maximize the score diff by incrementing X at any time.
+            if(i+j < piles->size()) my_taken_in_turn += (*piles)[i+j]; 
+            int competitor_more_than_mine = more_than_yours( i+j+1,  max(M, min(j+1, (int)piles->size())) );
+            max_more_than_yours = max(max_more_than_yours, my_taken_in_turn - competitor_more_than_mine);
         }     
-        return dp[i][M] = ans; //return the maximum diff
+        return dp[i][M] = max_more_than_yours;
     }    
 };
