d1cd0c3e333c72eb402cb19729016602d9e43df7
milochen0418 <milo_chen@mail2000.com.tw>
2022-01-24 12:08:31 +0800

    C++ detect-captial refactor and add article

--- C++/detect-capital.cpp
+++ C++/detect-capital.cpp
@@ -1,41 +1,25 @@
 class Solution {
     //https://leetcode.com/problems/detect-capital
+    //Article https://leetcode.com/problems/detect-capital/discuss/1714403/C%2B%2B-or-O(N)
 public:
     bool detectCapitalUse(string word) {
         const char* str = word.c_str();
         int i = 0;
         bool shouldAllLowerCase = false;
-        bool isFirstUpperCase = false;
         bool shouldAllUpperCase = false;
         while(str[i]!='\0') {
             char c = str[i];
             bool is_lower_case = (c>='a' && c<='z');
             bool is_upper_case = (c>='A' && c<='Z');
-            
             if(i==0) {
-                if(is_lower_case) {
-                    shouldAllLowerCase = true;
-                    isFirstUpperCase = false; 
-                } else {
-                    shouldAllLowerCase = false;
-                    isFirstUpperCase = true;
-                }
+                shouldAllLowerCase = is_lower_case;
             } else if(i==1){
-                if(shouldAllLowerCase) {
-                    if(is_upper_case) return false;
-                }
-                if(isFirstUpperCase) {
-                    shouldAllUpperCase = is_upper_case;
-                }
+                if(shouldAllLowerCase && is_upper_case) return false;
+                if(!shouldAllLowerCase)shouldAllUpperCase = is_upper_case;
             } else {
-                if(shouldAllLowerCase) {
-                    if(is_upper_case) return false;
-                }
-                if(shouldAllUpperCase) {
-                    if(is_lower_case) return false;
-                } else {
-                    if(is_upper_case) return false;
-                }           
+                if(shouldAllLowerCase && is_upper_case) return false;
+                if(shouldAllUpperCase && is_lower_case) return false;
+                if(!shouldAllUpperCase && is_upper_case) return false;
             }
             i++;
         }
