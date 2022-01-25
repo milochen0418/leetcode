959ae3254d6dc4ec49c84c7c69d65f36288aacce
milochen0418 <milo_chen@mail2000.com.tw>
2022-01-25 18:16:56 +0800

    improve fast for one time loop

--- C++/valid-mountain-array.cpp
+++ C++/valid-mountain-array.cpp
@@ -1,22 +1,24 @@
 class Solution {
     //https://leetcode.com/problems/valid-mountain-array
 public:
-    bool validMountainArray(vector<int>& arr) {
+    bool validMountainArray(vector<int>& arr) {        
         if(arr.size() < 3) return false;
+        if(!(arr[0] < arr[1])) return false;
+        if(!(arr[arr.size()-2] > arr[arr.size()-1])) return false;
         int candidate_idx = -1;
-        for(int i=1; i<arr.size()-1;i++) { 
-            if(arr[i-1] < arr[i] && arr[i] > arr[i+1]) {
-                candidate_idx = i;
-                break;
+        bool mountain_arrived = false;
+        for(int i =1; i<arr.size()-1; i++) {
+            if(!mountain_arrived) {
+                if(!(arr[i] < arr[i+1])) {
+                    if(arr[i] == arr[i+1]) { 
+                        return false;
+                    } else {
+                        mountain_arrived = true;              
+                    }
+                }
+            } else {
+                if(!(arr[i] > arr[i+1])) return false;
             }
-        }
-        if(candidate_idx == -1) return false;
-        
-        for(int i =0; i < candidate_idx; i++) {
-            if (!(arr[i] < arr[i+1])) return false;
-        }
-        for(int i = candidate_idx; i<arr.size()-1;i++) {
-            if(!(arr[i] > arr[i+1])) return false;
         }
         return true;
     }
