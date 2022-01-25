6413e88b6bfda38ef17fb961aeafbeccb88c489b
milochen0418 <milo_chen@mail2000.com.tw>
2022-01-25 19:06:20 +0800

    C++ valid-mountain-array (easy) extremely short

--- C++/valid-mountain-array.cpp
+++ C++/valid-mountain-array.cpp
@@ -2,16 +2,12 @@ class Solution {
     //https://leetcode.com/problems/valid-mountain-array
 public:
     bool validMountainArray(vector<int>& arr) {
-        if (arr.size() < 3) return false;
         bool mountain_arrived = false;
         for (int i = 0 ; i < arr.size()-1; i++)             
             if (mountain_arrived) 
                 if (!(arr[i] > arr[i+1])) return false; else continue;
-            else if (!(arr[i] < arr[i+1])) 
-                if (i==0 || arr[i] == arr[i+1]) 
-                    return false;
-                else 
-                    mountain_arrived = (i!=arr.size()-2 || arr[arr.size()-1] < arr[arr.size()-2]);                
+            else if (arr.size() < 3 || !(arr[i] < arr[i+1])) 
+                if(!(i==0 || arr[i] == arr[i+1])) mountain_arrived = (i!=arr.size()-2 || arr[arr.size()-1] < arr[arr.size()-2]); else break;
         return mountain_arrived;
     }
 };
