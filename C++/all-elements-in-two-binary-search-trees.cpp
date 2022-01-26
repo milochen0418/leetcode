6a51e77bc2c08f377fb389a6787f603454a7cef9
milochen0418 <milo_chen@mail2000.com.tw>
2022-01-26 18:36:49 +0800

    Init C++ all-elements-in-two-binary-search-trees

---
+++ C++/all-elements-in-two-binary-search-trees.cpp
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    //https://leetcode.com/problems/all-elements-in-two-binary-search-trees/
+public:
+    vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
+
+    }
+    
+};
