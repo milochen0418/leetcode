c60d6b0a2288f9bee3b9948b7b88330f11eaddcf
milochen0418 <milo_chen@mail2000.com.tw>
2022-01-26 19:29:38 +0800

    C++ all-elements-in-two-binary-search-trees failed in test case

--- C++/all-elements-in-two-binary-search-trees.cpp
+++ C++/all-elements-in-two-binary-search-trees.cpp
@@ -11,9 +11,40 @@
  */
 class Solution {
     //https://leetcode.com/problems/all-elements-in-two-binary-search-trees/
+    /*
+    Test case pass
+    [2,1,4]
+    [1,0,3]
+    Test case failed
+    [0,null,59,57,90]
+    [60,17,74,6,20,63,97,null,null,null,null,null,null,95]
+    */
+
 public:
     vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
-
+        vector<int> v;
+        stack<TreeNode*> s1;
+        stack<TreeNode*> s2;        
+        if(root1 != nullptr) s1.push(root1);
+        if(root2 != nullptr) s2.push(root2);
+        while( !(s1.empty() && s2.empty()) ) {
+            int s1topval = s1.empty()?INT_MAX:s1.top()->val;
+            int s2topval = s2.empty()?INT_MAX:s2.top()->val;
+            
+            stack<TreeNode*>*ps  = s1topval < s2topval? &s1: &s2;
+            TreeNode* e = ps->top();
+            
+            if(e->left != nullptr && e->left->val != INT_MIN) {
+                ps->push(e->left);
+            } else {
+                v.push_back(e->val);
+                e->val = INT_MIN;
+                ps->pop();
+                if(e->right != nullptr && e->right->val != INT_MIN) {
+                    ps->push(e->right);
+                }
+            }
+        }
+        return v;
     }
-    
 };
