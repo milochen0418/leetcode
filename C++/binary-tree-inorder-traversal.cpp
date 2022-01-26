6f1597bd58ca8f2629a0e796d4e5e48c326c26e2
milochen0418 <milo_chen@mail2000.com.tw>
2022-01-26 18:59:00 +0800

    C++ binary-tree-inorder-traversal complete with while

--- C++/binary-tree-inorder-traversal.cpp
+++ C++/binary-tree-inorder-traversal.cpp
@@ -10,9 +10,26 @@
  * };
  */
 class Solution {
-    //https://leetcode.com/problems/binary-tree-inorder-traversal/
+    //https://leetcode.com/problems/binary-tree-inorder-traversal
 public:
     vector<int> inorderTraversal(TreeNode* root) {
-        
+        vector<int> v;
+        stack<TreeNode*> s;
+        if(root == nullptr) return v;
+        s.push(root);
+        while(!s.empty()) {
+            TreeNode* e = s.top();
+            if(e->left != nullptr && e->left->val != INT_MIN) {
+                s.push(e->left);    
+            } else {
+                v.push_back(e->val);
+                e->val = INT_MIN;
+                s.pop();
+                if(e->right != nullptr && e->right->val != INT_MIN) {
+                    s.push(e->right);
+                }
+            }
+        }
+        return v;
     }
 };
