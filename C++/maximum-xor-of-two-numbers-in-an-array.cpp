8df99c4bc11346b9b6aedfa8532f9d0ceeb8a2e9
milochen0418 <milo_chen@mail2000.com.tw>
2022-01-28 18:25:49 +0800

    Optimize the soution into O(N)

--- C++/maximum-xor-of-two-numbers-in-an-array.cpp
+++ C++/maximum-xor-of-two-numbers-in-an-array.cpp
@@ -2,6 +2,30 @@ class Solution {
     //https://leetcode.com/problems/maximum-xor-of-two-numbers-in-an-array
 public:
     int findMaximumXOR(vector<int>& nums) {
+        int mymax = 0;
+        int n = nums.size();
+        int mask = 0;
+        for(int k=30; k>=0; k--) {
+            mymax += 1<<k;
+            mask |= 1<<k;
+            unordered_set<int> s;
+            for(auto val: nums) {
+                s.insert( (val^mymax)&mask);
+            } 
+            bool match = false;            
+            for(auto num: nums) {
+                if( s.find(num & mask) != s.end() ) {
+                    match = true;
+                    break;                    
+                }
+            }
+            if(match == false) {                
+                mymax -= 1<<k;
+            }
+        }
+        return mymax;
+    }
+    int version1(vector<int>& nums){
         int mymax = INT_MIN;
         int n = nums.size();
         if(n==1) return 0;
@@ -11,5 +35,6 @@ public:
             }
         }
         return mymax;
+
     }
 };
