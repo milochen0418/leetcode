ee266715b610cf9acd5b3f6ca942ff8a752946d1
milochen0418 <milo_chen@mail2000.com.tw>
2022-01-30 19:57:23 +0800

    refactor and add comment and article link

--- C++/largest-rectangle-in-histogram.cpp
+++ C++/largest-rectangle-in-histogram.cpp
@@ -4,7 +4,7 @@ class Solution {
     //https://www.geeksforgeeks.org/stack-of-pair-in-c-stl-with-examples/
     //https://haogroot.com/2020/09/01/monotonic-stack-leetcode/
     //https://labuladong.gitbook.io/algo-en/ii.-data-structure/monotonicstack
-    
+    //https://leetcode.com/problems/largest-rectangle-in-histogram/discuss/1727739/C%2B%2B-or-O(N)-or-monotonic-stack
 public:
     int largestRectangleArea(vector<int>& heights) {
         vector<int>& nums = heights;
@@ -14,10 +14,14 @@ public:
         vector<int> v_w(n);
         vector<int> v_area(n);
         figure(nums, v_w_to_right);
+        reverse(nums.begin(), nums.end()); 
+        figure(nums, v_w_to_left); 
+		//Use reverse order of heights figure out the result
+		//but remember the result is not normal order.
+        reverse(v_w_to_left.begin(), v_w_to_left.end()); 
+		//reverse the result to get real v_w_toleft 
         reverse(nums.begin(), nums.end());
-        figure(nums, v_w_to_left);
-        reverse(v_w_to_left.begin(), v_w_to_left.end());
-        reverse(nums.begin(), nums.end());
+		//nums become normal order of heights now.
         for(int i=0;i<n;i++) {
             v_w[i] = v_w_to_right[i] + v_w_to_left[i] - 1;
             v_area[i] = v_w[i] * nums[i];
@@ -28,46 +32,15 @@ public:
     
     void figure(vector<int>& nums,  vector<int>& v_w) {
         int n = nums.size();
-        vector<int> v(n);
-        vector<int> v_idx(n);
         stack<int>s;
-        stack<int>s_idx;
         for(int i =  nums.size()-1; i>=0; i--) {
-            while(!s.empty() && s.top() >= nums[i]) {
+			//keep pop until s is empty or the value 
+			//indexed by stack's top less than nums[i]]				
+            while( !(s.empty()||nums[s.top()]<nums[i]) ){
                 s.pop();
-                s_idx.pop();
-            }
-            v[i] = s.empty() ? -1: s.top();
-            v_idx[i] = s_idx.empty() ? -1: s_idx.top();
-            v_w[i] = v_idx[i]!=-1?v_idx[i]-i:n-i;
-            s.push(nums[i]);
-            s_idx.push(i);
+            } 
+            v_w[i] = s.empty()?n-i:s.top()-i;
+            s.push(i);
         }
-    }  
-
-
-    vector<int> test_width_figure(){
-        vector<int> nums = {2,1,5,6,2,3};  
-            // will return [1,5,2,1,2,1]  the width from current to right-most 
-        int n = nums.size();
-        vector<int> v(n);
-        vector<int> v_idx(n);
-        vector<int> v_w(n);
-        stack<int>s;
-        stack<int>s_idx;        
-        for(int i =  nums.size()-1; i>=0; i--) {
-            while(!s.empty() && s.top() >= nums[i]) {
-                s.pop();
-                s_idx.pop();
-            }
-            v[i] = s.empty() ? -1: s.top();
-            v_idx[i] = s_idx.empty() ? -1: s_idx.top();
-            v_w[i] = v_idx[i]!=-1?v_idx[i]-i:n-i;
-            s.push(nums[i]);
-            s_idx.push(i);
-            
-        }
-        return v_w;        
     }
-
 };
