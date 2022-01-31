54d301fb1a1323979e97afd6522af97e6b65c793
milochen0418 <milo_chen@mail2000.com.tw>
2022-01-31 14:16:15 +0800

    C++ group-shifted-strings optimized to map<int,int> version

--- C++/group-shifted-strings.cpp
+++ C++/group-shifted-strings.cpp
@@ -1,8 +1,7 @@
 class Solution {
-    //https://leetcode.com/problems/group-shifted-strings/
 public:
     
-    vector<vector<string>> groupStringsOld(vector<string>& strings) {                
+    vector<vector<string>> groupStringsOld4(vector<string>& strings) {                
         unordered_map<vector<int>,vector<string>,vector_hasher> mp;
         for(auto& s: strings) {
             vector<int> vs = extract(s);
@@ -42,8 +41,7 @@ public:
         }
         return r;
     }
-    
-    vector<vector<string>> groupStrings(vector<string>& strings) {
+    vector<vector<string>> groupStringsOld3(vector<string>& strings) {
         //return groupStringsOld(strings);
         unordered_map<string, vector<string>> mp;
         for(auto& s: strings) {
@@ -54,4 +52,60 @@ public:
         for(auto& [key,v]: mp) result.push_back(v);
         return result;
     }
+
+    int extractInt(string& s) {
+        long hash = s.length();
+        for(int i=0;i<s.length()-1;i++) {
+            int val = (26+s[i]-s[i+1])%26;
+            hash ^= (val + 0x9e3779b9 + (hash << 6) + (hash >> 2)) % INT_MAX;
+        }        
+        return hash;
+    }
+    
+    vector<vector<string>> groupStringsOld2(vector<string>& strings) {
+        //return groupStringsOld2(strings);Old1
+        unordered_map<int, vector<string>> mp;
+        for(auto& s: strings) {
+            int vi = extractInt(s);
+            mp[vi].push_back(s);
+        }        
+        vector<vector<string>> result;
+        for(auto& [key,v]: mp) result.push_back(v);
+        return result;
+    }
+    
+    vector<vector<string>> groupStringsOld1(vector<string>& strings) {
+        //return groupStringsOld2(strings)
+        unordered_map<int, vector<int>> mp;
+        for(int i=0;i<strings.size();i++) {        
+            int vi = extractInt(strings[i]);
+            mp[vi].push_back(i);
+        }
+        
+        vector<vector<string>> result;
+        for(auto& [key,v]: mp) {
+            vector<string> vs(v.size());
+            for(int i=0;i<v.size();i++) {
+                vs[i]=strings[v[i]];
+            }   
+            result.push_back(vs);
+        }
+        return result;
+    }
+    
+    vector<vector<string>> groupStrings(vector<string>& strings) {
+        //return groupStringsOld1(strings)
+        unordered_map<int,int> mp;
+        vector<vector<string>> result;
+        int cnt=0;
+        for(int i=0;i<strings.size();i++) {
+            int vi = extractInt(strings[i]);
+            if(mp.find(vi)==mp.end()) {
+                mp[vi]=cnt++;
+                result.push_back(vector<string>());
+            }
+            result[mp[vi]].push_back(strings[i]);
+        }
+        return result;
+    }  
 };
