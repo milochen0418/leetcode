8b0ded5f9f861705072ecea1c18f31421387e1bc
milochen0418 <milo_chen@mail2000.com.tw>
2022-01-31 12:16:06 +0800

    To practice use max_element function with compare

--- C++/richest-customer-wealth.cpp
+++ C++/richest-customer-wealth.cpp
@@ -1,9 +1,16 @@
 class Solution {
     //https://leetcode.com/problems/richest-customer-wealth
 public:
-    int maximumWealth(vector<vector<int>>& accounts) {
+    int maximumWealth(vector<vector<int>>& A) {
+        vector<int> b = *max_element(A.begin(),A.end(), [](auto l,auto r){ 
+            return accumulate(l.begin(),l.end(),0) > accumulate(r.begin(),r.end(),0); 
+        });
+        return accumulate(b.begin(), b.end(),0);
+        //return default_solution(A);
+    }
+    int default_solution(vector<vector<int>>& A) {
         int max_wealth = 0; 
-        for(auto& c:accounts) 
+        for(auto& c:A) 
             max_wealth = max(max_wealth, accumulate(c.begin(), c.end(), 0));
         return max_wealth;
     }
