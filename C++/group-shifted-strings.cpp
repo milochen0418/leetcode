3e0b34d8f9c5fe31593a8b8286fa8357a70cbc41
milochen0418 <milo_chen@mail2000.com.tw>
2022-01-31 13:14:17 +0800

    Improve by using string to instead feature vector

--- C++/group-shifted-strings.cpp
+++ C++/group-shifted-strings.cpp
@@ -1,8 +1,8 @@
 class Solution {
-    //https://leetcode.com/problems/group-shifted-strings
+    //https://leetcode.com/problems/group-shifted-strings/
 public:
     
-    vector<vector<string>> groupStrings(vector<string>& strings) {
+    vector<vector<string>> groupStringsOld(vector<string>& strings) {                
         unordered_map<vector<int>,vector<string>,vector_hasher> mp;
         for(auto& s: strings) {
             vector<int> vs = extract(s);
@@ -12,6 +12,7 @@ public:
         for(auto& [key,v]: mp) result.push_back(v);
         return result;
     }
+    
     
     struct vector_hasher {
         int operator()(const vector<int> &V) const {
@@ -33,5 +34,24 @@ public:
         return v;
     }
     
-
+    string extractStr(string s) {
+        //string r(s.length()-1);
+        string r(s.length()-1, ' ');
+        for(int i=1;i<s.length();i++) {
+            r[i-1] = (26+s[i-1]-s[i])%26;
+        }
+        return r;
+    }
+    
+    vector<vector<string>> groupStrings(vector<string>& strings) {
+        //return groupStringsOld(strings);
+        unordered_map<string, vector<string>> mp;
+        for(auto& s: strings) {
+            string vs = extractStr(s);
+            mp[vs].push_back(s);
+        }        
+        vector<vector<string>> result;
+        for(auto& [key,v]: mp) result.push_back(v);
+        return result;
+    }
 };
