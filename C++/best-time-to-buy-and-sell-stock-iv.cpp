ed53aee0ce065a6fb0bf71b0853b45ba48709a4f
milochen0418 <milo_chen@mail2000.com.tw>
2022-02-01 18:45:14 +0800

    C++ best-time-to-buy-and-sell-stock-iv fix test case 1 [1,2] for my wrong coding

--- C++/best-time-to-buy-and-sell-stock-iv.cpp
+++ C++/best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,6 +1,15 @@
 class Solution {
-    //https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iv
+    //https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iv/
 public:
+/*
+2
+[3,2,6,5,0,3]
+1
+[1,2]
+2
+[2,4,1]
+*/
+    
     int maxProfit(int k, vector<int>& prices) {
         vector<int>& p = prices;
         int n = p.size();
@@ -21,16 +30,14 @@ public:
                     is_falling = false;
                 }        
             } else {
-                if( p[i+1]>=n || p[i+1]<p[i]) {
+                if( i+1>=n || p[i+1]<p[i]) {
                     M.push_back(i);
-                    is_falling = true;
+                    is_falling = true;                    
                 }
             }
         }
-        
         int sum = 0;
         n = M.size();
-        //return m.size()+M.size();
         
         for(int i=0;i<n;i++) {
             sum += (p[M[i]] - p[m[i]]);
