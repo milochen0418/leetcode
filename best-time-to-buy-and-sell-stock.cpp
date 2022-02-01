b0bc2cbaedf2929219dc838890ffb4cab081b2a8
milochen0418 <milo_chen@mail2000.com.tw>
2022-02-01 09:35:59 +0800

    C++ best-time-to-buy-and-sell-stock finish

--- best-time-to-buy-and-sell-stock.cpp
+++ best-time-to-buy-and-sell-stock.cpp
@@ -1,13 +1,12 @@
 class Solution {
-    //https://leetcode.com/problems/best-time-to-buy-and-sell-stock
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
         int max_profit = 0; 
-        for(int i=0;i<n-1;i++) {
-            for(int j=i+1;j<n;j++) {
-                max_profit = max(max_profit, prices[j] - prices[i]);
-            }
+        int min_price = INT_MAX;
+        for(int i=0;i<n;i++) {
+            min_price = min(min_price, prices[i]);
+            max_profit = max(max_profit, prices[i] - min_price);
         }      
         return max_profit;
     }
