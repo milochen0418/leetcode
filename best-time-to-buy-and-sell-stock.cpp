1f275e44b3aa9051680954f9057d9f0f0d1c80be
milochen0418 <milo_chen@mail2000.com.tw>
2022-02-01 10:13:08 +0800

    C++ best-time-to-buy-and-sell-stock another solution

--- best-time-to-buy-and-sell-stock.cpp
+++ best-time-to-buy-and-sell-stock.cpp
@@ -3,11 +3,13 @@ public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
         int max_profit = 0; 
-        int min_price = INT_MAX;
-        for(int i=0;i<n;i++) {
-            min_price = min(min_price, prices[i]);
-            max_profit = max(max_profit, prices[i] - min_price);
-        }      
+        for(int j=1;j<n;j++) { 
+            int min_price = INT_MAX;
+            for(int i=0;i<j;i++) {
+                min_price = min(min_price, prices[i]);
+            }
+            max_profit = max( max_profit, prices[j] - min_price);            
+        }        
         return max_profit;
     }
 };
