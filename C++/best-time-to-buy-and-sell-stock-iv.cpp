a8d75d4f178aeb35fa45e853aceba7474d1123c1
milochen0418 <milo_chen@mail2000.com.tw>
2022-02-01 18:37:04 +0800

    C++ best-time-to-buy-and-sell-stock-iv init just pass basic test case

---
+++ C++/best-time-to-buy-and-sell-stock-iv.cpp
@@ -0,0 +1,40 @@
+class Solution {
+    //https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iv
+public:
+    int maxProfit(int k, vector<int>& prices) {
+        vector<int>& p = prices;
+        int n = p.size();
+        //m M m M m M
+        //(mM)(mM)(mM)
+        //(mMmM)(mM)
+        //When #(mM) is not enough, then use connect 2 (mM) as one 
+        //first m from [0..i] 
+        //last M is in [k..n-1]
+        vector<int> m;
+        vector<int> M;
+        
+        bool is_falling =true;
+        for(int i = 0; i < n; i++) {
+            if(is_falling==true) {
+                if(i+1<n && p[i+1]>p[i]) {
+                    m.push_back(i);
+                    is_falling = false;
+                }        
+            } else {
+                if( p[i+1]>=n || p[i+1]<p[i]) {
+                    M.push_back(i);
+                    is_falling = true;
+                }
+            }
+        }
+        
+        int sum = 0;
+        n = M.size();
+        //return m.size()+M.size();
+        
+        for(int i=0;i<n;i++) {
+            sum += (p[M[i]] - p[m[i]]);
+        }        
+        return sum;
+    }
+};
