d6652a108fcb6123945e3943dcff8514e12b2545
milochen0418 <milo_chen@mail2000.com.tw>
2022-02-02 18:24:17 +0800

    C++ contains-duplicate-ii pass

--- C++/contains-duplicate-ii.cpp
+++ C++/contains-duplicate-ii.cpp
@@ -3,17 +3,17 @@ class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
         int n = nums.size();
+        unordered_set<int> s;
         for(int i=0;i<n;i++) {
-            unordered_set<int> s;
-            for(int j=0;j<=k;j++) {
-                if(i+j>=n) break;
-                int num = nums[i+j];
-                if(s.find(num) == s.end()) {
-                    s.insert(num);
-                } else {
-                    return true;
-                }
-            }      
+            if(i>k){
+                s.erase(nums[i-k-1]);
+            }
+            int num = nums[i];
+            if(s.find(num) == s.end()) {
+                s.insert(num);
+            } else {
+                return true;
+            }
         }
         return false;
     }
