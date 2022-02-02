28ad692edee453081e09b44d3b78f161d5bfff46
milochen0418 <milo_chen@mail2000.com.tw>
2022-02-02 22:17:49 +0800

    C++ contains-duplicate-iii ending

--- C++/contains-duplicate-iii.cpp
+++ C++/contains-duplicate-iii.cpp
@@ -1,35 +1,6 @@
 class Solution {
     //https://leetcode.com/problems/contains-duplicate-iii/
-    /* In https://www.onlinegdb.com/online_c++_compiler. 
-    
-#include <iostream>
-using namespace std;
-#include<set>
-#include<vector>
-int main()
-{
-    std::set<int> s;
-    s.insert(1); 
-    s.insert(30);
-    s.insert(80);
-    vector<int> v = {-10, 15, 30, 31, 60, 100};
-    for(auto num: v) {
-        cout<<num <<"\tLB="<<*s.lower_bound(num )<<"\tUB="<<*s.upper_bound(num)<<"\n";
-    }
-    return 0;
-}    
-
-Result:
--10     LB=1    UB=1
-15      LB=30   UB=30
-30      LB=30   UB=80
-31      LB=80   UB=80
-60      LB=80   UB=80
-100     LB=3    UB=3
-upper_bound/lower_bound explain
-https://stackoverflow.com/questions/41958581/difference-between-upper-bound-and-lower-bound-in-stl
-    */
-    /*
+/*    
     test-cases
 [1,2,3,1]
 3
@@ -50,26 +21,18 @@ https://stackoverflow.com/questions/41958581/difference-between-upper-bound-and-
 public:
     bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
         int n = nums.size();
-        //unordered_set<int> s;
-        set<int> s;
+        set<long> s;
         for(int i=0;i<n;i++) {
             if(i>k){
                 s.erase(nums[i-k-1]);
             }
-            int num = nums[i];
-            
+            long num = nums[i];            
             for(auto iter=s.lower_bound(num-t); iter!=s.upper_bound(num+t);iter++) {
                 if(num-t <= *iter && *iter <= num+t){
                     return true;
                 }
             }
-            s.insert(num);
-            /*    
-            if(s.find(num) == s.end()) {
-                s.insert(num);
-            } else {
-                return true;
-            }*/
+            s.insert((long)num);
         }
         return false;
     }
