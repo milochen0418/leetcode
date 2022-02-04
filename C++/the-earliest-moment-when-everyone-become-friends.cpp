a9ee01a359276293371d9a0afe5515441a972e93
milochen0418 <milo_chen@mail2000.com.tw>
2022-02-05 02:03:01 +0800

    C++ the-earliest-moment-when-everyone-become-friends add article & FB post

--- C++/the-earliest-moment-when-everyone-become-friends.cpp
+++ C++/the-earliest-moment-when-everyone-become-friends.cpp
@@ -1,12 +1,14 @@
 class Solution {
     //https://leetcode.com/problems/the-earliest-moment-when-everyone-become-friends/
+    //article https://leetcode.com/problems/the-earliest-moment-when-everyone-become-friends/
+    //FB Post https://www.facebook.com/groups/1451299754892511/posts/5038828492806268/
 public:
     int set_cnt=0;
     int earliestAcq(vector<vector<int>>& logs, int n) {
         vector<int> ps(n);
         for(int i=0;i<n;i++) ps[i]=i;
         set_cnt = n;
-        
+        sort(logs.begin(), logs.end(), [](auto r, auto l){return r[0]<l[0];});
         for(auto& v:logs) {
             int t=v[0], x=v[1], y=v[2];
             UF_union(ps, x, y);
@@ -31,15 +33,5 @@ public:
         if(root_a == root_b) return; 
         ps[root_a] = root_b;
         set_cnt-=1;
-    }  
+    }
 };
-
-
-/* testcase
-[[0,2,0],[1,0,1],[3,0,3],[4,1,2],[7,3,1]]
-4
-[[20190101,0,1],[20190104,3,4],[20190107,2,3],[20190211,1,5],[20190224,2,4],[20190301,0,3],[20190312,1,2],[20190322,4,5]]
-6
-[[9,3,0],[0,2,1],[8,0,1],[1,3,2],[2,2,0],[3,3,1]]
-4
-*/
