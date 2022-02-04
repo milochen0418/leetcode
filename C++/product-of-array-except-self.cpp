36a525e29c5b0dd3459d794f6aef796078587997
milochen0418 <milo_chen@mail2000.com.tw>
2022-02-04 11:33:16 +0800

    C++ product-of-array-except-self add article

--- C++/product-of-array-except-self.cpp
+++ C++/product-of-array-except-self.cpp
@@ -1,21 +1,48 @@
 class Solution {
     //https://leetcode.com/problems/product-of-array-except-self
+    //https://leetcode.com/problems/product-of-array-except-self/discuss/1743372/C%2B%2B-or-simple-time-O(N)-space-O(1)-or-only-use-for-and-if
 /* testcase 
 [1,2,3,4]
 [-1,1,0,-3,3]
-
 */    
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
         long product = 1;
-        for(auto num: nums) {
-            product *= (long)num;
+        bool is_exist_zero = false;
+        int only_zero_idx = -1;
+        bool is_exist_two_zero = false;
+        for(int i = 0; i<n;i++) {
+            int num = nums[i];
+            if(!is_exist_zero) {
+                if(num ==0) {
+                    is_exist_zero = true;
+                    only_zero_idx = i;
+                } else {
+                    product *= (long)num;
+                }
+            } else if(is_exist_zero){ //case is_exist_zero
+                if(num == 0) {
+                    is_exist_two_zero = true;    
+                } else {
+                    product *= (long)num;
+                }
+                if(is_exist_two_zero) {
+                    break;
+                }
+            } 
         }
-        vector<int> ans(n);
-        for(int i=0;i<n;i++) {
-            ans[i] = (product/nums[i]);
-        }
-        return ans;
+        vector<int> ans(n,0);
+        if(is_exist_two_zero) {
+            return ans;            
+        }else if(is_exist_zero) {
+            ans[only_zero_idx] = product;
+            return ans;
+        } else {
+            for(int i=0;i<n;i++) {
+                ans[i] = (product/nums[i]);
+            }
+            return ans;            
+        }          
     }
 };
