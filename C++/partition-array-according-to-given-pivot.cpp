c7529cafbb269b0cca24ec4554d1731b780d0d8d
milochen0418 <milo_chen@mail2000.com.tw>
2022-02-06 19:27:58 +0800

    partition-array-according-to-given-pivot  add fB post

--- C++/partition-array-according-to-given-pivot.cpp
+++ C++/partition-array-according-to-given-pivot.cpp
@@ -1,4 +1,5 @@
 class Solution {
+    //FB Post https://www.facebook.com/groups/1451299754892511/posts/5043973125625138
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
         vector<int> v;
