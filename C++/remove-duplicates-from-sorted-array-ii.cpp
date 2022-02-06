99db1b0fa0eed7f8147e256bc4fd911e89835fca
milochen0418 <milo_chen@mail2000.com.tw>
2022-02-06 19:24:43 +0800

    remove-duplicates-from-sorted-array-ii add link

--- C++/remove-duplicates-from-sorted-array-ii.cpp
+++ C++/remove-duplicates-from-sorted-array-ii.cpp
@@ -1,12 +1,11 @@
 class Solution {
+    //https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii
+    //article https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/discuss/1749738/C%2B%2B-or-step-by-step-from-idea
+    //FB Post https://www.facebook.com/groups/1451299754892511/posts/5044285435593907/
 public:
-    int removeDuplicates(vector<int>& nums) {
-        int k = 0;
-        for (auto& num: nums) {
-            if ( k<2 || num > nums[k-2]) {
-                nums[k++] = num;
-            }
-        }
-        return k;
+    int removeDuplicates(vector<int>& vs) {
+        int i = 0;
+        for (auto& v: vs)  if ( i < 2 || v != vs[i-2] ) vs[i++] = v;
+        return i;
     }
 };
