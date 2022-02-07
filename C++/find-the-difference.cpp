bf72ed5e1479fd754a2411314a6ee58fbd9af03f
milochen0418 <milo_chen@mail2000.com.tw>
2022-02-07 14:27:06 +0800

    C++/find-the-difference.cpp add approach 1

--- C++/find-the-difference.cpp
+++ C++/find-the-difference.cpp
@@ -2,25 +2,30 @@ class Solution {
     //https://leetcode.com/problems/find-the-difference
 public:
     char findTheDifference(string s, string t) {
-        return approach2(s,t);
+        return approach1(s,t);
+        //return approach2(s,t);
     }
     
-    
+    //Time O(NlogN), Space O(1) 
     char approach1(string s, string t) {
-        return '\0';
+        int n = s.length();
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        for(int i=0; i<n; i++) if(s[i]!=t[i]) return t[i];
+        return t[n];
     }
 
+    //Time O(N), Space O(N)
     char approach2(string s, string t) {
         unordered_map<char,int> mp;
         for(auto& c:s) mp[c]+=1;
         for(auto& c:t) if(--mp[c]<0) return c;
-        return '\0';        
+        return '\0';
     }
     
     char approach3(string s, string t) {
         return '\0';
     }
-    
 };
 
 
