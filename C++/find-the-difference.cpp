96224eac28754aa6d7a0b9ef08180d4f33c4a5c6
milochen0418 <milo_chen@mail2000.com.tw>
2022-02-07 14:22:09 +0800

    Start to implement for 3 approach

--- C++/find-the-difference.cpp
+++ C++/find-the-difference.cpp
@@ -2,11 +2,25 @@ class Solution {
     //https://leetcode.com/problems/find-the-difference
 public:
     char findTheDifference(string s, string t) {
+        return approach2(s,t);
+    }
+    
+    
+    char approach1(string s, string t) {
+        return '\0';
+    }
+
+    char approach2(string s, string t) {
         unordered_map<char,int> mp;
         for(auto& c:s) mp[c]+=1;
         for(auto& c:t) if(--mp[c]<0) return c;
+        return '\0';        
+    }
+    
+    char approach3(string s, string t) {
         return '\0';
     }
+    
 };
 
 
