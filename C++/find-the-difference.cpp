4373d78dad6c2285cc19b701ca948b35a96ff815
milochen0418 <milo_chen@mail2000.com.tw>
2022-02-07 16:26:05 +0800

    C++/find-the-difference.cpp Update to 6 approach

--- C++/find-the-difference.cpp
+++ C++/find-the-difference.cpp
@@ -2,12 +2,13 @@ class Solution {
     //https://leetcode.com/problems/find-the-difference
 public:
     char findTheDifference(string s, string t) {
-        //return approach1(s,t);
-        //return approach2(s,t);
+        //return approach1(s,t); 
+        return approach2(s,t);
         //return approach3(s,t);
         //return approach4(s,t);
         return approach5(s,t);
     }
+    //1. Sort 
     //Time O(NlogN), Space O(1) 
     char approach1(string s, string t) {
         int n = s.length();
@@ -16,7 +17,8 @@ public:
         for(int i=0; i<n; i++) if(s[i]!=t[i]) return t[i];
         return t[n];
     }
-
+    
+    //2. Count by HashMap
     //Time O(N), Space O(N)
     char approach2(string s, string t) {
         unordered_map<char,int> mp;
@@ -24,22 +26,35 @@ public:
         for(auto& c:t) if(--mp[c]<0) return c;
         return '\0';
     }
+    
+    //3. Count by constant array
     //Time O(N), Space O(1)
     char approach3(string s, string t) {
+        vector<int> v(26, 0);
+        for(auto& c:s) v[c-'a']+=1;
+        for(auto& c:t) if(--v[c-'a']<0) return c;
+        return '\0';
+    }
+    
+    //4. XOR
+    //Time O(N), Space O(1)
+    char approach4(string s, string t) {
         char ret ='\0';
         for (auto &c: s) ret^=c;
         for (auto &c: t) ret^=c;
         return ret;
     }
+    //5. Summation
     //Time O(N), Space O(1) 
-    char approach4(string s, string t) {
+    char approach5(string s, string t) {
         int sum = 0;
         for (auto &c: t) sum+=c;
         for (auto &c: s) sum-=c;
         return (char)sum;
     }
+    //6. Summation with modulo
     //Time O(N), Space O(1)
-    char approach5(string s, string t) {
+    char approach6(string s, string t) {
         int sum = 0;
         for (auto &c: t) (sum+=c)%=256;
         for (auto &c: s) (sum-=c)%=256;
