3c67f802af1f2c0e3ae4214e7f91323b88077eda
milochen0418 <milo_chen@mail2000.com.tw>
2022-02-11 08:21:53 +0800

    C++/permutation-in-string init

---
+++ C++/permutation-in-string.cpp
@@ -0,0 +1,14 @@
+class Solution {
+    //https://leetcode.com/problems/permutation-in-string/
+public:
+    bool checkInclusion(string s1, string s2) {
+        unordered_map<char,int> mp_s1;
+        unordered_map<char,int> mp_s2;
+        for(auto &c:s1) mp_s1[c]+=1;
+        for(auto &c:s2) mp_s2[c]+=1;
+        for(auto& [key, val]:mp_s1) if( mp_s2[key] < val) return false;
+        
+        return true;
+            
+    }
+};
