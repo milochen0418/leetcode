99bb6a0d3f29ac679c61fe34be6a052a6bb1dbf3
milochen0418 <milo_chen@mail2000.com.tw>
2022-02-11 23:16:13 +0800

    C++/permutation-in-string add slide-window pattern

--- C++/permutation-in-string.cpp
+++ C++/permutation-in-string.cpp
@@ -2,11 +2,33 @@ class Solution {
     //https://leetcode.com/problems/permutation-in-string/
 public:
     bool checkInclusion(string s1, string s2) {
+        
+        int s1_len = s1.length();
+        int s2_len = s2.length();
+        if(s1_len > s2_len) return false;
+        vector<int> s1v(26,0);
+        vector<int> s2v(26,0);
+        for(int i=0;i<s1_len;i++) {
+            s1v[s1[i]-'a']+=1;
+            s2v[s2[i]-'a']+=1;
+        }
+        if(s1v == s2v) return true;
+        for(int i = 1; i<= s2_len - s1_len ; i++) { 
+            s2v[s2[i-1]-'a']-=1;
+            s2v[s2[i+s1_len-1]-'a']+=1;
+            if(s1v == s2v) return true;
+        }
+        return false;
+    }
+    
+    bool answer1(string s1, string s2) {
         int s1_len = s1.length();
         int s2_len = s2.length();
         if(s1_len > s2_len) false;
+        
         vector<int> s1v(26,0);
         for(auto &c:s1) s1v[c-'a']+=1;
+
         for(int i = 0; i<= s2_len - s1_len ; i++) { 
             string s2sub = s2.substr(i,s1_len);
             vector<int> s2v(26,0);
@@ -14,6 +36,7 @@ public:
             if(s1v == s2v) return true;
         }
         return false;
+        
     }
 };
 /* testcase 
