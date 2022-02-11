d9b6fab349dfaefe5fdd40bca034612ada515125
milochen0418 <milo_chen@mail2000.com.tw>
2022-02-11 22:52:12 +0800

    C++/permutation-in-string.cpp O(N^2) solution

--- C++/permutation-in-string.cpp
+++ C++/permutation-in-string.cpp
@@ -2,23 +2,20 @@ class Solution {
     //https://leetcode.com/problems/permutation-in-string/
 public:
     bool checkInclusion(string s1, string s2) {
-        if(s2.length()>s1.length()) false;
-        vector<int> s1v(26);
+        int s1_len = s1.length();
+        int s2_len = s2.length();
+        if(s1_len > s2_len) false;
+        vector<int> s1v(26,0);
         for(auto &c:s1) s1v[c-'a']+=1;
-        for(int i = 0; i<s2.length()-s1.length()+1; i++) { 
-            string s2sub = s2.substr(i,s1.length());
-            vector<int> s2v(26);
-            for(auto &c:s2sub) {
-                s2v[c-'a']+=1;
-            }
+        for(int i = 0; i<= s2_len - s1_len ; i++) { 
+            string s2sub = s2.substr(i,s1_len);
+            vector<int> s2v(26,0);
+            for(auto &c:s2sub) s2v[c-'a']+=1; 
             if(s1v == s2v) return true;
         }
         return false;
-
-            
     }
 };
-
 /* testcase 
 "ab"
 "eidbaooo"
@@ -26,6 +23,6 @@ public:
 "eidboaoo"
 "adc"
 "dcda"
-"horse"
 "ros"
+"horse"
 */
