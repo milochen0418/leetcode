585d23e9e6c4b618eadbf27de54aa0f09317296b
milochen0418 <milo_chen@mail2000.com.tw>
2022-02-12 14:49:00 +0800

    C++/class Solution {

--- word-ladder.cpp
+++ word-ladder.cpp
@@ -2,8 +2,10 @@ class Solution {
     //https://leetcode.com/problems/word-ladder/
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        
         unordered_map<string,vector<int>> mp;
         int n = wordList.size();
+        vector<bool> traveled(n, false);
         for(int i=0; i<n; i++) {
             string& str = wordList[i];
             int m = str.length();
@@ -14,12 +16,21 @@ public:
                     char new_char = k + 'a';
                     if(new_char != orig_char)
                     key[j]=new_char
-                    mp[key].push_back();
+                    mp[key].push_back(i);
                 }
                 s[j] = orig_char;
             }
         }
         
-        
+        queue<tuple<int,int,bool>> q;
+        for(auto& i:mp[beginWord]) q.push({i,1});
+        while(!q.empty()) {
+            pair<int,int> p = q.top();
+            string &str = wordList[i];
+            int len = p.second;
+            q.pop();
+            //for(auto& i:mp[str]) q.push({})
+            
+        }
     }
 };
