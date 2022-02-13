c33d3ad6ffbbac5573a93009461db447e7b2fd13
milochen0418 <milo_chen@mail2000.com.tw>
2022-02-13 19:28:47 +0800

    C++/subsets.cpp refactor

--- C++/subsets.cpp
+++ C++/subsets.cpp
@@ -2,23 +2,17 @@ class Solution {
     // https://leetcode.com/problems/subsets/
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        unordered_set<int> s;
-        vector<int> v;
-        for(auto& i:nums) s.insert(i);
-        for(auto& i:s) v.push_back(i);
-        
-        int n = v.size();
-        vector<vector<int>> result;
+        vector<int>& v= nums;
+        int n = v.size();        
         int max_val = (int)pow(2,n);
+        vector<vector<int>> result(max_val, vector<int>());
         for(int i =0;i<max_val;i++) {
-            vector<int> vv;
             for(int j=0;j<n;j++) {
                if( (i & (1<<j)) > 0) {
-                  vv.push_back(v[j]);
+                   result[i].push_back(v[j]);
                }
             }
-            result.push_back(vv);
         }
-        return result;
+        return result;   
     }
 };
