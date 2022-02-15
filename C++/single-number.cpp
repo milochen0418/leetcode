9f96362357606a18fa6a90b925fd88eeb63d969b
milochen0418 <milo_chen@mail2000.com.tw>
2022-02-15 08:14:23 +0800

    C++/single-number.cpp answer1 finish

---
+++ C++/single-number.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    //https://leetcode.com/problems/single-number
+public:
+    int singleNumber(vector<int>& nums) {
+        return answer1(nums);
+    }
+    
+    int answer1(vector<int>& nums) {
+        unordered_set<int> s1;
+        unordered_set<int> s2;
+        for(auto &num: nums) {
+            if(s1.find(num) == s1.end()) {
+                s1.insert(num);
+            } else {
+                s2.insert(num);
+            }
+        }
+        for(auto &num: nums) {
+            if(s2.find(num) == s2.end()) {
+                return num;
+            }
+        }
+        return 0;
+    }
+};
