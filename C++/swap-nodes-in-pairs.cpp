81ff425b1cd3f89a90a3ba348b58ce200f60a380
milochen0418 <milo_chen@mail2000.com.tw>
2022-02-16 12:15:03 +0800

    C++/swap-nodes-in-pairs.cpp add new answer

--- C++/swap-nodes-in-pairs.cpp
+++ C++/swap-nodes-in-pairs.cpp
@@ -11,4 +11,35 @@ public:
         }
         return ans;
     }
+    
+    ListNode* answer_new(ListNode* head) {
+        
+        ListNode* dummy = new ListNode(0); 
+        //dummy->next would be answer
+        ListNode* head_prev = dummy; 
+        head_prev->next = head;
+        while(!(head == nullptr || head->next == nullptr)  ){
+            //swap(head->val, head->next->val);
+            ListNode* _head_prev = head_prev;
+            ListNode* _head = head;
+            ListNode* _head_next = head->next;
+            ListNode* _head_next_next = head->next->next;
+            
+            //current status is 
+            //_head_prev -> _head -> _head_next -> _head_next_next
+            //And our purpose is to make the following status
+            //_head_prev -> _head_next -> _head -> _head_next_next
+            //So we just need to rewrite three next link as the following
+            _head_prev->next = _head_next;
+            _head_next->next = _head;
+            _head->next = _head_next_next;
+                        
+            //After rewrite done, then keep going the loop by change status
+            head = _head_next_next;
+            head_prev = _head;
+            
+        }
+        return dummy->next;
+    }
+
 };
