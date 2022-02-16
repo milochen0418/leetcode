e188058b9a77dd4fe1d072872eb52463b5d93b9f
milochen0418 <milo_chen@mail2000.com.tw>
2022-02-16 08:18:41 +0800

    C++/swap-nodes-in-pairs.cpp FB post

--- C++/swap-nodes-in-pairs.cpp
+++ C++/swap-nodes-in-pairs.cpp
@@ -1,6 +1,7 @@
 class Solution {
     //https://leetcode.com/problems/swap-nodes-in-pairs/
     //article https://leetcode.com/problems/swap-nodes-in-pairs/discuss/1774267/C%2B%2B-or-swap-or-O(N)
+    //FB post https://www.facebook.com/groups/1451299754892511/posts/5074225949266522/
 public:
     ListNode* swapPairs(ListNode* head) {
         ListNode* ans = head;
