72f0ad3e8e514ac64c6a5062ff638d340916cdfc
milochen0418 <milo_chen@mail2000.com.tw>
2022-02-16 14:44:09 +0800

    C++/find-k-length-substrings-with-no-repeated-characters.cpp pass

---
+++ C++/find-k-length-substrings-with-no-repeated-characters.cpp
@@ -0,0 +1,32 @@
+class Solution {
+    //https://leetcode.com/problems/find-k-length-substrings-with-no-repeated-characters/
+public:
+    int numKLenSubstrNoRepeats(string s, int k) {
+        int num = 0;
+        int n = s.length();
+        if(k>n) return num;
+        
+        unordered_map<char,int> mp;
+        int overcnt = 0;
+        for(int i=0; i<k;i++) {
+            overcnt += mp[s[i]]>0;
+            mp[s[i]] +=1;
+        }
+        num += overcnt==0;
+        
+        for(int i=1; i<n-k+1; i++) {
+            mp[s[i-1]] -= 1;
+            overcnt -= mp[s[i-1]] > 0;
+            overcnt += mp[s[i+k-1]] > 0;
+            mp[s[i+k-1]] +=1;
+            num += overcnt==0;
+        }
+        return num;
+    }
+};
+
+/* testcase
+"havefunonleetcode"
+5
+
+*/
