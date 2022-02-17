17d207694d1d912370cd3555f956196f7e6f583c
milochen0418 <milo_chen@mail2000.com.tw>
2022-02-17 21:52:45 +0800

    C++/combination-sum.cpp failed testcase [2,7,6,3,5,1] 9

---
+++ C++/combination-sum.cpp
@@ -0,0 +1,45 @@
+class Solution {
+    //https://leetcode.com/problems/combination-sum/
+public:
+    vector<vector<int>> ans;
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        int n = candidates.size();
+        if(n==0) return ans;
+        vector<int>& v = candidates;
+        vector<int> a;
+        int v_idx = 0;
+        combineSum(v,v_idx, a, target);
+        
+        return ans;
+    }
+    
+    void combineSum(vector<int>& v,int& v_idx, vector<int>& a, int target ) {
+        for(int i=v_idx;i<v.size();i++) {
+            int val = v[i];
+            if(val < target){
+                a.push_back(val);
+                //combineSum(v,v_idx, a, target-val);
+                combineSum(v,i, a, target-val);
+                a.pop_back();
+                
+            } else if(val == target) {
+                a.push_back(val);
+                ans.push_back(a);
+                a.pop_back();
+            } else {// case val > target
+                return;
+            }
+        }
+    }
+};
+
+/* testcase
+[2,3,6,7]
+7
+[2,3,5]
+8
+[2]
+1
+[2,7,6,3,5,1]
+9
+*/
