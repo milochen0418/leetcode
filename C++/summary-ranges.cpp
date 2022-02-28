d3bd93cd330f9229daf9a8c6390878a0e4cf8a3d
milochen0418 <milo_chen@mail2000.com.tw>
2022-02-28 15:59:39 +0800

    C++/summary-ranges not pass testcase [-2147483648,-2147483647,2147483647]

---
+++ C++/summary-ranges.cpp
@@ -0,0 +1,35 @@
+class Solution {
+    //https://leetcode.com/problems/summary-ranges
+public:
+    vector<string> summaryRanges(vector<int>& nums) {
+        vector<string> v;
+        nums.push_back(nums.size()==0?0:nums[nums.size()-1]+2);
+        int n = nums.size();
+        int begin,end;
+        begin = end = nums[0];
+        
+        for(int i=1;i<n;i++) {
+            if(nums[i]==nums[i-1]+1) {
+                end=nums[i];
+            } else {
+                v.push_back(begin!=end?to_string(begin)+"->"+to_string(end) : to_string(begin));
+                begin = end = nums[i];
+            }
+        }
+        return v;
+    }
+};
+
+/* testcase 
+[3,5,8,10,11]
+[0,2,3,4,6,8,9]
+[]
+[3]
+[0]
+[3,5,8,10,11]
+[0,2,3,4,6,8,9]
+[]
+[3]
+[0]
+[-2147483648,-2147483647,2147483647]
+*/
