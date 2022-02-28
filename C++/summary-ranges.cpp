2da9446d2bb7a029436bf2b2d6a65cdc87ad8eff
milochen0418 <milo_chen@mail2000.com.tw>
2022-03-01 00:00:43 +0800

    C++/summary-ranges pass overflow by another way and add articles

--- C++/summary-ranges.cpp
+++ C++/summary-ranges.cpp
@@ -1,19 +1,23 @@
 class Solution {
     //https://leetcode.com/problems/summary-ranges
+    //FB post https://www.facebook.com/groups/1451299754892511/posts/5112424425446674
+    //article https://leetcode.com/problems/summary-ranges/discuss/1806223/C%2B%2B-or-12-lines-or-put-dummy
+    
 public:
     vector<string> summaryRanges(vector<int>& nums) {
-        vector<string> v;
+        vector<string> ans;
         nums.push_back(nums.size()==0?0:nums.back());
         int begin=nums[0],end=nums[0];        
         for(int i=1;i<nums.size();i++) {
-            if((long)nums[i]==(long)nums[i-1]+1) {
+            //if((long)nums[i]==(long)nums[i-1]+1) {
+            if(nums[i]-1==nums[i-1]){                
                 end = nums[i];
             } else {
-                v.push_back(begin!=end?to_string(begin)+"->"+to_string(end) : to_string(begin));
+                ans.push_back(begin!=end?to_string(begin)+"->"+to_string(end) : to_string(begin));
                 begin = end = nums[i];
             }
         }
-        return v;
+        return ans;
     }
 };
 
