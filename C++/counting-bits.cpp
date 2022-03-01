ebbd30e4c96e245deeffe8faff04bcf86319fcbe
milochen0418 <milo_chen@mail2000.com.tw>
2022-03-01 13:05:00 +0800

    C++/counting-bits DP solution

--- C++/counting-bits.cpp
+++ C++/counting-bits.cpp
@@ -2,20 +2,27 @@ class Solution {
     //https://leetcode.com/problems/counting-bits
 public:
     vector<int> countBits(int n) {
-        vector<int> ans(n+1,0);
-        for(int i = 0; i<=n; i++) {
-            int cnt=0;
-            for(int j = 0; j<30; j++) {
-                if( (i & (1<<j)) > 0) {
-                    cnt++;
-                }
-            }
-            ans[i] = cnt;  
+        vector<int> ans(n+1,0);        
+        for(int i=1,j=1; i<n+1;i++) {
+            if(i==j*2) j=i;
+            ans[i] = ans[i-j]+1;
         }
         return ans;
     }
-};
+    
+    vector<int> answer1(int n) {
+        vector<int> ans(n+1,0);
+        for(int i = 0; i<=n; i++) {
+            int cnt=0;
+            for(int j = 0; j<17; j++) {
+                if ( (i & (1<<j))>0)cnt++;
+            }
+            ans[i] = cnt;  
+        }
+        return ans;                
+    }
 
+};
 /* testcase 
 100000
 */
