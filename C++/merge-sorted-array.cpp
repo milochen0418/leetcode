c0bdf302e64753ad3f427b6f74d4dc32eb29aaa0
milochen0418 <milo_chen@mail2000.com.tw>
2022-03-02 13:55:17 +0800

    C++/merge-sorted-array  Add more solution

--- C++/merge-sorted-array.cpp
+++ C++/merge-sorted-array.cpp
@@ -2,7 +2,7 @@ class Solution {
     //https://leetcode.com/problems/merge-sorted-array/
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        return answer2(nums1, m, nums2, n);
+        return answer3(nums1, m, nums2, n);
     }
     
     void answer1(vector<int>& nums1, int m, vector<int>& nums2, int n) {
@@ -20,5 +20,13 @@ public:
         while(j >= 0) 
             nums1[k--] = nums2[j--];                
     }
-    
+
+    void answer3(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = m-1, j = n-1, k = n+m-1;
+        while( j>=0) {
+            int v1 = i<0?INT_MIN:nums1[i];
+            int v2 = j<0?INT_MIN:nums2[j];
+            nums1[k--] = v2>v1? nums2[j--]: nums1[i--];
+        }
+    }
 };
