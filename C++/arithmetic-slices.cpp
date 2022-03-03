e111c022ba79ae69470e476e1b7ef3f941eba517
milochen0418 <milo_chen@mail2000.com.tw>
2022-03-03 12:10:18 +0800

    C++/arithmetic-slices finish

--- C++/arithmetic-slices.cpp
+++ C++/arithmetic-slices.cpp
@@ -3,13 +3,32 @@ class Solution {
 public:
     int numberOfArithmeticSlices(vector<int>& nums) {
         //Observation
-        //[1,2]
-        //[1,2] + [3]
-        //[1,2,3] + [4]
-        //[1,2,3,4] + [5]
-        //[1,2,3,4,5] + [6]
-        //[1,2,3,4,5] + [7]
-        //[1,2,3,4,5] + [7] + [9]
-        return 1;
+        //[1]                 (begin=0,end=0, diff=0)
+        //[1,2]               (begin=0,end=1, diff=1)
+        //[1,2] + [3] + 1     (end-begin-1 = 2-0-1)
+        //[1,2,3] + [4] + 2   (end-begin-1 = 3-0-1)
+        //[1,2,3,4] + [5] + 3 (end-begin-1 = 4-0-1, diff=1)  
+        //[1,2,3,4,5] + [7]   (begin=4, end=5, diff=2) //when diff change , then reset here
+        //[1,2,3,4,5] + [7] + [9]  + 1  (begin=4, end=6,  end-begin-1=1)
+        //[1,2,3,4,5] + [7] + [9] + [11] + 2  (begin=4, end=7,  end-begin-1=2)
+        
+        int n = nums.size();
+        if(n<3) return 0;
+        int begin = 0;
+        int end = 1;
+        int diff = nums[end] - nums[begin];
+        int cnt = 0;
+        ++end;
+        while(end<n) {
+            int podiff = nums[end]-nums[end-1];
+            if(podiff != diff) {
+                begin = end-1;
+                diff = podiff;
+            } else {
+                cnt += (end -begin-1);
+            }
+            ++end;
+        }
+        return cnt;
     }
 };
