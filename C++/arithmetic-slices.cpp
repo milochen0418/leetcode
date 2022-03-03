2f3a6e38d765e68c9b8e187e7075b0e15792a37f
milochen0418 <milo_chen@mail2000.com.tw>
2022-03-03 08:20:11 +0800

    C++/arithmetic-slices init with observation

---
+++ C++/arithmetic-slices.cpp
@@ -0,0 +1,15 @@
+class Solution {
+    //https://leetcode.com/problems/arithmetic-slices/
+public:
+    int numberOfArithmeticSlices(vector<int>& nums) {
+        //Observation
+        //[1,2]
+        //[1,2] + [3]
+        //[1,2,3] + [4]
+        //[1,2,3,4] + [5]
+        //[1,2,3,4,5] + [6]
+        //[1,2,3,4,5] + [7]
+        //[1,2,3,4,5] + [7] + [9]
+        return 1;
+    }
+};
