041094a9f073f53af17c71c30336c8482410b7ed
milochen0418 <milo_chen@mail2000.com.tw>
2022-03-10 20:07:36 +0800

    C++/add-two-numbers simplify code

--- C++/add-two-numbers.cpp
+++ C++/add-two-numbers.cpp
@@ -12,7 +12,26 @@ class Solution {
     //https://leetcode.com/problems/add-two-numbers/
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        
+        //return answer1(l1,l2);
+        ListNode *dummy = new ListNode(0);
+        ListNode *head = dummy;
+        int carry = 0;
+        while(l1!=nullptr || l2!=nullptr) {
+            int v1 = (l1!=nullptr)?l1->val:0;
+            int v2 = (l2!=nullptr)?l2->val:0;
+            
+            int sum = v1+v2+carry;
+            carry = sum/10;
+            head = head->next = new ListNode(sum%10);
+            
+            l1=(l1!=nullptr)?l1->next:l1;
+            l2=(l2!=nullptr)?l2->next:l2;
+        }
+        if(carry==1) head->next = new ListNode(1);
+        return dummy->next!=nullptr?dummy->next:dummy;
+
+    }
+    ListNode* answer1(ListNode* l1, ListNode* l2) {
         ListNode* ans = nullptr;
         ListNode* head = nullptr;
         int carry = 0;
