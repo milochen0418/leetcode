dfd631bc1d5e92a3604304a2dbaa6faddb2b7bdb
milochen0418 <milo_chen@mail2000.com.tw>
2022-03-10 20:20:14 +0800

    C++/add-two-numbers finish

--- C++/add-two-numbers.cpp
+++ C++/add-two-numbers.cpp
@@ -18,18 +18,15 @@ public:
         int carry = 0;
         while(l1!=nullptr || l2!=nullptr) {
             int v1 = (l1!=nullptr)?l1->val:0;
-            int v2 = (l2!=nullptr)?l2->val:0;
-            
+            int v2 = (l2!=nullptr)?l2->val:0;            
             int sum = v1+v2+carry;
             carry = sum/10;
             head = head->next = new ListNode(sum%10);
-            
             l1=(l1!=nullptr)?l1->next:l1;
             l2=(l2!=nullptr)?l2->next:l2;
         }
         if(carry==1) head->next = new ListNode(1);
-        return dummy->next!=nullptr?dummy->next:dummy;
-
+        return dummy->next;
     }
     ListNode* answer1(ListNode* l1, ListNode* l2) {
         ListNode* ans = nullptr;
