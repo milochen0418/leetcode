78c03175adfd4bd670fae6837a87dafd82ad93f7
milochen0418 <milo_chen@mail2000.com.tw>
2022-03-16 12:04:06 +0800

    C++/validate-stack-sequences.cpp

---
+++ C++/validate-stack-sequences.cpp
@@ -0,0 +1,22 @@
+class Solution {
+    // https://leetcode.com/problems/validate-stack-sequences
+public:
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+        stack<int> stk;
+        int n = pushed.size();
+        int pu = 0; //index of pushed vector
+        int po = 0; //index of popped vector
+        while(po<n) { 
+            if(stk.empty()) {
+                stk.push(pushed[pu++]);
+            } else if (stk.top()==popped[po]) {
+                stk.pop();
+                po++;
+            } else if(stk.top()!=popped[po]) {
+                if(pu>=n) return false;
+                stk.push(pushed[pu++]);
+            }
+        }
+        return true;        
+    }
+};
