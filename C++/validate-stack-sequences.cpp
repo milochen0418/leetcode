824f231700ac8137e0e9b16a3fd717840385783b
milochen0418 <milo_chen@mail2000.com.tw>
2022-03-16 19:40:56 +0800

    C++/validate-stack-sequences.cpp change to space O(1)

--- C++/validate-stack-sequences.cpp
+++ C++/validate-stack-sequences.cpp
@@ -2,17 +2,22 @@ class Solution {
     // https://leetcode.com/problems/validate-stack-sequences
 public:
     bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
-        stack<int> stk;
+        //stack<int> stk;
+        int stk_idx=-1;
         int n = pushed.size();
         int pu = 0; //index of pushed vector
         int po = 0; //index of popped vector
+        
         while(po<n) { 
-            if(!stk.empty() && stk.top()==popped[po] ) {
-                stk.pop();
+            //if(!stk.empty() && stk.top()==popped[po] ) {
+            if(stk_idx>=0 && pushed[stk_idx]==popped[po] ) {
+                //stk.pop();
+                --stk_idx;
                 po++;                
             } else {
                 if(pu>=n) return false;
-                stk.push(pushed[pu++]);                
+                //stk.push(pushed[pu++]);
+                pushed[++stk_idx] = pushed[pu++];
             }
         }
         return true;                
