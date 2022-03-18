1930d7b80b14bb040fbf28d8e4c5f17f1afe1118
milochen0418 <milo_chen@mail2000.com.tw>
2022-03-19 00:54:29 +0800

    C++/remove-duplicate-letters.cpp refer answer but don't know why

--- C++/remove-duplicate-letters.cpp
+++ C++/remove-duplicate-letters.cpp
@@ -13,38 +13,21 @@ public:
     // https://www.geeksforgeeks.org/deque-cpp-stl/
     string removeDuplicateLetters(string s) {
         unordered_map<char,int> mp;//char to maximum index
-        unordered_map<char,int> in_stack_mp;
-        deque<int> q;
+        unordered_map<char,int> in_stack_mp;//in_stack_mp[i]==1 iff char i in deque
+        deque<int> q; 
         
         int n = s.length();
         for(int i = 0; i<n; i++) mp[s[i]] = i;
         
         for(int i = 0; i<n; i++){ 
             char c = s[i];
-            if(q.empty()) {
+            if(in_stack_mp[c]==0){
+                while(!q.empty() && c<q.back() && mp[q.back()]>i) {
+                    in_stack_mp[q.back()]=0;
+                    q.pop_back();
+                }
+                in_stack_mp[c] = 1;
                 q.push_back(c);
-                in_stack_mp[c]=1;
-            } else if(c>q.back()) {
-                if(in_stack_mp[c]==0) {
-                    q.push_back(c);
-                    in_stack_mp[c]=1;
-                }
-            } else if(c<q.back()) {
-                if(mp[q.back()]>i) {
-                    if(in_stack_mp[c]==0){
-                        while(!q.empty() && mp[q.back()]>i) {
-                            in_stack_mp[q.back()]=0;
-                            q.pop_back();
-                        }
-                        q.push_back(c);
-                        in_stack_mp[c]=1;
-                    }
-                } else {
-                    if(in_stack_mp[c]==0) {
-                        q.push_back(c);
-                        in_stack_mp[c]=1;
-                    }
-                }
             }
         }
         string ans="";
@@ -59,7 +42,6 @@ public:
 };
         
 
-        
 
         
 
