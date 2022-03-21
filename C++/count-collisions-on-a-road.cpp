09bb46f821d8c09b2e059bcab526060008eae68b
milochen0418 <milo_chen@mail2000.com.tw>
2022-03-21 10:46:39 +0800

    C++/count-collisions-on-a-road.cpp 院征軍 solution

--- C++/count-collisions-on-a-road.cpp
+++ C++/count-collisions-on-a-road.cpp
@@ -1,15 +1,19 @@
 class Solution {
     //https://leetcode.com/problems/count-collisions-on-a-road
     //article https://leetcode.com/problems/count-collisions-on-a-road/discuss/1866943/what-a-stupid-solution-i-like-this-problem
+    //FB Post https://www.facebook.com/groups/1451299754892511/posts/5168075553214894/
 public:
     int countCollisions(string s) {
+        int S_cnt=0, L_cnt=0, R_cnt=0, n = s.length();
+        for(int i=0;i<n;i++) if(s[i]=='S') ++S_cnt;
+        for(int i=0;i<n && s[i]=='L';i++) ++L_cnt;
+        for(int i=n-1; i>=0 && s[i]=='R';--i) ++R_cnt;
+        return n-S_cnt-L_cnt-R_cnt;
+    }
+    int answer1(string s) {
         int ans = 0;
         int n = s.length();
-		//siv(S index vector) is to save all index of S after replace all "LR" to "SS"
-        vector<int> siv; 
-		
-		//Step 1.
-		//Replace all "LR" into "SS" and makeing the data of siv.
+        vector<int> siv; //S index vector
         for(int i=0; i<n;i++) {
             if( i+1<n && s[i] == 'R' && s[i+1] == 'L') {
                 s[i] = s[i+1] = 'S';
@@ -18,8 +22,7 @@ public:
             if(s[i]=='S') siv.push_back(i);
         }
         
-		//Step 2. 
-        //Start to solve this problem RRRSLLL... RRSLLL ... RRRSLLL 
+        
         for(auto &idx:siv) {
             int i = idx-1;
             while(i>=0 && s[i]=='R') {
@@ -34,5 +37,6 @@ public:
             }
         }
         return ans;
+        
     }
 };
