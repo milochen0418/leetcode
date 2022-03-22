0c6250b7d16bb0646dc74f8f7edaaea6929d1c4f
milochen0418 <milo_chen@mail2000.com.tw>
2022-03-22 23:20:56 +0800

    C++/move-zeroes.cpp Add article and FB post

--- C++/move-zeroes.cpp
+++ C++/move-zeroes.cpp
@@ -1,5 +1,7 @@
 class Solution {
     //https://leetcode.com/problems/move-zeroes/
+    //article https://leetcode.com/problems/move-zeroes/discuss/1873790/C%2B%2B-or-counter-approach-or-space-O(1)-time-O(n)-and-optimal
+    //FB post https://www.facebook.com/groups/1451299754892511/posts/5173709149318201/
 public:
     void moveZeroes(vector<int>& nums) {
         int cnt=0, n = nums.size();
