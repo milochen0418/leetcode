4fb45361741b45d07a027bc821835bcb3e7553eb
milochen0418 <milo_chen@mail2000.com.tw>
2022-03-24 20:33:50 +0800

    C++/broken-calculator.cpp Add FB post that include math proof

--- C++/broken-calculator.cpp
+++ C++/broken-calculator.cpp
@@ -1,7 +1,7 @@
 class Solution {
     //https://leetcode.com/problems/broken-calculator/
     //article https://leetcode.com/problems/broken-calculator/discuss/1876846/C%2B%2B-or-how-to-prove-the-O(logN)-solution-will-always-work
-    
+    //FB Post https://www.facebook.com/groups/1451299754892511/posts/5177816538907462/
 public:
     int brokenCalc(int startValue, int target) {
         int ans = 0; 
