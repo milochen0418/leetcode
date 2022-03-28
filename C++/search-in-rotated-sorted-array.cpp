7aab84bfea2f3cc3b4fc21c192beb4162b2c4d0c
milochen0418 <milo_chen@mail2000.com.tw>
2022-03-28 15:08:59 +0800

    C++/search-in-rotated-sorted-array.cpp optimize

--- C++/search-in-rotated-sorted-array.cpp
+++ C++/search-in-rotated-sorted-array.cpp
@@ -28,30 +28,21 @@ public:
         int M = L + (R-L)/2; //(L + R) / 2;
         if (a[M] == t) return M;
         
-        //case 1
-        if (a[L] <= a[M] && a[M] <= a[R]) {
-            return binSearch(L, R, t,a);
-        } else {            
-            //case 2   
-            if (a[L] <= a[M] && a[R] <= a[L]) {
-                if (a[L] <= t && t <= a[M]) {
-                    //return rotSearch(L, M - 1, t,a );
-                    return binSearch(L, M - 1, t,a);
-                } else {
-                    return rotSearch(M + 1, R, t,a);  
-                }
-            }    
-            //case 3
-            if (a[M] <= a[R] && a[L] >= a[R]) {
-                if (a[M] <= t && t <= a[R]) {
-                    //return rotSearch(M + 1, R, t,a );
-                    return binSearch(M + 1, R, t,a);
-                } else {
-                    return rotSearch(L, M - 1, t,a);
-                }
-            }  
-            //case otherwise      
-            return -2; //Un-expected Error. 
-        }    
+            
+        if (a[L] <= a[M] ) {
+            //case 1
+            if (a[L] <= t && t <= a[M]) {
+                return binSearch(L, M - 1, t,a);
+            } else {
+                return rotSearch(M + 1, R, t,a);  
+            }
+        } else {
+            //case 2
+            if (a[M] <= t && t <= a[R]) {
+                return binSearch(M + 1, R, t,a);
+            } else {
+                return rotSearch(L, M - 1, t,a);
+            }                
+        }
     }    
 };
