6f4080e99f2aaadc33ed63dedc523f9c6374a6a4
milochen0418 <milo_chen@mail2000.com.tw>
2022-03-30 10:52:14 +0800

    C++/search-a-2d-matrix.cpp Add FB Post

--- C++/search-a-2d-matrix.cpp
+++ C++/search-a-2d-matrix.cpp
@@ -1,6 +1,7 @@
 class Solution {
     //https://leetcode.com/problems/search-a-2d-matrix
     //article https://leetcode.com/problems/search-a-2d-matrix/discuss/1895843/C%2B%2B-or-binary-search-or-O(logN)
+    //FB Post https://www.facebook.com/groups/1451299754892511/posts/5193837967305319/
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int cols = matrix[0].size();
