6caee7f4a60ab9af4f0909fe2019ec4b24485b62
milochen0418 <milo_chen@mail2000.com.tw>
2022-03-30 21:37:43 +0800

    C++/third-maximum-number.cpp finish

---
+++ C++/third-maximum-number.cpp
@@ -0,0 +1,26 @@
+class Solution {
+    //https://leetcode.com/problems/third-maximum-number
+public:
+    int thirdMax(vector<int>& nums) {
+        unordered_set<int> s; 
+        int n = nums.size();
+        for(int i = 0;i<n;i++) {
+            s.insert(nums[i]);
+            if(s.size()>3) {
+                int delmin = INT_MAX;
+                for(auto& i:s) delmin = min(i,delmin);
+                s.erase(delmin);
+            }
+        }
+        if(s.size()<3) {
+            int findmax = INT_MIN;
+            for(auto& i:s) findmax = max(i,findmax);
+            return findmax;
+        } else {
+            int find3rdmax = INT_MAX;
+            for(auto& i:s) find3rdmax = min(i,find3rdmax);
+            return find3rdmax;
+        }
+        
+    }
+};
