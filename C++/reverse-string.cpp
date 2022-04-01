0dfaa98f93ad339324f0be6a9ca6d738f4db89d2
milochen0418 <milo_chen@mail2000.com.tw>
2022-04-01 10:47:59 +0800

    C++/reverse-string.cpp add article

--- C++/reverse-string.cpp
+++ C++/reverse-string.cpp
@@ -1,5 +1,6 @@
 class Solution {
     //https://leetcode.com/problems/reverse-string
+    //article https://leetcode.com/problems/reverse-string/discuss/1902008/C%2B%2B-or-two-pointer
 public:
     void reverseString(vector<char>& s) {
         int L = 0, R = s.size()-1;
