94b60fb5d45ecb40ecc7aea838ca9f9636b4743b
milochen0418 <milo_chen@mail2000.com.tw>
2022-04-02 19:19:36 +0800

    C++/valid-palindrome-ii.cpp finish

---
+++ C++/valid-palindrome-ii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+    //https://leetcode.com/problems/valid-palindrome-ii
+public:
+    bool validPalindrome(string s) {
+        int L=0,R = s.length()-1;
+        while(L<R) {
+            if(s[R]!=s[L]) {
+                int L1=L+1, R1=R;
+                int L2=L,R2=R-1;
+                bool case1=true, case2=true;
+                while(L1<R1){
+                    if(s[R1]!=s[L1]) {
+                        case1=false;
+                        break;
+                    }
+                    R1--;L1++;
+                }
+                while(L2<R2){
+                    if(s[R2]!=s[L2]) {
+                        case2=false;
+                        break;
+                    }
+                    R2--;L2++;
+                }
+                return case1||case2;
+            }            
+            R--;L++;
+        }
+            
+        return true;
+    }
+};
