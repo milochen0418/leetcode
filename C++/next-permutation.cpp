49340a5d8038331de0521bc69582e46e22a82a59
milochen0418 <milo_chen@mail2000.com.tw>
2022-04-03 22:53:29 +0800

    C++/next-permutation.cpp refer answer from Java to write C++

---
+++ C++/next-permutation.cpp
@@ -0,0 +1,14 @@
+class Solution {
+    //https://leetcode.com/problems/next-permutation
+public:
+    void nextPermutation(vector<int>& nums) {
+        int i = nums.size()-2;
+        while(i>=0 && nums[i+1] <= nums[i]) i--;
+        if(i>=0) {
+            int j = nums.size() - 1;
+            while(nums[j] <= nums[i]) j--;
+            swap(nums[i], nums[j]);
+        }
+        reverse(nums.begin()+i+1, nums.end());
+    }
+};
