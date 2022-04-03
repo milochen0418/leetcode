baf7d314db18aaf9ed26c681a3793f7ece81dc8e
milochen0418 <milo_chen@mail2000.com.tw>
2022-04-03 13:15:42 +0800

    C++/maximum-candies-allocated-to-k-children.cpp finish first version

---
+++ C++/maximum-candies-allocated-to-k-children.cpp
@@ -0,0 +1,34 @@
+class Solution {
+    //https://leetcode.com/problems/maximum-candies-allocated-to-k-children
+public:
+    using ll = long long ;
+    int maximumCandies(vector<int>& a, ll k) {
+        int ans = 1;
+        int L = 0; 
+        int R = 1000000000;
+        while(L<R) {
+            
+            int M = L + (R-L)/2;
+            //cout<<"(L,R)="<<L<<","<<R<<" -> M="<<M<<"\n";
+            if(M==0) return 0;
+            if(check(a,k,M) && !check(a,k,M+1)){
+                return M;
+            } else {
+                if(check(a,k, M+1)==true) {
+                    L=M+1;
+                } else {
+                    R=M;
+                }
+            }            
+        }
+        return 0;
+    }
+    
+    bool check(vector<int>& a, ll k, int m) {
+        ll sum = 0;
+        for(auto& i:a) sum+= i/m;
+        //if(sum>=k) return true;
+        return sum>=k;
+    }
+
+};
