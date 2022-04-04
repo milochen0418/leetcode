5902b2d641ac54f65832b00d4197dcae9f5803b3
milochen0418 <milo_chen@mail2000.com.tw>
2022-04-04 18:22:16 +0800

    C++/max-consecutive-ones-iii.cpp add article

--- C++/max-consecutive-ones-iii.cpp
+++ C++/max-consecutive-ones-iii.cpp
@@ -1,5 +1,6 @@
 class Solution {
     //https://leetcode.com/problems/max-consecutive-ones-iii
+    //article https://leetcode.com/problems/max-consecutive-ones-iii/discuss/1913253/C%2B%2B-or-two-pointer-or-simple-solution-or-time-O(N)-space-O(1)
 public:
     int longestOnes(vector<int>& a, int k) {
         int zeros=0, ans=0, L=0, R=0, n=a.size();
