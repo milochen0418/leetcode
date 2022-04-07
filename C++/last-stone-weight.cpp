f90e6fbcec24aa7427bc98e63e81cdb820f0bab4
milochen0418 <milo_chen@mail2000.com.tw>
2022-04-07 18:55:42 +0800

    C++/last-stone-weight.cpp add O(NlogN) solution

--- C++/last-stone-weight.cpp
+++ C++/last-stone-weight.cpp
@@ -2,8 +2,22 @@ class Solution {
     //https://leetcode.com/problems/last-stone-weight
 public:
     int lastStoneWeight(vector<int>& stones) {
+        //return answer1(stones);
+        priority_queue<int> maxQ;
+        for(auto &i:stones) maxQ.push(i);
+        while(maxQ.size()>1) {
+            int e1 = maxQ.top();
+            maxQ.pop();
+            e1 -= maxQ.top();
+            maxQ.pop();
+            if(e1>0) maxQ.push(e1);
+        }
+        return maxQ.empty()?0:maxQ.top();
+    }
+    int answer1(vector<int>& stones) {
         int stones_size = stones.size();        
         while(stones_size>1) {
+            
             sort(stones.begin(), stones.end(),greater<int>());
             //for(auto &i:stones) cout<<", "<<i;cout<<" size="<<stones_size<<"\n";
             int minus = stones[1];
@@ -23,5 +37,5 @@ public:
             return 0;
         } 
     }
-
+    
 };
