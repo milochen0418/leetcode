e89fedc6b08b2f879e64c3f17cb177c55ce37925
milochen0418 <milo_chen@mail2000.com.tw>
2022-04-21 22:18:24 +0800

    C++/design-hashset.cpp TODO

--- C++/design-hashset.cpp
+++ C++/design-hashset.cpp
@@ -1,21 +1,23 @@
 class MyHashSet {
 public:
+    vector<bool> hash_table;
     MyHashSet() {
-        
+        hash_table = vector<bool>(1000001, false);
     }
     
     void add(int key) {
-        
+        hash_table[key] = true;
     }
     
     void remove(int key) {
-        
+        hash_table[key] = false;
     }
     
     bool contains(int key) {
-        
+        return hash_table[key] == true;
     }
 };
+
 
 /**
  * Your MyHashSet object will be instantiated and called as such:
