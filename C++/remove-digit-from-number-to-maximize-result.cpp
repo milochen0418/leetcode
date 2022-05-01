27cad92ef24af99ca2f6f0d4c25623c1e02b1728
milochen0418 <milo_chen@mail2000.com.tw>
2022-05-02 00:58:48 +0800

    C++/remove-digit-from-number-to-maximize-result.cpp add article and FB post

--- C++/remove-digit-from-number-to-maximize-result.cpp
+++ C++/remove-digit-from-number-to-maximize-result.cpp
@@ -1,5 +1,7 @@
 class Solution {
     //https://leetcode.com/problems/remove-digit-from-number-to-maximize-result
+    //Article: https://leetcode.com/problems/remove-digit-from-number-to-maximize-result/discuss/1998434/C%2B%2B-or-TC%3AO(N)-SC%3AO(1)-or-no-string-compare-or-explanation
+    //FB Post: https://www.facebook.com/groups/1451299754892511/posts/5281480115207770/
 public:
     string removeDigit(string number, char digit) {
         int pos = -1, n = number.size();
