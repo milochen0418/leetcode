91fbcb32cf0a752448a57c858bbddf8ebb43b785
milochen0418 <milo_chen@mail2000.com.tw>
2022-05-04 14:11:42 +0800

    C++/max-number-of-k-sum-pairs.cpp refactor

--- C++/max-number-of-k-sum-pairs.cpp
+++ C++/max-number-of-k-sum-pairs.cpp
@@ -11,33 +11,21 @@ public:
             return lhs.first< rhs.first;
         });
         
-        //make sorted array. a[i].first is number. a[i].second is how many repeaed number for a[i].first
         int n = a.size();
         for(int i = 0;i<n;i++) {
-            if(a[i].second==0) continue;
             int t = k-a[i].first;
-            //printf("a[%d]=%d, k=%d, t=%d", i,a[i],k, t);
-            //cout<<"\n";
-            int L=i,R=n-1;
+            if(t==a[i].first) {
+                ans += a[i].second/2;
+                continue;
+            }
+            
+            int L=i+1,R=n-1;
             while(L<=R) {
                 int M = L+(R-L)/2;
                 if(a[M].first == t) {
-                    
-                    if(M==i) {
-                        int cnt = a[M].second/2;
-                        ans+=cnt;
-                        a[M].second -= cnt;
-                        //cout<<"match same\n";
-                        break;
-                        
-                    } else {
-                        int cnt = min(a[M].second, a[i].second);
-                        ans += cnt;
-                        a[M].second -= cnt;
-                        a[i].second -= cnt;
-                        //cout<<"match\n";
-                        break;                        
-                    }
+                    int cnt = min(a[M].second, a[i].second);
+                    ans += cnt;
+                    break;
                 } else if(t<a[M].first) {
                     R = M-1;
                 } else {
