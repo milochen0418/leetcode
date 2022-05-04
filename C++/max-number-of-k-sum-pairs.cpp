e9d75c92bac567e2bd9cfd757e6344e35e1b2019
milochen0418 <milo_chen@mail2000.com.tw>
2022-05-04 14:46:42 +0800

    C++/max-number-of-k-sum-pairs.cpp add FB post

--- C++/max-number-of-k-sum-pairs.cpp
+++ C++/max-number-of-k-sum-pairs.cpp
@@ -1,6 +1,7 @@
 class Solution {
     //https://leetcode.com/problems/max-number-of-k-sum-pairs
     //Article https://leetcode.com/problems/max-number-of-k-sum-pairs/discuss/2006689/C%2B%2B-or-binary-search-approach-or-O(N*log-N-)
+    //FB Post https://www.facebook.com/groups/1451299754892511/posts/5288808587808256/
 public:
     int maxOperationsByHashMap(vector<int>& nums, int k) {
         int ans = 0;
