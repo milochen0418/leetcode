f093f9786b4d4341fc47bc07741717f23ff32106
milochen0418 <milo_chen@mail2000.com.tw>
2022-05-07 00:25:30 +0800

    C++/find-permutation.cpp init observe

---
+++ C++/find-permutation.cpp
@@ -0,0 +1,15 @@
+class Solution {
+    //https://leetcode.com/problems/find-permutation/
+public:
+    vector<int> findPermutation(string s) {
+        return vector<int>();
+        /*
+        I [1,2]
+        DI [2,1,3]
+        DDI [3,2,1,4]
+        DDII [3,2,1,4,5]
+        DDIID [3,2,1,4,6,5]
+        DDIIDD [3,2,1,4,7,6,5]
+        */
+    }
+};
