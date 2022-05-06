1ecceafdc3fa59280ec04782d78e7d252d52e9bf
milochen0418 <milo_chen@mail2000.com.tw>
2022-05-07 00:33:58 +0800

    C++/find-permutation.cpp add more observation

--- C++/find-permutation.cpp
+++ C++/find-permutation.cpp
@@ -10,6 +10,12 @@ public:
         DDII [3,2,1,4,5]
         DDIID [3,2,1,4,6,5]
         DDIIDD [3,2,1,4,7,6,5]
+        DDIDD [3,2,1,6,5,4]
+        DDIDDDIDDDDI [3,2,1,7,6,5,4,12,11,10,9,8,13]
+        DDIDDDIDDDDII [3,2,1,7,6,5,4,12,11,10,9,8,13,14]
+        DDIDDDIDDDDIII [3,2,1,7,6,5,4,12,11,10,9,8,13,14,15]
+        DDIDDDIDDDDIIIDD [3,2,1,7,6,5,4,12,11,10,9,8,13,14,17,16,15]
         */
+        //The key point is to process How many IDD...D 
     }
 };
