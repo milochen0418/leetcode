bb0c7a996480481c813af3c67828f1fe4577dc32
milochen0418 <milo_chen@mail2000.com.tw>
2022-05-12 18:18:48 +0800

    C++/permutations.cpp finish

---
+++ C++/permutations.cpp
@@ -0,0 +1,26 @@
+class Solution {
+    //https://leetcode.com/problems/permutations
+public:
+    //a = [0,1,2,3]
+    //swap(a[0],a[1])
+    //a = [1,?,?,?]
+    
+    //[2,?,?,?]
+    //
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans = vector<vector<int>>();
+        per(ans, 0, nums);
+        return ans;
+    }
+    void per(vector<vector<int>>& ans, int start_idx,  vector<int>& a) {
+        if(start_idx == a.size()-1) {
+            ans.push_back(a);
+        } else {
+            for(int i=start_idx; i<a.size();i++) {            
+                swap(a[start_idx], a[i]);
+                per(ans, start_idx+1, a);
+                swap(a[start_idx], a[i]);
+            }
+        }
+    }
+};
