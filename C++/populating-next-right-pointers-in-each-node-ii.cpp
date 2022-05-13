4e25c700e5ec8c5ab2a0fad099da1453dbc5ca76
milochen0418 <milo_chen@mail2000.com.tw>
2022-05-13 20:11:52 +0800

    C++/populating-next-right-pointers-in-each-node-ii.cpp update

--- C++/populating-next-right-pointers-in-each-node-ii.cpp
+++ C++/populating-next-right-pointers-in-each-node-ii.cpp
@@ -22,20 +22,17 @@ public:
     Node* connect(Node* root) {
         if(root == nullptr) return nullptr;
         vector<queue<Node*>> qs = vector<queue<Node*>>(2,queue<Node*>());
-        int qidx = 0;
-        qs[qidx].push(root);
+        int idx = 0;
+        qs[idx].push(root);
         root->next = nullptr;
         
-        while(!qs[qidx].empty()) {
-            queue<Node*>& q = qs[qidx];
-            queue<Node*>& nextQ = qs[(qidx+1)%2];
-            Node* e = q.front();
-            cout<<e->val<<" ";
-            q.pop();
-            e->next = q.empty()?nullptr:q.front();
-            if(e->left != nullptr) nextQ.push(e->left);
-            if(e->right != nullptr) nextQ.push(e->right);
-            if(q.empty()) qidx = (qidx+1)%2;
+        while(!qs[idx].empty()) {
+            Node* e = qs[idx].front();
+            qs[idx].pop();
+            e->next = qs[idx].empty()?nullptr:qs[idx].front();
+            if(e->left != nullptr) qs[(idx+1)%2].push(e->left);
+            if(e->right != nullptr) qs[(idx+1)%2].push(e->right);
+            if(qs[idx].empty()) idx = (idx+1)%2;
         }
         return root;
     }
