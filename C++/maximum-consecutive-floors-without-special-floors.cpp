1923ad4b5c0557876466a2923108098196b37907
milochen0418 <milo_chen@mail2000.com.tw>
2022-05-15 11:10:39 +0800

    C++/maximum-consecutive-floors-without-special-floors.cpp contest finish

--- C++/maximum-consecutive-floors-without-special-floors.cpp
+++ C++/maximum-consecutive-floors-without-special-floors.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
+
     int maxConsecutive(int bottom, int top, vector<int>& special) {
+        vector<int>& v = special;
+        v.push_back(bottom-1);
+        v.push_back(top+1);
+        sort(v.begin(), v.end());
+        int n = v.size();
+        int max_v = 0;
+        cout<<v[0]<<" ";
+        for(int i = 1 ; i<n;i++) {
+            cout<<v[i]<<" ";
+            max_v = max(max_v, v[i]-v[i-1]-1);
+        }
+        cout<<"\n";
+        return max_v;
+        
+    }
+    int maxConsecutiveUnorderedSet(int bottom, int top, vector<int>& special) {
         unordered_set<int> ss; //all special
         for(auto&i :special ) ss.insert(i);
         int max_cnt = 0;
