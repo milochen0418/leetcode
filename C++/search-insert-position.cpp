ca066b16bf4583757482770182bb5f8f7b62c3ba
milochen0418 <milo_chen@mail2000.com.tw>
2022-05-18 13:54:52 +0800

    C++/search-insert-position.cpp update more solution

--- C++/search-insert-position.cpp
+++ C++/search-insert-position.cpp
@@ -15,4 +15,24 @@ public:
         }
         return L;
     }
+    int searchInsertAnswer2(vector<int>& a, int target) {    
+        //   [1,3,5,6]
+        //   0 1 2 3 4  <- there are five insertion position
+        int L = 0, R=a.size()-1;
+        while(L<=R) {
+            int M = L+(R-L)/2;
+            printf("a[M=%d]=%d\n",M, a[M]);
+            if( (M==0 || a[M-1]<t) && t<=a[M]) {
+                printf("hit M=%d\n",M);
+                return M;                
+            } else if(M>=a.size() || a[M]<t) {
+                printf("(L=%d -> L=%d, R=%d)\n",L, M+1,R);
+                L = M+1;
+            } else if( M==0 || t<=a[M-1]) {
+                printf("(L=%d,R=%d -> R=%d)\n",L, R, M-1);
+                R = M-1;
+            }
+        }
+        return L;
+    }
 };
