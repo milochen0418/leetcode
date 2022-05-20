26a16a483ddc1be29111eada390852d60f80285b
milochen0418 <milo_chen@mail2000.com.tw>
2022-05-20 12:54:35 +0800

    C++/unique-paths-ii.cpp finish

---
+++ C++/unique-paths-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+    //https://leetcode.com/problems/unique-paths-ii
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        vector<vector<int>>& a = obstacleGrid;
+        int m = a.size(), n = a[0].size();
+        vector<vector<int>> dp = vector<vector<int>>(m, vector<int>(n,-1));
+        dp[m-1][n-1] = a[m-1][n-1]==1?0:1;
+        return go(0,0,a,dp);
+    }
+    int go(int row, int col,vector<vector<int>>& a, vector<vector<int>>& dp) {
+        if(row >= a.size() || col >= a[0].size()) return 0;
+        if(dp[row][col ] >= 0) return dp[row][col];
+        if(a[row][col] == 1) {
+            dp[row][col] = 0;
+        } else {
+            dp[row][col] = go(row+1,col,a,dp) + go(row,col+1,a,dp);
+        }
+        return dp[row][col];
+    }
+};
