6d614afaddfe26911f2dba6bc1639ab56cb5fd85
milochen0418 <milo_chen@mail2000.com.tw>
2022-05-27 23:57:05 +0800

    C++/number-of-steps-to-reduce-a-number-to-zero.cpp add FB post

--- C++/number-of-steps-to-reduce-a-number-to-zero.cpp
+++ C++/number-of-steps-to-reduce-a-number-to-zero.cpp
@@ -1,6 +1,7 @@
 class Solution {
     //https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero
     //article https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/discuss/2079926/C%2B%2B-or-Only-lg(N)-times-and-Space-O(1)-or-Explanation
+    //FB Post https://www.facebook.com/groups/1451299754892511/posts/5357773284245119/
 public:
     int numberOfSteps(int num) {
         int ans=0;
