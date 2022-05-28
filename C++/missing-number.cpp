68752f9a6c49acd8c085b3d1208b65ebb2af9e24
milochen0418 <milo_chen@mail2000.com.tw>
2022-05-28 16:55:02 +0800

    C++/missing-number.cpp upadte article

--- C++/missing-number.cpp
+++ C++/missing-number.cpp
@@ -1,6 +1,6 @@
 class Solution {
     //https://leetcode.com/problems/missing-number/
-    //article https://leetcode.com/problems/missing-number/discuss/2082536/C%2B%2B-or-3-approach-or-xor-sort-hashsaet
+    //article https://leetcode.com/problems/missing-number/discuss/2082536/C%2B%2B-or-4-approach-or-sum-xor-sort-hashsaet
 public:
     int missingNumber(vector<int>& nums) {
         return missingNumberSum(nums);
