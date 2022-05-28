6394f650ce561b5152fe43ca594448201cb9ce4a
milochen0418 <milo_chen@mail2000.com.tw>
2022-05-28 16:27:14 +0800

    C++/missing-number.cpp finish by sorting

--- C++/missing-number.cpp
+++ C++/missing-number.cpp
@@ -7,4 +7,17 @@ public:
         for(auto& i:nums) ans = ans^i;
         return ans;
     }
+    int missingNumberXOR(vector<int>& nums) {
+        int ans = 0;
+        for(int i = 0;i<=nums.size();i++) ans= ans^i;
+        for(auto& i:nums) ans = ans^i;
+        return ans;        
+    }
+    int missingNumberSort(vector<int>& nums) {
+        int ans = 0;
+        sort(nums.begin(), nums.end());
+        for(ans = 0; ans<nums.size();ans++) if(ans!=nums[ans]) break;
+        return ans;
+    }
+
 };
