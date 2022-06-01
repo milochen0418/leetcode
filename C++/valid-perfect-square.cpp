8ae752329a21b30993dc63aba1345190f803eacc
milochen0418 <milo_chen@mail2000.com.tw>
2022-06-01 17:08:03 +0800

    C++/valid-perfect-square.cpp remove all long long

--- C++/valid-perfect-square.cpp
+++ C++/valid-perfect-square.cpp
@@ -2,19 +2,18 @@ class Solution {
     //https://leetcode.com/problems/valid-perfect-square/
 public:
     bool isPerfectSquare(int num) {
-        
-        using ll = long long ;
-        ll L=1, R=(ll)INT_MAX;
-        while(L<=R){
-            ll M = L+(R-L)/2;
-            if((ll)num>M*M) {
-                L=M+1;
-            } else if ((ll)num<M*M) {
-                R=M-1;
-            } else {
+        int L=1,  R= sqrt( INT_MAX-1 );
+        while(L<=R) {
+            int M = (L + (R-L)/2);
+            if(M*M == num) {
                 return true;
+            } else if(M*M > num) {
+                R = M-1;
+            } else { //case MM < num
+                L = M+1;
             }
         }
         return false;
     }
+
 };
