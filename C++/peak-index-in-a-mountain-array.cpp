f6dbc08be52a2cf13299d384be874dee88edc5fb
milochen0418 <milo_chen@mail2000.com.tw>
2022-06-01 16:50:40 +0800

    C++/peak-index-in-a-mountain-array.cpp add more answer related to matched case

--- C++/peak-index-in-a-mountain-array.cpp
+++ C++/peak-index-in-a-mountain-array.cpp
@@ -15,4 +15,17 @@ public:
         }
         return -1;
     }
+    int peakIndexInMountainArrayAnswer2(vector<int>& a) {
+        int L = 1, R=a.size()-2;
+        while(L<=R) {
+            int M = L + (R-L)/2;
+            if(a[M-1]<a[M] && a[M]>a[M+1]) return M; //case match  
+            if(a[M-1]>a[M]) { //case which is too large
+                R = M-1;
+            } else { // case which is too small
+                L = M+1;
+            }
+        }
+        return -1;
+    }
 };
