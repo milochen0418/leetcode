7a0a0a23c6d5c3ff5669e198c24339c47f424a60
milochen0418 <milo_chen@mail2000.com.tw>
2022-06-03 01:44:15 +0800

    C++/find-the-distance-value-between-two-arrays.cpp put bruteForce solution to understand the question

--- C++/find-the-distance-value-between-two-arrays.cpp
+++ C++/find-the-distance-value-between-two-arrays.cpp
@@ -29,4 +29,18 @@ public:
         
         return cnt;
     }
+
+    int findTheDistanceValueBruteForce(vector<int>& arr1, vector<int>& arr2, int d) {
+        int err = 0;
+        for(int i= 0;i<arr1.size();++i){
+            for(int j = 0;j<arr2.size();++j){
+                if(abs(arr1[i]-arr2[j])<=d){
+                    err++;
+                    break;
+                }
+            }
+        }
+        return arr1.size() - err;
+    }
+
 };
