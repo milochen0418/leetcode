a36d50f7d2fa28b26458f5014e55c2c859dd5908
milochen0418 <milo_chen@mail2000.com.tw>
2022-06-05 04:17:23 +0800

    C++/arranging-coins.cpp update

--- C++/arranging-coins.cpp
+++ C++/arranging-coins.cpp
@@ -2,6 +2,26 @@ class Solution {
     // https://leetcode.com/problems/arranging-coins
 public:
     int arrangeCoins(int n) {
+        using ll = long long; 
+        ll L = 1,R=(ll)(3*(sqrt(INT_MAX)/2)); //(R*(R+1))/2 = INT_MAX
+        while(L<=R) {
+            ll t = (ll)n;
+            ll M = L+(R-L)/2;
+            ll Q1 = (M*(M+1))/2;
+            ll Q2 = ((M+1)*(M+2))/2;
+            if(Q2>t && t>=Q1) {
+                return (int)M;
+            } 
+            if(t>=Q2)
+                L=M+1;
+            else
+                R=M-1;
+        }
+        return -1;        
+    }
+
+
+    int arrangeCoinsOld(int n) {
         //  n       n(n+1)
         //Sigma i = -------
         // i=1         2
