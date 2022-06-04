3ef73756bff15716a900fa9598fee79e71aa5d8d
milochen0418 <milo_chen@mail2000.com.tw>
2022-06-04 20:14:37 +0800

    C++/sqrtx.cpp update to finish

--- C++/sqrtx.cpp
+++ C++/sqrtx.cpp
@@ -3,6 +3,22 @@ class Solution {
 public:
     int mySqrt(int x) {
         using ll = long long;
+        int L = 0, R = x;
+        while(L<=R) {
+            ll M = L+(R-L)/2;
+            if( (M+1)*(M+1)>(ll)x && (ll)x>=M*M) {
+                return (int)M;
+            }
+            if(M*M < (ll)x) 
+                L=M+1;
+            else
+                R=M-1;
+        }
+        return -1;
+    }
+
+    int mySqrtOld(int x) {
+        using ll = long long;
         ll L = 0, R=x;
         while(L<=R) { 
             ll M = L + (R-L)/2;
@@ -16,4 +32,5 @@ public:
         }
         return R;
     }
+
 };
