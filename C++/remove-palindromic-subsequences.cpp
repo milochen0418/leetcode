bfc98a86882b529d3047eb70dd61ebabe524393b
milochen0418 <milo_chen@mail2000.com.tw>
2022-06-08 13:40:30 +0800

    C++/remove-palindromic-subsequences.cpp add article

--- C++/remove-palindromic-subsequences.cpp
+++ C++/remove-palindromic-subsequences.cpp
@@ -1,5 +1,6 @@
 class Solution {
     //https://leetcode.com/problems/remove-palindromic-subsequences
+    //article https://leetcode.com/problems/remove-palindromic-subsequences/discuss/2125167/C%2B%2B-or-two-pointers-or-explanation
 public:
     int removePalindromeSub(string s) {
         int L = 0, R=s.length()-1;
