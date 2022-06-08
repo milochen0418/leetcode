009077278bb24e6934b97abd041ac504a3c220d6
milochen0418 <milo_chen@mail2000.com.tw>
2022-06-08 14:43:43 +0800

    C++/remove-palindromic-subsequences.cpp FB post

--- C++/remove-palindromic-subsequences.cpp
+++ C++/remove-palindromic-subsequences.cpp
@@ -1,6 +1,7 @@
 class Solution {
     //https://leetcode.com/problems/remove-palindromic-subsequences
     //article https://leetcode.com/problems/remove-palindromic-subsequences/discuss/2125167/C%2B%2B-or-two-pointers-or-explanation
+    //FB post https://www.facebook.com/groups/1451299754892511/posts/5392345114121269/
 public:
     int removePalindromeSub(string s) {
         int L = 0, R=s.length()-1;
