e51692ae45d4af513a542e93dcde35f686f64a2a
milochen0418 <milo_chen@mail2000.com.tw>
2022-06-10 00:59:12 +0800

    C++/count-negative-numbers-in-a-sorted-matrix.cpp clear code

--- C++/count-negative-numbers-in-a-sorted-matrix.cpp
+++ C++/count-negative-numbers-in-a-sorted-matrix.cpp
@@ -10,6 +10,25 @@ public:
                 while(j>=0) {
                     if(grid[i][j]>=0) {
                         j=j+1;
+                        break;
+                    }
+                    j--;
+                }                
+                ans+=j<0?n:n-j;
+            }
+        }
+        return ans;
+    }
+
+    int countNegativesUgly(vector<vector<int>>& grid) {
+        int m = grid.size(), n=grid[0].size();        
+        int i = 0,j=n-1;
+        int ans = 0;
+        for(i = 0; i<m;i++){
+            if(j<0||grid[i][j]<0) {
+                while(j>=0) {
+                    if(grid[i][j]>=0) {
+                        j=j+1;
                         //[9,8,7,6,5,0,-1,-2,-3] -> j=6, n=9, ans+=3
                         printf("i=%d -> %d\n",i, n-j);
                         ans += (n-j);
