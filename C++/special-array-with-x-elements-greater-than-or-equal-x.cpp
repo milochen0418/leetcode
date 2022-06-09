9abaf596d50dbba9eafe10fff08ad4a767d5d10d
milochen0418 <milo_chen@mail2000.com.tw>
2022-06-09 23:42:23 +0800

    C++/special-array-with-x-elements-greater-than-or-equal-x.cpp finish by counting sort

--- C++/special-array-with-x-elements-greater-than-or-equal-x.cpp
+++ C++/special-array-with-x-elements-greater-than-or-equal-x.cpp
@@ -2,8 +2,43 @@ class Solution {
     //https://leetcode.com/problems/special-array-with-x-elements-greater-than-or-equal-x
 public:
     int specialArray(vector<int>& a) {
-        return specialArrayBySorting(a);
+        return specialArrayByCountingSort(a);
     }
+
+    //O(N) Counting Sort  
+    int specialArrayByCountingSort(vector<int>& a) {
+        int n = a.size();
+        auto countingSort = [](vector<int>&arr) {
+            int n = arr.size();   
+            int arr1[1001];
+            int count_arr[1001];
+            int x = arr[0];
+            for (int i = 1; i < n; i++) if (arr[i] > x) x = arr[i];
+            for (int i = 0; i <= x; ++i) count_arr[i] = 0;
+            for (int i = 0; i < n; i++) count_arr[arr[i]]++;
+            for (int i = 1; i <= x; i++) count_arr[i] += count_arr[i - 1];
+            for (int i = n - 1; i >= 0; i--) {
+                arr1[count_arr[arr[i]] - 1] = arr[i];
+                count_arr[arr[i]]--;
+            }
+            for (int i = 0; i < n; i++)  arr[i] = arr1[i];
+        };        
+        countingSort(a);
+        reverse(a.begin(), a.end());
+        
+        int ans = -1;
+        for(int x = 0; x<n;x++) {
+            if(a[x]>=x+1) {
+                //Think about the case [[9,7,6,5] and [9,7,6,5,4].
+                //make sure there is no next item larger than x+1
+                if(x+1>=n || !(a[x+1]>=x+1)) {
+                    ans = x+1;    
+                }
+            }
+        }
+        return ans;
+    }
+
 
     //O(Nlog(N)) 
     int specialArrayBySorting(vector<int>& a) {
