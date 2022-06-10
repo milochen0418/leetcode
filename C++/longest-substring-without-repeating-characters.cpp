b5e36a0c9d4bac8066641ca2dc1d1b8f619085bb
milochen0418 <milo_chen@mail2000.com.tw>
2022-06-10 12:55:23 +0800

    C++/longest-substring-without-repeating-characters.cpp clear code

--- C++/longest-substring-without-repeating-characters.cpp
+++ C++/longest-substring-without-repeating-characters.cpp
@@ -3,6 +3,22 @@ public:
     int lengthOfLongestSubstring(string s) {
         int n = s.length();
         if(n==0) return 0;
+        unordered_map<char,int> mp = {{s[0],1}};
+        int L = 0, R = 0, ans = 1;
+        while(L<n && R<n-1) {
+            if(mp[s[R+1]])             
+                mp[s[L++]]--;
+            else 
+                mp[s[++R]]++;
+            ans = max(ans, R-L+1);
+        }
+        return ans;
+    }
+
+
+    int lengthOfLongestSubstringComplicated(string s) {
+        int n = s.length();
+        if(n==0) return 0;
         unordered_map<char,int> mp;
         mp[s[0]]=1;
         int L = 0, R = 0, ans = 1;
