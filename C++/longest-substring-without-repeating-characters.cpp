a3c754d8e5508595a1ef59b817f3f4a5b9c22e46
milochen0418 <milo_chen@mail2000.com.tw>
2022-06-10 12:44:49 +0800

    C++/longest-substring-without-repeating-characters.cpp finish with two-pointer

---
+++ C++/longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int n = s.length();
+        if(n==0) return 0;
+        unordered_map<char,int> mp;
+        mp[s[0]]=1;
+        int L = 0, R = 0, ans = 1;
+        while(L<n && R<n-1) {
+            if(mp[s[R+1]] == 0) {
+                mp[s[++R]]++;
+            } else {
+                while(s[L]!=s[R+1]) mp[s[L++]]=0;
+                mp[s[L++]]--;
+            }
+            /*
+            printf("a[%d..%d] = ", L,R);
+            for(int i = L; i<=R;i++) {
+                printf("%c,",s[i]);
+            }
+            printf("\n");
+            */
+            ans = max(ans, R-L+1);
+        }
+        return ans;
+    }
+};
