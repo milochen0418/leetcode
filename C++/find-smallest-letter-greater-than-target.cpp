65fe9c4d8f62f309f95ccc8efbf43e82412cce20
milochen0418 <milo_chen@mail2000.com.tw>
2022-06-15 22:26:52 +0800

    C++/find-smallest-letter-greater-than-target.cpp update and add one more article

--- C++/find-smallest-letter-greater-than-target.cpp
+++ C++/find-smallest-letter-greater-than-target.cpp
@@ -1,8 +1,18 @@
 class Solution {
     //https://leetcode.com/problems/find-smallest-letter-greater-than-target/
     //article https://leetcode.com/problems/find-smallest-letter-greater-than-target/discuss/2109778/C%2B%2B-or-simple-binary-search-solution-or-explanation
+    //article https://leetcode.com/problems/find-smallest-letter-greater-than-target/discuss/2155245/C%2B%2B-or-return-aM-s.t.-M-in-LR-satisfy-aMgreatertgreateraM-1
 public:
-    char nextGreatestLetter(vector<char>& letters, char target) {
+    char nextGreatestLetter(vector<char>& a, char t) {
+        int n=a.size(), L = 0, R=n;
+        while(L<=R) { // return a[M] s.t. M in [L,R] satisfy a[M]>t>=a[M-1]
+            int M = L + (R-L)/2;
+            if( (M==n||a[M]>t) && (M-1<0||t>=a[M-1]) ) return a[M%n];
+            if(t>=a[M]) L=M+1; else R=M-1;
+        } return '\0';
+    }
+
+    char nextGreatestLetterAnswer2(vector<char>& letters, char target) {
         vector<char>&a = letters;
         char t = target, ans;
         int L = 0, R=a.size()-1;
@@ -22,7 +32,7 @@ public:
         return a[0];
     }
 
-    char nextGreatestLetterOld(vector<char>& letters, char target) {
+    char nextGreatestLetterAnswer1(vector<char>& letters, char target) {
         vector<char> v;
         for(auto& c:letters) if(v.size()==0 || v.back()!=c)v.push_back(c);
         
