c92e592b176229148736258d8cba594ce5b30daf
milochen0418 <milo_chen@mail2000.com.tw>
2022-06-16 14:41:42 +0800

    C++/delete-operation-for-two-strings.cpp finish by sol2 that focus on last character

--- C++/delete-operation-for-two-strings.cpp
+++ C++/delete-operation-for-two-strings.cpp
@@ -3,9 +3,26 @@ class Solution {
 public:
     unordered_map<string, unordered_map<string, int>> dp;
     int minDistance(string word1, string word2) {
-        return sol(word1, word2);        
+        return sol2(word1, word2);        
     }
-    int sol(string w1, string w2) {
+
+    int sol2(string w1, string w2) { //pass but near TLE
+        if(w1.size()==0 || w2.size()==0) return max(w2.size(),w1.size());
+        if(dp[w1][w2] != 0) return dp[w1][w2];
+        dp[w1][w2] = INT_MAX;
+        //The idea is only focus on last character of both w1 and w2
+        if(w1[w1.size()-1] == w2[w2.size()-1]) {
+            dp[w1][w2] = sol(w1.substr(0,w1.size()-1), w2.substr(0,w2.size()-1));
+        } else {
+            dp[w1][w2] = 1+min(
+                sol(w1.substr(0,w1.size()-1), w2),
+                sol(w1, w2.substr(0,w2.size()-1))
+            );
+        }
+        return dp[w1][w2];
+    }
+
+    int sol1(string w1, string w2) {//TLE
         if(w1==w2) return 0;
         if(dp[w1][w2] != 0) return dp[w1][w2];
         dp[w1][w2] = INT_MAX;
