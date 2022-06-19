fd3cb7d4e6f378c08ede11ea23358b72f8ce1571
milochen0418 <milo_chen@mail2000.com.tw>
2022-06-20 00:04:39 +0800

    C++/search-a-2d-matrix.cpp update

--- C++/search-a-2d-matrix.cpp
+++ C++/search-a-2d-matrix.cpp
@@ -4,6 +4,16 @@ class Solution {
     //FB Post https://www.facebook.com/groups/1451299754892511/posts/5193837967305319/
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size(), n = matrix[0].size(), L=0,R=m*n-1;
+        while(L<=R) {
+            int M = L+(R-L)/2;
+            if(target == matrix[M/n][M%n]) return true;
+            if(target > matrix[M/n][M%n]) L=M+1; else R=M-1;
+        } 
+        return false;
+    }
+
+    bool searchMatrixAnswer2(vector<vector<int>>& matrix, int target) {
         int cols = matrix[0].size();
         int n = matrix.size() * cols;
         int L=0, R=n-1;
