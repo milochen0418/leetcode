1d711f047188f6202c03c12649fe48b1bee9ea62
milochen0418 <milo_chen@mail2000.com.tw>
2022-06-20 21:36:25 +0800

    C++/sum-of-square-numbers.cpp finish by binary search

--- C++/sum-of-square-numbers.cpp
+++ C++/sum-of-square-numbers.cpp
@@ -1,7 +1,22 @@
 class Solution {
     //https://leetcode.com/problems/sum-of-square-numbers
 public:
-    bool judgeSquareSum(int c2) {
+    bool judgeSquareSum(int c2) { //N*logN
+        using ll = long long;
+        int rootC = sqrt(c2);
+        for(int i=0; i<=rootC;i++) {            
+            ll L=0,R=(ll)rootC;
+            ll t = (ll)c2-(ll)i*(ll)i;
+            while(L<=R) {
+                ll M = L+(R-L)/2;
+                if(M*M == t) return true;
+                if(M*M<t) L=M+1; else R=M-1;                
+            }
+        }
+        return false;
+    }
+
+    bool judgeSquareSumBruteForce(int c2) { //O(N^2)
         using ll = long long;
         int rootC = sqrt(c2);
         //printf("rootC=%d\n",rootC);
