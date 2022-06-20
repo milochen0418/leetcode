ec6dd50d7930c20fae5f63854e9d447e93ba95f0
milochen0418 <milo_chen@mail2000.com.tw>
2022-06-21 00:25:03 +0800

    C++/climbing-stairs.cpp finish by improving to dp solution

--- C++/climbing-stairs.cpp
+++ C++/climbing-stairs.cpp
@@ -1,8 +1,18 @@
 class Solution {
     //https://leetcode.com/problems/climbing-stairs
 public:
+    vector<int> dp;
     int climbStairs(int n) {
-        return sol_backtrace(0,n);   
+        //return sol_backtrace(0,n);
+        dp = vector<int>(n+1,0);
+        dp[0]=1;
+        return sol_dp(0,n);
+    }
+    int sol_dp(int start, int end) {
+        if(end-start<0) return 0;
+        if(dp[end-start] != 0) return dp[end-start];
+        dp[end-start] = sol_dp(start+2,end) + sol_dp(start+1,end);
+        return dp[end-start];
     }
     int sol_backtrace(int start, int end) {        
         if(end-start==0) return 1;
