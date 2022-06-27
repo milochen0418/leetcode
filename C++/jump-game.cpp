ec6d8b637b14a3501af423ff054a7999688a189b
milochen0418 <milo_chen@mail2000.com.tw>
2022-06-27 23:33:51 +0800

    C++/jump-game.cpp simplify code

--- C++/jump-game.cpp
+++ C++/jump-game.cpp
@@ -5,22 +5,18 @@ public:
     vector<int> dp;
     bool canJump(vector<int>& nums) {
         a = nums;
-        dp = vector<int>(a.size(),-1);
+        int n = a.size();
+        dp = vector<int>(n,-1);
+        dp[n-1] = 1;
         return sol(0);
         
     }
     bool sol(int i) {
         int n = a.size();
         if(i>=n) return false;
-        if(dp[i] != -1) return dp[i]==1;
-        
-        if(i==n-1) {
-            dp[i] = 1;
-        } else {
-            bool ans = false;
-            for(int j = 1;ans == false &&  j<=a[i];j++) ans |= sol(i+j);
-            dp[i] = ans?1:0;
-        }
-        return dp[i] == 1;
+        if(dp[i] != -1) return dp[i];
+        dp[i] = 0;
+        for(int j = 1;j<=a[i];j++) if(sol(i+j)) return dp[i] = 1;
+        return dp[i];
     }
 };
