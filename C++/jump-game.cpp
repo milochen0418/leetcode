02e0c8672cab1763f7a5e9d8b28477f7844a0ba1
milochen0418 <milo_chen@mail2000.com.tw>
2022-06-27 23:16:10 +0800

    C++/jump-game.cpp finished by DP

--- C++/jump-game.cpp
+++ C++/jump-game.cpp
@@ -1,17 +1,26 @@
 class Solution {
+    //https://leetcode.com/problems/jump-game
 public:
     vector<int> a;
+    vector<int> dp;
     bool canJump(vector<int>& nums) {
         a = nums;
+        dp = vector<int>(a.size(),-1);
         return sol(0);
         
     }
     bool sol(int i) {
         int n = a.size();
         if(i>=n) return false;
-        if(i==n-1) return true;
-        bool ans = false;
-        for(int j = 1;j<=a[i];j++) ans |= sol(i+j);  
-        return ans;
+        if(dp[i] != -1) return dp[i]==1;
+        
+        if(i==n-1) {
+            dp[i] = 1;
+        } else {
+            bool ans = false;
+            for(int j = 1;ans == false &&  j<=a[i];j++) ans |= sol(i+j);
+            dp[i] = ans?1:0;
+        }
+        return dp[i] == 1;
     }
 };
