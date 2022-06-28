95c8f04afe29fcdd1a2def859f7d64aca6070587
milochen0418 <milo_chen@mail2000.com.tw>
2022-06-28 22:42:34 +0800

    C++/meeting-scheduler.cpp finished by two-pointer

---
+++ C++/meeting-scheduler.cpp
@@ -0,0 +1,87 @@
+
+class Solution {
+    //https://leetcode.com/problems/meeting-scheduler
+public:
+    void printf(const char *fmt, ...)
+    {
+        const bool debug=false;
+        if(!debug) return;
+        va_list args;
+        va_start(args, fmt);
+        vfprintf(stdout, fmt, args);
+        va_end(args);
+    }
+    
+    vector<int> minAvailableDuration(vector<vector<int>>& slots1, vector<vector<int>>& slots2, int duration) {
+        vector<int> ans = vector<int>();
+        
+        vector<vector<int>>&a1 = slots1;
+        vector<vector<int>>&a2 = slots2;
+        sort(a1.begin(), a1.end(), [](auto&lhs, auto&rhs) {
+            return lhs[0] < rhs[0];
+        });
+        sort(a2.begin(), a2.end(), [](auto&lhs, auto&rhs) {
+            return lhs[0] < rhs[0];
+        });
+        
+        int i1 =0, i2=0;
+        while(i1<a1.size() && i2<a2.size()) {
+            if(a2[i2][0] > a1[i1][1]) {
+                //a1 [    ]
+                //a2           [    ]
+                i1++;
+            } else if(a2[i2][1] < a1[i1][0]) {
+                //a1           [    ]
+                //a2  [    ]
+                i2++;
+            } else { //intersection happened
+                //if(a2[i2][1] > a1[i1][0]) {
+                if(a2[i2][0] <= a1[i1][0]) {
+                    printf("case 1:a1[%d]=[%d,%d], a2[%d]=[%d,%d] \n",i1,a1[i1][0],a1[i1][1],i2,a2[i2][0],a2[i2][1]);
+                    // a1    [     ]
+                    // a2 [    ]
+                    if(min(a2[i2][1],a1[i1][1]) - a1[i1][0] >= duration) {
+                        ans.push_back(a1[i1][0]);
+                        ans.push_back(a1[i1][0]+duration);
+                        return ans;
+                    } else {
+                        
+                        if(a1[i1][1]>a2[i2][1]) {
+                            printf("case1: i2++\n");
+                            i2++;
+                        } else {
+                            printf("case1: i1++\n");
+                            i1++;
+                        }
+                        
+                    }
+                //} else if(a1[i1][1] > a2[i2][0]) {
+                } else if(a1[i1][0] < a2[i2][0]) {                    
+                    printf("case 2:a1[%d]=[%d,%d], a2[%d]=[%d,%d] \n",i1,a1[i1][0],a1[i1][1],i2,a2[i2][0],a2[i2][1]);
+                    
+                    
+                    // a1 [    ]
+                    // a2   [      ]
+                    //if(a1[i1][1] - a2[i2][0] >= duration) {
+                    if(min(a1[i1][1],a2[i2][1]) - a2[i2][0] >= duration) {                    
+                        ans.push_back(a2[i2][0]);
+                        ans.push_back(a2[i2][0]+duration);
+                        return ans;
+                    } else {
+                        if(a2[i2][1]>a1[i1][1]) {
+                            printf("case2: i1++\n");
+                            i1++;                               
+                        } else {
+                            printf("case2: i2++\n");
+                            i2++;
+                        }
+                    }
+                } else {
+                    printf("Error in case a1[%d]=[%d,%d], a2[%d]=[%d,%d] \n",i1,a1[i1][0],a1[i1][1],i2,a2[i2][0],a2[i2][1]);
+                    return ans;
+                }
+            }
+        }
+        return ans;
+    }
+};
