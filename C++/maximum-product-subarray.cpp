ca62bd24bb309c7d61beb13638dbfddac7ebb2f8
milochen0418 <milo_chen@mail2000.com.tw>
2022-06-29 19:45:40 +0800

    C++/maximum-product-subarray.cpp reduce code

--- C++/maximum-product-subarray.cpp
+++ C++/maximum-product-subarray.cpp
@@ -4,8 +4,7 @@ public:
     int maxProduct(vector<int>& nums) {
         int ans=INT_MIN, max_chasing=1, min_chasing=1;
         for(auto &v: nums) {
-            int maxv = max_chasing*v;
-            int minv = min_chasing*v;
+            int maxv = max_chasing*v, minv = min_chasing*v;
             min_chasing = min(v,min(maxv,minv));
             max_chasing = max(v,max(maxv,minv));
             ans = max(max_chasing,ans);
