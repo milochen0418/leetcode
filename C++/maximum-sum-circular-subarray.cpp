c42032d3f0bded3e4c3a8dfbfed4246118c7029e
milochen0418 <milo_chen@mail2000.com.tw>
2022-06-30 08:58:13 +0800

    C++/maximum-sum-circular-subarray.cpp finish by dirty work but improve to O(1) space

--- C++/maximum-sum-circular-subarray.cpp
+++ C++/maximum-sum-circular-subarray.cpp
@@ -1,6 +1,38 @@
 class Solution {
+    //https://leetcode.com/problems/maximum-sum-circular-subarray/
 public:
     int maxSubarraySumCircular(vector<int>& nums) {
+        vector<int>& a = nums;
+        int n = a.size()*2;
+        int w = a.size();
+        int L,R,acc;
+        int ans = INT_MIN;
+        L=0;R=0;acc =0;
+        while(L<n && R<n ) {
+            acc = acc + a[R%w];
+            ans = max(ans, acc);
+            if(acc<0) {
+                L=R+1;
+                R=R+1;
+                acc = 0;
+            } else {
+                R=R+1;
+            }
+            
+            if(R-L+1>w) { 
+                if(a[L%w]<0 || (L+1<n&& a[L%w]==a[(L+1)%w])) {
+                    acc = acc - a[L%w];
+                    L=L+1;                    
+                } else {
+                    R=L+1;
+                    L=L+1;
+                    acc = 0;   
+                }
+            }
+        }
+        return ans;
+    }
+    int maxSubarraySumCircularDirtyWithNSpace(vector<int>& nums) {
         vector<int> a;
         for(auto &i:nums) a.push_back(i);
         for(auto &i:nums) a.push_back(i);
