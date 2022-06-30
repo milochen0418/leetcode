2135af04a3a44c7d9eb2312a452f3378352a5576
milochen0418 <milo_chen@mail2000.com.tw>
2022-07-01 00:22:23 +0800

    C++/running-sum-of-1d-array.cpp refactor

--- C++/running-sum-of-1d-array.cpp
+++ C++/running-sum-of-1d-array.cpp
@@ -1,9 +1,8 @@
 class Solution {
+    //https://leetcode.com/problems/running-sum-of-1d-array/
 public:
     vector<int> runningSum(vector<int>& nums) {
-        int n = nums.size();
-        for(int i = 0; i<n;i++) 
-            nums[i] = (i==0)?nums[i]:nums[i]+nums[i-1];
+        for(int i = 0; i<nums.size();i++) nums[i]+= i>0?nums[i-1]:0;
         return nums;
     }
 };
