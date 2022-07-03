905b48829bedb43e40fbe5f016de1693f26d2f5f
milochen0418 <milo_chen@mail2000.com.tw>
2022-07-03 23:59:21 +0800

    C++/number-of-people-aware-of-a-secret.cpp finished by simulate

---
+++ C++/number-of-people-aware-of-a-secret.cpp
@@ -0,0 +1,20 @@
+class Solution {
+    //https://leetcode.com/problems/number-of-people-aware-of-a-secret
+public:
+    int peopleAwareOfSecret(int n, int delay, int forget) {
+        vector<int> a(n+forget,0);
+        a[1]=1;
+        for(int i = 1;i<=n;i++) 
+            for(int j = delay; j<forget;j++) 
+                a[i+j] = mod_add(a[i+j], a[i]);
+        for(int i=1;i<=n;i++) 
+            a[i] = mod_add(a[i], a[i-1]);
+        return mod_add(a[n], -a[n-forget]);
+    }
+    int mod_add(int a, int b){
+        const int MOD=1e9+7;
+        (a+=MOD)%=MOD;
+        (b+=MOD)%=MOD;
+        return (a+b)%MOD;
+    }
+};
