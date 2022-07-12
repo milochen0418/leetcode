127faf1f7b6463f1b2cd0368c4c54a608b4e7a6b
milochen0418 <milo_chen@mail2000.com.tw>
2022-07-12 22:37:17 +0800

    C++/two-sum.cpp finished  by unordered_map

---
+++ C++/two-sum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+    //https://leetcode.com/problems/two-sum/
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> mp;
+        int n = nums.size();
+        for(int i = 0; i<n;i++) mp[nums[i]]=i;
+        for(int i = 0; i<n;i++) {
+            int b = target-nums[i];
+            if(mp.find(b) != mp.end()) {
+                if(mp[b] != i) {
+                    return vector<int>{i,mp[b]};    
+                }
+            }
+        }
+        return vector<int>();
+    }
+};
