6dcea002899d405b53e3f61aa930357474fbac74
milochen0418 <milo_chen@mail2000.com.tw>
2022-07-12 22:40:48 +0800

    C++/two-sum.cpp refactor

--- C++/two-sum.cpp
+++ C++/two-sum.cpp
@@ -4,6 +4,18 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int,int> mp;
         int n = nums.size();
+        for(int i = 0; i<n;i++) {
+            int b = target - nums[i];
+            if(mp.find(b) != mp.end()) {
+                return vector<int>{i,mp[b]};
+            }
+            mp[nums[i]]=i;
+        }
+        return vector<int>();
+    }
+    vector<int> twoSumVer01(vector<int>& nums, int target) {
+        unordered_map<int,int> mp;
+        int n = nums.size();
         for(int i = 0; i<n;i++) mp[nums[i]]=i;
         for(int i = 0; i<n;i++) {
             int b = target-nums[i];
