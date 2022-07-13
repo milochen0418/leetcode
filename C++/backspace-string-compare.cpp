f958891f7373e189fb10d090f1f5c3e7f67a54e6
milochen0418 <milo_chen@mail2000.com.tw>
2022-07-13 19:26:48 +0800

    C++/backspace-string-compare.cpp finished by stack

--- C++/backspace-string-compare.cpp
+++ C++/backspace-string-compare.cpp
@@ -1,7 +1,27 @@
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
+        stack<char> stk1, stk2;
+        for(auto &c: s)
+            if(c!='#') 
+                stk1.push(c);
+            else 
+                if(!stk1.empty())stk1.pop();
         
+        for(auto &c: t) 
+            if(c!='#') 
+                stk2.push(c);
+            else 
+                if(!stk2.empty())stk2.pop();
+        
+        while( !(stk1.empty() || stk2.empty()) ){
+            if( stk1.top() != stk2.top()) return false;
+            stk1.pop();
+            stk2.pop();
+            //printf("stk1.size() = %d, stk2.size()=%d\n", (int)stk1.size(), (int)stk2.size());
+        }
+        return stk1.empty() && stk2.empty();
+
     }
 
     bool backspaceCompareVer01ByVector(string s, string t) {
