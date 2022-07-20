073fcfbb9b639ce16790fcb5eb00c45c35ee7e6c
milochen0418 <milo_chen@mail2000.com.tw>
2022-07-21 01:15:03 +0800

    C++/valid-anagram.cpp add article

--- C++/valid-anagram.cpp
+++ C++/valid-anagram.cpp
@@ -1,5 +1,6 @@
 class Solution {
     //https://leetcode.com/problems/valid-anagram
+    //article https://leetcode.com/problems/valid-anagram/discuss/2309667/C%2B%2B-or-Time-O(N)-Space-O(1)-by-vector
 public:
     bool isAnagram(string s, string t) {
         vector<int>mps = vector<int>(256,0);
