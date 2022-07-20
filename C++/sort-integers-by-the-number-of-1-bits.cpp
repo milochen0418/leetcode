3b2a748f988c047373098e0af7a55c826f11eea3
milochen0418 <milo_chen@mail2000.com.tw>
2022-07-21 00:55:27 +0800

    C++/sort-integers-by-the-number-of-1-bits.cpp finished by __builtin_popcount

--- C++/sort-integers-by-the-number-of-1-bits.cpp
+++ C++/sort-integers-by-the-number-of-1-bits.cpp
@@ -2,11 +2,21 @@ class Solution {
     //https://leetcode.com/problems/sort-integers-by-the-number-of-1-bits
     //refer article https://www.geeksforgeeks.org/count-set-bits-in-an-integer/ to implement fast bit 1 count
 public:
+
+    vector<int> sortByBits(vector<int>& a) {
+        sort(a.begin(), a.end(), [](auto& lhs, auto& rhs){
+            int va=__builtin_popcount(lhs);
+            int vb=__builtin_popcount(rhs);
+            return (va==vb)?lhs<rhs:va<vb;
+        });
+        return a;
+    }
+
     int tbl[256];//Bits Set Table 256
     int countSetBits(int n) {
         return tbl[n&0xff] +tbl[(n>>8)&0xff] +tbl[(n>>16)&0xff] +tbl[n>>24];
     }    
-    vector<int> sortByBits(vector<int>& a) {
+    vector<int> sortByBitsVer01(vector<int>& a) {
         //init table
         tbl[0] = 0;
         for (int i=0; i<256; i++) tbl[i] = (i&1) + tbl[i/2];
