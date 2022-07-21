00dae9cd191a24c2139df9db60ee7913d515844a
milochen0418 <milo_chen@mail2000.com.tw>
2022-07-21 23:20:46 +0800

    C++/invert-binary-tree.cpp finished by DFS

---
+++ C++/invert-binary-tree.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    //https://leetcode.com/problems/invert-binary-tree
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        dfs(root);
+        return root;
+    }
+    void dfs(TreeNode* root) {
+        if(!root) return;
+        swap(root->left, root->right);
+        dfs(root->left);
+        dfs(root->right);        
+    }
+};
