382a6abd0ea9b58195b6f66b9687e99556296ab3
milochen0418 <milo_chen@mail2000.com.tw>
2022-07-22 01:40:52 +0800

    SQL/employees-earning-more-than-their-managers.sql finsihed

---
+++ SQL/employees-earning-more-than-their-managers.sql
@@ -0,0 +1,3 @@
+# Write your MySQL query statement below
+# https://leetcode.com/problems/employees-earning-more-than-their-managers
+SELECT a.name AS Employee FROM Employee a LEFT JOIN Employee b ON a.managerId=b.id WHERE a.salary > b.salary;
