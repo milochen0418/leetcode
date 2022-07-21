305b91e4c1b0ff2d09d541ed20aa66029495d299
milochen0418 <milo_chen@mail2000.com.tw>
2022-07-22 01:47:12 +0800

    SQL/duplicate-emails.sql finished

---
+++ SQL/duplicate-emails.sql
@@ -0,0 +1,3 @@
+# Write your MySQL query statement below
+# https://leetcode.com/problems/duplicate-emails
+SELECT DISTINCT a.email AS Email FROM Person a LEFT JOIN Person b ON a.id <> b.id WHERE a.email = b.email;
