1f3162334549dfd9cea3c29f8d4c8ef9e78d1db7
milochen0418 <milo_chen@mail2000.com.tw>
2022-07-30 01:05:37 +0800

    C++/ransom-note.cpp add article

--- C++/ransom-note.cpp
+++ C++/ransom-note.cpp
@@ -1,5 +1,6 @@
 class Solution {
     //https://leetcode.com/problems/ransom-note/
+    //article https://leetcode.com/problems/ransom-note/discuss/2351644/C%2B%2B-or-4-lines-by-Hash-Map
 public:
     bool canConstruct(string ransomNote, string magazine) {
         vector<int> v = vector<int>(256,0);
