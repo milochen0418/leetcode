296ec328867f2d32c41e30884b0ddb911b734034
milochen0418 <milo_chen@mail2000.com.tw>
2022-07-30 00:59:26 +0800

    C++/valid-sudoku.cpp add article

--- C++/valid-sudoku.cpp
+++ C++/valid-sudoku.cpp
@@ -1,8 +1,9 @@
 class Solution {
     //https://leetcode.com/problems/valid-sudoku
+    //article https://leetcode.com/problems/valid-sudoku/discuss/2351601/C%2B%2B-or-Hash-Set-or-Simple
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        vector<vector<unordered_set<char>>> vs = vector<vector<unordered_set<char>>>(3,vector<unordered_set<char>>(3,unordered_set<char>()));        
+        vector<vector<unordered_set<char>>> vs = vector<vector<unordered_set<char>>>(3,vector<unordered_set<char>>(3,unordered_set<char>()));
         for(int i = 0; i<9;i++) {
             unordered_set<char> sr, sc;            
             for(int j = 0; j<9;j++) {
