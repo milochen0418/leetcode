92c8c748b5cf0a48122d256fc720e393b8020c71
milochen0418 <milo_chen@mail2000.com.tw>
2022-09-04 13:56:07 +0800

    longest-nice-subarray.cpp finished by two-pointer

---
+++ C++/longest-nice-subarray.cpp
@@ -0,0 +1,52 @@
+class Solution {
+    //https://leetcode.com/problems/longest-nice-subarray/
+    /*
+    max_cnt alwasy largest (R-L+1) 
+    [1,3,8,48,10]
+      0000 0001    1   ALLAND = 0000 0001
+      0000 0011    3   ALLAND = 0000 0011
+      0000 1000    8   ALLAND = 0000 1011
+ L    0011 0000    48  ALLAND = 0011 1011 
+   R  0000 1010    10  ALLAND = 0011 1010
+   
+   R-L+1 = 2  answer 
+   
+   ALLAND = 0011 1011 -> 0011 1000  -> 0011 0000 -> 0011 1010 
+   
+   
+    */
+    
+public:
+
+    int longestNiceSubarray(vector<int>& nums) {
+        //printf("\nlongestNiceSubarray\n");
+        int max_cnt = 1;
+        int n = nums.size();
+        int L = 0, R = 0;
+        int ALLAND =0;
+        while(L<n && R<n) {
+            if(L==R) {
+                ALLAND = nums[L];
+                max_cnt = max(max_cnt, R-L+1);
+                //printf("L=%d, R=%d, R-L+1=%d, ALLAND=%d, max_cnt=%d, \n", L,R,R-L+1, ALLAND, max_cnt);
+                R++;
+            } else {
+                if( (ALLAND & nums[R]) == 0) {
+                    ALLAND = (ALLAND | nums[R]);
+                    max_cnt = max(max_cnt, R-L+1);
+                    //printf("L=%d, R=%d, R-L+1=%d, ALLAND=%d, max_cnt=%d, \n", L,R,R-L+1, ALLAND, max_cnt);
+                    R++;
+                } else {
+                    while(L<R) {
+                        ALLAND = (ALLAND & (~(nums[L]) ));
+                        L++;
+                        if( (nums[L] & nums[R]) == 0 ) {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        return max_cnt;
+    }
+};
