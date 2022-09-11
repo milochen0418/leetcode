e7339334eeb5cd494e603a29aac9d6b978c2498a
milochen0418 <milo_chen@mail2000.com.tw>
2022-09-11 23:24:16 +0800

    C++/optimal-partition-of-string.cpp add article

--- C++/optimal-partition-of-string.cpp
+++ C++/optimal-partition-of-string.cpp
@@ -1,5 +1,6 @@
 class Solution {
     //https://leetcode.com/problems/optimal-partition-of-string/
+    //https://leetcode.com/problems/optimal-partition-of-string/discuss/2562885/C%2B%2B-or-Hash-Set-or-O(N)
 public:
     int partitionString(string s) {
         int ans = 0, n = s.length();
