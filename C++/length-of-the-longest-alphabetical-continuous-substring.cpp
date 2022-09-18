a93f8ac6e4b5f61cd779e78b72174324605af175
milochen0418 <milo_chen@mail2000.com.tw>
2022-09-18 17:24:06 +0800

    C++/length-of-the-longest-alphabetical-continuous-substring.cpp add article

--- C++/length-of-the-longest-alphabetical-continuous-substring.cpp
+++ C++/length-of-the-longest-alphabetical-continuous-substring.cpp
@@ -1,5 +1,6 @@
 class Solution {
     //https://leetcode.com/problems/length-of-the-longest-alphabetical-continuous-substring/
+    //add article https://leetcode.com/problems/length-of-the-longest-alphabetical-continuous-substring/discuss/2592120/C%2B%2B-or-Time-O(N)-Space-O(1)
 public:
     int longestContinuousSubstring(string s) {
         int n = s.length();
