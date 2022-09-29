422d6a82b5ed4630623a15ff2a217b7ebe0770de
milochen0418 <milo_chen@mail2000.com.tw>
2022-09-29 17:04:12 +0800

    C++/remove-interval.cpp finished by array

---
+++ C++/remove-interval.cpp
@@ -0,0 +1,18 @@
+class Solution {
+    //https://leetcode.com/problems/remove-interval
+public:
+    vector<vector<int>> removeInterval(vector<vector<int>>& intervals, vector<int>& toBeRemoved) {
+        vector<vector<int>> ans;
+        int begin = toBeRemoved[0], end = toBeRemoved[1];
+        for(auto &i: intervals) {
+            if(i[1]<=end && i[0]>=begin) continue;
+            if(i[0]>=end || i[1]< begin)
+                ans.push_back(i);
+            else {
+                if(i[0] < begin) ans.push_back({i[0],begin});
+                if(i[1] > end) ans.push_back({end,i[1]});                    
+            }
+        }
+        return ans;
+    }
+};
