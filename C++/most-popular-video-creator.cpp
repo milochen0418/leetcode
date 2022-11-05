80d2f5b23ea03786a11e55fd608ee604f40645e8
milochen0418 <milo_chen@mail2000.com.tw>
2022-11-05 19:14:44 +0800

    C++/most-popular-video-creator.cpp refactor

--- C++/most-popular-video-creator.cpp
+++ C++/most-popular-video-creator.cpp
@@ -7,13 +7,11 @@ public:
         long long highest_popularity=0; 
         unordered_map <string,long long> popularity_mp;
         unordered_map <string,int> max_viewcount_mp; //creator, count of one view with max 
-        unordered_map <string, vector<int>> idx_mp;//creator to idx vector. use idx can get id and view from ids and views
         unordered_map <string, string> id_mp;
         
         for(int i = 0; i< n;i++) {
             string &creator = creators[i];
             popularity_mp[creator] += (long long)views[i];
-            
             if(id_mp.find(creator) == id_mp.end()) {
                 id_mp[creator] = ids[i];
                 max_viewcount_mp[creator] = views[i];
@@ -23,17 +21,16 @@ public:
             } else if(max_viewcount_mp[creator] == views[i]){
                 if(id_mp[creator] > ids[i])  id_mp[creator] = ids[i];
             }
-            idx_mp[creator].push_back(i);
             highest_popularity = max(highest_popularity, popularity_mp[creator]);
         }
-        
-        for(auto& [creator, idxs]: idx_mp) {
+        for(auto& [creator,id]: id_mp) {
             if(popularity_mp[creator] != highest_popularity) continue;
             ans.push_back({creator, id_mp[creator]});
         }
         return ans;
                 
     }
+    
     vector<vector<string>> mostPopularCreatorVer01(vector<string>& creators, vector<string>& ids, vector<int>& views) {
         vector<vector<string>> ans;
         int n = creators.size();
