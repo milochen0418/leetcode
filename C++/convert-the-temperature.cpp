188c7ef24860ed8e95d2bbbbc798531fd99d62de
milochen0418 <milo_chen@mail2000.com.tw>
2022-11-13 12:49:39 +0800

    C++/convert-the-temperature.cpp finished

---
+++ C++/convert-the-temperature.cpp
@@ -0,0 +1,10 @@
+class Solution {
+    //https://leetcode.com/problems/convert-the-temperature/
+public:
+    vector<double> convertTemperature(double celsius) {
+        vector<double> ans;
+        ans.push_back(celsius + 273.15);
+        ans.push_back(celsius * 1.80 + 32.00);
+        return ans;
+    }
+};
