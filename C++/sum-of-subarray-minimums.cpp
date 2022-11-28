2a38a56be1c5dc04fb04ef85497cf4ccf32a5165
milochen0418 <milo_chen@mail2000.com.tw>
2022-11-28 12:12:43 +0800

    C++/sum-of-subarray-minimums.cpp debug for issue of same elements in array

---
+++ C++/sum-of-subarray-minimums.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int sumSubarrayMins(vector<int>& arr) {
+        int mod = 1e9+7;
+        int n = arr.size();
+        int ans = 0;
+        
+        vector<vector<int>> A = vector<vector<int>>(n,vector<int>(5,0)); 
+        for(int i = 0; i<n;i++) {
+            int L=i, R=i;
+            for(; L>=0;L--) {
+                if(arr[L] < arr[i]) break;
+            }
+            for(; R<n;R++) {
+                if(arr[R] < arr[i]) break;
+            }
+            //This is (L .. R), but not [L .. R]
+            A[i][0] = L;
+            A[i][1] = R;
+            A[i][2] = R-L-1;
+            A[i][3] = (R-i) * (i-L);
+            A[i][4] = arr[i] * (R-i) * (i-L);
+            //ans= ((ans%mod) + (A[i][2]%mod)) %mod;
+            ans+=A[i][4];
+        }
+        
+        for(int i = 0; i <A.size();i++) {
+            printf("(L,R,cnt) = (%d, %d, %d)\n", A[i][0], A[i][1], A[i][2] );
+        }
+        return ans; 
+    }  
+};
