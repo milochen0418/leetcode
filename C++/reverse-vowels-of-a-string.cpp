81099a9553c343884128b63525d0e1155b6e307c
milochen0418 <milo_chen@mail2000.com.tw>
2022-12-01 02:06:45 +0800

    C++/reverse-vowels-of-a-string.cpp speed up

--- C++/reverse-vowels-of-a-string.cpp
+++ C++/reverse-vowels-of-a-string.cpp
@@ -4,9 +4,11 @@ public:
     string reverseVowels(string s) {
         vector<int> v;//save all idx with vowels
         unordered_set<char> vowels = {'a', 'e', 'i', 'o','u','A','E','I','O','U'};
+        vector<bool> vv = vector<bool>(256,false);
+        for(auto &c:vowels) vv[(int)(c)] = true;
+
         for(int i = 0; i<s.length();i++) 
-            if(vowels.find(s[i]) != vowels.end()) 
-                v.push_back(i);
+            if(vv[s[i]]) v.push_back(i);
         int L=0,R=v.size()-1;
         while(L<R) swap(s[v[L++]], s[v[R--]]);
         return s;        
