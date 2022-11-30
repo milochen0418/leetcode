2023c315fd976178125351ef7e6a83197281a5b2
milochen0418 <milo_chen@mail2000.com.tw>
2022-11-30 15:22:43 +0800

    C++/ugly-number.cpp finished by loop

---
+++ C++/ugly-number.cpp
@@ -0,0 +1,9 @@
+class Solution {
+    //https://leetcode.com/problems/ugly-number
+public:
+    bool isUgly(int n) {
+        if(n<=0) return false;
+        for(auto &i:{2,3,5}) while(n%i==0) n/=i;
+        return n == 1;
+    }
+};
