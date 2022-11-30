aca43c24972e07c0098f1489f8f2ab60cc15044a
milochen0418 <milo_chen@mail2000.com.tw>
2022-11-30 10:48:39 +0800

    C++/insert-delete-getrandom-o1.cpp finished by hash map and vector

--- C++/insert-delete-getrandom-o1.cpp
+++ C++/insert-delete-getrandom-o1.cpp
@@ -1,18 +1,26 @@
 class RandomizedSet {
+    //https://leetcode.com/problems/insert-delete-getrandom-o1
 public:
     /* Refer time complexity of hash set here
     https://www.geeksforgeeks.org/hashset-in-java/ 
     
     Time Complexity of HashSet Operations: The underlying data structure for HashSet is hashtable. So amortize (average or usual case) time complexity for add, remove and look-up (contains method) operation of HashSet takes O(1) time.
     */
-    unordered_set<int> s;
+    //unordered_set<int> s;
+    
+    unordered_map<int,int> mp;
+    vector<int> v;
+    
     RandomizedSet() {
         
     }
     
     bool insert(int val) {
-        if(s.find(val) == s.end()) {
-            s.insert(val);
+        if(mp.find(val) == mp.end()) {
+        //if(s.find(val) == s.end()) {
+            //s.insert(val);
+            v.push_back(val);
+            mp[val] = v.size()-1;
             return true;
         } else {
             return false;
@@ -21,8 +29,14 @@ public:
     }
     
     bool remove(int val) {
-        if(s.find(val) != s.end()) {
-            s.erase(val);
+        if(mp.find(val) != mp.end()) {        
+        //if(s.find(val) != s.end()) {
+            //s.erase(val);
+            int vv = v[v.size()-1];
+            swap(v[v.size()-1], v[mp[val]]);
+            mp[vv] = mp[val];
+            mp.erase(val);
+            v.pop_back();
             return true;
         } else {
             return false;
@@ -30,8 +44,7 @@ public:
     }
     
     int getRandom() {
-        //TODO
-        return rand()%s.size();
+        return v[rand()%v.size()];
     }
 };
 
