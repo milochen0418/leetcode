495302d5d79283d45262d096a12a3d7b579ef64c
milochen0418 <milo_chen@mail2000.com.tw>
2022-12-01 14:09:43 +0800

    C++/determine-if-string-halves-are-alike.cpp optimized by constant vector

--- C++/determine-if-string-halves-are-alike.cpp
+++ C++/determine-if-string-halves-are-alike.cpp
@@ -1,12 +1,45 @@
 class Solution {
     //https://leetcode.com/problems/determine-if-string-halves-are-alike/
 public:
+
+    const vector<int> isVowels = {
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+        0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 
+        0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+        0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 
+        0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+    };
     bool halvesAreAlike(string s) {
+        int cntA=0,cntB=0,n=s.length();
+        for(int i = 0;i<n;i++) {
+            if(isVowels[s[i]]) {
+                if(i<n/2) 
+                    cntA++;
+                else 
+                    cntB++;
+            }
+        }
+        return cntA==cntB;
+    }
+
+
+    bool halvesAreAlikeVer01(string s) {
         unordered_set<char> vowels = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
-        int cntA=0,cntB=0;
-        for(int i = 0;i<s.length();i++) {
+        int cntA=0,cntB=0,n=s.length();
+        for(int i = 0;i<n;i++) {
             if (vowels.find(s[i]) != vowels.end()) {
-                if(i<s.length()/2) 
+                if(i<n/2) 
                     cntA++;
                 else 
                     cntB++;
