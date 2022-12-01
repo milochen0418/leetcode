916ddc79ce266973c58777ee08ef1ff63865f8cb
milochen0418 <milo_chen@mail2000.com.tw>
2022-12-01 22:17:57 +0800

    C++/find-leaves-of-binary-tree.cpp finished by dfs

---
+++ C++/find-leaves-of-binary-tree.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    //https://leetcode.com/problems/find-leaves-of-binary-tree/
+public:
+    vector<vector<int>> ans;
+    vector<vector<int>> findLeaves(TreeNode* root) {
+        dfs(root);
+        return ans;        
+    }
+    int dfs(TreeNode* root) {
+        if(!root) return -1;
+        int h = 1 + max(dfs(root->left), dfs(root->right));
+        if(h>=ans.size()) ans.push_back(vector<int>());
+        ans[h].push_back(root->val);
+        return h;
+    }
+};
