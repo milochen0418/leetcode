6cb1529e355397ad78e21682c37df890e794d730
milochen0418 <milo_chen@mail2000.com.tw>
2022-12-02 10:19:51 +0800

    C++/determine-if-two-strings-are-close.cpp add article

--- C++/determine-if-two-strings-are-close.cpp
+++ C++/determine-if-two-strings-are-close.cpp
@@ -1,8 +1,10 @@
 class Solution {
     //https://leetcode.com/problems/determine-if-two-strings-are-close/
+    //article https://leetcode.com/problems/determine-if-two-strings-are-close/discuss/2868180/C%2B%2B-or-array-approach-or-easy-or-explanation
 public:
     bool closeStrings(string word1, string word2) {
         if(word1.length()!=word2.length()) return false;
+        
         vector<int> v1=vector<int>(26,0), v2=vector<int>(26,0);
         vector<int> v1s=vector<int>(26,0), v2s=vector<int>(26,0);
         for(int i = 0; i<word1.length();i++) {
