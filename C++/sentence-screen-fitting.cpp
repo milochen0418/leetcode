ef668f92b917a09105113e8686cbe889c8b3b1da
milochen0418 <milo_chen@mail2000.com.tw>
2022-12-03 16:07:41 +0800

    C++/sentence-screen-fitting.cpp LTE another simulate way but precisely

--- C++/sentence-screen-fitting.cpp
+++ C++/sentence-screen-fitting.cpp
@@ -3,6 +3,33 @@ class Solution {
 public:
     int wordsTyping(vector<string>& sentence, int rows, int cols) {
         vector<int> v;
+        int max_width = 0;
+        for(auto &s:sentence){
+            v.push_back(s.length());
+            max_width = max(max_width, (int)s.length());
+        }
+        if(max_width > cols) return 0;
+        
+        int n=v.size(),ans=0,i=0, col=0, row=0, cur = 0;
+        while(1) {
+            int width = v[i%n];
+            int next_col = col + (col!=0) + width;
+            if(next_col>cols) {
+                row++;
+                col = width;
+            } else {
+                col = next_col;
+            }
+            cur = row*cols + col;
+            if(cur > rows*cols) break;
+            if((i+1)%n==0) ans++;
+            i=i+1;
+        }
+        return ans;
+    }
+    
+    int wordsTypingVer01(vector<string>& sentence, int rows, int cols) {
+        vector<int> v;
         for(auto &s:sentence)v.push_back(s.length());
         int n = v.size();
         int row=0, col=0;
