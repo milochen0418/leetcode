4b66c949382cc790760424f4c0abf97a4d7c3e35
milochen0418 <milo_chen@mail2000.com.tw>
2022-12-05 00:52:52 +0800

    C++/number-of-distinct-averages.cpp add article

--- C++/number-of-distinct-averages.cpp
+++ C++/number-of-distinct-averages.cpp
@@ -1,5 +1,6 @@
 class Solution {
     //https://leetcode.com/problems/number-of-distinct-averages
+    //article https://leetcode.com/problems/number-of-distinct-averages/discuss/2877692/C%2B%2B-or-sort-%2B-two-pointer-or-O(NlogN)
 public:
     int distinctAverages(vector<int>& nums) {
         sort(nums.begin(), nums.end());
