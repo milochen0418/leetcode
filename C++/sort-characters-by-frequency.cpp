5432769eb73bf25bc3c168eb23001ce53a0f0b65
milochen0418 <milo_chen@mail2000.com.tw>
2022-12-04 09:41:48 +0800

    C++/sort-characters-by-frequency.cpp optimized by vector

--- C++/sort-characters-by-frequency.cpp
+++ C++/sort-characters-by-frequency.cpp
@@ -2,6 +2,18 @@ class Solution {
     //https://leetcode.com/problems/sort-characters-by-frequency
 public:
     string frequencySort(string s) {
+        vector<int> mp = vector<int>(256,0);
+        for(auto &c: s) mp[c]++;
+        sort(s.begin(), s.end(), [&mp](auto&lhs, auto&rhs){
+            if(mp[lhs] ==  mp[rhs]) 
+                return lhs>rhs;
+            else 
+                return mp[lhs]> mp[rhs];
+        });
+        return s;
+    }
+
+    string frequencySortVer01(string s) {
         unordered_map<char,int> mp;
         for(auto &c: s) mp[c]++;
         sort(s.begin(), s.end(), [&mp](auto&lhs, auto&rhs){
