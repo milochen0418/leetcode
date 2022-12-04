4a9e258a36dc9f6bc6d83494d62d5862fdcfc0aa
milochen0418 <milo_chen@mail2000.com.tw>
2022-12-04 13:44:04 +0800

    C++/circular-sentence.cpp add article

--- C++/circular-sentence.cpp
+++ C++/circular-sentence.cpp
@@ -1,5 +1,6 @@
 class Solution {
     //https://leetcode.com/problems/circular-sentence/
+    //article https://leetcode.com/problems/circular-sentence/discuss/2875247/C%2B%2B-or-easy-or-space-O(1)-time-O(N)-or-no-split-or-explanation
 public:
     bool isCircularSentence(string sentence) {
         int n = sentence.length();
