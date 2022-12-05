27350e14f0d0845472bfaa2bf3a87c4849411105
milochen0418 <milo_chen@mail2000.com.tw>
2022-12-05 15:21:47 +0800

    C++/count-asterisks.cpp finished by loop

---
+++ C++/count-asterisks.cpp
@@ -0,0 +1,13 @@
+class Solution {
+    //https://leetcode.com/problems/count-asterisks
+public:
+    int countAsterisks(string s) {
+        int pipe_cnt = 0,ans=0;
+        for(auto &c:s) 
+            if(c=='|') 
+                pipe_cnt++;
+            else if(c=='*' && pipe_cnt%2==0)
+                ans++;
+        return ans;
+    }
+};
