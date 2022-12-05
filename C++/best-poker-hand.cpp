1bec2e08dc59ca67588add3702e1288614f2b1e0
milochen0418 <milo_chen@mail2000.com.tw>
2022-12-05 14:27:19 +0800

    C++/best-poker-hand.cpp optimized

--- C++/best-poker-hand.cpp
+++ C++/best-poker-hand.cpp
@@ -2,6 +2,16 @@ class Solution {
     //https://leetcode.com/problems/best-poker-hand
 public:
     string bestHand(vector<int>& ranks, vector<char>& suits) {
+        int is_flush=1,maxv = 0;
+        for(auto &c:suits) if(c!=suits[0]) is_flush=false;
+        if(is_flush) return "Flush";
+        vector<int> v(14,0);
+        for(auto &i:ranks) maxv = max(maxv, ++v[i]);
+        if(maxv>=2) return maxv>=3?"Three of a Kind":"Pair";
+        return "High Card";
+    }    
+
+    string bestHand_v01(vector<int>& ranks, vector<char>& suits) {
         if(is_flush(ranks, suits)) return "Flush";
         int v = max_repeat_rank(ranks, suits);
         if(v>=2) return v>=3?"Three of a Kind":"Pair";
