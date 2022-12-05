e846c68acbf9819dcde5f87f2df045045f388e06
milochen0418 <milo_chen@mail2000.com.tw>
2022-12-05 15:29:59 +0800

    C++/count-asterisks.cpp add article

--- C++/count-asterisks.cpp
+++ C++/count-asterisks.cpp
@@ -1,5 +1,6 @@
 class Solution {
     //https://leetcode.com/problems/count-asterisks
+    //article https://leetcode.com/problems/count-asterisks/discuss/2879481/C%2B%2B-or-3-lines
 public:
     int countAsterisks(string s) {
         int pipe_cnt = 0,ans=0,tmp=0;
