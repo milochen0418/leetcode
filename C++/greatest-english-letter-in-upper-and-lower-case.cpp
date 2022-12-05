28e6aebfa428a7eda4e8f67684db44a7757199bb
milochen0418 <milo_chen@mail2000.com.tw>
2022-12-05 14:48:03 +0800

    C++/greatest-english-letter-in-upper-and-lower-case.cpp finished by hash set

--- C++/greatest-english-letter-in-upper-and-lower-case.cpp
+++ C++/greatest-english-letter-in-upper-and-lower-case.cpp
@@ -1,5 +1,6 @@
 class Solution {
     //https://leetcode.com/problems/greatest-english-letter-in-upper-and-lower-case
+    //article https://leetcode.com/problems/greatest-english-letter-in-upper-and-lower-case/discuss/2879369/C%2B%2B-or-6-lines-or-O(N)-or-Hash-Set
 public:
     string greatestLetter(string s) {
         unordered_set<char> set,SET;
