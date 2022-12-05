73301bd969894d783566143ecf030c0bb43e84bf
milochen0418 <milo_chen@mail2000.com.tw>
2022-12-05 11:59:00 +0800

    C++/largest-local-values-in-a-matrix.cpp finished by loop

---
+++ C++/largest-local-values-in-a-matrix.cpp
@@ -0,0 +1,12 @@
+class Solution {
+    //https://leetcode.com/problems/largest-local-values-in-a-matrix
+public:
+    vector<vector<int>> largestLocal(vector<vector<int>>& grid) {
+        vector<vector<int>> ans = vector<vector<int>>(grid.size()-2, vector<int>(grid.size()-2,0));
+        for(int i = 0; i<ans.size();i++ ) 
+            for(int j = 0; j<ans.size();j++) 
+                for(int k = 0;k<9;k++) 
+                    ans[i][j] = max(ans[i][j], grid[i+k/3][j+k%3]);
+        return ans;
+    }
+};
