80f61bfc5c956ce1317bcc4d97e30a16264c9f0f
milochen0418 <milo_chen@mail2000.com.tw>
2022-12-05 12:00:14 +0800

    C++/largest-local-values-in-a-matrix.cpp add article

--- C++/largest-local-values-in-a-matrix.cpp
+++ C++/largest-local-values-in-a-matrix.cpp
@@ -1,5 +1,6 @@
 class Solution {
     //https://leetcode.com/problems/largest-local-values-in-a-matrix
+    //article https://leetcode.com/problems/largest-local-values-in-a-matrix/discuss/2878868/C%2B%2B-or-O(N2)
 public:
     vector<vector<int>> largestLocal(vector<vector<int>>& grid) {
         vector<vector<int>> ans = vector<vector<int>>(grid.size()-2, vector<int>(grid.size()-2,0));
