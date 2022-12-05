e6c45a62b0d27b96cba7011321784b4a2a08f60d
milochen0418 <milo_chen@mail2000.com.tw>
2022-12-05 15:23:40 +0800

    C++/count-asterisks.cpp refactor

--- C++/count-asterisks.cpp
+++ C++/count-asterisks.cpp
@@ -2,6 +2,12 @@ class Solution {
     //https://leetcode.com/problems/count-asterisks
 public:
     int countAsterisks(string s) {
+        int pipe_cnt = 0,ans=0,tmp=0;
+        for(auto &c:s) (c=='|'?pipe_cnt:((c=='*' && pipe_cnt%2==0)?ans:tmp))++;
+        return ans;
+    }
+
+    int countAsterisks_v01(string s) {
         int pipe_cnt = 0,ans=0;
         for(auto &c:s) 
             if(c=='|') 
