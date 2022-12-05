da26f22cf120cd20d1fbe84adf06923a34280b8a
milochen0418 <milo_chen@mail2000.com.tw>
2022-12-05 10:52:16 +0800

    C++/count-days-spent-together.cpp refactor

--- C++/count-days-spent-together.cpp
+++ C++/count-days-spent-together.cpp
@@ -1,3 +1,4 @@
+
 class Solution {
     //https://leetcode.com/problems/count-days-spent-together
 public:
@@ -5,8 +6,7 @@ public:
     int countDaysTogether(string arriveAlice, string leaveAlice, string arriveBob, string leaveBob) {
         vector<int> alice = get_day_interval(arriveAlice, leaveAlice);
         vector<int> bob = get_day_interval(arriveBob, leaveBob);
-        if(alice[1] < bob[0]) return 0;
-        if(bob[1] < alice[0]) return 0;
+        if(alice[1] < bob[0] || bob[1] < alice[0]) return 0;
         return min(alice[1],bob[1]) - max(alice[0],bob[0]) + 1;        
     }
     vector<int> get_day_interval(string arrive, string leave) {
@@ -18,11 +18,8 @@ public:
     int get_day(string date) {
         int m = date[1]-'0' + 10*(date[0]-'0');
         int d = date[4]-'0' + 10*(date[3]-'0');
-        int day = 0;
-        for(int i =0;i<m-1;i++) {
-            day+=md[i];
-        }
-        day += d;
+        int day = d;
+        for(int i =0;i<m-1;i++) day+=md[i];
         return day;
     }
 };
