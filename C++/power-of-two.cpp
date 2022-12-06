1c013e0f9c83135a8a310f02bb5f002093cce089
milochen0418 <milo_chen@mail2000.com.tw>
2022-12-07 00:50:01 +0800

    C++/power-of-two.cpp finished O(1)

--- C++/power-of-two.cpp
+++ C++/power-of-two.cpp
@@ -1,10 +1,7 @@
 class Solution {
     //https://leetcode.com/problems/power-of-two
 public:
-    bool isPowerOfTwo(int n) { 
-        if(n<=0)return false;
-        int cnt=0;
-        for(int i = 0;i<32;i++) if( (1<<i)&n ) cnt++;
-        return cnt==1;
+    bool isPowerOfTwo(int n) {
+        return n > 0 && (0==(1<<30)%n);
     }
 };
