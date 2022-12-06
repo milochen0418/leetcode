d276cf0ea09a957c1773004f588067d853d8ff30
milochen0418 <milo_chen@mail2000.com.tw>
2022-12-06 22:28:18 +0800

    C++/maximum-difference-between-increasing-elements.cpp optimized to O(N)

--- C++/maximum-difference-between-increasing-elements.cpp
+++ C++/maximum-difference-between-increasing-elements.cpp
@@ -2,6 +2,15 @@ class Solution {
     //https://leetcode.com/problems/maximum-difference-between-increasing-elements
 public:
     int maximumDifference(vector<int>& nums) {
+        int ans = INT_MIN, bottom=INT_MAX;
+        for(auto &i:nums) {
+            bottom = min(bottom,i);
+            ans = max(i-bottom,ans);
+        }
+        return ans==0?-1:ans;
+    }
+
+    int maximumDifference_v01(vector<int>& nums) {
         int ans = 0;
         int n = nums.size();
         for(int i = 0; i<n;i++) { 
@@ -11,4 +20,5 @@ public:
         } 
         return ans==0?-1:ans;
     }
+
 };
