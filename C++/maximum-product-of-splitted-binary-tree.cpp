f1f25533db3e0e4cd3af710b1286c0f393662fff
milochen0418 <milo_chen@mail2000.com.tw>
2022-12-10 17:48:28 +0800

    C++/maximum-product-of-splitted-binary-tree.cpp finished by hard consideration of big integer number

---
+++ C++/maximum-product-of-splitted-binary-tree.cpp
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    typedef unsigned long long valm;
+    const valm mod_num = 1e9 + 7; 
+    /*
+    valm mod(valm a) {return a%mod_num;}
+    valm mod_add(valm a, valm b) { return mod( mod(a) + mod(b) );}
+    valm mod_mul(valm a, valm b) { return mod( mod(a) * mod(b) );}
+    valm mod_sub(valm a, valm b) { 
+        valm v=mod( mod(a) - mod(b) );
+        return v<0?v+mod_num:v;
+    }
+    */
+    valm mod(valm a) {return a%mod_num;}
+    valm mod_add(valm a, valm b) { return a+b;}
+    valm mod_mul(valm a, valm b) { return mod( mod(a) * mod(b) );}
+    valm mod_sub(valm a, valm b) { 
+        return a-b;
+    }
+    
+    unordered_map<TreeNode*,valm> mp;
+    int maxProduct(TreeNode* root) {
+        valm total = sum_dfs(root);
+        
+        //valm ans = (valm)INT_MIN;
+        valm ans = (valm)0;
+        for(auto &[node, sum]: mp) {
+            valm total_sum = mod_sub(total, sum);
+            //valm prod = mod_mul(sum, total_sum);
+            ans = max(ans, sum* (total-sum));
+            //ans = max(ans, sum*(total-sum));
+            //printf("prod = %ld ,ans = %ld, max() = %ld\n", prod, ans, max(ans,prod));
+        }
+        return (int)(mod(ans)) ;
+        //return ans;
+    }
+    valm sum_dfs(TreeNode *root){
+        if(!root) return 0;
+        mp[root] = mod_add(sum_dfs(root->left), sum_dfs(root->right));
+        mp[root] = mod_add(mp[root], root->val);
+        //mp[root] = root->val + sum_dfs(root->left) + sum_dfs(root->right);
+        return mp[root];
+    } 
+};
