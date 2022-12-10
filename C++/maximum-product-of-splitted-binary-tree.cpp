777f61f35df192f7d1c660bdb73f3fd532c200a0
milochen0418 <milo_chen@mail2000.com.tw>
2022-12-10 17:53:08 +0800

    C++/maximum-product-of-splitted-binary-tree.cpp add comment

--- C++/maximum-product-of-splitted-binary-tree.cpp
+++ C++/maximum-product-of-splitted-binary-tree.cpp
@@ -10,6 +10,7 @@
  * };
  */
 class Solution {
+    //https://leetcode.com/problems/maximum-product-of-splitted-binary-tree
 public:
     
     typedef unsigned long long valm;
@@ -23,12 +24,19 @@ public:
         return v<0?v+mod_num:v;
     }
     */
+
+    /* if you use unsigned long long to figure out solution in this problem, that's enough for all caculation
+    But the only problem is that the problem request you to return int.  
+    */
+    
     valm mod(valm a) {return a%mod_num;}
+    /*
     valm mod_add(valm a, valm b) { return a+b;}
     valm mod_mul(valm a, valm b) { return mod( mod(a) * mod(b) );}
     valm mod_sub(valm a, valm b) { 
         return a-b;
     }
+    */
     
     unordered_map<TreeNode*,valm> mp;
     int maxProduct(TreeNode* root) {
@@ -37,7 +45,7 @@ public:
         //valm ans = (valm)INT_MIN;
         valm ans = (valm)0;
         for(auto &[node, sum]: mp) {
-            valm total_sum = mod_sub(total, sum);
+            //valm total_sum = mod_sub(total, sum);
             //valm prod = mod_mul(sum, total_sum);
             ans = max(ans, sum* (total-sum));
             //ans = max(ans, sum*(total-sum));
@@ -48,9 +56,10 @@ public:
     }
     valm sum_dfs(TreeNode *root){
         if(!root) return 0;
-        mp[root] = mod_add(sum_dfs(root->left), sum_dfs(root->right));
-        mp[root] = mod_add(mp[root], root->val);
-        //mp[root] = root->val + sum_dfs(root->left) + sum_dfs(root->right);
+        //mp[root] = mod_add(sum_dfs(root->left), sum_dfs(root->right));
+        //mp[root] = mod_add(sum_dfs(root->left), sum_dfs(root->right));
+        //mp[root] = mod_add(mp[root], root->val);
+        mp[root] = root->val + sum_dfs(root->left) + sum_dfs(root->right);
         return mp[root];
     } 
 };
