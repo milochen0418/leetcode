a342d9c499b36abfd53662d4ae3777c3db678321
milochen0418 <milo_chen@mail2000.com.tw>
2022-12-11 13:26:10 +0800

    C++/delete-greatest-value-in-each-row.cpp finished by sorting

--- C++/delete-greatest-value-in-each-row.cpp
+++ C++/delete-greatest-value-in-each-row.cpp
@@ -1,6 +1,19 @@
 class Solution {
+    //https://leetcode.com/problems/delete-greatest-value-in-each-row/
 public:
     int deleteGreatestValue(vector<vector<int>>& grid) {
+        int ans = 0;
+        int m = grid.size(), n = grid[0].size();
+        for(int i = 0; i<m;i++) sort(grid[i].begin(), grid[i].end());
+        for(int j = 0; j<n;j++) {
+            int maxv = 0;
+            for(int i = 0; i<m;i++) maxv = max(maxv, grid[i][j]);
+            ans+=maxv;
+        }
+        return ans;
+    }
+
+    int deleteGreatestValue_V01(vector<vector<int>>& grid) {
         int ans = 0;
         while(grid[0].size()>0) {
             int m = grid.size(), n = grid[0].size();
