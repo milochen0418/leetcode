a04540f0b5471d3691651731d29281ab90ff7c64
milochen0418 <milo_chen@mail2000.com.tw>
2022-12-12 08:56:14 +0800

    C++/climbing-stairs.cpp to make code clearly

--- C++/climbing-stairs.cpp
+++ C++/climbing-stairs.cpp
@@ -3,8 +3,23 @@ class Solution {
     //article https://leetcode.com/problems/climbing-stairs/discuss/2175372/C%2B%2B-or-easy-understand-to-do-from-backtrace-to-top-down-DP
     //refer Detailed Explanation of DP https://leetcode.com/explore/featured/card/dynamic-programming/631/strategy-for-solving-dp-problems/4096/
 public:
+
     vector<int> dp;
     int climbStairs(int n) {
+        dp = vector<int>(n+1,0);
+        return sol(n);
+    }
+    int sol(int n) {
+        if(dp[n] > 0) return dp[n];
+        if(n<=2) {
+            dp[n] = n; 
+        } else {
+            dp[n] = sol(n-1) + sol(n-2);     
+        }
+        return dp[n];
+    }
+
+    int climbStairs_v01(int n) {
         //return sol_backtrace(0,n);
         dp = vector<int>(n+1,0);
         dp[0]=1;
