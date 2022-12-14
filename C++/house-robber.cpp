116276da10c904f39522309ca120712b735fcd07
milochen0418 <milo_chen@mail2000.com.tw>
2022-12-14 10:33:07 +0800

    C++/house-robber.cpp finished  by optimized DP

--- C++/house-robber.cpp
+++ C++/house-robber.cpp
@@ -2,9 +2,47 @@ class Solution {
     //https://leetcode.com/problems/house-robber
     //article https://leetcode.com/problems/house-robber/discuss/2204532/C%2B%2B-or-2-kind-of-view-to-make-Top-Down-DP
 public:
-    vector<int> a;
-    vector<int> dp;
+
+    //by the code of rob_v02, you can write done DP code with space O(1)
     int rob(vector<int>& nums) {
+        vector<int>& A = nums;
+        int n = nums.size();
+        int next_next = 0;
+        int next = A[n-1];
+        n = n-2;
+        int cur=next;
+        while(n>=0) {
+            cur = max(A[n] + next_next, next);
+            next_next = next;
+            next = cur;
+            n=n-1;
+        }
+        return cur;
+    }
+    
+
+    vector<int> dp;
+    int rob_v02(vector<int>& nums) {
+        int n = nums.size();
+        dp = vector<int>(n+1,-1);
+        return sol_v02(0, nums);
+    }
+    int sol_v02(int i, vector<int>& A) {
+        if(dp[i] >=0) return dp[i];
+        int n = A.size();
+        if(i>=n) 
+            dp[i] = 0;
+        else if(i==n-1) 
+            dp[i] = A[n-1];
+        else 
+            dp[i] = max(A[i] + sol_v02(i+2,A),  sol_v02(i+1,A));
+        return dp[i];
+    }
+
+
+    vector<int> a;
+    
+    int rob_v01(vector<int>& nums) {
         a = nums;
         int n = a.size();
         dp = vector<int>(n+1,-1);
