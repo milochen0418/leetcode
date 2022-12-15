bfc267c81b0c34a4b23143f4963e212fba99faac
milochen0418 <milo_chen@mail2000.com.tw>
2022-12-15 17:54:47 +0800

    C++/concatenation-of-array.cpp finished by loop

---
+++ C++/concatenation-of-array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+    //https://leetcode.com/problems/concatenation-of-array
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        int n = nums.size();
+        for(int i = 0;i<n;i++) {
+            nums.push_back(nums[i]);
+        }
+        return nums;
+    }
+};
