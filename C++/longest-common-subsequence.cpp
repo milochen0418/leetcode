e0597a48767d770cf87f9d888f51ad2ee465c6b4
milochen0418 <milo_chen@mail2000.com.tw>
2022-12-15 14:01:22 +0800

    C++/longest-common-subsequence.cpp finished by DP

--- C++/longest-common-subsequence.cpp
+++ C++/longest-common-subsequence.cpp
@@ -1,19 +1,41 @@
 class Solution {
 public:
+    vector<vector<int>> dp;
     string str1,str2;
     int m,n;
+    /*
+    string str1,str2;
+    int m,n;
+    */
     int longestCommonSubsequence(string text1, string text2) {
         str1 = text1;
         str2 = text2;
         m = text1.size();
         n = text2.size();
+        dp = vector<vector<int>>(m,vector<int>(n,-1));
         return sol(0,0);
+        /*
+        str1 = text1;
+        str2 = text2;
+        m = text1.size();
+        n = text2.size();
+        return sol(0,0);
+        */
     }
     int sol(int i, int j) { 
+        if(i>=m || j>=n) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(str1[i] == str2[j]) 
+            dp[i][j] = 1+sol(i+1,j+1);
+        else 
+            dp[i][j] = max(sol(i+1,j), sol(i,j+1)); 
+        return dp[i][j];
+        /*
         if(i>=m || j>=n) return 0;
         if(str1[i] == str2[j]) 
             return 1+sol(i+1,j+1);
         else 
             return max(sol(i+1,j), sol(i,j+1)); 
+        */
     }
 };
