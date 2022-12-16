1f39c91850c6c7a35d4462a50f5044aec9060466
milochen0418 <milo_chen@mail2000.com.tw>
2022-12-16 13:40:55 +0800

    C++/remove-one-element-to-make-the-array-strictly-increasing.cpp finished by vector

---
+++ C++/remove-one-element-to-make-the-array-strictly-increasing.cpp
@@ -0,0 +1,24 @@
+class Solution {
+    //https://leetcode.com/problems/remove-one-element-to-make-the-array-strictly-increasing/
+public:
+    bool canBeIncreasing(vector<int>& nums) {
+        if(removeOne(nums)==false) return true;
+        if(removeOne(nums)==true) return false;
+        return true;
+    }
+    
+    bool removeOne(vector<int>& A) {
+        for(int i = 1; i<A.size();i++) {
+            if(!(A[i-1]<A[i])) {
+                int L = i-2>=0?A[i-2]:INT_MIN;
+                int R = i+1<A.size()?A[i+1]:INT_MAX;
+                if(L<A[i-1]&&A[i-1]<R) 
+                    A.erase(A.begin() + i);    
+                else 
+                    A.erase(A.begin() + (i-1));
+                return true;
+            }            
+        }
+        return false;
+    }
+};
