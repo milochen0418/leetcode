f1d144c5646ba9495a045987b1581000709564d6
milochen0418 <milo_chen@mail2000.com.tw>
2022-12-16 22:12:35 +0800

    C++/minimum-moves-to-convert-string.cpp greedy idea but debugging

--- C++/minimum-moves-to-convert-string.cpp
+++ C++/minimum-moves-to-convert-string.cpp
@@ -1,19 +1,46 @@
 class Solution {
     //https://leetcode.com/problems/minimum-moves-to-convert-string/
 public:
+    /* 
+    Case Analysis
+    OOXOXOO
+    XXOX
+    XX XOX XX
+    XXOXOXOXX
+    */
     int minimumMoves(string s) {
-        int L=0,R=0,n=s.length(), ans=0;
-        while(L<n && R<n) {
-            if(s[L]!='X') {
-                ++L;
-                R=L;
-            } else {
-                while(R<n && s[R]=='X')++R;
-                int gap = R-L;
-                ans += (gap/3) + (gap%3>0);
-                L=R;
-            }
+        int ans = 0,n=s.length();
+        for(int i = 0;i<n-2;i++) {
+            ans+=true==clear3(i,s);
+        }
+        for(int i = 0;i<n-2;i++) {
+            ans+=true==clear2(i,s);
+        }
+        
+        for(int i = 0;i<n;i++) {
+            ans+=s[i]=='X';
         }
         return ans;
     }
+    
+    bool clear3(int i,string& s) {
+        if(i+2>=s.length()) return false;
+        if(s[i]!='X' || s[i+1]!='X' || s[i+2]!='X') return false;
+        s[i]=s[i+1]=s[i+2]='O';
+        return true;
+    }
+    
+    bool clear2(int i,string& s) {
+        if(i+2>=s.length()) return false;
+        int cnt = 0;
+        cnt+=s[i]=='X';
+        cnt+=s[i+1]=='X';
+        cnt+=s[i+2]=='X';
+        if(cnt<2) return false;
+        s[i] = s[i+1] = s[i+2] = 'O';
+        return true;
+    }
+    //Key idea is to delete XXX first. And then try to delete XXO first and XOX letter
+    //Error Case 
+    //"XOXXOXXXX"
 };
