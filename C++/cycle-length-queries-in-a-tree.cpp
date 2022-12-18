ea247d9f205482e75b8fa8191be683876b21f52e
milochen0418 <milo_chen@mail2000.com.tw>
2022-12-18 15:26:08 +0800

    C++/cycle-length-queries-in-a-tree.cpp refactor

--- C++/cycle-length-queries-in-a-tree.cpp
+++ C++/cycle-length-queries-in-a-tree.cpp
@@ -2,6 +2,19 @@ class Solution {
 public:
     vector<int> cycleLengthQueries(int n, vector<vector<int>>& queries) {
         vector<int> ans;
+        for(auto &v:queries) {
+            int len=1;//1 is edge length
+            while(v[0]!=v[1]) { //use two-pointer to find out lowest common ancestor
+                (v[0]>v[1]?v[0]:v[1])/=2;//always get half of largest value. 
+                len++;
+            }
+            ans.push_back(len);
+        }
+        return ans;
+    }
+
+    vector<int> cycleLengthQueries_v01(int n, vector<vector<int>>& queries) {
+        vector<int> ans;
         for(auto &v:queries) ans.push_back(getLength(n,v[0],v[1]));
         return ans;
     }
