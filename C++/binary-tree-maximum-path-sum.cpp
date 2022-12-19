4b3c08c4798db627c5d3f4a220865dc28e8ad8c7
milochen0418 <milo_chen@mail2000.com.tw>
2022-12-19 21:48:44 +0800

    C++/binary-tree-maximum-path-sum.cpp finished by DFS

---
+++ C++/binary-tree-maximum-path-sum.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    //https://leetcode.com/problems/binary-tree-maximum-path-sum
+public:
+    unordered_map<TreeNode*, int> mp;
+    int ans=INT_MIN;
+    int maxPathSum(TreeNode* root) {
+        dfs1(root);
+        dfs2(root);
+        return ans;
+    }
+    
+    int dfs1(TreeNode* root) { //DFS1 to find out all maximum falling path 
+        if(mp.find(root) == mp.end()) {
+            if(!root) 
+                mp[root]=0;
+            else {
+                int L = dfs1(root->left);
+                int R = dfs1(root->right);
+                int maxLR = max(0,max(L,R));
+                int maxfalling = max(0, root->val + maxLR);
+                mp[root] = maxfalling;
+            }
+        }
+        return mp[root];
+    }
+    void dfs2(TreeNode* root) {//DFS2 to find out who have maximum path sum
+        if(!root) return;
+        int val = root->val + mp[root->left] + mp[root->right];
+        ans = max(ans, val);
+        dfs2(root->left);
+        dfs2(root->right);
+    }  
+};
