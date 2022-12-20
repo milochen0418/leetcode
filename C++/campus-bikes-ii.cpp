8a5bad650b6b4dd672f19d815246d9bf130299cd
milochen0418 <milo_chen@mail2000.com.tw>
2022-12-20 21:57:50 +0800

    C++/campus-bikes-ii.cpp rewrite permutation code with my lambda permutation trackbacking

--- C++/campus-bikes-ii.cpp
+++ C++/campus-bikes-ii.cpp
@@ -2,12 +2,6 @@ class Solution {
     //https://leetcode.com/problems/campus-bikes-ii/
 public:
     vector<vector<int>> dis;
-    vector<int> test={0,1,2};
-    void show(vector<int>& v) {
-        for(auto &i:v) 
-            printf("%d, ",i );
-        printf("\n");
-    }
     
     //C++ lambda example from https://learn.microsoft.com/zh-tw/cpp/cpp/examples-of-lambda-expressions?view=msvc-170
     //function<int(int, int)> f2 = [](int x, int y) { return x + y; };
@@ -21,13 +15,28 @@ public:
         }
     }
     
+
     int assignBikes(vector<vector<int>>& workers, vector<vector<int>>& bikes) {
+        int n = workers.size(), m = bikes.size();
+        int ans = INT_MAX;
+        dis = vector<vector<int>>(n, vector<int>(m,0));
+        for(int i = 0; i<n;i++) 
+            for(int j = 0; j<m;j++) 
+                dis[i][j] = abs(workers[i][0] - bikes[j][0]) + abs(workers[i][1]-bikes[j][1]);
         
-        permutate(test,0, [](vector<int>& v) { 
-            for(auto &i:v) printf("%d,",i );
-            printf("\n");
+        vector<int> A = vector<int>(m,0);
+        for(int i = 0; i<m;i++) A[i] = i;        
+        permutate(A,0, [&](vector<int>& A) {
+            int sum_dis = 0;
+            for(int i = 0;i<n;i++) 
+                sum_dis += dis[i][A[i]];
+            ans = min(ans, sum_dis);            
         });
-        return 0;
+        return ans;
+    }
+    
+    int assignBikes_stdlib(vector<vector<int>>& workers, vector<vector<int>>& bikes) {
+        
         
         int n = workers.size(), m = bikes.size();
         int ans = INT_MAX;
