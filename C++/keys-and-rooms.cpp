3b9004111c7b2a37b68c0279dc64a9ac07d96a53
milochen0418 <milo_chen@mail2000.com.tw>
2022-12-20 10:59:12 +0800

    C++/keys-and-rooms.cpp add DFS solution

--- C++/keys-and-rooms.cpp
+++ C++/keys-and-rooms.cpp
@@ -1,7 +1,33 @@
 class Solution {
     //https://leetcode.com/problems/keys-and-rooms
 public:
+
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        return canVisitAllRooms_dfs(rooms);
+        //return canVisitAllRooms_bfs(rooms);
+    }
+    
+    int n;
+    unordered_set<int> visited;
+    vector<vector<int>> *pRooms;
+    bool canVisitAllRooms_dfs(vector<vector<int>>& rooms) {
+        pRooms = &rooms;
+        n = rooms.size();
+        dfs(0);
+        return n==visited.size();
+    }
+    void dfs(int room) {
+        visited.insert(room);
+        vector<int>& keys = (*pRooms)[room];
+        for(auto &key:keys) {
+            if(visited.find(key) == visited.end()) {
+                dfs(key);
+            }
+        }
+    }
+
+
+    bool canVisitAllRooms_bfs(vector<vector<int>>& rooms) {
         unordered_set<int> s;
         int n = rooms.size();
         queue<int> Q;
