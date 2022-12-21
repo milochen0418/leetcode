22eb84076991741f31cf000c4a7fd6639a4b40a3
milochen0418 <milo_chen@mail2000.com.tw>
2022-12-21 14:40:52 +0800

    C++/minimum-operations-to-make-the-array-increasing.cpp add article

--- C++/minimum-operations-to-make-the-array-increasing.cpp
+++ C++/minimum-operations-to-make-the-array-increasing.cpp
@@ -1,5 +1,6 @@
 class Solution {
     //https://leetcode.com/problems/minimum-operations-to-make-the-array-increasing
+    //article https://leetcode.com/problems/minimum-operations-to-make-the-array-increasing/discuss/2934599/C%2B%2B-or-easy-O(N)
 public:
     int minOperations(vector<int>& nums) {
         int ans = 0, n = nums.size();
