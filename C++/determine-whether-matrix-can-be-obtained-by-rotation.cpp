921da282643ceddef9450f96a421ffb477f2917c
milochen0418 <milo_chen@mail2000.com.tw>
2022-12-21 19:12:22 +0800

    C++/determine-whether-matrix-can-be-obtained-by-rotation.cpp finished by rorate lambda

---
+++ C++/determine-whether-matrix-can-be-obtained-by-rotation.cpp
@@ -0,0 +1,69 @@
+class Solution {
+    //https://leetcode.com/problems/determine-whether-matrix-can-be-obtained-by-rotation
+public:
+    int n;
+    bool findRotation(vector<vector<int>>& mat, vector<vector<int>>& target) {
+        vector<vector<int>>& M=mat, &T = target;
+        n = mat.size();
+        vector<function<vector<int>(vector<int>)>> Rfs = {
+            [&](vector<int>v){ return R({v[0],v[1]}); }, 
+            [&](vector<int>v){ return R(R({v[0],v[1]})); }, 
+            [&](vector<int>v){ return R(R(R({v[0],v[1]}))); }, 
+            [&](vector<int>v){ return R(R(R(R({v[0],v[1]})))); }
+        };
+        
+        int n2 = n*n;
+        for(auto& Rf: Rfs) {
+            bool is_break=false;
+            for(int k = 0;k<n2;k++) {
+                int x=k%n, y=k/n;
+                vector<int> t = Rf({x,y});
+                if(M[t[0]][t[1]] != T[x][y]) {
+                    is_break=true;
+                    break;
+                }
+            }
+            if(is_break==false) return true;
+        }
+        return false;
+    }
+    
+    vector<int> R(vector<int> xy) { //find rotate index
+        /*
+        n=6, xy={2,1}
+         0 0 0 0 0 0
+         0 0 1 0 0 0
+         0 0 0 0 0 0
+         0 0 0 0 0 0
+         0 0 0 0 0 0
+         0 0 0 0 0 0
+         
+        n=6, xy={4,2} = {5-y', x}
+         0 0 0 0 0 0
+         0 0 0 0 0 0
+         0 0 0 0 1 0
+         0 0 0 0 0 0
+         0 0 0 0 0 0
+         0 0 0 0 0 0
+         
+        n=6, xy={3,4} = {5-y', x}
+         0 0 0 0 0 0
+         0 0 0 0 0 0
+         0 0 0 0 0 0
+         0 0 0 0 0 0
+         0 0 0 1 0 0
+         0 0 0 0 0 0
+         
+        n=6, xy={1,3} = {5-y', x}         
+         0 0 0 0 0 0
+         0 0 0 0 0 0
+         0 0 0 0 0 0
+         0 1 0 0 0 0
+         0 0 0 0 0 0
+         0 0 0 0 0 0
+        */
+        
+        int x=xy[0], y=xy[1];
+        return {n-1-y,x};
+    }
+};
