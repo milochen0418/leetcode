cf1d4d35bf22f13fcbd83e43267619424005bfb3
milochen0418 <milo_chen@mail2000.com.tw>
2022-12-21 17:02:31 +0800

    C++/largest-odd-number-in-string.cpp add article

---
+++ C++/largest-odd-number-in-string.cpp
@@ -0,0 +1,17 @@
+class Solution {
+    //https://leetcode.com/problems/largest-odd-number-in-string
+    //article https://leetcode.com/problems/largest-odd-number-in-string/discuss/2934977/C%2B%2B-or-O(N)-or-explanation
+public:
+    string largestOddNumber(string num) {
+        int n = num.length(), i=n-1;
+        while(i>=0) {
+            if( (num[i]-'0')%2==1 ) break;//find first odd value from right
+            i--;
+        }
+        if(i<0) return "";
+        
+        string ans; 
+        for(int j = 0;j<=i;j++)ans.push_back(num[j]);
+        return ans;
+    }
+};
