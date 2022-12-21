029a3f8084c405f29d19abaea426ca6dcb44084b
milochen0418 <milo_chen@mail2000.com.tw>
2022-12-21 15:19:17 +0800

    C++/sum-of-digits-of-string-after-convert.cpp finished

--- C++/sum-of-digits-of-string-after-convert.cpp
+++ C++/sum-of-digits-of-string-after-convert.cpp
@@ -1,20 +1,19 @@
 class Solution {
-    //https://leetcode.com/problems/sum-of-digits-of-string-after-convert/
+    //https://leetcode.com/problems/sum-of-digits-of-string-after-convert
 public:
     int getLucky(string s, int k) {
-        unordered_map<char,int> mp;
         unordered_map<char,int> sump;
         for(char i='a';i<='z';i++) {
-            mp[i] = (i-'a')+1;
-            sump[i] = mp[i]%10 + mp[i]/10;
+            int q = (i-'a')+1;
+            sump[i] = q%10 + q/10;
         }
         int ans = 0;
         for(int ik = 1; ik<=k;ik++) {
             if(ik==1) {
                 for(auto &c:s) {
-                    ans+=sump[s[ik]];
+                    ans+=sump[c];
                 }
-            } else {//case ik>=3
+            } else {//case ik>=2
                 int tmp = ans;
                 ans = 0; 
                 while(tmp>0) {
