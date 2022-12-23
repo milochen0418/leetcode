db791c9b6020b561e825b2334f00023fdd0d4b46
milochen0418 <milo_chen@mail2000.com.tw>
2022-12-23 21:49:40 +0800

    C++/check-if-word-equals-summation-of-two-words.cpp add article

--- C++/check-if-word-equals-summation-of-two-words.cpp
+++ C++/check-if-word-equals-summation-of-two-words.cpp
@@ -1,5 +1,6 @@
 class Solution {
     //https://leetcode.com/problems/check-if-word-equals-summation-of-two-words
+    //https://leetcode.com/problems/check-if-word-equals-summation-of-two-words/discuss/2942339/C%2B%2B-or-simple-4-lines
 public:
     bool isSumEqual(string firstWord, string secondWord, string targetWord) {
         for(string* sp:{&firstWord, &secondWord, &targetWord}) 
