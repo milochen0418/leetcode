1a5a0eb658d87106bcbb96e8eb79e0e1711aa6e6
milochen0418 <milo_chen@mail2000.com.tw>
2022-12-23 12:52:37 +0800

    C++/best-time-to-buy-and-sell-stock.cpp add 3 pass solution

--- C++/best-time-to-buy-and-sell-stock.cpp
+++ C++/best-time-to-buy-and-sell-stock.cpp
@@ -11,4 +11,17 @@ public:
         }   
         return max_gap;
     }
+
+    int maxProfit_3pass(vector<int>& prices) {        
+        //for each min point i, find out the j>i s.t. prices[j] is maximum in prices[i..n-1]  
+        int n = prices.size(), ans = 0;
+        vector<int> minp = vector<int>(n, prices[0]);
+        vector<int> maxp = vector<int>(n, prices[n-1]);        
+        for(int i = 1; i<n;i++) minp[i] = min(prices[i], minp[i-1]);
+        for(int i = n-2;i>=0;i--) maxp[i] = max(prices[i], maxp[i+1]);
+        for(int i = 0; i<n;i++) ans = max(ans, maxp[i]-minp[i]);
+        return ans;
+    }
+
+
 };
