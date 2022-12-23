6445d30f891d6ee9e7d904ecc97bc524afcb5233
milochen0418 <milo_chen@mail2000.com.tw>
2022-12-23 22:28:15 +0800

    C++/sorting-the-sentence.cpp finished by O(N) array approach

---
+++ C++/sorting-the-sentence.cpp
@@ -0,0 +1,23 @@
+class Solution {
+    //https://leetcode.com/problems/sorting-the-sentence
+public:
+    string sortSentence(string s) {
+        int n = s.length();
+        vector<int> mp = vector<int>(10,-1);
+        for(int i = n-1;i>=0;i--) {
+            if('1'<=s[i] && s[i]<='9') {
+                int tmp = i;
+                while(i>=0 && s[i]!=' ')i--;
+                mp[s[tmp]-'0'] = i+1;
+            }
+        }
+        string ans;
+        for(int i=1; i<=9; i++) {
+            int j = mp[i];
+            if(j==-1) break;
+            if(i!=1)ans.push_back(' ');
+            while(!('0'<=s[j] && s[j]<='9')) ans.push_back(s[j++]);
+        }
+        return ans;
+    }
+};
