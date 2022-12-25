8288643a3169e529013394bbc6b8058befed3cea
milochen0418 <milo_chen@mail2000.com.tw>
2022-12-26 00:44:26 +0800

    C++/latest-time-by-replacing-hidden-digits.cpp finished by call by reference

---
+++ C++/latest-time-by-replacing-hidden-digits.cpp
@@ -0,0 +1,26 @@
+class Solution {
+    //https://leetcode.com/problems/latest-time-by-replacing-hidden-digits
+public:
+    string& maximumTime(string &time) {
+        string &s= time;
+        if(s[0] == '?' && s[1] == '?') {
+            s[0]='2';
+            s[1]='3';
+        }
+        if(s[0]=='?') {
+            if(s[1]<='3')
+                s[0]='2';
+            else
+                s[0]='1';
+        }
+        if(s[1]=='?') {
+            if(s[0]<'2')
+                s[1]='9';
+            else
+                s[1]='3';
+        }
+        if(s[3]=='?') s[3]='5';
+        if(s[4]=='?') s[4]='9';
+        return s;
+    }
+};
