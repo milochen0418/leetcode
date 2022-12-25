4e7ab4c869e8918059dbd4262ec05aadc9da7360
milochen0418 <milo_chen@mail2000.com.tw>
2022-12-25 23:08:05 +0800

    C++/number-of-different-integers-in-a-string.cpp finished by string process

---
+++ C++/number-of-different-integers-in-a-string.cpp
@@ -0,0 +1,37 @@
+class Solution {
+    //https://leetcode.com/problems/number-of-different-integers-in-a-string/
+public:
+    int numDifferentIntegers(string word) {
+        int n = word.length();
+        unordered_set<string> ss;
+        string s = "";
+        for(int i = 0;i<n+1;i++) {
+            char c = i==n?'z':word[i];
+            if('9'>=c && c>='0') {
+                s.push_back(c);    
+            } else {
+                if(i>0) {
+                    if(s.length()>0) {
+                        ss.insert(process(s));
+                    }
+                }
+                s="";
+            }
+        }
+        return ss.size();
+        
+    }
+    string process(string& s) {
+        //delete prefix zero
+        string ret;
+        int n = s.length();
+        int i=0;
+        
+        while(i<n && s[i]=='0')i++;
+        if(n>=1 && i>=n) return "0";
+        for(int j = i;j<n;j++) {
+            ret.push_back(s[j]);
+        }
+        return ret;
+    }
+};
