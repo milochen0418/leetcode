3f1b941bc9db9b5adfcdef34f097b4e23c86d39b
milochen0418 <milo_chen@mail2000.com.tw>
2022-12-26 03:19:26 +0800

    C++/n-th-tribonacci-number.cpp rewrite

--- C++/n-th-tribonacci-number.cpp
+++ C++/n-th-tribonacci-number.cpp
@@ -4,14 +4,14 @@ public:
     vector<int> dp;
     int tribonacci(int n) {
         dp = vector<int>(n+3,-1);
-        dp[0]=0;
-        dp[1]=1;
-        dp[2]=1;
         return sol(n);
     }
     int sol(int n) {
         if(dp[n]!=-1) return dp[n];
-        dp[n] = sol(n-1) + sol(n-2) + sol(n-3);
+        if(n<=2) 
+            dp[n] = n>=1;
+        else 
+            dp[n] = sol(n-1) + sol(n-2) + sol(n-3);
         return dp[n];
-    }  
+    }
 };
