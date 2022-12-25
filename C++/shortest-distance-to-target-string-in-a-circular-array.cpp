00a76df159ad9a532736eceedc873b2af8105d35
milochen0418 <milo_chen@mail2000.com.tw>
2022-12-25 17:12:12 +0800

    C++/shortest-distance-to-target-string-in-a-circular-array.cpp optimized

--- C++/shortest-distance-to-target-string-in-a-circular-array.cpp
+++ C++/shortest-distance-to-target-string-in-a-circular-array.cpp
@@ -1,8 +1,20 @@
 class Solution {
+    //https://leetcode.com/problems/shortest-distance-to-target-string-in-a-circular-array
 public:
+    int closetTarget(vector<string>& words, string target, int startIndex) {
+        int n = words.size(),ans = INT_MAX;
+        for(int i = 0; i<n;i++) 
+            if(words[i] == target) {
+                int len = abs(startIndex - i);
+                ans = min(ans, len);
+                ans = min(ans, n-len);
+            }
+        return ans==INT_MAX?-1:ans;
+    }
+
     //["hello","i","am","leetcode","hello"]
     // n=5, -1, 4  ...  idx 0,1.  abs(startIdx - idx),  n-abs(startIdx - idx)
-    int closetTarget(vector<string>& words, string target, int startIndex) {
+    int closetTarget_v01(vector<string>& words, string target, int startIndex) {
         int n = words.size();
         vector<int> idxs;
         
