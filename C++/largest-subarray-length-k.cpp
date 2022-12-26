87fb347c6167b1cea4fad21d988876da5b7183d7
milochen0418 <milo_chen@mail2000.com.tw>
2022-12-27 02:18:54 +0800

    C++/largest-subarray-length-k.cpp optimized

--- C++/largest-subarray-length-k.cpp
+++ C++/largest-subarray-length-k.cpp
@@ -2,7 +2,21 @@ class Solution {
     //https://leetcode.com/problems/largest-subarray-length-k/
     //article https://leetcode.com/problems/largest-subarray-length-k/discuss/2954751/C%2B%2B-or-O(N*Log(N))-by-priority_queue-and-hash-map
 public:
-    vector<int> largestSubarray(vector<int>& nums, int k) {
+    vector<int> largestSubarray(vector<int>& nums, int k) {//O(N)
+        vector<int> ans;
+        int n=nums.size(), j = n-k; 
+        int maxv = nums[n-k];
+        for(int i = n-k; i>=0;i--) {
+            if(maxv<nums[i]) {
+                maxv = nums[i];
+                j = i;
+            }
+        }
+        for(int i=j;i<j+k;i++) ans.push_back(nums[i]);
+        return ans;
+    }
+
+    vector<int> largestSubarray_v01(vector<int>& nums, int k) {//O(NlogN)
         vector<int> ans;
         int n = nums.size() ;
         unordered_map<int,vector<int>> mp;
