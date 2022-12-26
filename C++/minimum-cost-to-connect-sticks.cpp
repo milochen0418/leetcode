92632c2e3f6598b4a27b7c05d97d3a69d87b84fd
milochen0418 <milo_chen@mail2000.com.tw>
2022-12-26 11:55:59 +0800

    C++/minimum-cost-to-connect-sticks.cpp finished by priority_queue minQ

--- C++/minimum-cost-to-connect-sticks.cpp
+++ C++/minimum-cost-to-connect-sticks.cpp
@@ -2,6 +2,22 @@ class Solution {
     //https://leetcode.com/problems/minimum-cost-to-connect-sticks/
 public:
     int connectSticks(vector<int>& sticks) {
+        int ans = 0;
+        priority_queue<int, vector<int>, greater<int>> minQ;
+        for(auto &i: sticks) minQ.push(i);
+        while(minQ.size()>1) {
+            int a = minQ.top();
+            minQ.pop();
+            int b = minQ.top();
+            minQ.pop();
+            ans+=(a+b);
+            minQ.push(a+b);
+        }        
+        return ans;
+    }
+
+
+    int connectSticks_N2logN(vector<int>& sticks) {
         vector<int>& A = sticks;
         int ans=0;
         while(A.size()>1) {
