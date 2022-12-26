602011d7edd64b896df95aca307e2d8d6f1423a9
milochen0418 <milo_chen@mail2000.com.tw>
2022-12-26 09:37:23 +0800

    C++/jump-game.cpp put backtracking and dp solution

--- C++/jump-game.cpp
+++ C++/jump-game.cpp
@@ -1,22 +1,41 @@
 class Solution {
     //https://leetcode.com/problems/jump-game
 public:
-    vector<int> a;
     vector<int> dp;
     bool canJump(vector<int>& nums) {
-        a = nums;
-        int n = a.size();
-        dp = vector<int>(n,-1);
-        dp[n-1] = 1;
-        return sol(0);
-        
+        //return sol_backtracking(0, nums);
+        dp = vector<int>(nums.size(), -1);
+        return sol_dp(0,nums);
     }
-    bool sol(int i) {
-        int n = a.size();
-        if(i>=n) return false;
-        if(dp[i] != -1) return dp[i];
-        dp[i] = 0;
-        for(int j = 1;j<=a[i];j++) if(sol(i+j)) return dp[i] = 1;
-        return dp[i];
+    int sol_dp(int j, vector<int>& A) {
+        if(dp[j]!=-1) return dp[j];
+        if(j==A.size()-1) {
+            dp[j] = 1;
+        } else {
+            dp[j] = 0;
+            for(int i = j+1;i<A.size() && i<=j+A[j];i++) {
+                if(sol_dp(i,A)) {                
+                    dp[j]=1;
+                    break;
+                }
+            }
+        }
+        return dp[j];
+    }
+    
+    int sol_backtracking(int j, vector<int>& A) {
+        int ret;
+        if(j==A.size()-1) {
+            ret = 1;
+        } else {
+            ret = 0;
+            for(int i = j+1;i<A.size() && i<=j+A[j];i++) {
+                if(sol_backtracking(i,A)) {
+                    ret = 1;
+                    break;
+                }
+            }
+        }
+        return ret;
     }
 };
