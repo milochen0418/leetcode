eca06f83866027e58ba7f4bbfeec446cee81ab18
milochen0418 <milo_chen@mail2000.com.tw>
2022-12-26 15:55:36 +0800

    C++/calculate-money-in-leetcode-bank.cpp optimized

--- C++/calculate-money-in-leetcode-bank.cpp
+++ C++/calculate-money-in-leetcode-bank.cpp
@@ -2,6 +2,16 @@ class Solution {
     //https://leetcode.com/problems/calculate-money-in-leetcode-bank
 public:
     int totalMoney(int n) {
+        const vector<int> prefix_v = {1,3,6,10,15,21,28};
+        n-=1;
+        int ans = 0, i = 0;
+        int q = n/7;//q weeks
+        int r = n%7;//remain day
+        for(i = 0; i<q;i++) ans += prefix_v[6] + 7*i;
+        ans += (r+1)*(q) + prefix_v[r];
+        return ans;        
+    }
+    int totalMoney_v01(int n) {
         n-=1;
         vector<int> v = vector<int>(7,0); //0:Mon, 1:Tue...5:Sat. 6:Sun
         vector<int> prefix_v = vector<int>(7,0);
