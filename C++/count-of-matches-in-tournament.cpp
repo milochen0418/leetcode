5cc895ecfd0f4478cfd6f3dc65f2b14571fea99c
milochen0418 <milo_chen@mail2000.com.tw>
2022-12-28 01:50:51 +0800

    C++/count-of-matches-in-tournament.cpp finished by loop

---
+++ C++/count-of-matches-in-tournament.cpp
@@ -0,0 +1,18 @@
+class Solution {
+    //https://leetcode.com/problems/count-of-matches-in-tournament
+public:
+    int numberOfMatches(int n) {
+        int ans=0;
+        while(n>1) {
+            ans+=n/2;
+            if(n%2==0) {
+                ans+=n/2;
+                n=n/2;
+            } else {
+                ans+=(n-1)/2;
+                n=((n-1)/2) + 1;
+            }
+        }
+        return ans;
+    }
+};
