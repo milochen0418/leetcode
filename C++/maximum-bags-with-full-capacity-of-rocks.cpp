ab1aece1be6a30bcbfe69057b2cf5a9ddea3134b
milochen0418 <milo_chen@mail2000.com.tw>
2022-12-27 09:33:46 +0800

    C++/maximum-bags-with-full-capacity-of-rocks.cpp refactor

--- C++/maximum-bags-with-full-capacity-of-rocks.cpp
+++ C++/maximum-bags-with-full-capacity-of-rocks.cpp
@@ -3,6 +3,19 @@ class Solution {
 public:
     int maximumBags(vector<int>& capacity, vector<int>& rocks, int additionalRocks) {
         int n = rocks.size();
+        for(int i = 0; i < n;i++) capacity[i]-=rocks[i];
+        sort(capacity.begin(), capacity.end());
+        int ans = 0;
+        for(int i = 0;i<n;i++) {
+            if(capacity[i]>additionalRocks) break;
+            ans++;
+            additionalRocks-=capacity[i];
+        }
+        return ans;
+    }
+
+    int maximumBags_v01(vector<int>& capacity, vector<int>& rocks, int additionalRocks) {
+        int n = rocks.size();
         for(int i = 0; i < n;i++) {
             capacity[i]-=rocks[i];
         }   
