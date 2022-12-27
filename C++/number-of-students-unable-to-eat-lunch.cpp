2b74fded34b2f09310ab7937516c47fae8e43266
milochen0418 <milo_chen@mail2000.com.tw>
2022-12-27 10:50:24 +0800

    C++/number-of-students-unable-to-eat-lunch.cpp add article

--- C++/number-of-students-unable-to-eat-lunch.cpp
+++ C++/number-of-students-unable-to-eat-lunch.cpp
@@ -1,5 +1,6 @@
 class Solution {
     //https://leetcode.com/problems/number-of-students-unable-to-eat-lunch
+    //article https://leetcode.com/problems/number-of-students-unable-to-eat-lunch/discuss/2955782/C%2B%2B-or-O(N)
 public:
     int countStudents(vector<int>& students, vector<int>& sandwiches) {
         int ans = 0, n = students.size(), i=0;
