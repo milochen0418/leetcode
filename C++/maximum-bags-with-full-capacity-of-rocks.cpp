82605ae9ec709ab0382353db1efddb10e3b8ee09
milochen0418 <milo_chen@mail2000.com.tw>
2022-12-27 09:29:48 +0800

    C++/maximum-bags-with-full-capacity-of-rocks.cpp finished by sorting

---
+++ C++/maximum-bags-with-full-capacity-of-rocks.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    //https://leetcode.com/problems/maximum-bags-with-full-capacity-of-rocks
+public:
+    int maximumBags(vector<int>& capacity, vector<int>& rocks, int additionalRocks) {
+        int n = rocks.size();
+        for(int i = 0; i < n;i++) {
+            capacity[i]-=rocks[i];
+        }   
+        sort(capacity.begin(), capacity.end());
+        int ans = 0;
+        for(int i = 0;i<n;i++) {
+            if(capacity[i]==0) {
+                ans++;
+                continue;
+            }
+            int minus = min(capacity[i], additionalRocks);
+            capacity[i] -= minus;
+            additionalRocks -= minus;
+            if(capacity[i]==0) {
+                ans++;
+                continue;
+            } else {
+                break;
+            }
+        }
+        return ans;
+    }
+};
