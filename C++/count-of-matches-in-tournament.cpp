bd3baee09bdc6f7124d2d8850d1bb6fd8c1e67cc
milochen0418 <milo_chen@mail2000.com.tw>
2022-12-28 01:53:10 +0800

    C++/count-of-matches-in-tournament.cpp refactor

--- C++/count-of-matches-in-tournament.cpp
+++ C++/count-of-matches-in-tournament.cpp
@@ -4,6 +4,15 @@ public:
     int numberOfMatches(int n) {
         int ans=0;
         while(n>1) {
+            ans+=n/2;
+            n=n/2+n%2;
+        }
+        return ans;
+    }
+
+    int numberOfMatches_v01(int n) {
+        int ans=0;
+        while(n>1) {
             if(n%2==0) {
                 ans+=n/2;
                 n=n/2;
