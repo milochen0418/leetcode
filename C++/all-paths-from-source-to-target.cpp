216bc0b341ae6ea266d53bdacc5c7ee38297dcd1
milochen0418 <milo_chen@mail2000.com.tw>
2022-12-30 13:09:51 +0800

    C++/all-paths-from-source-to-target.cpp optimized

--- C++/all-paths-from-source-to-target.cpp
+++ C++/all-paths-from-source-to-target.cpp
@@ -1,21 +1,19 @@
 class Solution {
     //https://leetcode.com/problems/all-paths-from-source-to-target
 public:
-    vector<vector<int>>* pGraph;
-    vector<vector<int>> ans;
-    vector<int> tmp; //temp vector in dfs.
-    int t; // target
+    vector<vector<int>> ans, *pGraph;
+    vector<int> path;
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
-        int n = graph.size();
         pGraph = &graph;
-        t = n-1;
         dfs(0);
         return ans;
     }
-    void dfs(int s) {
-        tmp.push_back(s);
-        if(s == t) ans.push_back(tmp);
-        for(auto& next_s: (*pGraph)[s]) dfs(next_s);
-        tmp.pop_back();
+    void dfs(int root) {
+        path.push_back(root);
+        if(path.back() == pGraph->size()-1) 
+            ans.push_back(path);
+        else
+            for(auto &i: (*pGraph)[root]) dfs(i);
+        path.pop_back();
     }
 };
