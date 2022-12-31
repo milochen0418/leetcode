40dba9ba801c0d03e7f95c871fd8435f5d3e695f
milochen0418 <milo_chen@mail2000.com.tw>
2022-12-31 21:29:35 +0800

    C++/counting-elements.cpp finished by sort. optimized by space complexity O(1)

--- C++/counting-elements.cpp
+++ C++/counting-elements.cpp
@@ -2,6 +2,18 @@ class Solution {
     //https://leetcode.com/problems/counting-elements
 public:
     int countElements(vector<int>& arr) {
+        vector<int>& A = arr;
+        int ans = 0, n = A.size();
+        sort(A.begin(), A.end());
+        int accu = 1;
+        for(int i = 1; i<n;i++) {
+            if(A[i-1]+1 == A[i]) ans+=accu; 
+            accu = A[i-1]==A[i]?accu+1:1;
+        }
+        return ans;
+    }
+
+    int countElements_hashset(vector<int>& arr) {
         unordered_set<int> s;
         for(auto &i:arr) s.insert(i);
         int ans = 0;
