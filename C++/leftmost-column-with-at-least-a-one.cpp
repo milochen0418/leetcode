8debf06b29b2921848b28bd500f7c032eb65011a
milochen0418 <milo_chen@mail2000.com.tw>
2022-12-31 17:37:14 +0800

    C++/leftmost-column-with-at-least-a-one.cpp chage to use binary search but still use too much call

--- C++/leftmost-column-with-at-least-a-one.cpp
+++ C++/leftmost-column-with-at-least-a-one.cpp
@@ -11,6 +11,34 @@
 class Solution {
 public:
     int leftMostColumnWithOne(BinaryMatrix &binaryMatrix) {
+        //You made too many calls to BinartMatrix.get().
+        int m = binaryMatrix.dimensions()[0];
+        int n = binaryMatrix.dimensions()[1];
+        int ans = INT_MAX;
+        for(int i =0;i<m;i++) {
+            int first_one = INT_MAX; 
+            int L=0,R=n-1;
+            while(L<=R) {
+                int M = L + (R-L)/2;
+                int AM = binaryMatrix.get(i, M);
+                int AM_1 = M-1>=0?binaryMatrix.get(i, M-1):0;
+                if(AM_1==0 && AM==1) {
+                    first_one = M;
+                    break;
+                }
+                if(AM==0) 
+                    L=M+1;
+                else
+                    R=M-1;
+            }
+            ans = min(ans, first_one);
+        }
+        return ans==INT_MAX?-1:ans;
+
+    }
+
+    int leftMostColumnWithOne_v01(BinaryMatrix &binaryMatrix) {
+        //It's O(N^2) alogirthm to many call
         int m = binaryMatrix.dimensions()[0];
         int n = binaryMatrix.dimensions()[1];
         int ans = INT_MAX;
