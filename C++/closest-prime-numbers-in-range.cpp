799c1407b5d07de4e15bc70646081366fb031248
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-01 20:02:17 +0800

    C++/closest-prime-numbers-in-range.cpp optimized to double speed by use figure out half prime

--- C++/closest-prime-numbers-in-range.cpp
+++ C++/closest-prime-numbers-in-range.cpp
@@ -2,16 +2,49 @@ class Solution {
     //https://leetcode.com/problems/closest-prime-numbers-in-range/submissions/
 public:
     #define printf(...) empty_printf(__VA_ARGS__)
-    void empty_printf(const char *fmt, ...) {
-        /*
-        va_list args;
-        va_start(args, fmt);
-        vprintf(fmt, args);
-        va_end(args);
-        */
-    }
-    
+    #define empty_printf(...) 
+
+
     vector<int> closestPrimes(int left, int right) {
+        vector<int> is_primes = vector<int>(right+1,1);
+        vector<int> primes=vector<int>(), ans=vector<int>(2,-1);
+        if(left<=2 && right>=3) return {2,3};
+        
+        is_primes[0] = is_primes[1] = 0;
+        for(int i = 3;i<=right;i+=2) {
+            if(is_primes[i]==1) {
+                for(int j = i+(i+i);j<=right;j+=(i+i)) {
+                    is_primes[j] = 0;
+                }                
+            }
+        }
+        int min_gap = INT_MAX;
+        
+        printf("primes = ");
+        if(left%2==0) left+=1;
+        for(int i = left;i<=right;i+=2) {
+            //if(i%2==0) continue;
+            if(is_primes[i]==1) { 
+                printf("%d,",i);
+                if(primes.size()>0) {
+                    int gap = i - primes.back();
+                    if(gap<min_gap) {
+                        printf("gap<min_gap, gap = %d, min_gap = %d\n", gap, min_gap);
+                        min_gap = gap;
+                        ans[0] = primes.back();
+                        ans[1] = i;
+                        if(min_gap==2) return ans;
+                    }
+                }
+                primes.push_back(i);
+            }
+        }
+        printf("\n");
+        if(ans[0]!=-1)return ans;
+        return vector<int>{-1,-1};
+    }
+        
+    vector<int> closestPrimes_v01(int left, int right) {
         //vector<int> is_primes = vector<int>(right+1,1);
         vector<int> is_primes = vector<int>(right+1,1);
         vector<int> primes=vector<int>(), ans=vector<int>(2,-1);
