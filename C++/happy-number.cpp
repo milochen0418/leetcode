2c1b6b3b5bebe1baa16c57f66fa2202bd438cd38
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-02 23:35:35 +0800

    C++/happy-number.cpp refactor

--- C++/happy-number.cpp
+++ C++/happy-number.cpp
@@ -1,7 +1,24 @@
 class Solution {
     //https://leetcode.com/problems/happy-number
 public:
+    typedef unsigned long long ull;
     bool isHappy(int n) {
+        unordered_set<ull> ss;
+        ull sum = n;
+        do {
+            ss.insert(sum);
+            n = sum;
+            sum = 0;
+            while(n>0) {
+                int d = n%10;
+                sum+=d*d;
+                n/=10;
+            }
+        } while(ss.find(sum) == ss.end());
+        return sum == 1;
+    }
+    
+    bool isHappy_v01(int n) {
         unordered_set<int> s;
         while(s.find(n)==s.end()) {
             if(n==1) return true;
