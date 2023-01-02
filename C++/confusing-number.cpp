3b2e22f43b50fae8d30053a6180571ddc299e91c
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-02 14:39:49 +0800

    C++/confusing-number.cpp finished by one pass

---
+++ C++/confusing-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool confusingNumber(int n) {
+        //After rorate 180 degree for the n to get m. if m is number and m!=n, 
+        //then we say n is the confusing number. 
+        int v = n;
+        int m = 0; 
+        vector<int> mp={0, 1,-1,-1,-1,-1, 9,-1, 8, 6};
+                      //0. 1. 2. 3. 4. 5. 6. 7  8. 9
+        while(v>0) {
+            int i = v%10;
+            if(mp[i]==-1) return false;
+            m = m*10 + mp[i];
+            v/=10;
+        }
+        return n!=m;
+    }
+};
