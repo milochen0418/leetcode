2ea284ff7df76efa41d8653aa5e7d1f72ecbfe74
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-02 20:30:57 +0800

    C++/slowest-key.cpp finished by array

---
+++ C++/slowest-key.cpp
@@ -0,0 +1,26 @@
+class Solution {
+    //https://leetcode.com/problems/slowest-key
+public:
+    char slowestKey(vector<int>& releaseTimes, string keysPressed) {
+        vector<int>& pressedTimes = releaseTimes;
+        int n = keysPressed.length();
+        
+        //convert releaseTimes into pressedTime
+        for(int i = releaseTimes.size()-1; i>=0;i--) {
+            int prev=i==0?0:releaseTimes[i-1];
+            pressedTimes[i]-=prev;
+        }
+        
+        int maxv = -1;
+        char maxc = 0;
+        for(int i = 0;i<n;i++) {
+            int dur=pressedTimes[i];
+            char c = keysPressed[i];
+            if(dur>maxv || (dur==maxv && c>maxc)) {
+                maxv = dur;
+                maxc = c;
+            }
+        }
+        return maxc;
+    }
+};
