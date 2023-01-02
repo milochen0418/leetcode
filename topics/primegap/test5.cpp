f71a10b4e34b65981bb335ebcaba226fa7f9570b
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-02 12:30:30 +0800

    find out the (max,min,avg) distance of the same prime gaps

--- topics/primegap/test5.cpp
+++ topics/primegap/test5.cpp
@@ -11,6 +11,7 @@ $ cat qq5.txt
 using namespace std;
 #include "primegaps.hpp"
 //vector<int> primegaps;
+typedef unsigned long long ull;
 int main()
 {
     //primegaps = vector<int>({1,2,2,4,2,4});
@@ -22,17 +23,33 @@ int main()
 
     //figure out the maximum distance of prime gap 
     vector<vector<int>> mp = vector<vector<int>>(256,vector<int>()); 
-    vector<int> mp_dist = vector<int>(256,0); 
+    vector<int> mp_max_dist = vector<int>(256,0); 
+    vector<int> mp_min_dist = vector<int>(256,100000); 
+    vector<float> mp_avg_dist = vector<float>(256,0.0f);
     for(int i = 0;i<primegaps.size();i++) {
         vector<int> &v= mp[primegaps[i]];
         v.push_back(i);
         if(v.size()>=2) { 
             int nn = v.size();
-            mp_dist[primegaps[i]] = max(mp_dist[primegaps[i]], v[nn-1] - v[nn-2] );
+            mp_max_dist[primegaps[i]] = max(mp_max_dist[primegaps[i]], v[nn-1] - v[nn-2] );
+            mp_min_dist[primegaps[i]] = min(mp_min_dist[primegaps[i]], v[nn-1] - v[nn-2] );
+        } 
+    }
+    for(int i = 0;i<256;i++) {
+        ull sum_dist = 0;
+        if(mp[i].size()>=2) {
+            //for(auto &val:mp[i])sum_dist+=val;
+            for(int j =1;j<mp[i].size();j++) {
+                sum_dist+=(mp[i][j] - mp[i][j-1]);
+            }
+            double avg_double = sum_dist / mp[i].size()-1;
+            mp_avg_dist[i] = (float) avg_double;
         }
     }
     for(int i = 0;i<256;i++) {
-        printf("%3d:repeat=%5d, max_distance=%d\n", i, (int)mp[i].size(), mp_dist[i] );
+        //prime gap only >=2 for all prime>=3
+        if(i%2==0)
+            printf("%3d:repeat=%5d, max_dist=%5d, min_dist=%5d, avg_dist=%5.0f\n", i, (int)mp[i].size(), mp_max_dist[i],  mp_min_dist[i]==100000?-1:mp_min_dist[i], mp_avg_dist[i]);
     }
 
 
