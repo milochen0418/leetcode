b11a4e3b092b2b68c9be11ec66a3fd14b209771c
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-04 10:40:39 +0800

    C++/mean-of-array-after-removing-some-elements.cpp add article

--- C++/mean-of-array-after-removing-some-elements.cpp
+++ C++/mean-of-array-after-removing-some-elements.cpp
@@ -1,5 +1,6 @@
 class Solution {
     //https://leetcode.com/problems/mean-of-array-after-removing-some-elements
+    //article https://leetcode.com/problems/mean-of-array-after-removing-some-elements/discuss/2995045/C%2B%2B-simple-4-lines-by-sort
 public:
     double trimMean(vector<int>& arr) {
         int n = arr.size(), m = n/20, sum = 0;
