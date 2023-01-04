3dd94e4389fd65851ebe4f3a386a69e7b24ba02a
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-04 14:19:40 +0800

    C++/crawler-log-folder.cpp add article

--- C++/crawler-log-folder.cpp
+++ C++/crawler-log-folder.cpp
@@ -1,5 +1,6 @@
 class Solution {
     //https://leetcode.com/problems/crawler-log-folder
+    //article https://leetcode.com/problems/crawler-log-folder/discuss/2996057/C%2B%2B-One-Pass-3-lines
 public:
     int minOperations(vector<string>& logs) {
         int ans=0;
@@ -7,4 +8,16 @@ public:
             if( 0 > (ans+=(s[0]!='.')-(s[1]=='.')) ) ans=0;
         return ans;
     }
+    int minOperations_v01(vector<string>& logs) {
+        int ans=0;
+        for(auto &s:logs) {
+            if(s[0]!='.') 
+                ans++;
+            else if(s[1]=='.')
+                ans--;
+            if(ans<0) ans = 0;
+        }
+        return ans;
+    }
+
 };
