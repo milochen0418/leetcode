1e96924d7cee88263dbfd04bd212abbd8c5d7719
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-04 23:12:56 +0800

    C++/special-positions-in-a-binary-matrix.cpp finished by hash set

---
+++ C++/special-positions-in-a-binary-matrix.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    //https://leetcode.com/problems/special-positions-in-a-binary-matrix
+public:
+    int numSpecial(vector<vector<int>>& mat) {
+        int m = mat.size(), n = mat[0].size();
+        vector<unordered_set<int>> col_sets=vector<unordered_set<int>>(n,unordered_set<int>());
+        vector<unordered_set<int>> row_sets=vector<unordered_set<int>>(m,unordered_set<int>());
+            
+        for(int i = 0;i<m;i++) {
+            for(int j = 0;j<n;j++) {
+                if(mat[i][j]) {
+                    row_sets[i].insert(j);
+                    col_sets[j].insert(i);
+                }
+            }
+        }
+        
+        int ans = 0;
+        for(int i = 0;i<m;i++) {
+            for(int j = 0;j<n;j++) {
+                if(mat[i][j]==1) {
+                    ans+= ((row_sets[i].size()==1) && ((col_sets[j].size()==1)));
+                }
+            }
+        }
+        return ans;
+    }
+};
