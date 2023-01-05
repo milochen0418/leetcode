4b87b6aed219752cfd5695405a1e2c72a75e8825
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-05 20:10:47 +0800

    C++/final-prices-with-a-special-discount-in-a-shop.cpp finished by O(N) monotonic stack

--- C++/final-prices-with-a-special-discount-in-a-shop.cpp
+++ C++/final-prices-with-a-special-discount-in-a-shop.cpp
@@ -1,7 +1,32 @@
 class Solution {
     //https://leetcode.com/problems/final-prices-with-a-special-discount-in-a-shop
 public:
+    #define printf(...) empty_printf(__VA_ARGS__)
+    #define empty_printf(...)
     vector<int> finalPrices(vector<int>& prices) {
+        vector<int> stk;
+        int n = prices.size();
+        vector<int> ans = prices;
+        for(int i = 0; i<n;i++) {
+            if(stk.empty() || prices[stk.back()] < prices[i]) {
+                stk.push_back(i);
+            } else {
+                while(!stk.empty() && prices[stk.back()] >= prices[i]) {
+                    prices[stk.back()]-=prices[i];
+                    stk.pop_back();
+                }
+                stk.push_back(i);
+            }
+            printf("i=%d, stk = ", i);
+            for(auto &e:stk) {
+                printf("%d, ",e);
+            }
+            printf("\n");
+        }
+        return prices;
+    }
+
+    vector<int> finalPrices_v01(vector<int>& prices) {
         int n = prices.size();
         vector<int> ans = prices;
         for(int i = 0; i<n;i++) 
