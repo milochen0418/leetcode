7dacf1cc04206301967e702a9eb53f48f171692d
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-05 16:17:47 +0800

    C++/count-good-triplets.cpp optimized

--- C++/count-good-triplets.cpp
+++ C++/count-good-triplets.cpp
@@ -1,8 +1,25 @@
 class Solution {
     //https://leetcode.com/problems/count-good-triplets
 public:
-    int a,b,c;
+
     int countGoodTriplets(vector<int>& arr, int _a, int _b, int _c) {
+        vector<int>& A=arr;
+        a=_a, b=_b, c=_c;
+        int ans=0,  n = arr.size();
+        for(int i =0;i<n;i++) {
+            for(int j =i+1;j<n;j++) {
+                if(! (abs(A[i] - A[j]) <= a)) continue;
+                for(int k=j+1;k<n;k++) {
+                    if(!(abs(A[j] - A[k]) <= b)) continue;
+                    if(abs(A[i] - A[k]) <= c) ans++;
+                }              
+            }
+        }                
+        return ans;       
+    }
+
+    int a,b,c;
+    int countGoodTriplets_v01(vector<int>& arr, int _a, int _b, int _c) {
         a=_a, b=_b, c=_c;
         int ans=0,  n = arr.size();
         for(int i =0;i<n;i++)
