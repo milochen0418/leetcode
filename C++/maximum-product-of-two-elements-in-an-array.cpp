c8a112b3653cab57553084641392ff933ded94f5
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-07 00:29:29 +0800

    C++/maximum-product-of-two-elements-in-an-array.cpp add article

--- C++/maximum-product-of-two-elements-in-an-array.cpp
+++ C++/maximum-product-of-two-elements-in-an-array.cpp
@@ -1,5 +1,6 @@
 class Solution {
     //https://leetcode.com/problems/maximum-product-of-two-elements-in-an-array
+    //article https://leetcode.com/problems/maximum-product-of-two-elements-in-an-array/discuss/3009659/C%2B%2B-O(N)
 public:
     int maxProduct(vector<int>& nums) {
         priority_queue<int> maxQ;
