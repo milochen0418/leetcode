44c516ba9a1da57534f7dc7bb1923a2c6c62ba77
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-07 12:20:47 +0800

    C++/maximum-ice-cream-bars.cpp finished by counting approach

--- C++/maximum-ice-cream-bars.cpp
+++ C++/maximum-ice-cream-bars.cpp
@@ -2,7 +2,28 @@ class Solution {
     //https://leetcode.com/problems/maximum-ice-cream-bars
     //Study Counting Sort https://leetcode.com/explore/learn/card/sorting/695/non-comparison-based-sorts/4437/
 public:
+    typedef long long ll;
     int maxIceCream(vector<int>& costs, int coins) {
+        int maxv = INT_MIN;
+        for(auto &i:costs) maxv = max(maxv, i);
+        vector<int> mp=vector<int>(maxv+1,0);
+        for(auto &i: costs) mp[i]++;
+        int ans = 0;
+        for(int i = 0;i<mp.size() && coins>0;i++) {
+            ll product = (ll)i*(ll)mp[i];
+            //printf("i=%d, mp[i]=%d\n", i,mp[i]);
+            if((ll)coins >= product) {
+                coins -= i * mp[i];
+                ans = ans + mp[i];
+            } else {
+                ans = ans + coins/i;
+                coins = 0;
+            }
+        }
+        return ans;
+    }
+
+    int maxIceCream_v02(vector<int>& costs, int coins) {
         //sorting
         int ans = 0;
         sort(costs.begin(),costs.end());
