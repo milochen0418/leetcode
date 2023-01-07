2a62e5a9fd6d7682b244277ed48a14883ac57557
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-07 08:02:04 +0800

    C++/maximum-ice-cream-bars.cpp finished by sorting

--- C++/maximum-ice-cream-bars.cpp
+++ C++/maximum-ice-cream-bars.cpp
@@ -2,6 +2,21 @@ class Solution {
     //https://leetcode.com/problems/maximum-ice-cream-bars
 public:
     int maxIceCream(vector<int>& costs, int coins) {
+        //sorting
+        int ans = 0;
+        sort(costs.begin(),costs.end());
+        for(auto &i: costs) {
+            coins -= i;
+            if(coins>=0) 
+                ans++;
+            else
+                break;
+        }
+        return ans;
+    }
+
+    int maxIceCream_v01(vector<int>& costs, int coins) {
+        //priority_queue
         int ans=0;
         priority_queue<int, vector<int>, greater<int>> minQ;
         for(auto &i:costs) minQ.push(i); 
