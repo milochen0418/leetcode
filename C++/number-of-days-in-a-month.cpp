524f2482fb1e22afa52651aa7e64392f8466a2f8
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-09 00:26:01 +0800

    C++/number-of-days-in-a-month.cpp optimized

--- C++/number-of-days-in-a-month.cpp
+++ C++/number-of-days-in-a-month.cpp
@@ -2,6 +2,24 @@ class Solution {
     //https://leetcode.com/problems/number-of-days-in-a-month
 public:
     int numberOfDays(int year, int month) {
+        vector<int> days_in_months = {0,31,28,31,30,31,30,31,31,30,31,30,31};
+                                      // 1     3     5     7  8    10    12
+        if(month==2){
+            if(year%100==0) {
+                if(year%400==0) 
+                    return 29;
+                else 
+                    return 28;
+            } else {
+                if(year%4==0) 
+                    return 29;
+                else
+                    return 28;
+            }
+        } 
+        return days_in_months[month];
+    }
+    int numberOfDays_v01(int year, int month) {
         //data reference here.
         //refer https://leetcode.com/problems/number-of-days-in-a-month 
         //refer https://m.law.iask.sina.com.cn/jx/sh/dhw3vEwSwe34.html  Feb in 1900 & 1800 only have 28 days
