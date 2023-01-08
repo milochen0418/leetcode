1cde2d986e4c0071aabe28d3a172d1f89e75f600
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-08 23:55:43 +0800

    C++/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp finished by loop

---
+++ C++/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
@@ -0,0 +1,24 @@
+class Solution {
+    //https://leetcode.com/problems/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence
+public:
+    int isPrefixOfWord(string sentence, string searchWord) {
+        int n = sentence.length(), m = searchWord.length(), idx=1,j=0;
+        for(int i = 0;i<n;) {
+            if(sentence[i]==' ') {
+                idx++;
+                j=0;
+                i++;
+                continue;
+            } else {
+                if(sentence[i]==searchWord[j]) {
+                    if(j==m-1) return idx;
+                    j++;
+                } else {
+                    while(i+1<n && sentence[i+1]!=' ')i++;
+                }
+            }
+            i++;
+        }   
+        return -1;
+    }
+};
