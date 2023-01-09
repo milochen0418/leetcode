cea48d2a770dd43b7f9a9d9cf0b990c8831e36c0
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-09 15:37:33 +0800

    C++/check-if-all-1s-are-at-least-length-k-places-away.cpp optimized to one-pass

--- C++/check-if-all-1s-are-at-least-length-k-places-away.cpp
+++ C++/check-if-all-1s-are-at-least-length-k-places-away.cpp
@@ -3,6 +3,21 @@ class Solution {
 public:
     bool kLengthApart(vector<int>& nums, int k) {
         int n = nums.size(), i=0;
+        int prev_i=-1;
+        for(i=prev_i+1;i<n;i++) {        
+            if(nums[i]==1) {
+                if(prev_i!=-1) {
+                    int len = i-prev_i-1;
+                    if(len<k) return false;                    
+                }
+                prev_i=i;
+            }
+        }
+        return true;
+    }
+
+    bool kLengthApart_v01(vector<int>& nums, int k) {
+        int n = nums.size(), i=0;
         while(i<n && nums[i]!=1)i++;
         int prev_i = i;
         for(i=prev_i+1;i<n;i++) {
