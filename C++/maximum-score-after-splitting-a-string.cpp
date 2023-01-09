3ab5a2377ecb5b0453ec7a722f337fdca9c99893
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-09 19:14:59 +0800

    C++/maximum-score-after-splitting-a-string.cpp refactor

--- C++/maximum-score-after-splitting-a-string.cpp
+++ C++/maximum-score-after-splitting-a-string.cpp
@@ -2,6 +2,18 @@ class Solution {
     //https://leetcode.com/problems/maximum-score-after-splitting-a-string
 public:
     int maxScore(string s) {
+        int n = s.length(), L=0,R=0; 
+        for(int i = 1; i<n;i++) R+=s[i]=='1';
+        L = s[0]=='0';
+        int maxv = L+R;
+        for(int i = 1; i<n-1;i++) {
+            s[i]=='1'?R--:L++;
+            maxv = max(maxv, L+R) ;
+        }
+        return maxv;
+    }
+
+    int maxScore_v01(string s) {
         int n = s.length();
         int L = 0,R=0; 
         for(int i = 1; i<n;i++) R+=s[i]=='1';
