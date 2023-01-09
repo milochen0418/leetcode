52044ce855f13340c43d83f3a8ea70b521216fcb
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-09 18:45:54 +0800

    C++/perform-string-shifts.cpp finished

--- C++/perform-string-shifts.cpp
+++ C++/perform-string-shifts.cpp
@@ -1,26 +1,16 @@
 class Solution {
 public:
     string stringShift(string s, vector<vector<int>>& shift) {
-        int shift_val = 1, n = s.length();
+        int shift_val = 0, n = s.length();
         for(auto &v: shift) {
             shift_val += (v[0]*2-1)*v[1];
             shift_val = (shift_val+n)%n;
         }
+        //printf("shift %d.\n",shift_val);
         string ans = string(n,' ');
         for(int i = 0;i<n;i++) {
-            ans[i] = s[(i+shift_val)%n];
+            ans[i] = s[(i+(n-shift_val))%n];
         }
         return ans;
     }
 };
-/* 
-Pass
-"abcdefg"
-[[1,1],[1,1],[0,2],[1,3]]
-"abc"
-[[0,1],[1,2]]
-
-Fail
-"mecsk"
-[[1,4],[0,5],[0,4],[1,1],[1,5]]
-*/
