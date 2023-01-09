5c3bcd510948661a95dc8afc8c44b5620f6d973c
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-09 19:26:59 +0800

    C++/reformat-the-string.cpp add article

--- C++/reformat-the-string.cpp
+++ C++/reformat-the-string.cpp
@@ -1,5 +1,6 @@
 class Solution {
     //https://leetcode.com/problems/reformat-the-string
+    //article https://leetcode.com/problems/reformat-the-string/discuss/3024492/C%2B%2B-Two-Pass-O(N)-6-lines
 public:
     string reformat(string s) {
         string A,B;
