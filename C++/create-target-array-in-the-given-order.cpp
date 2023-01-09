9064d135177473fcc66c03f9f1a9ddf9701e656f
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-09 20:43:26 +0800

    C++/create-target-array-in-the-given-order.cpp finished by vector.insert

---
+++ C++/create-target-array-in-the-given-order.cpp
@@ -0,0 +1,11 @@
+class Solution {
+    //https://leetcode.com/problems/create-target-array-in-the-given-order
+public:
+    vector<int> createTargetArray(vector<int>& nums, vector<int>& index) {
+        int n = nums.size();
+        vector<int> target;
+        for(int i = 0; i<n;i++) 
+            target.insert(target.begin()+index[i], nums[i]);
+        return target;
+    }
+};
