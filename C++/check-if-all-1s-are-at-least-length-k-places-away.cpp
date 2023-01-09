64c193646a615ac9c57b8eefa99188aacf452b0b
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-09 15:36:43 +0800

    C++/check-if-all-1s-are-at-least-length-k-places-away.cpp finished

---
+++ C++/check-if-all-1s-are-at-least-length-k-places-away.cpp
@@ -0,0 +1,18 @@
+class Solution {
+    //https://leetcode.com/problems/check-if-all-1s-are-at-least-length-k-places-away
+public:
+    bool kLengthApart(vector<int>& nums, int k) {
+        int n = nums.size(), i=0;
+        while(i<n && nums[i]!=1)i++;
+        int prev_i = i;
+        for(i=prev_i+1;i<n;i++) {
+            if(nums[i]==1) {                
+                int len = i-prev_i-1;
+                if(len<k) return false;                    
+                prev_i=i;
+            }
+        }
+        return true;
+    }
+
+};
