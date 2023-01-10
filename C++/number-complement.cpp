f23124ac38706e8f4b08248ae666bcfe252133d2
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-10 17:24:45 +0800

    C++/number-complement.cpp finsihed by one pass stack

--- C++/number-complement.cpp
+++ C++/number-complement.cpp
@@ -2,6 +2,24 @@ class Solution {
     //https://leetcode.com/problems/number-complement
 public:
     int findComplement(int num) {
+        //One Pass Stack
+        stack<int> s;
+        int ans=0;
+        while(num>0 || !s.empty()){
+            if(num) {
+                s.push(num%2);
+                num/=2;
+            } else {
+                ans=ans*2 + (s.top()==0);
+                s.pop();
+            }
+        }
+        return ans;
+    }
+
+
+    int findComplement_v01(int num) {
+        //Two Pass Stack
         stack<int> s;
         while(num>0){
             s.push(num%2);
