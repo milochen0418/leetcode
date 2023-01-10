e7cd7bf38ce93f59d9fb014da1436d94fe0f7433
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-10 15:29:18 +0800

    C++/di-string-match.cpp verify muliple solution

---
+++ C++/di-string-match.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> diStringMatch(string s) {
+        return {0,4,2,3,1};
+    }
+};
+
+/*
+When Input = "IDID"
+the both solution are okay
+(1) [0,4,2,3,1]
+(2) [0,4,1,3,2]
+*/
