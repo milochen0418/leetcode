5e6113535aee6f8ced22c695f60910b5f5a1dae5
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-10 11:28:27 +0800

    C++/maximum-69-number.cpp finished by stack

---
+++ C++/maximum-69-number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+    //https://leetcode.com/problems/maximum-69-number/
+public:
+    int maximum69Number (int num) {
+        stack<int> s;
+        int first6notound = 1;
+        while(num>0) {
+            int r = num%10;
+            s.push(r);
+            num/=10;
+        }
+        while(!s.empty()) {
+            int r = s.top();
+            if(first6notound && r==6) {
+                first6notound = 0;
+                r=9;
+            }
+            num=num*10+r;
+            s.pop();
+        }
+        return num;
+    }
+};
