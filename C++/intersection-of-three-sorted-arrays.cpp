31504521cfe33fd676aa11ec07e1695e4437e73c
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-11 23:48:29 +0800

    C++/intersection-of-three-sorted-arrays.cpp optimized

--- C++/intersection-of-three-sorted-arrays.cpp
+++ C++/intersection-of-three-sorted-arrays.cpp
@@ -8,6 +8,37 @@ public:
         return ans;
     }
     vector<int> intersection(vector<int>& A, vector<int>& B){
+        vector<int> ans;
+        int i=0,j=0;
+        while(i<A.size() || j<B.size()) 
+            if(i<A.size() && j<B.size())
+                if(A[i]==B[j]) 
+                    ans.push_back(A[i++]=B[j++]);
+                else 
+                    A[i]<B[j]?i++:j++;            
+            else 
+                i>A.size()?j++:i++;
+        return ans;
+    }
+
+    vector<int> intersection_v01(vector<int>& A, vector<int>& B){
+        vector<int> ans;
+        int i=0,j=0;
+        while(i<A.size() || j<B.size()) {
+            if(i<A.size() && j<B.size()) {
+                if(A[i]==B[j]) {
+                    ans.push_back(A[i]);
+                    i++,j++;
+                } else {
+                    A[i]<B[j]?i++:j++;
+                }                
+            } else {
+                i>A.size()?j++:i++;
+            }
+        }
+        return ans;
+    }
+    vector<int> intersection_v01(vector<int>& A, vector<int>& B){
         vector<int> ans = {};
         int i=0,j=0;
         while(i<A.size() || j<B.size()) {
