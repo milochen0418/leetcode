eb6b4a9e366b1f2cd4b0095700596ce78c383a0f
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-11 23:43:14 +0800

    C++/intersection-of-three-sorted-arrays.cpp finished by function

---
+++ C++/intersection-of-three-sorted-arrays.cpp
@@ -0,0 +1,29 @@
+class Solution {
+    //https://leetcode.com/problems/intersection-of-three-sorted-arrays
+public:
+    vector<int> ans;
+    vector<int> arraysIntersection(vector<int>& arr1, vector<int>& arr2, vector<int>& arr3) {
+        vector<int> A = intersection(arr1, arr2);
+        vector<int> ans = intersection(A, arr3);
+        return ans;
+    }
+    vector<int> intersection(vector<int>& A, vector<int>& B){
+        vector<int> ans = {};
+        int i=0,j=0;
+        while(i<A.size() || j<B.size()) {
+            if(i>=A.size()) {
+                j++;
+            } else if(j>=B.size()){
+                i++;
+            } else if(A[i]==B[j]) {
+                ans.push_back(A[i]);
+                i++,j++;
+            } else if(A[i]<=B[j]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+        return ans;
+    }
+};
