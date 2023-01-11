9348fb4d6aed0f5e662b10f4a372d3e77c608843
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-11 22:41:27 +0800

    C++/hexspeak.cpp finished by one pass

--- C++/hexspeak.cpp
+++ C++/hexspeak.cpp
@@ -1,5 +1,6 @@
 class Solution {
     //https://leetcode.com/problems/hexspeak
+    //article https://leetcode.com/problems/hexspeak/discuss/3035944/C%2B%2B-One-Pass-O(N)
 public:
     vector<char> digit_to_letter = {
         'O','I','2','3','4','5','6','7',
