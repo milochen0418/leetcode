fcaa20617b38219df68fbd9c4aa229f8d94b33ff
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-12 11:18:16 +0800

    C++/number-of-nodes-in-the-sub-tree-with-the-same-label.cpp refactor

--- C++/number-of-nodes-in-the-sub-tree-with-the-same-label.cpp
+++ C++/number-of-nodes-in-the-sub-tree-with-the-same-label.cpp
@@ -2,36 +2,31 @@ class Solution {
     //https://leetcode.com/problems/number-of-nodes-in-the-sub-tree-with-the-same-label
 public:
     unordered_map<int, vector<int>> mp;
-    vector<vector<int>> res;
-    vector<int> empty_feature;
+    vector<vector<int>> extend;
     vector<bool> visited;
     string labels;
     vector<int> countSubTrees(int n, vector<vector<int>>& edges, string labelss) {
         labels = labelss;
-        res=vector<vector<int>>(n,vector<int>(26,0));
+        extend=vector<vector<int>>(n,vector<int>(26,0));
         visited = vector<bool>(n,false);
         for(auto &e: edges) {
             mp[e[0]].push_back(e[1]);
             mp[e[1]].push_back(e[0]);
         }
         dfs(0);
-        
         vector<int> ans = vector<int>(n,0);
-        for(int i = 0; i<n;i++)  ans[i] = res[i][labels[i]-'a'];
+        for(int i = 0; i<n;i++)  ans[i] = extend[i][labels[i]-'a'];
         return ans;
     }
-    
-    
+
     void dfs(int root) {
         visited[root] = true;
-        res[root][labels[root]-'a']++;
+        extend[root][labels[root]-'a']++;
         for(auto &child: mp[root]) {
             if(visited[child] == false) {
                 dfs(child);
-                for(int i = 0;i<26;i++) res[root][i]+=res[child][i];
+                for(int i = 0;i<26;i++) extend[root][i]+=extend[child][i];
             }
         }
     }
-    
-    
 };
