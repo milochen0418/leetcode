9cce5197b4c74360f19fc1b786acae4167ea6604
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-12 15:51:12 +0800

    C++/how-many-apples-can-you-put-into-the-basket.cpp add priority queue solution

--- C++/how-many-apples-can-you-put-into-the-basket.cpp
+++ C++/how-many-apples-can-you-put-into-the-basket.cpp
@@ -1,6 +1,20 @@
 class Solution {
 public:
     int maxNumberOfApples(vector<int>& weight) {
+        priority_queue<int, vector<int>, greater<int>> q;//maxQ
+        for(auto &w:weight) q.push(w);
+        int ans=0, total =0;
+        while(!q.empty()) {
+            int w = q.top(); q.pop();
+            if(total+w>5000)break;            
+            ans++;
+            total+=w;            
+        }
+        return ans;
+    }
+
+    int maxNumberOfApples_sort(vector<int>& weight) {
+        //Sorting 
         sort(weight.begin(), weight.end());
         int ans=0;
         int total = 0;
