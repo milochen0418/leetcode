dae120b29f1d3a4efeb032deace36761a51c3a67
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-12 10:25:28 +0800

    C++/minimum-health-to-beat-game.cpp optimized to one pass

--- C++/minimum-health-to-beat-game.cpp
+++ C++/minimum-health-to-beat-game.cpp
@@ -2,6 +2,13 @@ class Solution {
     //https://leetcode.com/problems/minimum-health-to-beat-game
 public:
     long long minimumHealth(vector<int>& damage, int armor) {
+        int n = damage.size(), maxv = damage[0], maxi = 0;
+        long long ans =1+damage[0];
+        for(int i = 1;i<n;ans+=damage[i++]) if(damage[i]>maxv) maxv = damage[maxi=i];
+        return ans -= min(maxv, armor);
+    }
+    
+    long long minimumHealth_v01(vector<int>& damage, int armor) {
         int maxv = damage[0];
         int maxi = 0;
         int n = damage.size();
