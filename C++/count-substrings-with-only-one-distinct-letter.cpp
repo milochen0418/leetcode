7e785f1f231553b872724252af8839f208581676
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-13 11:49:59 +0800

    C++/count-substrings-with-only-one-distinct-letter.cpp add article

--- C++/count-substrings-with-only-one-distinct-letter.cpp
+++ C++/count-substrings-with-only-one-distinct-letter.cpp
@@ -1,5 +1,6 @@
 class Solution {
     //https://leetcode.com/problems/count-substrings-with-only-one-distinct-letter
+    //article https://leetcode.com/problems/count-substrings-with-only-one-distinct-letter/discuss/3043321/C%2B%2B-One-Pass-O(N)-10-lines
 public:
     int countLetters(string s) {
         s.push_back('+');
