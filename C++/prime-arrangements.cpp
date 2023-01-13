8a95abf9620b84fcdfb11b7eb67a10e29ae97434
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-13 15:24:13 +0800

    C++/prime-arrangements.cpp optimized by one pass

--- C++/prime-arrangements.cpp
+++ C++/prime-arrangements.cpp
@@ -5,6 +5,23 @@ public:
     int numPrimeArrangements(int n) {
         vector<int> is_prime = vector<int>(101,1);
         is_prime[0] = is_prime[1] = 0;
+        int cnt_prime = 0,cnt_combine = 1;        
+
+        for(int i = 2;i<=n;i++) {
+            if(is_prime[i]==1) {
+                cnt_prime++;
+                for(int j = i+i;j<=n;j+=i) is_prime[j] = 0;
+            } else {
+                cnt_combine++;
+            }
+        }
+                
+        return mul(factorial(cnt_prime), factorial(cnt_combine));
+    }
+    
+    int numPrimeArrangements_v01(int n) {
+        vector<int> is_prime = vector<int>(101,1);
+        is_prime[0] = is_prime[1] = 0;
         for(int i = 2;i<=n;i++) 
             if(is_prime[i]==1) 
                 for(int j = i+i;j<=n;j+=i) 
