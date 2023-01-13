10889a9bf0a04be9fa041ca28dfc70d17009afb0
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-13 18:22:12 +0800

    C++/largest-unique-number.cpp add article

--- C++/largest-unique-number.cpp
+++ C++/largest-unique-number.cpp
@@ -1,5 +1,6 @@
 class Solution {
     //https://leetcode.com/problems/largest-unique-number
+    //article https://leetcode.com/problems/largest-unique-number/discuss/3044779/C%2B%2B-Hash-Map-O(N)
 public:
     unordered_map<int,int> mp;
     int largestUniqueNumber(vector<int>& nums) {
