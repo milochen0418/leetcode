af346f3ee9b55c44676b55ce239f7ac4d4d9da85
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-13 11:31:22 +0800

    C++/distance-between-bus-stops.cpp finished by one pass

--- C++/distance-between-bus-stops.cpp
+++ C++/distance-between-bus-stops.cpp
@@ -2,6 +2,15 @@ class Solution {
     //https://leetcode.com/problems/distance-between-bus-stops
 public:
     int distanceBetweenBusStops(vector<int>& distance, int start, int destination) {
+        int total = 0, dist = 0, begin = min(start, destination), end = max(start, destination);
+        for(int i = 0;i<distance.size();i++) {
+            total+=distance[i];
+            if(i>=begin && i<end) dist+=distance[i];
+        }
+        return min(dist, total-dist);
+    }
+
+    int distanceBetweenBusStops_v01(vector<int>& distance, int start, int destination) {
         if(start == destination) return 0;
         int total = 0, dist = 0;
         for(auto &i:distance) total+=i;
