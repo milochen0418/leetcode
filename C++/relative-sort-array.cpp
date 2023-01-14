e00cfa83e7dcb1e927a871fa16c36ba2bdfb5ea9
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-14 10:50:53 +0800

    C++/relative-sort-array.cpp optimized by mapping of vector

--- C++/relative-sort-array.cpp
+++ C++/relative-sort-array.cpp
@@ -2,6 +2,18 @@ class Solution {
     //https://leetcode.com/problems/relative-sort-array
 public:
     vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
+        vector<int> mp = vector<int>(1001,-1);//mapping arr2's element to its index
+        for(int i = 0; i<arr2.size();i++) mp[arr2[i]] = i;
+        sort(arr1.begin(), arr1.end(), [&](auto &lhs, auto &rhs) {        
+            if(mp[rhs] != -1 && mp[lhs] != -1) return mp[lhs] < mp[rhs];
+            if(mp[lhs]!=-1) return true;
+            if(mp[rhs]!=-1) return false;
+            return lhs<rhs;
+        });
+        return arr1;
+    }
+
+    vector<int> relativeSortArray_v01(vector<int>& arr1, vector<int>& arr2) {
         unordered_map<int, int> mp;//mapping arr2's element to its index
         for(int i = 0; i<arr2.size();i++) mp[arr2[i]] = i;
         sort(arr1.begin(), arr1.end(), [&](auto &lhs, auto &rhs){
