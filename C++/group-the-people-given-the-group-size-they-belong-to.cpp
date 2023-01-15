20088070b2b633718bdab6cfb14579d4a3d78169
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-15 23:57:32 +0800

    C++/group-the-people-given-the-group-size-they-belong-to.cpp finished by vector

---
+++ C++/group-the-people-given-the-group-size-they-belong-to.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    //https://leetcode.com/problems/group-the-people-given-the-group-size-they-belong-to
+public:
+    vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
+        unordered_map<int,int> mp;
+        for(auto &i:groupSizes) mp[i]++;
+        
+        unordered_map<int, vector<vector<int>>> amp;
+        unordered_map<int, int> amp_idx;
+        
+        for(auto &[k,v]: mp) 
+            amp[k] = vector<vector<int>>(mp[k]/k, vector<int>(k,0));
+        
+        
+        for(int i = 0; i<groupSizes.size(); i++) {
+            int k = groupSizes[i];
+            int idx = amp_idx[k]++;
+            amp[k][idx/k][idx%k] = i;
+        }
+        
+        vector<vector<int>> ans;
+        for(auto&[k,vv]: amp)
+            for(auto &v:vv)
+                ans.push_back(v);
+        return ans;
+        
+    }
+};
