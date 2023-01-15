9ca3dad3d6b7c264ff234e68524d87fbcf1a7834
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-15 21:08:12 +0800

    C++/matrix-cells-in-distance-order.cpp finished by sorting

---
+++ C++/matrix-cells-in-distance-order.cpp
@@ -0,0 +1,16 @@
+class Solution {
+    //https://leetcode.com/problems/matrix-cells-in-distance-order
+public:
+    vector<vector<int>> allCellsDistOrder(int rows, int cols, int rCenter, int cCenter) {
+        vector<vector<int>> ans;
+        for(int r=0; r<rows; r++) 
+            for(int c=0; c<cols; c++) 
+                ans.push_back({r,c});
+        sort(ans.begin(), ans.end(), [&](auto &lhs, auto &rhs) {
+            int L = abs(lhs[0]-rCenter) + abs(lhs[1]-cCenter);
+            int R = abs(rhs[0]-rCenter) + abs(rhs[1]-cCenter);
+            return (L==R)?lhs[0]*cols+lhs[1]<rhs[0]*cols+rhs[1]:L<R;
+        });
+        return ans;
+    }
+};
