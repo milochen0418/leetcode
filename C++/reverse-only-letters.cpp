bb527a1280222ea74b723af9c47cd9a9def534ad
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-16 15:37:51 +0800

    C++/reverse-only-letters.cpp add article

--- C++/reverse-only-letters.cpp
+++ C++/reverse-only-letters.cpp
@@ -1,10 +1,10 @@
 class Solution {
     //https://leetcode.com/problems/reverse-only-letters
+    //article https://leetcode.com/problems/reverse-only-letters/discuss/3057687/C%2B%2B-One-Pass-O(N)-9-lines
 public:
     #define is_letter(c)  ( ('Z'>=(c)&&(c)>='A') || ('z'>=(c)&&(c)>='a') )
     string reverseOnlyLetters(string s) {
-        int n = s.length();
-        int L = 0, R=n-1;
+        int n = s.length(), L = 0, R=n-1;
         while(L<R) {
             while(L<n && !is_letter(s[L]))L++;
             while(R>=0 && !is_letter(s[R]))R--;
