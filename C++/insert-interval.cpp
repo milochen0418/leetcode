ead51baef4a0012f0a1dfdcb406fec3979cb2659
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-16 10:28:10 +0800

    C++/insert-interval.cpp finished O(N)

---
+++ C++/insert-interval.cpp
@@ -0,0 +1,40 @@
+class Solution {
+    //https://leetcode.com/problems/insert-interval
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        vector<vector<int>> ans;
+        int n = intervals.size();
+        if(n==0) {
+            ans.push_back(newInterval);
+            return ans;
+        }
+        int start = newInterval[0];
+        int end = newInterval[1];
+        for(int i = 0; i<n; i++) {
+            if(intervals[i][0]<=start && start<=intervals[i][1]) 
+                start = intervals[i][0];
+            if(intervals[i][0]<=end && end<=intervals[i][1]) 
+                end = intervals[i][1];
+        }
+        
+        printf("{start,end} = %d,%d\n",start,end);
+        int i = 0;
+        for(i=0;i<n;i++) {
+            vector<int> & inter =intervals[i];
+            if(inter[1]<start) 
+                ans.push_back(inter);
+            else 
+                break;
+        }
+        ans.push_back({start,end});
+        for(;i<n;i++) {
+            vector<int> & inter =intervals[i];
+            if(inter[0]>end) {
+                ans.push_back(inter);
+            } 
+        }
+        
+        
+        return ans;
+    }
+};
