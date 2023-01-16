55da1ad87a2512d425681c7cf43e3ad84d4fe053
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-16 12:45:29 +0800

    C++/partition-array-into-three-parts-with-equal-sum.cpp finished by prefix sum

--- C++/partition-array-into-three-parts-with-equal-sum.cpp
+++ C++/partition-array-into-three-parts-with-equal-sum.cpp
@@ -4,6 +4,22 @@ public:
         vector<int>& A = arr;
         int n = A.size();
         for(int i = 1;i<n;i++) A[i]+=A[i-1];
+        
+        int sum = A[n-1];
+        if(sum%3!=0) return false;
+        sum/=3;
+        int L,R;
+        for(L=1;L<n-1;L++) if(A[L-1]==sum) break;
+        if(L==n-1) return false;
+        for(R=n-2; R>=L;R--) if(A[n-1]-A[R] == sum) break;
+        if(R<L) return false;
+        return true;
+    }
+    
+    bool canThreePartsEqualSum_v01(vector<int>& arr) {
+        vector<int>& A = arr;
+        int n = A.size();
+        for(int i = 1;i<n;i++) A[i]+=A[i-1];
         //presum of [0..L] = A[L]
         //presum of [i..j] = A[j]-A[i-1];  
         //presum of [R..n-1] =A[n-1]-A[R];
