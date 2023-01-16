78a1e6659cec0bb17a0c83cec20a019bcdf48a8d
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-16 10:58:57 +0800

    C++/insert-interval.cpp add article

--- C++/insert-interval.cpp
+++ C++/insert-interval.cpp
@@ -1,13 +1,10 @@
 class Solution {
     //https://leetcode.com/problems/insert-interval
+    //article https://leetcode.com/problems/insert-interval/discuss/3056773/C%2B%2B-O(N)
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
         vector<vector<int>> ans;
         int n = intervals.size();
-        if(n==0) {
-            ans.push_back(newInterval);
-            return ans;
-        }
         int start = newInterval[0];
         int end = newInterval[1];
         for(int i = 0; i<n; i++) {
