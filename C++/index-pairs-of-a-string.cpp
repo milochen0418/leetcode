45666d3d9250e3c4b7a0136963a61b4b8621e16b
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-16 20:04:59 +0800

    C++/index-pairs-of-a-string.cpp finished by find all substring

---
+++ C++/index-pairs-of-a-string.cpp
@@ -0,0 +1,19 @@
+class Solution {
+    //https://leetcode.com/problems/index-pairs-of-a-string
+public:
+    vector<vector<int>> indexPairs(string text, vector<string>& words) {
+        vector<vector<int>> ans;
+        for(auto &w:words) {
+            //find all substring in string text that equal to w
+            int index =-1;
+            while ((index = text.find(w, index+1)) != std::string::npos) {
+                ans.push_back({index,index+(int)w.length()-1});
+            }            
+        }
+        sort(ans.begin(), ans.end(), [](auto &lhs, auto &rhs) {
+            return lhs[0]==rhs[0]?lhs[1]<rhs[1]:lhs[0]<rhs[0];
+        });
+        return ans;
+        
+    }
+};
