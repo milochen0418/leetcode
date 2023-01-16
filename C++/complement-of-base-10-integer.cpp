ad758dae836ff2c7ba4a84ee10a65a2c103dabe7
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-16 14:12:53 +0800

    C++/complement-of-base-10-integer.cpp add article

--- C++/complement-of-base-10-integer.cpp
+++ C++/complement-of-base-10-integer.cpp
@@ -1,5 +1,6 @@
 class Solution {
     //https://leetcode.com/problems/complement-of-base-10-integer
+    //article https://leetcode.com/problems/complement-of-base-10-integer/discuss/3057347/C%2B%2B-One-Pass-O(logN)-8-lines
 public:
     int bitwiseComplement(int n) {
         int ans=0, base = 1;
