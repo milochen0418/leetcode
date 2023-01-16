2ac94d8f92cce50885ba91c37270333e72314faa
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-16 13:39:38 +0800

    C++/find-the-index-of-the-large-integer.cpp finished by binary search

---
+++ C++/find-the-index-of-the-large-integer.cpp
@@ -0,0 +1,48 @@
+/**
+ * // This is the ArrayReader's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * class ArrayReader {
+ *   public:
+ *     // Compares the sum of arr[l..r] with the sum of arr[x..y] 
+ *     // return 1 if sum(arr[l..r]) > sum(arr[x..y])
+ *     // return 0 if sum(arr[l..r]) == sum(arr[x..y])
+ *     // return -1 if sum(arr[l..r]) < sum(arr[x..y])
+ *     int compareSub(int l, int r, int x, int y);
+ *
+ *     // Returns the length of the array
+ *     int length();
+ * };
+ */
+
+class Solution {
+    //https://leetcode.com/problems/find-the-index-of-the-large-integer
+public:
+    int getIndex(ArrayReader &reader) {
+        int n = reader.length();
+        int L = 0, R=n-1;
+        while(L<=R) {
+            n=R-L+1;
+            if(L==R) return L;
+            if(n%2==1) {
+                int M = L+(R-L)/2;
+                //printf("n%2==1, L,M,R=%d,%d,%d compare[%d..%d][%d..%d]\n",L,M,R, L,M-1,M+1,R);
+                int ret = reader.compareSub(L,M-1,M+1,R);
+                if(ret == 0) return M;
+                if(ret>0)
+                    R=M-1;
+                else 
+                    L=M+1;
+            } else { //case n%2==0
+                int M = L+(R-L)/2;//L=0,R=3->M=1
+                //printf("n%2==0, L,M,R=%d,%d,%d compare[%d..%d][%d..%d]\n",L,M,R, L,M,M+1,R);
+                int ret = reader.compareSub(L,M,M+1,R);
+                if(ret>0) 
+                    R=M;
+                else 
+                    L=M+1;
+            }
+        }
+        return -1;
+            
+    }
+};
