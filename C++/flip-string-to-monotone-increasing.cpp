a8fbf9e35fcc3bada3a6ba847eb951689c203929
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-17 13:14:20 +0800

    C++/flip-string-to-monotone-increasing.cpp refactor

--- C++/flip-string-to-monotone-increasing.cpp
+++ C++/flip-string-to-monotone-increasing.cpp
@@ -2,6 +2,30 @@ class Solution {
     //https://leetcode.com/problems/flip-string-to-monotone-increasing/
 public:
     int minFlipsMonoIncr(string s) {
+        int v = 0, n = s.length(), ans = INT_MAX;
+        for(auto &c:s) v+=c=='0';
+        ans = min(ans, v);
+        for(int i = 0;i<n; i++) {
+            v+=2*(s[i]=='1')-1;
+            ans = min(ans, v);
+        }
+        return ans;
+    }
+    
+    int minFlipsMonoIncr_v02(string s) {
+        int v = 0, n = s.length(), ans = INT_MAX;
+        for(auto &c:s) v+=c=='0';
+        ans = min(ans, v);
+        for(int i = 0;i<n; i++) {
+            if(s[i]=='0')
+                v--;
+            else
+                v++;
+            ans = min(ans, v);
+        }
+        return ans;
+    }
+    int minFlipsMonoIncr_v01(string s) {
         int L_ocnt = 0, R_zcnt = 0, n = s.length();//left one count, right zero count,        
         int ans = INT_MAX;//find min value min {left ocnt + right zcnt}
         for(auto &c:s) R_zcnt+=c=='0';
