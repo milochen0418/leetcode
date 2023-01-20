aab282d76bb1cbb720ddd4ee184dec7a928fb50b
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-20 15:38:51 +0800

    C++/count-good-nodes-in-binary-tree.cpp finished by DFS with stack

---
+++ C++/count-good-nodes-in-binary-tree.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    //https://leetcode.com/problems/count-good-nodes-in-binary-tree
+public:
+    stack<int> stk;
+    int ans = 0;
+    int goodNodes(TreeNode* root) {
+        dfs(root);
+        return ans;
+    }
+    void dfs(TreeNode* root) {
+        if(!root) return;
+        int need_push = 0;
+        if(stk.empty() || root->val >= stk.top()) {
+            ans++;
+            need_push = 1;
+        }
+        if(need_push) stk.push(root->val);
+        dfs(root->left);
+        dfs(root->right);
+        if(need_push) stk.pop();
+    }
+};
