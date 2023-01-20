7ec26c220e8f65b5e7f561f29d4f49407dbec4c2
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-20 15:01:24 +0800

    C++/strictly-palindromic-number.cpp finished by math

---
+++ C++/strictly-palindromic-number.cpp
@@ -0,0 +1,8 @@
+class Solution {
+    //https://leetcode.com/problems/strictly-palindromic-number/
+public:
+    bool isStrictlyPalindromic(int n) {
+        //The number n in base (n - 2) is always 12, which is not palindromic.
+        return false;
+    }
+};
