55078e1ce9bef2a1acf495271f7f2192c91d5bc4
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-21 02:53:53 +0800

    C++/similar-rgb-color.cpp enhance table

--- C++/similar-rgb-color.cpp
+++ C++/similar-rgb-color.cpp
@@ -4,9 +4,33 @@ public:
     
     
     vector<int>hh = {0,17,34,51,68,85,102,119,136,153,170,187,204,221,238,255};
-       string hhc = "0123456789abcdef";
-    //{"00","11","22","33","44","55","66","77","88","99","aa","bb","cc","dd","ee","ff"};
+    string hhc = "0123456789abcdef";
+    
+    string to_hhc="000000000111111111111111112222222222222222233333333333333333444444444444444445555555555555555566666666666666666777777777777777778888888888888888899999999999999999aaaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbcccccccccccccccccdddddddddddddddddeeeeeeeeeeeeeeeeefffffffff";
+    
     string similarRGB(string color) {
+        /*
+        //Genearte to_hhc
+        vector<int> to_hh = vector<int>(256,0);
+        string to_hhc = "";
+        for(int i = 0;i<256;i++) {
+            int v = i;
+            int idx=0, dist = abs(hh[0]-v);
+            for(int i = 1;i<hh.size();i++) {
+                if(abs(hh[i]-v)<dist) {
+                    dist = abs(hh[i]-v);
+                    idx = i;
+                }
+            }
+            to_hh[i] = hh[idx];
+            to_hhc.push_back(hhc[idx]);
+        }
+        //for(int i = 0;i<256;i++)printf("%d,",to_hh[i]);
+        //printf("\n");
+        cout<<to_hhc;
+        */
+        
+        /*
         char r = find_closest_hh(color[1],color[2]);
         char g = find_closest_hh(color[3],color[4]);
         char b = find_closest_hh(color[5],color[6]);
@@ -14,30 +38,20 @@ public:
         ans[1]=ans[2]=r;
         ans[3]=ans[4]=g;
         ans[5]=ans[6]=b;
-        
+        */
+        string ans = "#000000";
+        char r = to_hhc[getVfromHH(color[1],color[2])];
+        char g = to_hhc[getVfromHH(color[3],color[4])];
+        char b = to_hhc[getVfromHH(color[5],color[6])];
+        ans[1]=ans[2]=r;
+        ans[3]=ans[4]=g;
+        ans[5]=ans[6]=b;
         
         return ans;
         
-        /*
-        //Generate mp by the following code
-        vector<int> mp = vector<int>(256,0);
-        for(int i =0;i<=9;i++) mp[i+'0']=i;
-        for(int i='a';i<='f';i++) mp[i]=i-'a'+10;
-        for(int i='A';i<='F';i++) mp[i]=i-'A'+10;
-        for(int i=0;i<256;i++) printf("%d,",mp[i]);
-        */
-        
-        /*
-        //Generate hh = {0,17,34,51,68,85,102,119,136,0,170,187,204,221,238,255};
-        string a = "0123456789abcdef";
-        printf("\ngenerate hh = ");
-        for(int i = 0;i<a.length();i++) printf("%d,",getVfromHH(a[i],a[i]));
-        */
-        return "";   
     }
     char find_closest_hh (char b, char s) {
         int v = getVfromHH(b,s);
-        //printf("v = %d, ", v);
         int idx=0, dist = abs(hh[0]-v);
         for(int i = 1;i<hh.size();i++) {
             if(abs(hh[i]-v)<dist) {
@@ -45,7 +59,6 @@ public:
                 idx = i;
             }
         }
-        //printf("dist = %d, idx=%d\n",dist,idx);
         return hhc[idx];
     } 
     
