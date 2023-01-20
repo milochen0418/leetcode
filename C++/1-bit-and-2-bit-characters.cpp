19922ec2a1c6c81e877fc29b6f3195737552a028
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-21 00:40:00 +0800

    C++/1-bit-and-2-bit-characters.cpp add article

--- C++/1-bit-and-2-bit-characters.cpp
+++ C++/1-bit-and-2-bit-characters.cpp
@@ -1,5 +1,6 @@
 class Solution {
     //https://leetcode.com/problems/1-bit-and-2-bit-characters
+    //article https://leetcode.com/problems/1-bit-and-2-bit-characters/discuss/3077937/C%2B%2B-One-Pass-O(N)-8-lines
 public:
     bool isOneBitCharacter(vector<int>& bits) {
         int n = bits.size();
