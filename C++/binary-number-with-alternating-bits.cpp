351d984a4385bbe73e57f96f681cc7aceea07250
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-21 19:42:22 +0800

    C++/binary-number-with-alternating-bits.cpp opzimied by Bit Manipulation O(1)

--- C++/binary-number-with-alternating-bits.cpp
+++ C++/binary-number-with-alternating-bits.cpp
@@ -2,6 +2,11 @@ class Solution {
     //https://leetcode.com/problems/binary-number-with-alternating-bits
 public:
     bool hasAlternatingBits(int n) {
+        //O(1)
+        return ((n/4 & n)== n/4 && (n/2&n)==0);
+    }
+    bool hasAlternatingBits_(int n) {
+        //O(Log(N))
         int prev = -1;
         while(n>0) {
             if(n%2==prev) {
