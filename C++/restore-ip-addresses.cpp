60399b873c91c7e4675547133db856d8b75bcd83
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-21 11:24:52 +0800

    C++/restore-ip-addresses.cpp refactor

--- C++/restore-ip-addresses.cpp
+++ C++/restore-ip-addresses.cpp
@@ -1,36 +1,35 @@
 class Solution {
     //https://leetcode.com/problems/restore-ip-addresses
 public:
-    stack<int> stk;//most 3 element that mean the index of first digit after dot
-    string s;
-    vector<int> octets;
-    vector<string> ans;
-    vector<string> restoreIpAddresses(string ss) {
-        s = ss;       
+    vector<string> restoreIpAddresses(string s) {
+        stack<int> stk;//most 3 element that mean the index of first digit after dot
+        vector<int> octets;
+        vector<string> ans;
+        int n = s.length();
+        
+        function<void()> sol = [&](){
+            int prev_idx=stk.empty()?0:stk.top();
+            for(int len = 1;len<=3 && prev_idx+len-1<n;len++) {
+                if(stk.size()==3 && prev_idx+len-1!=n-1) continue;
+                string octetstr = s.substr(prev_idx,len);
+                if(legal_octet_str(octetstr)) {
+                    stk.push(prev_idx+len);
+                    octets.push_back(stoi(octetstr));
+                    if(octets.size()==4) {
+                        vector<int>& ip = octets;
+                        ans.push_back(string_format("%d.%d.%d.%d",ip[0],ip[1],ip[2],ip[3]));
+                    } else {
+                        sol();
+                    }
+                    octets.pop_back();
+                    stk.pop();                
+                }
+            }
+        };
         sol();
         return ans;
     }
-    void sol() {
-        int n = s.length();
-        int prev_idx=stk.empty()?0:stk.top();
-        for(int len = 1;len<=3 && prev_idx+len-1<n;len++) {
-            if(stk.size()==3 && prev_idx+len-1!=n-1) continue;
-            string octetstr = s.substr(prev_idx,len);
-            if(legal_octet_str(octetstr)) {
-                stk.push(prev_idx+len);
-                octets.push_back(stoi(octetstr));
-                if(octets.size()==4) {
-                    vector<int>& ip = octets;
-                    ans.push_back(string_format("%d.%d.%d.%d",ip[0],ip[1],ip[2],ip[3]));
-                } else {
-                    sol();
-                }
-                octets.pop_back();
-                stk.pop();                
-            }
-        }
-        return;
-    }
+    
     int legal_octet_str(string str) {
         int n = str.length();
         if(n==0) return 0;
