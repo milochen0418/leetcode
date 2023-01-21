c82b09652ddbbb3d4f3e0b6690b78ee501c91482
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-21 21:41:39 +0800

    C++/binary-gap.cpp add article

--- C++/binary-gap.cpp
+++ C++/binary-gap.cpp
@@ -1,5 +1,6 @@
 class Solution {
     //https://leetcode.com/problems/binary-gap
+    //article https://leetcode.com/problems/binary-gap/discuss/3081503/C%2B%2B-One-Pass-O(logN)-6-lines
 public:
     int binaryGap(int n) {
         int L =-1,R=-1,cnt=0, ans=0;
