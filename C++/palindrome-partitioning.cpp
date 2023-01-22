a258ca82e0f026c0f9d0b5b06991801f8afb54cb
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-22 23:29:06 +0800

    C++/palindrome-partitioning.cpp show all partition

---
+++ C++/palindrome-partitioning.cpp
@@ -0,0 +1,30 @@
+class Solution {
+    //https://leetcode.com/problems/palindrome-partitioning
+public:
+    vector<vector<string>> partition(string s) {
+        int n = s.length();//there will be number of n-1 , max(n)=16. max(n-1)=15
+        if(n==1) return vector<vector<string>>(1,vector<string>(1,s));
+        int m = 1<<(n-1);
+        vector<vector<string>> ans;
+        for(int i = 0; i<m;i++) {
+            vector<string> vs;
+            string e="";
+            for(int j = 0;j<n-1;j++) {
+                e.push_back(s[j]);
+                if((i&1<<j)!=0) {
+                    vs.push_back(e);
+                    e="";
+                }
+            }
+            e.push_back(s[n-1]);
+            vs.push_back(e);
+            ans.push_back(vs);
+        }
+        return ans;
+    }
+    int is_palindrome(string &s) {
+        int L=0,R=s.length()-1;
+        while(L<R) if(s[L++]!=s[R--])return 0;
+        return 1;
+    }
+};
