55598e07ffee53916254ed1a1242c1ae0fa3659e
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-22 22:58:22 +0800

    C++/number-of-segments-in-a-string.cpp finished by One Pass O(N) 6 lines

--- C++/number-of-segments-in-a-string.cpp
+++ C++/number-of-segments-in-a-string.cpp
@@ -2,6 +2,15 @@ class Solution {
     //https://leetcode.com/problems/number-of-segments-in-a-string
 public:
     int countSegments(string s) {
+        int ans = 0, state = 0;//0:unknown, 1:space, -1:non-space
+        for(auto &c:s) {
+            if(state!=-1 && c!=' ') ans++, state = -1;
+            if(state==-1 && c==' ') state = 1;
+        }
+        return ans;
+    }
+
+    int countSegment_v01(string s) {
         int ans = 0;
         int state = 0;//0:unknown, 1:space, -1:non-space
         
