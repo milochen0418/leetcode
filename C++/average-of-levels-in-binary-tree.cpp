5077a7a4bd1507f0d2ed7b07b58f1f907b3826ef
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-22 10:01:17 +0800

    C++/average-of-levels-in-binary-tree.cpp finished by DFS

---
+++ C++/average-of-levels-in-binary-tree.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    //https://leetcode.com/problems/average-of-levels-in-binary-tree
+public:
+    vector<double> averageOfLevels(TreeNode* root) {
+        typedef long long ll;
+        vector<vector<ll>> v; 
+        function<void(TreeNode*, int)>dfs = [&](TreeNode* root, int level){
+            if(!root) return;
+            while(level>=v.size()) v.push_back(vector<ll>());
+            v[level].push_back(root->val);
+            dfs(root->left, level+1);
+            dfs(root->right, level+1);
+        };
+        dfs(root, 0);
+        
+        vector<double> ans = vector<double>(v.size(),0);
+        for(int i = 0;i<v.size();i++) {
+            ll sum = 0;
+            for(auto &l:v[i]) sum+=l;
+            double avg = (double)sum/(double)(v[i].size());
+            ans[i] = avg;
+        }
+        return ans;
+    }
+};
