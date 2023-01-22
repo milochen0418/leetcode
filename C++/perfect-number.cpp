89838ac50465514651718cdcb5e5dccd3c543a20
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-22 22:19:15 +0800

    C++/perfect-number.cpp imrpove to O(sqrt(N))

--- C++/perfect-number.cpp
+++ C++/perfect-number.cpp
@@ -2,6 +2,19 @@ class Solution {
     //https://leetcode.com/problems/perfect-number
 public:
     bool checkPerfectNumber(int num) {
+        //O(sqrt(N))
+        int sum = 0;
+        for(int i =1; i*i<=num;i++) 
+            if(num%i==0) {
+                sum+=i;
+                if(i*i!=num) {
+                    sum+=num/i;
+                }
+            }
+        return sum-num==num;
+    }    
+    bool checkPerfectNumber_v02(int num) {
+        //O(N)
         int sum = 0;
         for(int i =1; i<=num/2;i++) 
             if(num%i==0) sum+=i;
