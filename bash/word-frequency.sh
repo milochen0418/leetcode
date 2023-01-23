eafbc992d4cd9a6cd5346470ec2f74f8f6abc087
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-23 23:06:45 +0800

    bash/word-frequency.sh finished

---
+++ bash/word-frequency.sh
@@ -0,0 +1,6 @@
+# Read from the file words.txt and output the word frequency list to stdout.
+# for i in $(cat words.txt); do echo $i; done
+# tr ' ' '\n' < words.txt | sort | uniq -c | awk '{print $2, $1}'
+# for i in $(cat words.txt); do echo $i; done | tr ' ' '\n' | sort | uniq -c | awk '{print $2, $1}'
+# The sort order in above is wrong
+for i in $(cat words.txt); do echo $i; done | tr ' ' '\n' | sort | uniq -c | sort -rn | awk '{print $2, $1}'
