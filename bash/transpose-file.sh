da59397ae07ca91206ea03873698878bd3879346
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-23 23:34:03 +0800

    bash/transpose-file.sh finished by awk

---
+++ bash/transpose-file.sh
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/transpose-file
+# Read from the file file.txt and print its transposed content to stdout.
+awk \
+'{
+    for(i=1; i<=NF; ++i) {
+        if (word[i] == "") {
+            word[i] = $i
+        } else {
+            word[i] = word[i]" "$i
+        }
+    }
+} END {
+    for(i=1; i<=NF; ++i) {
+        print word[i]
+    }
+}' \
+file.txt
+    
