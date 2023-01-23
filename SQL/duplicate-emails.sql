384dc61a280e0a3f345d768d24387708cfd9dc94
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-24 00:56:16 +0800

    .sql add correct format of comment

--- SQL/duplicate-emails.sql
+++ SQL/duplicate-emails.sql
@@ -1,3 +1,5 @@
+/*
 # Write your MySQL query statement below
 # https://leetcode.com/problems/duplicate-emails
+*/
 SELECT DISTINCT a.email AS Email FROM Person a LEFT JOIN Person b ON a.id <> b.id WHERE a.email = b.email;
