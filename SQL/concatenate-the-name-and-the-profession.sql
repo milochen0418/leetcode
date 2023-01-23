a222424272ef14331d8bac533af6e5f63f3abdf1
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-24 01:05:41 +0800

    SQL/concatenate-the-name-and-the-profession.sql finished by CONCAT and SUBSTR

---
+++ SQL/concatenate-the-name-and-the-profession.sql
@@ -0,0 +1,5 @@
+/*
+# https://leetcode.com/problems/concatenate-the-name-and-the-profession
+# Write your MySQL query statement below
+*/
+select person_id, CONCAT(name,'(',SUBSTR(profession,1,1),')') as name from Person order by person_id DESC
