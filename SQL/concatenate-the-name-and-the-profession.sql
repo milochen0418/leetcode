e0679299b30d443389c4d75d13dec0b6b2a1bef2
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-24 01:07:49 +0800

    SQL/concatenate-the-name-and-the-profession.sql change to lowercase

--- SQL/concatenate-the-name-and-the-profession.sql
+++ SQL/concatenate-the-name-and-the-profession.sql
@@ -2,4 +2,4 @@
 # https://leetcode.com/problems/concatenate-the-name-and-the-profession
 # Write your MySQL query statement below
 */
-select person_id, CONCAT(name,'(',SUBSTR(profession,1,1),')') as name from Person order by person_id DESC
+select person_id, concat(name,'(',substr(profession,1,1),')') as name from Person order by person_id desc
