63d653511b2a039c7f4ebdf9c07620001c5cb921
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-24 17:14:56 +0800

    C++/jump-game-ii.cpp add and rewrite in lambda solution

--- C++/jump-game-ii.cpp
+++ C++/jump-game-ii.cpp
@@ -1,9 +1,26 @@
 class Solution {
     //https://leetcode.com/problems/jump-game-ii/
 public:
+    int jump(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp = vector<int>(n,-1);
+        function<int(int)> sol = [&](int i){
+            if(i>=n) return 0;
+            if(dp[i]!=-1) return dp[i];
+            if(i==n-1) return dp[i] = 0;
+            int limit = min(n-1, i+nums[i]);
+            int minv = INT_MAX/2;
+            for(int j = i+1;j<=limit;j++) {
+                minv = min(minv, sol(j));
+            }
+            return dp[i]=minv+1;
+        };
+        return sol(0);
+    }
+
     vector<int> a;
     vector<int> dp;
-    int jump(vector<int>& nums) {
+    int jump_v01(vector<int>& nums) {
         a = nums;
         int n = a.size();
         dp = vector<int>(n,-1);
