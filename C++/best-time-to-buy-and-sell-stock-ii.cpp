60f86cbf3bca2af210f00548dfda8207052787c3
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-24 22:07:05 +0800

    C++/best-time-to-buy-and-sell-stock-ii.cpp redo and rewrite

--- C++/best-time-to-buy-and-sell-stock-ii.cpp
+++ C++/best-time-to-buy-and-sell-stock-ii.cpp
@@ -2,6 +2,16 @@ class Solution {
     //https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii
 public:
     int maxProfit(vector<int>& prices) {
+        //Idea, just sell it in every rise edge
+        int ans = 0, n = prices.size();
+        for(int i =1;i<n;i++) {
+            int v = prices[i] - prices[i-1];
+            if(v>0) ans+=v;
+        }
+        return ans;
+    }
+
+    int maxProfit_v01(vector<int>& prices) {
         vector<int>& a = prices;
         int n = a.size(), ans=0;
         for(int i = 1; i<n;i++) if(a[i]>a[i-1])ans+=a[i]-a[i-1];
