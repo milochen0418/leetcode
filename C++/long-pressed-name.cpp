5e9fc4e4724bb20e5cdeadce24baefd79604178a
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-25 18:55:08 +0800

    C++/long-pressed-name.cpp finished by loop

---
+++ C++/long-pressed-name.cpp
@@ -0,0 +1,31 @@
+class Solution {
+    //https://leetcode.com/problems/long-pressed-name
+public:
+    bool isLongPressedName(string name, string typed) {
+        int n = name.length(), m=typed.length();
+        char c = 0;
+        int cnt = 0;
+        int j = 0;
+        for(int i = 0; i<n;i++) {
+            if(c!=name[i]) {
+                c=name[i];
+                cnt=1;
+            } else {
+                cnt++;
+            }
+            
+            if(i+1>=n||c != name[i+1] ) {
+                //printf("i=%d, cnt=%d ... start j=%d\n",i,cnt,j);
+                int t = cnt;
+                while(j<m && c==typed[j]){
+                    t--;
+                    j++;
+                }
+                if(t>0) return false;
+            }
+        }
+        if(j<m) return false;
+        //printf("start j=%d\n",j);
+        return true;
+    }
+};
