602aa73ae9bcf8776db79c447586d12fc02e9bc6
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-26 01:32:07 +0800

    SQL/the-latest-login-in-2020.sql using where with group by

---
+++ SQL/the-latest-login-in-2020.sql
@@ -0,0 +1,6 @@
+/*
+ # https://leetcode.com/problems/the-latest-login-in-2020
+ # Write your MySQL query statement below
+*/
+select user_id, max(time_stamp) as last_stamp from Logins 
+where year(time_stamp)=2020 group by user_id 
