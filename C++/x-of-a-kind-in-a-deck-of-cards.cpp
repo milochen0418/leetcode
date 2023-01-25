d7948c90b19b26e85c181861da58f4994e74e1c6
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-25 19:11:19 +0800

    C++/x-of-a-kind-in-a-deck-of-cards.cpp add complexity description  in comment

--- C++/x-of-a-kind-in-a-deck-of-cards.cpp
+++ C++/x-of-a-kind-in-a-deck-of-cards.cpp
@@ -2,6 +2,9 @@ class Solution {
     //https://leetcode.com/problems/x-of-a-kind-in-a-deck-of-cards/
 public:
     bool hasGroupsSizeX(vector<int>& deck) {
+        //Because Time complexity for GCD(a,b) is O(log^2(min(a,b)) )
+        //So the time complexity here is O(Nlog^2(N))
+        //space compelxity is O(N)
         unordered_map<int,int> cnt;
         for(auto &i:deck) cnt[i]++;
         int common = 0;
