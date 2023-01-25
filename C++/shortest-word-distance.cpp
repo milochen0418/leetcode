bcc3a8bdd3cfcdcc92a2d5c30768d897f57cecc8
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-26 01:08:57 +0800

    C++/shortest-word-distance.cpp finsihed by vector

---
+++ C++/shortest-word-distance.cpp
@@ -0,0 +1,22 @@
+class Solution {
+    //https://leetcode.com/problems/shortest-word-distance
+public:
+    int shortestDistance(vector<string>& wordsDict, string word1, string word2) {
+        vector<pair<string,int>> vs;
+        for(int i = 0; i<wordsDict.size();i++) {
+            string &w = wordsDict[i];
+            if(w == word1 || w == word2) vs.push_back({w,i});
+        }
+        int ans = INT_MAX;
+        int j1=-1,j2=-1;//j1 chase the index of word1, j2 chase the index of word2
+        for(int i = 0; i<vs.size();i++) {
+            if(vs[i].first==word1) {
+                j1=vs[i].second;
+            } else {
+                j2=vs[i].second;
+            }
+            if(j1>=0 && j2>=0) ans = min(ans, abs(j2-j1));
+        }
+        return ans;
+    }
+};
