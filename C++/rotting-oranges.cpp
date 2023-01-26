928886eec8feb7cc3b3a1bed3ed477f3c446fff9
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-27 00:35:06 +0800

    C++/rotting-oranges.cpp optimized space complexity to O(K), K maximum queue size using by BFS

--- C++/rotting-oranges.cpp
+++ C++/rotting-oranges.cpp
@@ -2,6 +2,45 @@ class Solution {
     //https://leetcode.com/problems/rotting-oranges/
 public:
     int orangesRotting(vector<vector<int>>& grid) {
+        int fresh_oranges_cnt = 0;
+        int m = grid.size(), n = grid[0].size();
+        queue<vector<int>> currQ, nextQ;
+        for(int i = 0; i<m;i++) { //O(N*M)
+            for(int j = 0;j<n;j++) {
+                if(grid[i][j] == 2) 
+                    currQ.push({i,j});
+                else if(grid[i][j] == 1)
+                    fresh_oranges_cnt++;
+            }
+        }
+        if(fresh_oranges_cnt==0) return 0;
+        int ans = -1, time = 0;//
+        vector<vector<int>> dirs={ {1,0}, {-1,0}, {0,1}, {0,-1}};
+        while(!currQ.empty()) { //O(N*M)
+            vector<int>e = currQ.front();
+            currQ.pop();
+            if(grid[e[0]][e[1]] == 1) fresh_oranges_cnt--;
+            if(grid[e[0]][e[1]]>0) 
+                grid[e[0]][e[1]] = -1*time;
+            else 
+                grid[e[0]][e[1]] = max(grid[e[0]][e[1]], -1*time);            
+            if(grid[e[0]][e[1]]<0)ans = min(ans, grid[e[0]][e[1]]);
+            
+            for(auto &d:dirs) {
+                int r = e[0]+d[0], c = e[1]+d[1];
+                if(r<0 || r>=m || c<0 || c>=n || grid[r][c]<=0) continue;
+                nextQ.push({r,c});
+            }
+            if(currQ.empty()) {
+                time++;
+                swap(currQ,nextQ);
+            }
+        }
+        if(fresh_oranges_cnt>0) return -1;
+        return ans*-1;
+    }
+    
+    int orangesRotting_v01(vector<vector<int>>& grid) {
         vector<vector<int>> rotten_time;
         int fresh_oranges_cnt = 0;
         int m = grid.size(), n = grid[0].size();
@@ -16,14 +55,13 @@ public:
             }
         }
         if(fresh_oranges_cnt==0) return 0;
-        int ans = -1;
+        int ans = -1, time = 0;
         vector<vector<int>> dirs={ {1,0}, {-1,0}, {0,1}, {0,-1}};
-        int time = 0;
         while(!currQ.empty()) { //O(N*M)
             vector<int>e = currQ.front();
             rotten_time[e[0]][e[1]] = min(time,rotten_time[e[0]][e[1]]) ;
             ans = max(ans, rotten_time[e[0]][e[1]]);
-            if(grid[e[0]][e[1]] == 1)fresh_oranges_cnt--;
+            if(grid[e[0]][e[1]] == 1) fresh_oranges_cnt--;
             grid[e[0]][e[1]] = 0;
             currQ.pop();
             for(auto &d:dirs) {
