c46246ca6db3063418345f3e7b0f30b4c29930c2
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-27 01:51:21 +0800

    C++/construct-string-from-binary-tree.cpp finished by preorder DFS

---
+++ C++/construct-string-from-binary-tree.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    //https://leetcode.com/problems/construct-string-from-binary-tree/
+public:
+    string tree2str(TreeNode* root) {
+        string ans;
+        function<void(TreeNode*)> dfs = [&](TreeNode*root){
+            if(!root) return ;
+            ans.append(to_string(root->val));
+            if(!root->left && !root->right) return;
+            ans.push_back('(');
+            dfs(root->left);
+            ans.push_back(')');
+            
+            if(root->right) {
+                ans.push_back('(');
+                dfs(root->right);
+                ans.push_back(')');
+            }            
+        };
+        dfs(root);
+        return ans;
+    }
+};
