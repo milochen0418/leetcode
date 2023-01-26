0d9c0273589c21766af49fa05d008bac0f9a7a84
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-27 00:51:33 +0800

    C++/rotting-oranges.cpp refactor

--- C++/rotting-oranges.cpp
+++ C++/rotting-oranges.cpp
@@ -5,26 +5,21 @@ public:
         int fresh_oranges_cnt = 0;
         int m = grid.size(), n = grid[0].size();
         queue<vector<int>> currQ, nextQ;
-        for(int i = 0; i<m;i++) { //O(N*M)
-            for(int j = 0;j<n;j++) {
+        for(int i = 0; i<m;i++)  //O(N*M)
+            for(int j = 0;j<n;j++) 
                 if(grid[i][j] == 2) 
                     currQ.push({i,j});
                 else if(grid[i][j] == 1)
                     fresh_oranges_cnt++;
-            }
-        }
         if(fresh_oranges_cnt==0) return 0;
-        int ans = -1, time = 0;//
+        int ans = -1, time = 0;
         vector<vector<int>> dirs={ {1,0}, {-1,0}, {0,1}, {0,-1}};
         while(!currQ.empty()) { //O(N*M)
             vector<int>rc = currQ.front();
             currQ.pop();
             int &e = grid[rc[0]][rc[1]];
             if(e == 1) fresh_oranges_cnt--;
-            if(e>0) 
-                e = -1*time;
-            else 
-                e = max(e, -1*time);            
+            e = e>0?-1*time: max(e, -1*time);
             if(e<0)ans = min(ans,e);
             
             for(auto &d:dirs) {
@@ -40,7 +35,7 @@ public:
         if(fresh_oranges_cnt>0) return -1;
         return ans*-1;
     }
-
+    
     int orangesRotting_v01(vector<vector<int>>& grid) {
         vector<vector<int>> rotten_time;
         int fresh_oranges_cnt = 0;
