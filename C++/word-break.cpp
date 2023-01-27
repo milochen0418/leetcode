c6a4ca724afa4d7ff51b471e2ed90c29f1887e32
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-28 01:17:21 +0800

    C++/word-break.cpp optimized by Trie

--- C++/word-break.cpp
+++ C++/word-break.cpp
@@ -2,6 +2,54 @@ class Solution {
     //https://leetcode.com/problems/word-break
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
+        //Implement by Trie
+        struct TNode {
+            TNode(int _match=false) {
+                match = _match;
+                children = vector<TNode*> (26,nullptr);
+            }
+            ~TNode() {
+                for(auto &child:children)
+                    if(child!=nullptr)
+                        delete child;
+            }
+            vector<TNode*> children;
+            int match;
+        };
+        TNode* root = new TNode();// Trie root node
+        function<void(string&)> T_insert = [&root](string &s) { 
+            TNode* node = root;
+            for(auto &c:s) {
+                if(node->children[c-'a']==nullptr)
+                    node->children[c-'a'] = new TNode();
+                node = node->children[c-'a'];
+            }
+            node->match = true;   
+        };
+        
+        for(auto &w: wordDict) T_insert(w);//Trie Insert
+
+        int n = s.length();
+        vector<int> dp = vector<int>(n,-1);
+        function<int(int)> sol = [&](int i) {
+            if(i>=n) return 1;
+            if(dp[i]!=-1)return dp[i];
+            TNode* node = root;
+            dp[i]=0;
+            for(int j=i;j<n;j++) {
+                char c = s[j];
+                if(node->children[c-'a'] == nullptr) break;
+                node = node->children[c-'a'];
+                if(node->match) dp[i] = dp[i] | sol(j+1);
+                if(dp[j]>0) return dp[j];
+            }
+            return dp[i];
+        };
+        return sol(0);
+    }
+
+
+    bool wordBreak_v01(string s, vector<string>& wordDict) {
         unordered_set<string> words;
         int n = s.length();
         vector<vector<int>>dp (n+1, vector<int>(n+1, -1));
@@ -11,7 +59,7 @@ public:
     
     }
     
-    int wordbreak( const string& s, unordered_set<string>& words, vector<vector<int>>&dp,int i ,int j) {
+    int wordbreak_v02( const string& s, unordered_set<string>& words, vector<vector<int>>&dp,int i ,int j) {
         
         if(dp[i][j] > -1) return dp[i][j];
         if(i==j) return dp[i][j] = 1;
