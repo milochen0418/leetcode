c8887da12cf96255067ff6ab4182a4a722d38bae
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-27 16:55:22 +0800

    SQL/second-highest-salary.sql finished by case-when , distinct and LIMIT

--- SQL/second-highest-salary.sql
+++ SQL/second-highest-salary.sql
@@ -5,9 +5,13 @@ LIMIT begin, len mean start from begin and get len number of element
 When begin=0, it is the first element
 
 # select salary as SecondHighestSalary from Employee order by salary desc LIMIT 1,1
-Wrong Answer
-Input: {"headers":{"Employee":["id","salary"]},"rows":{"Employee":[[1,100]]}}
-Output: {"headers": ["SecondHighestSalary"], "values": []}
-Expected: {"headers": ["SecondHighestSalary"], "values": [[null]]}
 # When the number of rows in table is smaller than 2, this SQL cannot be passed.
+
+# select null as SecondHighestSalary;
+# select count(*) from Employee
+
 */
+
+select case when count(*)<=1 then null 
+        else (select distinct salary from Employee order by salary  desc LIMIT 1,1)
+        end as SecondHighestSalary from Employee;
