b6427dc1bc8289b46ff923538e857c8f3a3722c9
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-27 23:24:47 +0800

    C++/maximal-square.cpp optimized from 3D to 2D Dynamic Programming

--- C++/maximal-square.cpp
+++ C++/maximal-square.cpp
@@ -4,6 +4,39 @@ public:
     int maximalSquare(vector<vector<char>>& matrix) {
         int m = matrix.size(), n=matrix[0].size();
         int k = min(m,n);
+        vector<vector<int>> dp = vector<vector<int>>(m,vector<int>(n,0));
+        int max_width = 0;
+        for(int i=m-1;i>=0;i--) {
+            for(int j=n-1;j>=0;j--) {
+                dp[i][j]=matrix[i][j]-'0';
+                if(dp[i][j]) 
+                    max_width=1;             
+            }
+        }
+        if(max_width==0)return 0;
+        
+        for(int l = 1; l<k;l++) {
+            for(int i=m-1;i>=0;i--) {
+                for(int j=n-1;j>=0;j--) {
+                    if(i-1<0||j-1<0 || !dp[i][j]) {
+                        dp[i][j] = 0;
+                    } else {
+                        dp[i][j] = dp[i][j-1] && dp[i-1][j] && dp[i-1][j-1];
+                    }                        
+
+                    if(dp[i][j]) {
+                        max_width=l+1;
+                    }
+                }
+            }
+            if(max_width !=l+1) break;
+        }
+        return max_width*max_width;
+    }
+
+    int maximalSquare_TLE(vector<vector<char>>& matrix) {
+        int m = matrix.size(), n=matrix[0].size();
+        int k = min(m,n);
         vector<vector<vector<int>>> dp 
             = vector<vector<vector<int>>>(m,vector<vector<int>>(n,vector<int>(k,-1)));
         int max_width = 0;
