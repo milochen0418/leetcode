6784dc65790286dcec4a791447b2796ad898dafd
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-27 16:39:49 +0800

    SQL/second-highest-salary.sql TODO for process null case

---
+++ SQL/second-highest-salary.sql
@@ -0,0 +1,9 @@
+/*
+# //https://leetcode.com/problems/second-highest-salary/
+# Write your MySQL query statement below
+LIMIT begin, len mean start from begin and get len number of element
+When begin=0, it is the first element
+
+# select salary as SecondHighestSalary from Employee order by salary desc LIMIT 1,1
+# When the number of rows in table is smaller than 2, this SQL cannot be passed.
+*/
