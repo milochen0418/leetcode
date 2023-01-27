d96c18e3d26096a2eed2a43f3c00a2374a8a0d2d
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-27 23:00:24 +0800

    C++/maximal-square.cpp TLE

---
+++ C++/maximal-square.cpp
@@ -0,0 +1,39 @@
+class Solution {
+    //https://leetcode.com/problems/maximal-square
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        int m = matrix.size(), n=matrix[0].size();
+        int k = min(m,n);
+        vector<vector<vector<int>>> dp 
+            = vector<vector<vector<int>>>(m,vector<vector<int>>(n,vector<int>(k,-1)));
+        int max_width = 0;
+        for(int l = 0; l<k;l++) {
+            for(int i=m-1;i>=0;i--) {
+                for(int j=n-1;j>=0;j--) {
+                    if(l==0) {
+                        dp[i][j][l]=matrix[i][j]-'0';
+                    } else {
+                        if(i+1>=m || j+1>=n) {
+                            dp[i][j][l] = 0;
+                        } else if(dp[i][j][l-1] == 0) {
+                            dp[i][j][l] = 0;
+                        } else if(dp[i][j+1][l-1] && dp[i+1][j][l-1] && dp[i+1][j+1][l-1]) {
+                            dp[i][j][l] = 1;
+                        } else {
+                            dp[i][j][l] = 0;
+                        }
+                    }
+                    if(dp[i][j][l]) {
+                        if(max_width<l+1) {
+                            max_width =l+1;
+                            //printf("i,j,l=%d,%d,%d with dp=%d\n",i,j,l,dp[i][j][l]);
+                        }
+                    }
+                }
+            }            
+        }
+        
+        //return max_width*max_width;
+        return max_width*max_width;
+    }
+};
