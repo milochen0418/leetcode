0a9ddf5f710225f4581b45b5809aa22f0822fe4e
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-28 12:35:55 +0800

    C++/best-time-to-buy-and-sell-stock-with-cooldown.cpp Use the type of robber house analysis way to this problem

--- C++/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ C++/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -2,13 +2,26 @@ class Solution {
     //https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown
 public:
     int maxProfit(vector<int>& prices) {
-        int sold = INT_MIN, held = INT_MIN, reset = 0;
-        for(auto price:prices) {
-            int pre_sold = sold;
-            sold = held + price;
-            held = max(held, reset - price);
-            reset = max(reset, pre_sold);
-        }
-        return max(sold, reset);
+        //Use a little bit idea of  Robber House to think about this idea
+        /*
+         0 1 2 3 4 
+        [1,2,3,0,2]
+        cost(i=0) + sol(i+2=2) <-- no way. because buy first and then sell
+        .So the first sell must happen in cost(i=1)
+        
+        cost(i=1) + sol(i+2=3)
+        cost(i=2) + sol(i+2=4)
+        */
+        
+        /*[2,1,2,3,0,2]
+        cost(i=1) + sol(i+2=3)
+        if(we buy for A[0], that's the worst decision)
+        cost(i=2) + sol(i+2=4)
+        cost(i=3) + sol(i+2=5)
+        
+        sol(i+2=5) also mean sell (i=3) before. so that why we have sol(i+2=5)
+        So sol(0) is mean we sell in cost(-1)=0. And start to find optimize value for sol(0)
+        */
+        return 0;
     }
 };
