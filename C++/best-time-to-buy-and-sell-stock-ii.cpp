f104572f899213a005e2fd38c727fbf2fbe43057
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-29 01:31:20 +0800

    C++/best-time-to-buy-and-sell-stock-ii.cpp write new version code to process add operation in valley

--- C++/best-time-to-buy-and-sell-stock-ii.cpp
+++ C++/best-time-to-buy-and-sell-stock-ii.cpp
@@ -2,6 +2,28 @@ class Solution {
     //https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii
 public:
     int maxProfit(vector<int>& prices) {
+        //Idea 2, every valley to Peak but only add on value
+        //in the end of valley (the pointer is in previous of rising edge)
+        int ans = 0, n = prices.size();
+        int v = 0;
+        for(int i =1;i<n;i++) {
+            if(prices[i] - prices[i-1] >0) {
+                v+=prices[i] - prices[i-1];
+            } else {
+                if(i+1<n && prices[i+1] - prices[i] > 0) {
+                    //The point in the valley
+                    ans+=v;
+                    v = 0;
+                } else {
+                    //case of keeping in the falling edge
+                }
+            }
+        }
+        ans+=v;
+        return ans;
+    }
+
+    int maxProfit_v03(vector<int>& prices) {
         //Idea 2, every valley to Peak
         int ans = 0, n = prices.size();
         int v = 0;
