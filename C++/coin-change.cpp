dcf83ebab9ac8e6ea273f91d0f66e73a8ab55915
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-29 01:58:38 +0800

    C++/coin-change.cpp rewrite new version

--- C++/coin-change.cpp
+++ C++/coin-change.cpp
@@ -4,6 +4,29 @@ class Solution {
     //FB Post https://www.facebook.com/groups/1451299754892511/posts/5348544365168011/
 public:
     int coinChange(vector<int>& coins, int amount) {
+        typedef long long ll;
+        const ll VAL_MAX=(((ll)INT_MAX)*(ll)2);
+        ll n = amount;                       
+        if(n == 0) return 0;
+        
+        vector<ll> dp = vector<ll>(n+1,-1);
+        dp[n] = 0;//mean achive
+        function<ll(ll)> sol = [&](ll i){
+            if(i>n) return VAL_MAX;
+            if(dp[i]!=-1) return dp[i];
+            ll minv = VAL_MAX;
+            for(int j = 0;j<coins.size();j++) {
+                minv = min(minv, sol(i+coins[j]));
+            }
+            if(minv >= VAL_MAX) return dp[i] = VAL_MAX;
+            return dp[i] = 1+minv;
+        };
+        ll ans = sol(0);
+        if(ans>=VAL_MAX) return -1;
+        return (int)ans;
+    }
+    
+    int coinChange_v01(vector<int>& coins, int amount) {
         vector<int> dp= vector<int>(amount+1, -2);
         dp[0] = 0;
         return sol(amount, coins, dp);
