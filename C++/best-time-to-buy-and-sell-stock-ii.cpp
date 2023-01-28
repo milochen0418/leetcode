a8ca92b743047727d21ea0b6cccca50891627ada
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-28 23:13:32 +0800

    C++/best-time-to-buy-and-sell-stock-ii.cpp add new method accumulate datas of valley to peak

--- C++/best-time-to-buy-and-sell-stock-ii.cpp
+++ C++/best-time-to-buy-and-sell-stock-ii.cpp
@@ -2,6 +2,21 @@ class Solution {
     //https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii
 public:
     int maxProfit(vector<int>& prices) {
+        //Idea 2, every valley to Peak
+        int ans = 0, n = prices.size();
+        int v = 0;
+        for(int i =1;i<n;i++) {
+            if(prices[i] - prices[i-1] >0) { //rising edge
+                v+=prices[i] - prices[i-1];
+            } else { //peak arrive or in the falling
+                ans+=v;
+                v = 0;
+            }
+        }
+        ans+=v;
+        return ans;
+    }
+    int maxProfit_v02(vector<int>& prices) {
         //Idea, just sell it in every rise edge
         int ans = 0, n = prices.size();
         for(int i =1;i<n;i++) {
