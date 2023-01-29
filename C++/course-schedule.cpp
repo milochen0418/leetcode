1e3abcdc52479081e658abcc17eac70e004bf5c1
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-30 00:12:02 +0800

    C++/course-schedule.cpp solve but TLE

--- C++/course-schedule.cpp
+++ C++/course-schedule.cpp
@@ -3,21 +3,35 @@ public:
     //https://leetcode.com/problems/course-schedule
     //Refer this article first to understand topological sort more
     //https://www.geeksforgeeks.org/detect-cycle-in-directed-graph-using-topological-sort/
-    
-     #define print_container(name) printf("%s = ",#name);for(auto &i:name)printf("%d,",i);printf("\n");
+    #define printf(...) empty_printf(__VA_ARGS__)
+    #define empty_printf(...)
+    #define print_container(name) printf("%s = ",#name);for(auto &i:name)printf("%d,",i);printf("\n");
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         unordered_map<int,vector<int>> mp;
         for(auto &e:prerequisites) {
-            mp[e[1]].push_back(e[0]);
+            //mp[e[1]].push_back(e[0]);
+            mp[e[0]].push_back(e[1]);//reverse edge
         }
+
+        for(int i = 0;i<numCourses;i++) {
+            printf("i=%d ->",i);
+            print_container(mp[i]);
+        }
+
+        
+        
         vector<int> v;
         vector<int> visited = vector<int>(numCourses,0);
         int detect_cycle=0;
         
+        
         function<void(int)> dfs = [&](int parent) {
             visited[parent] = 1;
+            
             for(auto &child:mp[parent]){
+                printf("(%d->%d),",parent,child);
                 if(visited[child] == 0) {
+                    printf("=>%d",child);
                     dfs(child);
                 } else {
                     printf("detect_cycle for parent=%d, child=%d\n", parent, child);
@@ -25,15 +39,18 @@ public:
                 }
             }
             v.push_back(parent);
+            visited[parent] = 0;
         };
         
         for(int i = 0;i<numCourses;i++) {
             if(!visited[i]) {
+                printf("\nDFS init to start from %d", i);
                 detect_cycle=0;
                 dfs(i);
                 if(detect_cycle)return false;
             }
         }
+        
         
         reverse(v.begin(), v.end());
         printf("v.size() = %d\n", (int)v.size());
