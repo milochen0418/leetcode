a73b55f763c2a3f5c044e0b1bd0a87847cac4cea
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-30 01:22:50 +0800

    make code clear to demonstrate the preorder-DFS solution

--- C++/course-schedule.cpp
+++ C++/course-schedule.cpp
@@ -1,15 +1,20 @@
 class Solution {
-public:
     //https://leetcode.com/problems/course-schedule
+    //add article https://leetcode.com/problems/course-schedule/discuss/3114572/C%2B%2B-preorder-DFS-approach
     //Refer this article first to understand topological sort more
-    //https://www.geeksforgeeks.org/detect-cycle-in-directed-graph-using-topological-sort/
-    //#define printf(...) empty_printf(__VA_ARGS__)
+    //https://www.geeksforgeeks.org/detect-cycle-in-directed-graph-using-topological-sort/    
+    
+public:
+    #define DEBUG
+    #ifndef DEBUG
+        #define printf(...) empty_printf(__VA_ARGS__)
+    #endif 
     #define empty_printf(...)
     #define print_container(name) printf("%s = ",#name);for(auto &i:name)printf("%d,",i);printf("\n");
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        //Preorder-DFS approach
         unordered_map<int,vector<int>> mp;
         for(auto &e:prerequisites) {
-            //mp[e[1]].push_back(e[0]);
             mp[e[0]].push_back(e[1]);//reverse edge
         }
 
@@ -17,20 +22,14 @@ public:
             printf("i=%d ->",i);
             print_container(mp[i]);
         }
-
         
-        
-        vector<int> v;
         vector<int> color = vector<int>(numCourses,-1);//-1 is mean no color
         vector<int> pathover = vector<int>(numCourses,0);
-        int detect_cycle=0;
-        
-        
+        int detect_cycle=0;        
         function<void(int,int)> dfs = [&](int parent,int root) {
             if(detect_cycle) return;
             pathover[parent] = 1;
-            color[parent] = root;
-            //v.push_back(parent);
+            color[parent] = root;//preorder-DFS here by putting the color of starting node, which is called root.
             for(auto &child:mp[parent]){
                 if(pathover[child]==1) {
                     printf(" || detect_cycle for parent=%d, child=%d\n", parent, child);
@@ -43,7 +42,6 @@ public:
                 }
                 if(detect_cycle) break;
             }
-            v.push_back(parent);
             pathover[parent] = 0;
         };
         
@@ -53,19 +51,16 @@ public:
                 detect_cycle=0;
                 dfs(i,i);
                 if(detect_cycle)return false;
+            } else {
+                printf("\nDFS cannot init to start from %d, since its color=%d\n ", i,color[i]);
             }
         }
-        
-        
-        //reverse(v.begin(), v.end());
-        printf("v.size() = %d\n", (int)v.size());
-        print_container(v);
         if(detect_cycle) return false;
         return true;
     }
-    
-    
 };
+
+
 /* Test Cases
 2
 [[1,0],[0,1]]
