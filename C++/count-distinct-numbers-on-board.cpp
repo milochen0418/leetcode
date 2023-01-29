0921efe4549d15edfda1d5b3175bc6fd6fec745f
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-29 15:33:09 +0800

    C++/count-distinct-numbers-on-board.cpp finished by math

---
+++ C++/count-distinct-numbers-on-board.cpp
@@ -0,0 +1,8 @@
+class Solution {
+    //https://leetcode.com/problems/count-distinct-numbers-on-board/
+public:
+    int distinctIntegers(int n) {
+        if(n==1) return 1;
+        return n-1;
+    }
+};
