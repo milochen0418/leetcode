414d6fe198cabd827d26a2756a1d710fc1f95600
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-29 17:42:17 +0800

    C++/count-increasing-quadruplets.cpp finished by O(N^2) prefix suffix of counting

--- C++/count-increasing-quadruplets.cpp
+++ C++/count-increasing-quadruplets.cpp
@@ -18,7 +18,33 @@ public:
 
         This observation is contributed by ZZZ, 哎呀呀 in LeetCode Contest After Party
         */
-        return 0;
-    }
+        int n = nums.size();
+        vector<vector<int>> h_cnt = vector<vector<int>>(n, vector<int>(n,0));
+        vector<vector<int>> k_cnt = vector<vector<int>>(n, vector<int>(n,0));
 
+        for(int j=2;j<n-1;j++){
+            int max_i = j-1;
+            for(int h =0;h<max_i;h++) {
+                int h_cnt_minus = h-1<0?0:h_cnt[j][h-1];
+                h_cnt[j][h] =  h_cnt_minus + (nums[h]<nums[j]);
+            }
+        }
+
+        for(int i=1;i<n-2;i++) {
+            int min_j = i+1;
+            for(int k=n-1;k>min_j;k--){
+                int k_cnt_plus = k+1>=n?0:k_cnt[i][k+1];
+                k_cnt[i][k] = k_cnt_plus + (nums[k]>nums[i]);
+            }
+        }
+        ll ans = 0;
+        for(int i=1;i<n-2;i++) {
+            for(int j=i+1;j<n-1;j++) {
+                if(nums[i]>nums[j]) {
+                    ans += (ll)h_cnt[j][i-1]*(ll)k_cnt[i][j+1];
+                }
+            }
+        }
+        return ans;
+    }
 };
