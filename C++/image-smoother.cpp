163a4451594c659d9894dfbcdec79ac9451277c1
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-31 00:01:38 +0800

    C++/image-smoother.cpp refactor to 15 lines

--- C++/image-smoother.cpp
+++ C++/image-smoother.cpp
@@ -2,6 +2,23 @@ class Solution {
 public:
     vector<vector<int>>& imageSmoother(vector<vector<int>>& img) {
         int m = img.size(), n=img[0].size();
+        vector<vector<int>> &ans = img, dirs=vector<vector<int>>(9,vector<int>(2,0));
+        for(int i = 0;i<9;i++) dirs[i] = {(i/3)-1, (i%3)-1};
+        for(int i = 0; i<m*n; i++) {
+            int cnt=0, sum=0;
+            for(auto &d:dirs) {
+                int r = (i/n)+d[0], c=(i%n)+d[1];
+                if(r<0||r>=m||c<0||c>=n)continue;
+                cnt++;
+                sum+=(img[r][c]&0x00ff);
+            }
+            ans[i/n][i%n]|=(sum/cnt)<<8;
+        }
+        for(int i=0; i<m*n; i++) ans[i/n][i%n]>>=8;
+        return ans;
+    }
+    vector<vector<int>>& imageSmoother_v02(vector<vector<int>>& img) {
+        int m = img.size(), n=img[0].size();
         vector<vector<int>> &ans = img;
         vector<vector<int>> dirs= vector<vector<int>>(9,vector<int>(2,0));
         for(int i = 0;i<9;i++) dirs[i] = {(i/3)-1, (i%3)-1};
