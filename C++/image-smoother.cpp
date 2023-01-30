3ef3459be841cd838bb0e821de624efee70c60b4
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-30 23:51:46 +0800

    C++/image-smoother.cpp refactor

--- C++/image-smoother.cpp
+++ C++/image-smoother.cpp
@@ -2,13 +2,9 @@ class Solution {
 public:
     vector<vector<int>> imageSmoother(vector<vector<int>>& img) {
         int m = img.size(), n=img[0].size();
-        vector<vector<int>> next_img = vector<vector<int>>(m,vector<int>(n,0));
-        
-        vector<vector<int>> dirs=vector<vector<int>>(9,vector<int>(2,0));
-        //Set dirs = (-1,-1),(-1,0),(-1,1),(0,-1),(0,0),(0,1),(1,-1),(1,0),(1,1),
-        for(int i = 0;i<9;i++) 
-            dirs[i] = {(i/3)-1, (i%3)-1};
-        
+        vector<vector<int>> ans = vector<vector<int>>(m,vector<int>(n,0));
+        vector<vector<int>> dirs= vector<vector<int>>(9,vector<int>(2,0));
+        for(int i = 0;i<9;i++) dirs[i] = {(i/3)-1, (i%3)-1};
         for(int i = 0; i<m; i++) {
             for(int j=0; j<n;j++) {
                 int cnt=0, sum=0;
@@ -18,10 +14,9 @@ public:
                     cnt++;
                     sum+=img[r][c];
                 }
-                next_img[i][j]=sum/cnt;
+                ans[i][j]=sum/cnt;
             }
         }
-        return next_img;
-        
+        return ans;
     }
 };
