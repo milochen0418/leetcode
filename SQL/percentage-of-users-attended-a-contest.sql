d7f4ebd8d0448d2f495e693441b2eddef6c9b925
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-30 16:05:19 +0800

    SQL/percentage-of-users-attended-a-contest.sql optimzied by saving query result like a variable

--- SQL/percentage-of-users-attended-a-contest.sql
+++ SQL/percentage-of-users-attended-a-contest.sql
@@ -7,11 +7,26 @@ But we cannot use variable
 # select count(user_id) into @total from Users;
 # select contest_id, round( (100*count(user_id))/@total, 2) as percentage from Register group by contest_id;
 
-Thus we can have the following solution
-*/
-
+Thus we can have the following accepted solution
 
 select contest_id, 
     round ((100*count(user_id))/(select count(user_id) from Users),2) as percentage 
 from Register group by contest_id 
+order by percentage desc, contest_id asc;
+
+But Our purpose is to saveing query result of  (select count(user_id) from Users),2)
+For this purpose , we can use WITH 
+(refer WITH in this article: https://zhuanlan.zhihu.com/p/397195826) 
+
+with var as (...)  select ... from Table, var ... 
+*/
+
+
+with var as
+(
+    select count(*) as total from Users
+)
+select contest_id, 
+    round ((100*count(user_id))/var.total,2) as percentage 
+from Register, var group by contest_id 
 order by percentage desc, contest_id asc;
