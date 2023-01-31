79062cefc0f7ffce67fd41d80edce099606ca26f
milochen0418 <milo_chen@mail2000.com.tw>
2023-01-31 22:52:53 +0800

    C++/implement-trie-prefix-tree.cpp implements in no private way

--- C++/implement-trie-prefix-tree.cpp
+++ C++/implement-trie-prefix-tree.cpp
@@ -1,64 +1,55 @@
 class Trie {
-    //https://leetcode.com/problems/implement-trie-prefix-tree
+    //https://leetcode.com/problems/implement-trie-prefix-tree/
 public:
-    Trie() {
-        
-    }
     
-    void insert(string word) {
-        //build the Trie dictionary structure by insert word
-        T_insert(root, word);
-    }
-    
-    bool search(string word) {
-        //Not only check the path is exist in Trie but also
-        //the path need stop at the TNode which have match=true
-        TNode *node = T_find(root, word);
-        if(!node)return false;
-        return node->match;
-    }
-    
-    bool startsWith(string prefix) {
-        //Only check the path is exist in Trie by prefix 
-        TNode *node = T_find(root, prefix);
-        if(!node) return false;
-        return true;
-    }
-private:
-    
-    struct TNode { //TNode mean Trie Node
+    struct TNode {
         int match;
         vector<TNode*> children;
         TNode(int _match=false) {
             match = _match;
             children = vector<TNode*>(26,nullptr);
         }
-        ~TNode() {
+        ~TNode(){
             for(auto &child:children) 
-                if(child) delete child;
+                if(child!=nullptr)
+                    delete child;
         }
-    };
-    TNode* root= new TNode();
+    }; 
     
+    TNode *root;
+    Trie() {
+        root = new TNode();
+    }
+    ~Trie() {
+        delete root;
+    }
     
-    void T_insert(TNode* root, string& s) {
+    void insert(string word) {
         TNode* node = root;
-        for(auto &c:s) {
-            if(!node->children[c-'a']) 
+        for(auto &c:word) {
+            if(node->children[c-'a']==nullptr)
                 node->children[c-'a'] = new TNode();
             node = node->children[c-'a'];
         }
         node->match = true;
     }
     
-    TNode* T_find(TNode* root, string& s) {
-        TNode* node = root;
-        for(auto& c:s) {
-            if(node->children[c-'a'] == nullptr)
-                return nullptr;
-            node = node->children[c-'a'];
+    bool search(string word) {
+        TNode *node = root;
+        for(auto& c:word) {
+            node=node->children[c-'a'];
+            if(!node) return false;
         }
-        return node;
+        return node->match;
+    }
+    
+    bool startsWith(string prefix) {
+        TNode *node = root;
+        for(auto& c:prefix) {
+            node=node->children[c-'a'];
+            if(node==nullptr) return false;
+        }
+        return true;
     }
 };
 
