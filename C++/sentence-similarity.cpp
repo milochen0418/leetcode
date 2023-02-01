5c263332eaedcd1ea609408031d843c440fb7141
milochen0418 <milo_chen@mail2000.com.tw>
2023-02-01 15:42:46 +0800

    C++/sentence-similarity.cpp remove lambda

--- C++/sentence-similarity.cpp
+++ C++/sentence-similarity.cpp
@@ -12,6 +12,26 @@ public:
             mp[pair[1]].insert(pair[0]);
         }
         
+        for(int i = 0;i<n;i++) {
+            string& s1=sentence1[i], &s2=sentence2[i];
+            if(s1==s2 || mp[s1].find(s2) !=  mp[s1].end())continue;
+            return false;
+        }
+        
+        return true;
+    }
+
+    bool areSentencesSimilar_v01(vector<string>& sentence1, vector<string>& sentence2, vector<vector<string>>& similarPairs) {
+        int n = sentence1.size();
+        if(n!=sentence2.size()) 
+            return false;
+        
+        unordered_map<string,unordered_set<string>> mp;
+        for(auto &pair:similarPairs) {
+            mp[pair[0]].insert(pair[1]);
+            mp[pair[1]].insert(pair[0]);
+        }
+        
         function<bool(string&,string&)>similar = [&](string &s1, string &s2){
             if(s1==s2) return true;
             if(mp[s1].find(s2) !=  mp[s1].end()) return true;
