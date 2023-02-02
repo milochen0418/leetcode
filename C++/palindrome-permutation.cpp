dd90c58819f458abd63fc2107693edc84e5aa78f
milochen0418 <milo_chen@mail2000.com.tw>
2023-02-02 23:13:05 +0800

    C++/palindrome-permutation.cpp add article

--- C++/palindrome-permutation.cpp
+++ C++/palindrome-permutation.cpp
@@ -1,5 +1,6 @@
 class Solution {
     //https://leetcode.com/problems/palindrome-permutation
+    //article https://leetcode.com/problems/palindrome-permutation/discuss/3132171/C%2B%2B-Two-Pass-O(N)-5-lines
 public:
     bool canPermutePalindrome(string s) {
         unordered_map<char,int> mp;
