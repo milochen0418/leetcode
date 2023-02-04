b9a70cdbe20e966ab82563d54abafb0386076e9e
milochen0418 <milo_chen@mail2000.com.tw>
2023-02-05 03:57:37 +0800

    C++/separate-the-digits-in-an-array.cpp optimized

--- C++/separate-the-digits-in-an-array.cpp
+++ C++/separate-the-digits-in-an-array.cpp
@@ -3,6 +3,19 @@ class Solution {
 public:
     vector<int> separateDigits(vector<int>& nums) {
         vector<int> ans;
+        for(int i = nums.size()-1;i>=0;i--) {
+            int n = nums[i];
+            while(n>0) {
+                ans.push_back(n%10);
+                n/=10;
+            }            
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+
+    vector<int> separateDigits_v01(vector<int>& nums) {
+        vector<int> ans;
         for(auto &i:nums) {
             vector<int> v;
             int n = i;
