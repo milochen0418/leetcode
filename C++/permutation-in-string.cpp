d5dd12f1ab5974aff98f5de0370a8bcf80927526
milochen0418 <milo_chen@mail2000.com.tw>
2023-02-04 20:59:44 +0800

    C++/permutation-in-string.cpp redo

--- C++/permutation-in-string.cpp
+++ C++/permutation-in-string.cpp
@@ -2,50 +2,18 @@ class Solution {
     //https://leetcode.com/problems/permutation-in-string/
 public:
     bool checkInclusion(string s1, string s2) {
+        int k = s1.length(), n = s2.length();
+        if(k>n) return false;
+        vector<int> mp1 = vector<int>(26,0), mp2=mp1;
+        for(auto &c:s1) mp1[c-'a']++;
+        for(int i = 0;i<k;i++) mp2[s2[i]-'a']++;
+        if(mp1==mp2) return true;
         
-        int s1_len = s1.length();
-        int s2_len = s2.length();
-        if(s1_len > s2_len) return false;
-        vector<int> s1v(26,0);
-        vector<int> s2v(26,0);
-        for(int i=0;i<s1_len;i++) {
-            s1v[s1[i]-'a']+=1;
-            s2v[s2[i]-'a']+=1;
-        }
-        if(s1v == s2v) return true;
-        for(int i = 1; i<= s2_len - s1_len ; i++) { 
-            s2v[s2[i-1]-'a']-=1;
-            s2v[s2[i+s1_len-1]-'a']+=1;
-            if(s1v == s2v) return true;
+        for(int i = k;i<n;i++) {
+            mp2[s2[i]-'a']++;
+            mp2[s2[i-k]-'a']--;
+            if(mp2==mp1) return true;
         }
         return false;
-    }
-    
-    bool answer1(string s1, string s2) {
-        int s1_len = s1.length();
-        int s2_len = s2.length();
-        if(s1_len > s2_len) false;
-        
-        vector<int> s1v(26,0);
-        for(auto &c:s1) s1v[c-'a']+=1;
-
-        for(int i = 0; i<= s2_len - s1_len ; i++) { 
-            string s2sub = s2.substr(i,s1_len);
-            vector<int> s2v(26,0);
-            for(auto &c:s2sub) s2v[c-'a']+=1; 
-            if(s1v == s2v) return true;
-        }
-        return false;
-        
     }
 };
-/* testcase 
-"ab"
-"eidbaooo"
-"ab"
-"eidboaoo"
-"adc"
-"dcda"
-"ros"
-"horse"
-*/
