4831ed9d3d1f33bb1c71ced7f2e29c3db8a96bc8
milochen0418 <milo_chen@mail2000.com.tw>
2023-02-08 03:27:09 +0800

    C++/house-robber-iv.cpp optimized

--- C++/house-robber-iv.cpp
+++ C++/house-robber-iv.cpp
@@ -9,6 +9,40 @@ public:
     #define print_container(name) printf("%s = ",#name);for(auto &i:name)printf("%d,",i);printf("\n");
     
     int minCapability(vector<int>& nums, int k) {
+        vector<int> A = nums;
+		sort(A.begin(), A.end());
+        nums.push_back(INT_MAX);
+        int n = nums.size();
+        
+        function<int(int)> check_cnt =  [&](int v) {
+            int cnt = 0, L = 0;
+            for(int R = 0;R<n;R++) {
+                if(nums[R] > v) {
+                    cnt+=(R-L+1)/2;
+                    L=R+1;
+                }
+            }
+            return cnt;
+        };
+		
+        int L = 0, R=A.size()-1;
+        vector<int> cnt = vector<int>(A.size(),-1);
+        while(L<=R) {
+            int M = L+(R-L)/2;
+            if(M-1>=0 && cnt[M-1]==-1) cnt[M-1] = check_cnt(A[M-1]);
+            if(cnt[M]==-1) cnt[M] = check_cnt(A[M]);
+            
+            if( (M-1<0 || cnt[M-1]<k) && cnt[M] == k ) 
+                return A[M];
+            if(cnt[M] >= k) 
+                R=M-1;
+            else 
+                L=M+1;
+        }
+        return -1;      
+    }
+
+    int minCapability_v01(vector<int>& nums, int k) {
         printf("\nminCapability, k= %d\n",k);
         print_container(nums);
         vector<int> A = nums;
