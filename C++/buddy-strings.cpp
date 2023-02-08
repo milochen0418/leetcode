48a4be8a7b8a7ad46b6176e037f4efbc9507d385
milochen0418 <milo_chen@mail2000.com.tw>
2023-02-08 15:50:08 +0800

    C++/buddy-strings.cpp optimized

--- C++/buddy-strings.cpp
+++ C++/buddy-strings.cpp
@@ -4,6 +4,23 @@ public:
     bool buddyStrings(string s, string goal) {
         int n = s.length();
         if(n!=(int)goal.length()) return false;
+        if(s==goal) {
+            vector<int> mp=vector<int>(26,0);
+            for(auto &c:s) if(++mp[c-'a']>=2) return true;
+            return false;
+        } else {
+            int L=0, R=n-1;
+            while(s[L]==goal[L])L++;
+            while(s[R]==goal[R])R--;
+            if(L==R) return false;
+            swap(s[L], s[R]);
+            return s==goal;
+        }
+    }
+
+    bool buddyStrings_v01(string s, string goal) {
+        int n = s.length();
+        if(n!=(int)goal.length()) return false;
         vector<int> mp=vector<int>(26,0);
         if(s==goal) {
             for(auto &c:s) if(++mp[c-'a']>=2) return true;
