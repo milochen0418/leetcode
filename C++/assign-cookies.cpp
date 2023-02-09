111e47f76ba74217a756a38c887333ab3dbf31be
milochen0418 <milo_chen@mail2000.com.tw>
2023-02-09 19:00:51 +0800

    C++/assign-cookies.cpp refactor to one-pass O(N) 5 lines

--- C++/assign-cookies.cpp
+++ C++/assign-cookies.cpp
@@ -2,6 +2,14 @@ class Solution {
     //https://leetcode.com/problems/assign-cookies/
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
+        int n = s.size(), m = g.size(), ans=0,j=0;
+        sort(s.begin(), s.end());
+        sort(g.begin(), g.end());
+        while(ans<m && j<n) ans+=s[j++]>=g[ans];
+        return ans;
+    }
+    
+    int findContentChildren_v01(vector<int>& g, vector<int>& s) {
         int ans = 0, n = s.size(), m = g.size(), i=0,j=0;
         sort(s.begin(), s.end());
         sort(g.begin(), g.end());
