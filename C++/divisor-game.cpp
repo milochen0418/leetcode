98d86f435ad15c326e12194d2a86148c1cb7e449
milochen0418 <milo_chen@mail2000.com.tw>
2023-02-10 11:20:54 +0800

    C++/divisor-game.cpp finished by Math

---
+++ C++/divisor-game.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool divisorGame(int n) {
+        /*
+        Alice get Win when = 2,4,6,8,...
+        
+        
+        */
+        return n%2==0;
+        
+    }
+};
