ab4ecb365771ec2c619dc2fb47d6f07f7e15e85b
milochen0418 <milo_chen@mail2000.com.tw>
2023-02-13 09:31:56 +0800

    C++/count-odd-numbers-in-an-interval-range.cpp update to clear version

--- C++/count-odd-numbers-in-an-interval-range.cpp
+++ C++/count-odd-numbers-in-an-interval-range.cpp
@@ -2,6 +2,12 @@ class Solution {
     //https://leetcode.com/problems/count-odd-numbers-in-an-interval-range
 public:
     int countOdds(int low, int high) {
+        low+=(1-low%2);
+        high-=(1-high%2);
+        return 1+(high-low)/2;
+    }
+
+    int countOddsVer02(int low, int high) {
         return (high-low+(low%2)+1)/2;
     }
 
