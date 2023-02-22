114adcbeccfda9a202ac9751f2997eefe3625b00
milochen0418 <milo_chen@mail2000.com.tw>
2023-02-22 23:26:52 +0800

    C++/capacity-to-ship-packages-within-d-days.cpp optimized by binary search

--- C++/capacity-to-ship-packages-within-d-days.cpp
+++ C++/capacity-to-ship-packages-within-d-days.cpp
@@ -9,6 +9,33 @@ public:
         }
         weights.push_back(INT_MAX/2);//dummy
         int L = maxv, R = sum;
+        int M;
+        while(L<=R) {
+            M = L+(R-L)/2;
+            //printf("([L,R], M) = [%d,%d],%d\n", L,R,M);
+            int QQ = !check(weights, days, M);
+            //if(!check(weights, days, M) && (M-1<maxv || check(weights, days, M-1))) {
+            if(QQ && (M-1<maxv || check(weights, days, M-1))) {                
+                break;
+            }
+            //if(!check(weights, days, M)) 
+            if(QQ) 
+                R=M-1;
+            else 
+                L=M+1;
+        }
+        return M;
+    }
+
+
+    int shipWithinDays_v01(vector<int>& weights, int days) {
+        int maxv = 0, sum = 0;
+        for(auto &w:weights) {
+            maxv = max(w,maxv);
+            sum += w;
+        }
+        weights.push_back(INT_MAX/2);//dummy
+        int L = maxv, R = sum;
         for(int i = L;i<=R;i++) {
             if(!check(weights, days, i)){
                 return i;//return i when find out first false checkint result
