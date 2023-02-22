42f2cf5d0f33b7862c121e1e42099e4f845c98e6
milochen0418 <milo_chen@mail2000.com.tw>
2023-02-22 23:33:38 +0800

    C++/capacity-to-ship-packages-within-d-days.cpp refactor

--- C++/capacity-to-ship-packages-within-d-days.cpp
+++ C++/capacity-to-ship-packages-within-d-days.cpp
@@ -8,21 +8,30 @@ public:
             sum += w;
         }
         weights.push_back(INT_MAX/2);//dummy
+        int n = weights.size();
+        
+        function<int(int)> check = [&](int capacity) {
+            int D = days;
+            int sum = 0;
+            for(int i = 0; i<n-1;i++) {
+                sum += weights[i];
+                if(sum + weights[i+1] > capacity) {
+                    D--;
+                    sum = 0;
+                    if(D<0) return 1;
+                }
+            }
+            return 0;            
+        };
+        
         int L = maxv, R = sum;
         int M;
         while(L<=R) {
             M = L+(R-L)/2;
-            //printf("([L,R], M) = [%d,%d],%d\n", L,R,M);
-            int QQ = !check(weights, days, M);
-            //if(!check(weights, days, M) && (M-1<maxv || check(weights, days, M-1))) {
-            if(QQ && (M-1<maxv || check(weights, days, M-1))) {                
-                break;
-            }
-            //if(!check(weights, days, M)) 
-            if(QQ) 
-                R=M-1;
-            else 
-                L=M+1;
+            int QQ = !check(M);
+            if(QQ && (M-1<maxv || check(M-1))) break;
+            //if(QQ) R=M-1; else L=M+1;
+            (QQ?R:L)=M+(QQ?-1:1);
         }
         return M;
     }
