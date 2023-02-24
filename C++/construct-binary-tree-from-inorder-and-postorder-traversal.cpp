657cc34e4ee66d76e41441660288e341f6df1a50
milochen0418 <milo_chen@mail2000.com.tw>
2023-02-24 23:30:39 +0800

    C++/construct-binary-tree-from-inorder-and-postorder-traversal.cpp finished by DFS

---
+++ C++/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        /*
+        111102222 I (inorder)
+        111122220 P (postorder)
+        */
+        
+        int n = inorder.size();
+        unordered_map<int,int> mp;//find the index of val in inorder
+        for(int i = 0; i<n;i++) mp[inorder[i]] = i;
+        function<TreeNode*(int,int,int,int)> build = [&](int Pi, int Pj, int Ii, int Ij) {
+            int val = postorder[Pj];
+            TreeNode* node = new TreeNode(val);
+            int Im = mp[val];
+            int LW = Im-Ii;//width of left subtree;
+            int RW = Ij-Im;//width of right subtree;
+            if(LW>0) node->left = build(Pi, Pi+LW-1, Ii, Im-1);
+            if(RW>0) node->right = build(Pi+LW, Pj-1, Im+1, Ij);
+            return node;
+        };
+        TreeNode* t = build(0,n-1,0,n-1);
+        return t;
+    }
+};
