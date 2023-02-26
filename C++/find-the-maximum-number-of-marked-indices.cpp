61655f3a3c6b57bf278c016bcb20df4bbbc2705b
milochen0418 <milo_chen@mail2000.com.tw>
2023-02-26 22:06:36 +0800

    C++/find-the-maximum-number-of-marked-indices.cpp finished by greedy start from n/2

--- C++/find-the-maximum-number-of-marked-indices.cpp
+++ C++/find-the-maximum-number-of-marked-indices.cpp
@@ -1,64 +1,41 @@
 class Solution {
     //https://leetcode.com/problems/find-the-maximum-number-of-marked-indices/
 public:
-    #define DEBUG
+    //#define DEBUG
     #ifndef DEBUG
         #define printf(...) empty_printf(__VA_ARGS__)
     #endif 
     #define empty_printf(...)
-    #define print_container(name) printf("%s = ",#name);for(auto &i:name)printf("%3d,",i);printf("\n");    
+    #define print_container(name) printf("%s = ",#name);for(auto &i:name)printf("%3d,",i);printf("\n");
     int maxNumOfMarkedIndices(vector<int>& nums) {
-        
         vector<int>& A = nums; 
         int n = A.size();
         sort(A.begin(), A.end());
         vector<int> D = A;
-        for(int i = 0; i<n;i++) D[i]*=2;
+        for(int i = 0; i<n;i++) D[i]*=2;        
         
-        
-        /*
-        [3,5,2,4] -> [2,3,4,5]
-        (2,5), -> ans = 2
-        [9,2,5,4] -> 
-        
-        [2,4, 5,9] nums[]   aka A
-        [4,8,10,18] dnums[]( aka D) . find i<j s.t. dnums[i] < nums[j];
-        find i<j s.t. D[i] < A[j];
-        It's looke like two-pointer solution here
-        
-        (4,9), (2,4)
-        [7,6,8] -> [6,7,8]
-        */  
-        
-        /*
-        A = [2,4, 5, 9] 
-        D = [4,8,10,18] . 
-        find i<j s.t. D[i] < A[j];
-        */
         print_container(A);
         print_container(D);
-        
         int ans = 0;
         unordered_set<int> mark;
-        int i = n-1, j=n-1;
-        while(i>=0 && j>=0) { 
-            if(D[i]<=A[j]) {
-                printf("(i,j) =(%d,%d), D[i]=%d < A[j]=%d\n", i,j,D[i],A[j]);
-                
-                if(mark.find(i) == mark.end()) {
-                    mark.insert(i);    
-                    ans++;
-                }
-                if(mark.find(j) == mark.end()) {
-                    mark.insert(j);    
-                    ans++;
-                }
-                //ans+=2;
-                i--;
-                j--;
-                
-            } else {//D[i]>=A[j]
-                i--;
+        int L = 0, R=n/2;
+        while(L<n && R<n) {
+            if(mark.find(R) != mark.end()) {
+                R++;
+                continue;
+            } 
+            if(mark.find(L) != mark.end()) {
+                L++;
+                continue;
+            } 
+            if(D[L]<=A[R]) {
+                ans+=2;
+                mark.insert(L);
+                mark.insert(R);
+                L++;
+                R++;
+            } else { //D[i]>=A[j]
+                R++;  
             }
         }
         return ans;
