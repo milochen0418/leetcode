834bc5d870ce6b953a30017e977554d03df7e056
milochen0418 <milo_chen@mail2000.com.tw>
2023-02-26 22:44:46 +0800

    C++/unique-email-addresses.cpp refactor

--- C++/unique-email-addresses.cpp
+++ C++/unique-email-addresses.cpp
@@ -3,6 +3,21 @@ class Solution {
 public:
     int numUniqueEmails(vector<string>& emails) {
         unordered_set<string> set;
+        for(auto &s:emails) {
+            string r;
+            int i = 0, n = s.length();
+            while(s[i]!='@' ) 
+                if(s[i]=='+') while(s[i]!='@') i++;
+                else if(s[i]!='.') r.push_back(s[i++]);    
+                else i++;
+            while(i<n) r.push_back(s[i++]);            
+            set.insert(r);
+        }
+        return set.size();
+    }
+
+    int numUniqueEmails_v01(vector<string>& emails) {
+        unordered_set<string> set;
         function<string(string&)> convert = [](string &s) {
             string r;
             int i = 0, n = s.length();
