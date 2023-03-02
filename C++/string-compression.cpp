78a53ec24deacc49aed97c6dbfe18d0ec1163b5e
milochen0418 <milo_chen@mail2000.com.tw>
2023-03-02 15:58:53 +0800

    C++/string-compression.cpp finished by array approach

---
+++ C++/string-compression.cpp
@@ -0,0 +1,27 @@
+class Solution {
+    //https://leetcode.com/problems/string-compression
+public:
+    int compress(vector<char>& chars) {
+        chars.push_back('\t');
+        int n = chars.size();
+        int cnt = 1;
+        string ans;
+        for(int i = 0; i<n-1;i++) {
+            if(chars[i] != chars[i+1]) {
+                ans.push_back(chars[i]);
+                if(cnt>1) ans.append(to_string(cnt));
+                cnt = 1;
+            } else {
+                cnt++;
+            }
+        }
+        
+        int ret = ans.length();
+        for(int i = 0; i<ret;i++) 
+            if(i<n) 
+                chars[i] = ans[i];
+            else 
+                chars.push_back(ans[i]);
+        return ret;
+    }
+};
