0ca8ea81d49a1e0cd98e611587f35322e8a2e2f6
milochen0418 <milo_chen@mail2000.com.tw>
2023-03-06 16:44:28 +0800

    C++/kth-missing-positive-number.cpp update binary search solution

--- C++/kth-missing-positive-number.cpp
+++ C++/kth-missing-positive-number.cpp
@@ -2,8 +2,25 @@ class Solution {
     //https://leetcode.com/problems/kth-missing-positive-number/
     //article https://leetcode.com/problems/kth-missing-positive-number/discuss/2163408/C%2B%2B-or-find-aM-greater-k%2BM-greater-aM-1
 public:
-    int findKthPositive_v03(vector<int>& A, int k) {
-        //Brute Force solution
+    int findKthPositive(vector<int>& A, int k) {
+        //return findKthPositive_v01(A,k);
+        //binary search solution
+        if(k<A.front()) return k;
+        if(k>=A.back()) return k+A.size();
+        A.push_back(INT_MAX);
+        int n = A.size(), L = 0, R=n-1;
+        while(L<=R) {
+            int M = L+(R-L)/2;
+            if(k+M>=A[M] && k+M+1<A[M+1]) return M+k+1;
+            if(k+M+1>=A[M+1]) 
+                L=M+1;
+            else 
+                R=M-1;
+        }
+        return -1;
+    }
+    int findKthPositive_v01(vector<int>& A, int k) {
+        //Brute Force solution O(N+K)
         int j = 0,ans=0;
         for(int i = 0;i<k;i++) {
             ans++;
@@ -14,27 +31,7 @@ public:
         }
         return ans;
     }
-
-
-    int findKthPositive_v02(vector<int>& a, int k) {
-        a.push_back(INT_MAX);
-        int L=0,R=a.size()-1;
-        while(L<=R) { //find a[M-1] < k+M < a[M] 
-            int M = L + (R-L)/2, aM_1=M-1>=0?a[M-1]:0; 
-            if(aM_1 < k+M && k+M < a[M] ) return k+M;
-            if(k+M < a[M]) R=M-1; else L=M+1;
-        } return -1;
-    }
-
-    int findKthPositive_v01(vector<int>& a, int k) {
-        a.push_back(INT_MAX);
-        int L=0,R=a.size()-1;
-        while(L<=R) {
-            int M = L + (R-L)/2; //find a[M]-M-1 >= k > a[M-1]-(M-1)-1
-            if(a[M]-M-1 >= k && (M-1<0 || k > a[M-1]-(M-1)-1) ) return k+M;
-            if(a[M]-M-1 >= k) R=M-1; else L=M+1;
-        } return -1;
-
+    
         /*              8
                     5   9
            1        6  10
@@ -64,5 +61,5 @@ public:
        因此  ,在 a[M] 值之前，總共有  a[M] - M - 1 個 missing number
        a[M] - M - 1 < k <= a[M] - M - 1
         */
-    }
+    
 };
