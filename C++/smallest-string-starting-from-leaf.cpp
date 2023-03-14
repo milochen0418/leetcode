a6ab74903f3c325e031c067572c07228b8c61af9
milochen0418 <milo_chen@mail2000.com.tw>
2023-03-14 18:07:15 +0800

    C++/smallest-string-starting-from-leaf.cpp optimized by implementation of reversed string compare

--- C++/smallest-string-starting-from-leaf.cpp
+++ C++/smallest-string-starting-from-leaf.cpp
@@ -18,6 +18,33 @@ public:
         function<void(TreeNode*)> dfs = [&](TreeNode* root) {            
             if(!root)return;
             tmp.push_back('a'+(char)root->val);
+            if(!root->left && !root->right) {                
+                if(ans.length()==0) {
+                    ans = tmp;
+                } else {
+                    //get result from reversed string compare
+                    int i1 = (int)ans.length(), i2=(int)tmp.length();
+                    while(--i2>=0 && --i1>=0 && ans[i1]==tmp[i2]);
+                    int cmp = i2<0?1:ans[i1]-tmp[i2];
+                    if(cmp>0) ans=tmp;//set ans=tmp if reversed(s1)>reversed(s2)
+                }
+            } else {
+                dfs(root->left);
+                dfs(root->right);
+            }
+            tmp.pop_back();
+        };
+        dfs(root);
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+
+    string smallestFromLeaf_v01(TreeNode* root) {
+        string ans;
+        string tmp;
+        function<void(TreeNode*)> dfs = [&](TreeNode* root) {            
+            if(!root)return;
+            tmp.push_back('a'+(char)root->val);
             if(!root->left && !root->right) {
                 reverse(tmp.begin(),tmp.end());
                 if(ans=="")ans=tmp;
