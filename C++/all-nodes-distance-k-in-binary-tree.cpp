604b32eea2bc8530d8d85203852ba92fd2153114
milochen0418 <milo_chen@mail2000.com.tw>
2023-03-15 11:08:26 +0800

    C++/all-nodes-distance-k-in-binary-tree.cpp finished by BFS + DFS

---
+++ C++/all-nodes-distance-k-in-binary-tree.cpp
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+    //https://leetcode.com/problems/all-nodes-distance-k-in-binary-tree
+public:
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        if(k==0) return {target->val};
+        unordered_map<int,vector<int>> mp;//edges   
+        function<void(TreeNode*)> dfs_build_edges = [&](TreeNode* root) {
+            for(auto &child: {root->left, root->right}) if(child) {
+                mp[root->val].push_back(child->val);
+                mp[child->val].push_back(root->val);
+                dfs_build_edges(child);
+            }
+        };
+        dfs_build_edges(root);//build bi-direction tree graph
+        
+        //Use BFS to search distance k node from target by the above built graph
+        unordered_set<int> visited;
+        vector<int>ans;
+        queue<int> currQ,nextQ;
+        visited.insert(target->val);
+        currQ.push(target->val);
+        while(!currQ.empty()) {
+            int e = currQ.front();currQ.pop();
+            for(auto& node:mp[e]) {
+                if(visited.find(node) == visited.end()) {
+                    visited.insert(node);
+                    nextQ.push(node);
+                }
+            }
+            if(currQ.empty()) {
+                k--;
+                if(k==0) {
+                    while(!nextQ.empty()) {
+                        ans.push_back(nextQ.front());
+                        nextQ.pop();
+                    }
+                    break;
+                } else {
+                    swap(currQ,nextQ);
+                }
+            }
+        }
+        return ans;
+    }
+};
