efa93d6f698a82339cdb861ee36387b38b185fa1
milochen0418 <milo_chen@mail2000.com.tw>
2023-03-21 20:49:30 +0800

    SQL/big-countries.sql finished by select where

---
+++ SQL/big-countries.sql
@@ -0,0 +1,6 @@
+/*
+# Write your MySQL query statement below
+# https://leetcode.com/problems/big-countries/
+*/
+select name, population, area from World 
+where area>=3000000 or population>=25000000
