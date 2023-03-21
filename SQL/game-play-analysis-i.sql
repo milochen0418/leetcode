8056461202fdc2c39685cf1cf838f4f063997891
milochen0418 <milo_chen@mail2000.com.tw>
2023-03-21 20:56:24 +0800

    SQL/game-play-analysis-i.sql finished by group by + min

---
+++ SQL/game-play-analysis-i.sql
@@ -0,0 +1,6 @@
+/*
+# Write your MySQL query statement below
+# https://leetcode.com/problems/game-play-analysis-i
+*/
+select player_id ,min(event_date) as first_login 
+from Activity group by player_id
