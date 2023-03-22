eaa70fb271028ad6340e773e0264dfea247dc5b5
milochen0418 <milo_chen@mail2000.com.tw>
2023-03-22 16:12:21 +0800

    C++/rectangle-overlap.cpp wrong answer and why it is wrong

--- C++/rectangle-overlap.cpp
+++ C++/rectangle-overlap.cpp
@@ -2,7 +2,10 @@ class Solution {
     //https://leetcode.com/problems/rectangle-overlap/
 public:
     bool isRectangleOverlap(vector<int>& P, vector<int>& Q) {
-        //I guess
+        return false;
+    }
+    bool isRectangleOverlap_wrongans_01(vector<int>& P, vector<int>& Q) {
+        //I assume
         //two rectangle P,Q overlap iff one point of P in rect Q or one point of Q in rect P
         vector<vector<int>> Pxy = {{P[0],P[1]}, {P[2],P[1]}, {P[0],P[3]}, {P[2],P[3]}};
         vector<vector<int>> Qxy = {{Q[2],Q[3]}, {Q[0],Q[3]}, {Q[2],Q[1]}, {Q[0],Q[1]}};
@@ -17,6 +20,14 @@ public:
         cannot pass this test-case after submit
         [7,8,13,15]
         [10,8,12,20]
+        The case is like this
+        +------+--+--+
+        |      |  |  |
+        +------+--+--+
+               |  |
+               |  |
+               +--+
+        Thus, this assumption is not work.
         */
     }
 };
