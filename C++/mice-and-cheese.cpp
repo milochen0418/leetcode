cf4051e936dde4e632537c26fc486389b0e1f398
Milo Chen <milo_chen@mail2000.com.tw>
2023-04-03 13:17:16 +0800

    C++/mice-and-cheese.cpp update article

--- C++/mice-and-cheese.cpp
+++ C++/mice-and-cheese.cpp
@@ -1,6 +1,6 @@
 class Solution {
     //https://leetcode.com/problems/mice-and-cheese/
-    //article https://leetcode.com/problems/mice-and-cheese/discuss/3368768/C%2B%2B-or-8-lines-O(N-log-N)-greedy-and-explanation
+    //article https://leetcode.com/problems/mice-and-cheese/discuss/3373067/C%2B%2B-or-easy-8-lines-greedy-and-explanation
 public:
     int miceAndCheese(vector<int>& reward1, vector<int>& reward2, int k) {
         vector<vector<int>>v;
