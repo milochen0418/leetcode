8f74dac9c1d029e8dd3f63c2a9088671c1a3765f
Milo Chen <milo_chen@mail2000.com.tw>
2023-04-23 22:51:40 +0800

    C++/minimum-number-of-operations-to-make-all-array-elements-equal-to-1.cpp finished by brute force

---
+++ C++/minimum-number-of-operations-to-make-all-array-elements-equal-to-1.cpp
@@ -0,0 +1,27 @@
+class Solution {
+    //https://leetcode.com/problems/minimum-number-of-operations-to-make-all-array-elements-equal-to-1/
+public:
+    int minOperations(vector<int>& nums) {
+        int n = nums.size();
+        //Case A. if there is an one in default nums, then return the solution
+        for(int i = 0;i<n;i++) {
+            if(nums[i]==1) {
+                int cntone = 0;
+                for(int j=0;j<n;j++) if(nums[j]==1)cntone++;
+                return n-cntone;
+            }
+        }
+        //Case B. We should make the first one in nums. minv is minimum steps to make one
+        int minv = INT_MAX;
+        //We try all possible gcd(nums[i..j]) to find out the minumum range[i..j] that can be 1
+        for(int i = 0; i<n;i++) {
+            for(int j = i;j<n;j++) {
+                int tmp = 0;
+                for(int k = i;k<=j;k++) 
+                    tmp = gcd(tmp, nums[k]);
+                if(tmp == 1) minv=min(minv, j-i);
+            }
+        }
+        return minv==INT_MAX?-1:minv+n-1;
+    }    
+};
