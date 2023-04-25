89723eae6c8774fedf1c1c3a60435f6e798f0779
milochen0418 <milo_chen@mail2000.com.tw>
2023-04-25 09:14:17 +0800

    C++/smallest-number-in-infinite-set.cpp optimized

--- C++/smallest-number-in-infinite-set.cpp
+++ C++/smallest-number-in-infinite-set.cpp
@@ -2,21 +2,26 @@ class SmallestInfiniteSet {
     //https://leetcode.com/problems/smallest-number-in-infinite-set
 public:
     int minv = 1;
-    unordered_set<int> s;
+    //unordered_set<int> s;
+    vector<int> s;
     SmallestInfiniteSet() {
-        
+        s = vector<int>(1001,0);
     }
     
     int popSmallest() {
-        while(s.find(minv) != s.end()) minv++;
-        s.insert(minv);
+        //while(s.find(minv) != s.end()) minv++;
+        while(s[minv]==1) minv++;
+        //s.insert(minv);
+        s[minv]=1;
         int ret = minv++;
         return ret;
     }
     
     void addBack(int num) {
-        if(s.find(num) != s.end()) {
-            s.erase(num);
+        //if(s.find(num) != s.end()) {
+        if(s[num]==1) {
+            //s.erase(num);
+            s[num]=0;
             if(num<minv) minv=num;
         }
     }
