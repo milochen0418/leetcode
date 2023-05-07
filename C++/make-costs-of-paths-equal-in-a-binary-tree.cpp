1809b841aec4557ed0481df450864f8602f51131
milochen0418 <milo_chen@mail2000.com.tw>
2023-05-07 13:33:30 +0800

    C++/make-costs-of-paths-equal-in-a-binary-tree.cpp add article

--- C++/make-costs-of-paths-equal-in-a-binary-tree.cpp
+++ C++/make-costs-of-paths-equal-in-a-binary-tree.cpp
@@ -1,5 +1,6 @@
 class Solution {
     //https://leetcode.com/problems/make-costs-of-paths-equal-in-a-binary-tree/
+    //add post https://leetcode.com/problems/make-costs-of-paths-equal-in-a-binary-tree/discuss/3495472/C%2B%2B-or-simple-8-lines-DFS-O(N)
 public:
     int minIncrements(int n, vector<int>& cost) {
         cost.insert(cost.begin(),0);
