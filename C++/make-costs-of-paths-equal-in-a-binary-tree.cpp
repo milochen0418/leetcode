cfacbe9cff96f7648fcd0a845b8ed42a3f4cafed
milochen0418 <milo_chen@mail2000.com.tw>
2023-05-07 13:29:18 +0800

    C++/make-costs-of-paths-equal-in-a-binary-tree.cpp finished by DFS

---
+++ C++/make-costs-of-paths-equal-in-a-binary-tree.cpp
@@ -0,0 +1,16 @@
+class Solution {
+    //https://leetcode.com/problems/make-costs-of-paths-equal-in-a-binary-tree/
+public:
+    int minIncrements(int n, vector<int>& cost) {
+        cost.insert(cost.begin(),0);
+        function<vector<int>(int)> sol = [&](int root) {
+            if(root>n) return vector<int>(2,0);
+            vector<int> L = sol(root*2);
+            vector<int> R = sol(root*2+1);
+            return vector<int>{max(L[0],R[0])+cost[root], L[1]+R[1]+abs(L[0]-R[0]) };
+        };
+        vector<int> ret = sol(1);
+        return ret[1];
+    }
+    
+};
