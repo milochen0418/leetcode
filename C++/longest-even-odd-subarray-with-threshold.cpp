0ad89d81e796d60dae37d5a644d9ae5ea5a0f82b
milochen0418 <milo_chen@mail2000.com.tw>
2023-07-02 13:37:28 +0800

    C++/longest-even-odd-subarray-with-threshold.cpp finished by sliding-window

---
+++ C++/longest-even-odd-subarray-with-threshold.cpp
@@ -0,0 +1,19 @@
+class Solution {
+    //https://leetcode.com/problems/longest-even-odd-subarray-with-threshold
+public:
+    int longestAlternatingSubarray(vector<int>& nums, int threshold) {
+        int L=0, R=0, ans=0, n=nums.size();        
+        while(L<n && R<n) {
+            if(! (nums[L]%2==0 && nums[L]<=threshold)) {
+                R=++L;
+            } else if(R+1<n && nums[R+1]<=threshold && nums[R]%2!=nums[R+1]%2) {
+                R=R+1;
+                ans = max(ans, R-L+1);
+            } else {
+                L=++R;
+                ans = max(ans ,1);
+            }
+        }
+        return ans;
+    }
+};
