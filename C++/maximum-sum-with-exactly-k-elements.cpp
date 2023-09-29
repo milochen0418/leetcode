fa2c7c21b0fb0a945c5ed146d83e95e278f1fbf5
Milo Chen <milo_chen@mail2000.com.tw>
2023-09-29 10:47:01 +0800

    C++/maximum-sum-with-exactly-k-elements.cpp add article

--- C++/maximum-sum-with-exactly-k-elements.cpp
+++ C++/maximum-sum-with-exactly-k-elements.cpp
@@ -1,5 +1,6 @@
 class Solution {
     //https://leetcode.com/problems/maximum-sum-with-exactly-k-elements/
+    //article https://leetcode.com/problems/maximum-odd-binary-number/discuss/4103079/C%2B%2B-2-pass-and-1-pass-O(n)-solution-with-O(1)-space
 public:
     int maximizeSum(vector<int>& nums, int k) {
         int n = s.length(), L = 0, R=n-1, i = 0;
