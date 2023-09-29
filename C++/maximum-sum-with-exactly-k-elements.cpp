bfac06380b92a802e774860d37505316c93423dd
Milo Chen <milo_chen@mail2000.com.tw>
2023-09-29 10:43:19 +0800

    C++/maximum-sum-with-exactly-k-elements.cpp two-pointer one-pass in-placed array approach

--- C++/maximum-sum-with-exactly-k-elements.cpp
+++ C++/maximum-sum-with-exactly-k-elements.cpp
@@ -2,9 +2,12 @@ class Solution {
     //https://leetcode.com/problems/maximum-sum-with-exactly-k-elements/
 public:
     int maximizeSum(vector<int>& nums, int k) {
-        int maxv = 0, ans = 0;
-        for(auto &v:nums) maxv = max(maxv, v);
-        for(int i = 0; i<k;i++)  ans+=maxv++;
-        return ans;      
+        int n = s.length(), L = 0, R=n-1, i = 0;
+        while(L<R) 
+            if(s[L]=='1')i=L++;
+            else if(s[R] == '0') R--;
+            else swap(s[i=L], s[R]);
+        swap(s[i],s[n-1]);
+        return s;
     }
 };
