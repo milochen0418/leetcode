d150d175b0794448e43d6525a0bdfde7fc5bbaa8
milochen0418 <milo_chen@mail2000.com.tw>
2023-10-08 13:46:05 +0800

    C++/find-the-maximum-achievable-number.cpp finished by Math

---
+++ C++/find-the-maximum-achievable-number.cpp
@@ -0,0 +1,7 @@
+class Solution {
+    //https://leetcode.com/problems/find-the-maximum-achievable-number
+public:
+    int theMaximumAchievableX(int num, int t) {
+        return num+t*2;
+    }
+};
