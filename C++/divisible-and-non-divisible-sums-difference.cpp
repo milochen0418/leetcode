995f17318abbb8b7a6bf2bfcd9e12fdf431172bb
milochen0418 <milo_chen@mail2000.com.tw>
2023-10-09 14:48:30 +0800

    C++/divisible-and-non-divisible-sums-difference.cpp add article

--- C++/divisible-and-non-divisible-sums-difference.cpp
+++ C++/divisible-and-non-divisible-sums-difference.cpp
@@ -1,5 +1,6 @@
 class Solution {
     //https://leetcode.com/problems/divisible-and-non-divisible-sums-difference
+    //article https://leetcode.com/problems/divisible-and-non-divisible-sums-difference/discuss/4148635/C%2B%2B-One-Pass-simple-3-lines
 public:
     int differenceOfSums(int n, int m) {
         int ans=0;
