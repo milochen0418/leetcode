e32d4a8b4580088c61c6989633c7666783b8ad85
milochen0418 <milo_chen@mail2000.com.tw>
2023-10-18 00:05:54 +0800

    C++/rearrange-words-in-a-sentence.cpp finished by array approach and sorting

---
+++ C++/rearrange-words-in-a-sentence.cpp
@@ -0,0 +1,31 @@
+class Solution {
+    //https://leetcode.com/problems/rearrange-words-in-a-sentence
+public:
+    string arrangeWords(string text) {
+        vector<vector<int>> v; //{word_idx_start, word_idx_end, original_order}
+        text.push_back(' ');
+        int n = text.length(), L=0,R=0,order=0;
+        text[0] = text[0]-'A' +'a';
+        while(R<n) {
+            if(text[R]!=' ')
+                R++;
+            else {
+                v.push_back({L,R-L,order++});
+                L=++R;
+            }
+        }
+        
+        sort(v.begin(), v.end(), [&](auto &lhs, auto &rhs) {
+            return lhs[1] == rhs[1]?lhs[2] < rhs[2]:lhs[1] < rhs[1];
+        });
+        string ans;
+        for(auto &w: v) {
+            int m = w[0] + w[1];
+            for(int i = w[0]; i<m;i++) ans.push_back(text[i]);  
+            ans.push_back(' ');
+        }
+        ans.pop_back();
+        ans[0] = ans[0]-'a'+'A';
+        return ans;
+    }
+};
