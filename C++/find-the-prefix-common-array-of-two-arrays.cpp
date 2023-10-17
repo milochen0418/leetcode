df0610c1844840eca9d951f8bae19e7e86cc45c0
milochen0418 <milo_chen@mail2000.com.tw>
2023-10-17 23:33:04 +0800

    C++/find-the-prefix-common-array-of-two-arrays.cpp finished by one-pass O(N)

---
+++ C++/find-the-prefix-common-array-of-two-arrays.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    //https://leetcode.com/problems/find-the-prefix-common-array-of-two-arrays
+public:
+    vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
+        int n = A.size();
+        vector<int> ans = {0};
+        if(A[0] == B[0]) ans[0] = 1;
+        unordered_set<int> sA={A[0]}, sB={B[0]};
+        for(int i = 1; i<n; i++) {
+            int tmp = 0;
+            int valA = A[i], valB = B[i];
+            if(sA.find(valA) == sA.end()) 
+                if(sB.find(valA) != sB.end()) tmp++;
+            if(sB.find(valB) == sB.end()) 
+                if(sA.find(valB) != sA.end()) tmp++;
+            if(valA == valB) 
+                if(sA.find(valA) == sA.end() && sB.find(valB) == sB.end())  tmp++;
+            sA.insert(valA);
+            sB.insert(valB);
+            ans.push_back(ans.back() + tmp);
+        }
+        return ans;
+        
+    }
+};
