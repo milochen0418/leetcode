fba49ffb4685a93faea6e153f857d12e6f482db9
milochen0418 <milo_chen@mail2000.com.tw>
2023-10-19 20:20:22 +0800

    C++/find-the-score-of-all-prefixes-of-an-array.cpp finished by prefix sum

---
+++ C++/find-the-score-of-all-prefixes-of-an-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+    //https://leetcode.com/problems/find-the-score-of-all-prefixes-of-an-array
+public:
+    typedef long long ll;
+    vector<long long> findPrefixScore(vector<int>& nums) {
+        int n = nums.size();
+        vector<ll> conver = vector<ll>(n,0);
+        ll maxv = (ll)(INT_MIN);
+        for(int i = 0; i<n; i++) {
+            maxv = max(maxv, (ll)nums[i]);
+            conver[i] = (ll)nums[i] + maxv;
+            if(i>0)conver[i] += conver[i-1];
+        }
+        return conver;
+    }
+};
