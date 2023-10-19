775e4716d7bea26eaa8cdb3870b6418f572b6e92
milochen0418 <milo_chen@mail2000.com.tw>
2023-10-20 06:56:49 +0800

    C++/add-minimum-number-of-rungs.cpp add article

--- C++/add-minimum-number-of-rungs.cpp
+++ C++/add-minimum-number-of-rungs.cpp
@@ -1,5 +1,6 @@
 class Solution {
     //https://leetcode.com/problems/add-minimum-number-of-rungs
+    //article https://leetcode.com/problems/add-minimum-number-of-rungs/discuss/4187751/C%2B%2B-or-One-Pass-O(N)-simple-7-lines
 public:
     int addRungs(vector<int>& rungs, int dist) {
         int prev = 0, ans = 0;
