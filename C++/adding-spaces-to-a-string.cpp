234f802641832cf7e69051d5492e3937c38b0578
milochen0418 <milo_chen@mail2000.com.tw>
2023-10-19 20:30:46 +0800

    C++/adding-spaces-to-a-string.cpp add article

--- C++/adding-spaces-to-a-string.cpp
+++ C++/adding-spaces-to-a-string.cpp
@@ -1,5 +1,6 @@
 class Solution {
     //https://leetcode.com/problems/adding-spaces-to-a-string
+    //article https://leetcode.com/problems/adding-spaces-to-a-string/discuss/4186048/C%2B%2B-One-Pass-O(N)-simple-10-lines
 public:
     string addSpaces(string s, vector<int>& spaces) {
         int j = 0, i = 0, n = s.length();
