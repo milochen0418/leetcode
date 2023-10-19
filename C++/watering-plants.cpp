b3a8a7e4cca3bb303799918bddbbbffe07265ef2
milochen0418 <milo_chen@mail2000.com.tw>
2023-10-19 21:58:56 +0800

    C++/watering-plants.cpp add article

--- C++/watering-plants.cpp
+++ C++/watering-plants.cpp
@@ -1,5 +1,6 @@
 class Solution {
     //https://leetcode.com/problems/watering-plants
+    //article https://leetcode.com/problems/watering-plants/discuss/4186354/C%2B%2B-or-simple-9-lines-Simulate
 public:
     int wateringPlants(vector<int>& plants, int capacity) {
         int next_idx=0, n = plants.size(), ans=n, curr_capacity;
