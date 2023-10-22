04fdd6f4f962d6580fca71654c9e2c0406304690
milochen0418 <milo_chen@mail2000.com.tw>
2023-10-22 22:43:20 +0800

    C++/maximum-number-of-integers-to-choose-from-a-range-i.cpp add article

--- C++/maximum-number-of-integers-to-choose-from-a-range-i.cpp
+++ C++/maximum-number-of-integers-to-choose-from-a-range-i.cpp
@@ -1,5 +1,6 @@
 class Solution {
     //https://leetcode.com/problems/maximum-number-of-integers-to-choose-from-a-range-i
+    //article https://leetcode.com/problems/maximum-number-of-integers-to-choose-from-a-range-i/discuss/4196191/C%2B%2B-simple-10-lines-by-hash-set
 public:
     int maxCount(vector<int>& banned, int n, int maxSum) {
         unordered_set<int> bs;
