77ff4e756fe4cace07e3f8f641a2b098a5fc39c3
milochen0418 <milo_chen@mail2000.com.tw>
2023-10-24 18:43:05 +0800

    C++/destroying-asteroids.cpp

--- C++/destroying-asteroids.cpp
+++ C++/destroying-asteroids.cpp
@@ -1,5 +1,6 @@
 class Solution {
     //https://leetcode.com/problems/destroying-asteroids
+    //article https://leetcode.com/problems/destroying-asteroids/discuss/4203046/C%2B%2B-simple-8-lines-by-sort
 public:
     bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
         typedef long long ll;
