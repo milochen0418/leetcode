48c83baf624d68b7746f437ec5c121cf1f4a4da7
milochen0418 <milo_chen@mail2000.com.tw>
2023-10-24 22:18:51 +0800

    C++/rearrange-array-to-maximize-prefix-score.cpp finished by sort (refactor)

--- C++/rearrange-array-to-maximize-prefix-score.cpp
+++ C++/rearrange-array-to-maximize-prefix-score.cpp
@@ -2,29 +2,14 @@ class Solution {
     //https://leetcode.com/problems/rearrange-array-to-maximize-prefix-score
 public:
     int maxScore(vector<int>& nums) {
-        typedef long long ll;
-        int ans = 0; 
-        vector<int> pos;
-        vector<int> neg;
-        for(auto &i:nums) {
-            if(i== 0) ans++;
-            if(i < 0) neg.push_back(i);
-            if(i > 0) pos.push_back(i);
+        sort(nums.begin(), nums.end() ,[&](auto &lhs, auto &rhs){return lhs > rhs;});
+        int ans = 0;
+        long long sum = 0; 
+        for(auto &i : nums) {
+            sum+=(long long)i;
+            if(sum <= 0)break;
+            ans++;
         }
-        if(pos.size()==0) return 0;
-        sort(neg.begin(), neg.end());
-        reverse(neg.begin(), neg.end());
-        
-        ll sum = 0;
-        for(auto &i: pos) sum += (ll)i;
-        ans += pos.size();
-        
-        for(auto &i: neg) {
-            sum+= (ll)i;
-            if(sum>0) ans++;
-            else 
-                break;
-        }
-        return ans;        
+        return ans;
     }
 };
