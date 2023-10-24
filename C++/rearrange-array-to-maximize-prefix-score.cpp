029930ea8ddc794946b8e9cc1a6d01e2ef8126d0
milochen0418 <milo_chen@mail2000.com.tw>
2023-10-24 22:21:01 +0800

    C++/rearrange-array-to-maximize-prefix-score.cpp add article

--- C++/rearrange-array-to-maximize-prefix-score.cpp
+++ C++/rearrange-array-to-maximize-prefix-score.cpp
@@ -1,5 +1,6 @@
 class Solution {
     //https://leetcode.com/problems/rearrange-array-to-maximize-prefix-score
+    //article https://leetcode.com/problems/rearrange-array-to-maximize-prefix-score/discuss/4203831/C%2B%2B-simple-9-lines-by-sort
 public:
     int maxScore(vector<int>& nums) {
         sort(nums.begin(), nums.end() ,[&](auto &lhs, auto &rhs){return lhs > rhs;});
