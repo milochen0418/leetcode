3021e29e269bf83ce648f67fa5a52fa019dbf4ac
milochen0418 <milo_chen@mail2000.com.tw>
2023-10-25 22:42:33 +0800

    C++/count-number-of-teams.cpp add article

--- C++/count-number-of-teams.cpp
+++ C++/count-number-of-teams.cpp
@@ -1,5 +1,6 @@
 class Solution {
     //https://leetcode.com/problems/count-number-of-teams/
+    //article https://leetcode.com/problems/count-number-of-teams/discuss/4207613/C%2B%2B-explain-the-solution-from-brute-force-to-O(N2)
 public:
     int numTeams(vector<int>& rating) {
         int n = rating.size(), ans=0;
@@ -24,29 +25,5 @@ public:
             }
         }
         return ans;
-    }
-    int numTeams_v02(vector<int>& rating) {
-        int n = rating.size(), ans=0;;
-        printf("n = %d\n",n);
-        for(int i = 0; i<n; i++) {
-            for(int j = i+1;j<n;j++) {
-                if(rating[i]==rating[j]) continue;
-                if(rating[i]<rating[j]) {
-                    for(int k = j+1;k<n;k++) ans+=(rating[j]<rating[k]);
-                } else {
-                    for(int k = j+1;k<n;k++) ans+=(rating[j]>rating[k]);
-                }
-            }
-        }
-        return ans;
-    }
-
-    int numTeams_v01(vector<int>& rating) {
-        int n = rating.size(), ans=0;;
-        for(int i = 0; i<n; i++)
-            for(int j = i+1;j<n;j++)
-                for(int k = j+1;k<n;k++)
-                    ans+=( (rating[i]<rating[j] && rating[j]<rating[k]) || (rating[i]>rating[j] && rating[j]>rating[k]));
-        return ans;      
     }
 };
