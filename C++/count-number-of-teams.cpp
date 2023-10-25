4a2aff9ef13c45e74cd287d4b13fb3cccffbfd42
milochen0418 <milo_chen@mail2000.com.tw>
2023-10-25 22:32:22 +0800

    C++/count-number-of-teams.cpp refactor but TLE

--- C++/count-number-of-teams.cpp
+++ C++/count-number-of-teams.cpp
@@ -3,6 +3,21 @@ class Solution {
 public:
     int numTeams(vector<int>& rating) {
         int n = rating.size(), ans=0;;
+        printf("n = %d\n",n);
+        for(int i = 0; i<n; i++) {
+            for(int j = i+1;j<n;j++) {
+                if(rating[i]==rating[j]) continue;
+                if(rating[i]<rating[j]) {
+                    for(int k = j+1;k<n;k++) ans+=(rating[j]<rating[k]);
+                } else {
+                    for(int k = j+1;k<n;k++) ans+=(rating[j]>rating[k]);
+                }
+            }
+        }
+        return ans;
+    }
+    int numTeams_v01(vector<int>& rating) {
+        int n = rating.size(), ans=0;;
         for(int i = 0; i<n; i++)
             for(int j = i+1;j<n;j++)
                 for(int k = j+1;k<n;k++)
