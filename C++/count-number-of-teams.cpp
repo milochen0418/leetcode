cb87f72b2deee6a7fd6d88a778c15653121301d1
milochen0418 <milo_chen@mail2000.com.tw>
2023-10-25 22:37:22 +0800

    C++/count-number-of-teams.cpp finished by O(N^2)

--- C++/count-number-of-teams.cpp
+++ C++/count-number-of-teams.cpp
@@ -2,6 +2,30 @@ class Solution {
     //https://leetcode.com/problems/count-number-of-teams/
 public:
     int numTeams(vector<int>& rating) {
+        int n = rating.size(), ans=0;
+        vector<int> greater = vector<int>(n,0);
+        vector<int> smaller = vector<int>(n,0);
+        for(int j = 0;j<n;j++) {
+            for(int k = j+1;k<n; k++) {
+                if(rating[j]<rating[k]) smaller[j]++;
+                if(rating[j]>rating[k]) greater[j]++;
+            }
+        }
+        for(int i = 0; i<n; i++) {
+            for(int j = i+1;j<n;j++) {
+                if(rating[i]==rating[j]) continue;
+                if(rating[i]<rating[j]) {
+                    //for(int k = j+1;k<n;k++) ans+=(rating[j]<rating[k]);
+                    ans+=smaller[j];
+                } else {
+                    //for(int k = j+1;k<n;k++) ans+=(rating[j]>rating[k]);
+                    ans+=greater[j];
+                }
+            }
+        }
+        return ans;
+    }
+    int numTeams_v02(vector<int>& rating) {
         int n = rating.size(), ans=0;;
         printf("n = %d\n",n);
         for(int i = 0; i<n; i++) {
@@ -16,6 +40,7 @@ public:
         }
         return ans;
     }
+
     int numTeams_v01(vector<int>& rating) {
         int n = rating.size(), ans=0;;
         for(int i = 0; i<n; i++)
