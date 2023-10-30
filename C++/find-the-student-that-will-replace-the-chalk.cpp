1291e63434a87453b8a077d33542e0d7e60cc740
milochen0418 <milo_chen@mail2000.com.tw>
2023-10-30 09:34:29 +0800

    C++/find-the-student-that-will-replace-the-chalk.cpp finished by math

--- C++/find-the-student-that-will-replace-the-chalk.cpp
+++ C++/find-the-student-that-will-replace-the-chalk.cpp
@@ -2,6 +2,19 @@ class Solution {
     //https://leetcode.com/problems/find-the-student-that-will-replace-the-chalk/
 public:
     int chalkReplacer(vector<int>& chalk, int k) {
+        typedef long long ll;
+        int i = 0,n = chalk.size();
+        ll sum = 0;;
+        for(int i = 0; i<n;i++) sum += (ll)chalk[i];
+        k%=sum;
+        while(1) {
+            if(chalk[i]>k) break;
+            k-= chalk[i];
+            i = (i+1)%n;
+        }
+        return i;
+    }
+    int chalkReplacer_TLE(vector<int>& chalk, int k) {
         int i = 0,n = chalk.size();
         while(1) {
             if(chalk[i]>k) break;
