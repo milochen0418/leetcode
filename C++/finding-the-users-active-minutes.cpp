af3d98ad81e98dec3268850f84f8a466f16c3a55
milochen0418 <milo_chen@mail2000.com.tw>
2023-11-03 07:56:03 +0800

    C++/finding-the-users-active-minutes.cpp add article

--- C++/finding-the-users-active-minutes.cpp
+++ C++/finding-the-users-active-minutes.cpp
@@ -1,5 +1,6 @@
 class Solution {
     //https://leetcode.com/problems/finding-the-users-active-minutes
+    //article https://leetcode.com/problems/finding-the-users-active-minutes/discuss/4241190/C%2B%2B-or-Two-Pass-or-5-lines
 public:
     vector<int> findingUsersActiveMinutes(vector<vector<int>>& logs, int k) {
         vector<int> ans = vector<int>(k,0);
