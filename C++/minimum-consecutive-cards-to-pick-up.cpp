d78d0686ef96abc3b691da25fd5a721df2d41f9d
milochen0418 <milo_chen@mail2000.com.tw>
2023-11-11 18:47:43 +0800

    C++/minimum-consecutive-cards-to-pick-up.cpp finished by hashmap onepass

---
+++ C++/minimum-consecutive-cards-to-pick-up.cpp
@@ -0,0 +1,15 @@
+class Solution {
+    //https://leetcode.com/problems/minimum-consecutive-cards-to-pick-up
+public:
+    int minimumCardPickup(vector<int>& cards) {
+        unordered_map<int,int> mp;
+        int n = cards.size();
+        int ans = INT_MAX;
+        for(int i = 0; i<n; i++) {
+            if(mp.find(cards[i]) != mp.end()) 
+                ans = min(ans, i - mp[cards[i]] + 1);
+            mp[cards[i]] = i;
+        }
+        return ans == INT_MAX ? -1:ans; 
+    }
+};
