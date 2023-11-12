d97f3503040e253cfa117ef67641cf67af5906f1
milochen0418 <milo_chen@mail2000.com.tw>
2023-11-12 21:00:08 +0800

    C++/distribute-candies-among-children-i.cpp finished by loop

---
+++ C++/distribute-candies-among-children-i.cpp
@@ -0,0 +1,12 @@
+class Solution {
+    //https://leetcode.com/problems/distribute-candies-among-children-i
+public:
+    int distributeCandies(int n, int limit) {
+        int ans = 0,m = min(n,limit);
+        for(int i = 0; i<=m;i++) 
+            for(int j = 0; j<=m;j++) 
+                for(int k = 0; k<=m;k++) 
+                    if(n == i+j+k ) ans++;
+        return ans;
+    }
+};
