615140837d2328fe7fc32ff08216061c8a388968
milochen0418 <milo_chen@mail2000.com.tw>
2023-11-12 23:42:44 +0800

    C++/maximum-strong-pair-xor-i.cpp add article

--- C++/maximum-strong-pair-xor-i.cpp
+++ C++/maximum-strong-pair-xor-i.cpp
@@ -1,5 +1,6 @@
 class Solution {
     //https://leetcode.com/problems/maximum-strong-pair-xor-i
+    //article https://leetcode.com/problems/maximum-strong-pair-xor-i/discuss/4279833/C%2B%2B-or-simple-7-lines
 public:
     int maximumStrongPairXor(vector<int>& nums) {
         int n=nums.size(), ans=0;
