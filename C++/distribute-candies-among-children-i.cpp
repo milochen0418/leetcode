0daf73f5d753cd4095cfa9b51000d1b6f25fccf0
milochen0418 <milo_chen@mail2000.com.tw>
2023-11-12 21:03:01 +0800

    C++/distribute-candies-among-children-i.cpp add article

--- C++/distribute-candies-among-children-i.cpp
+++ C++/distribute-candies-among-children-i.cpp
@@ -1,5 +1,6 @@
 class Solution {
     //https://leetcode.com/problems/distribute-candies-among-children-i
+    //article https://leetcode.com/problems/distribute-candies-among-children-i/discuss/4279629/C%2B%2B-or-simple-6-lines-Space-O(1)-Time-O(M3)
 public:
     int distributeCandies(int n, int limit) {
         int ans = 0,m = min(n,limit);
