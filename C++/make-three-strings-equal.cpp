65bdf35e02c316f8b0a9c2fef61d79f3f2c5323e
milochen0418 <milo_chen@mail2000.com.tw>
2023-11-29 23:20:16 +0800

    C++/make-three-strings-equal.cpp finished by loop

---
+++ C++/make-three-strings-equal.cpp
@@ -0,0 +1,10 @@
+class Solution {
+    //https://leetcode.com/problems/make-three-strings-equal
+public:
+    int findMinimumOperations(string s1, string s2, string s3) {
+        int n1 = s1.length(), n2=s2.length(), n3=s3.length();
+        int L=0, H = min(n1,min(n2,n3));
+        for(L = 0; L<H; L++) if(s1[L] != s2[L] ||  s2[L] != s3[L]) break;
+        return L==0?-1:(n1-L) + (n2-L) +(n3-L);
+    }
+};
