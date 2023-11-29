9991bb00c6838219ee6086f7f6686c29d2ab7f90
milochen0418 <milo_chen@mail2000.com.tw>
2023-11-29 23:22:03 +0800

    C++/make-three-strings-equal.cpp add article

--- C++/make-three-strings-equal.cpp
+++ C++/make-three-strings-equal.cpp
@@ -1,5 +1,6 @@
 class Solution {
     //https://leetcode.com/problems/make-three-strings-equal
+    //article https://leetcode.com/problems/make-three-strings-equal/discuss/4343407/C%2B%2B-or-simple-4-lines-O(N)-solution
 public:
     int findMinimumOperations(string s1, string s2, string s3) {
         int n1 = s1.length(), n2=s2.length(), n3=s3.length();
