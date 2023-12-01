b082e63c91bea2e37e0e1639279dd20d338765f0
milochen0418 <milo_chen@mail2000.com.tw>
2023-12-02 00:41:11 +0800

    C++/pairs-of-songs-with-total-durations-divisible-by-60.cpp solved by int array only

--- C++/pairs-of-songs-with-total-durations-divisible-by-60.cpp
+++ C++/pairs-of-songs-with-total-durations-divisible-by-60.cpp
@@ -2,6 +2,23 @@ class Solution {
     //https://leetcode.com/problems/pairs-of-songs-with-total-durations-divisible-by-60
 public:
     int numPairsDivisibleBy60(vector<int>& time) {
+        vector<int> mp = vector<int>(60,0);
+        for(auto &t:time) mp[(t%60)]++;
+        int ans = 0;
+        for(int i = 0;i<=30;i+=30) {
+            int n=mp[i];
+            if(n%2==0) {
+                int c = n/2;
+                ans+=(c*c*2-c);
+            } else {
+                int a = (n+1)/2, b = (n-1)/2;
+                ans+= (a*b*2-b);
+            }
+        }
+        for(int i = 1;i<=29;i++) ans+=mp[i]*mp[60-i];
+        return ans;
+    }
+    int numPairsDivisibleBy60_v01(vector<int>& time) {
         typedef long long ll;
         vector<ll> mp = vector<ll>(60,0);
         for(auto &t:time) mp[(t%60)]++;
