55f2f618dd8a9cd14716ecd69287ee60fed31108
milochen0418 <milo_chen@mail2000.com.tw>
2023-12-02 21:13:15 +0800

    C++/maximum-matching-of-players-with-trainers.cpp add aritcle

--- C++/maximum-matching-of-players-with-trainers.cpp
+++ C++/maximum-matching-of-players-with-trainers.cpp
@@ -1,5 +1,6 @@
 class Solution {
     //https://leetcode.com/problems/maximum-matching-of-players-with-trainers
+    //article https://leetcode.com/problems/maximum-matching-of-players-with-trainers/discuss/4353873/C%2B%2B-or-time-O(MlogM%2BNlogN)
 public:
     int matchPlayersAndTrainers(vector<int>& players, vector<int>& trainers) {
         sort(players.begin(), players.end());
