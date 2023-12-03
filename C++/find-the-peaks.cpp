44a4985f37e52a9ff2aa5a724c7a4b6b9a66dc0c
milochen0418 <milo_chen@mail2000.com.tw>
2023-12-03 20:27:33 +0800

    C++/find-the-peaks.cpp refactor

--- C++/find-the-peaks.cpp
+++ C++/find-the-peaks.cpp
@@ -3,10 +3,11 @@ class Solution {
 public:
     vector<int> findPeaks(vector<int>& mountain) {
         vector<int> ans;
+        int n = mountain.size(); 
         mountain.push_back(INT_MAX);
-        for(int i = 1;i<mountain.size()-1;i++) 
+        for(int i = 1;i<n-1;i++) 
             if(mountain[i]>mountain[i-1] && mountain[i]>mountain[i+1]) 
-                ans.push_back(i);
+                ans.push_back(i++);//Make i go step 2 because the two peak cannot be neighbor.
         return ans;
     }
 };
