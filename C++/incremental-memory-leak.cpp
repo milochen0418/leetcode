b7a80193e409cab98d85d312159346f0251c933b
milochen0418 <milo_chen@mail2000.com.tw>
2023-12-05 22:35:44 +0800

    C++/incremental-memory-leak.cpp add article

--- C++/incremental-memory-leak.cpp
+++ C++/incremental-memory-leak.cpp
@@ -1,7 +1,14 @@
 class Solution {
     //https://leetcode.com/problems/incremental-memory-leak
+    //article https://leetcode.com/problems/incremental-memory-leak/discuss/4366151/C%2B%2B-simple-3-lines-with-explain
 public:
-    vector<int> memLeak(int memory1, int memory2) {
+    vector<int> memLeak(int m1, int m2) {
+        int i = 1;
+        while(i<=m1||i<=m2) (m2>m1?m2:m1)-=i++;
+        return {i,m1, m2};
+    }
+
+    vector<int> memLeak_v01(int memory1, int memory2) {
         int i = 1;
         while(true) {
             if(i>memory1  && i >memory2) break;
