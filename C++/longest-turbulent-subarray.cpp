92301cfdd3393ad7751cdf7df3caccc850d9313e
milochen0418 <milo_chen@mail2000.com.tw>
2023-12-09 03:41:07 +0800

    C++/longest-turbulent-subarray.cpp update article

--- C++/longest-turbulent-subarray.cpp
+++ C++/longest-turbulent-subarray.cpp
@@ -1,6 +1,6 @@
 class Solution {
     //https://leetcode.com/problems/longest-turbulent-subarray
-    //article https://leetcode.com/problems/longest-turbulent-subarray/discuss/4379337/C%2B%2B-One-Pass-O(N)-and-space-O(1)-with-explain
+    //article https://leetcode.com/problems/longest-turbulent-subarray/discuss/4379337/C%2B%2B-9-lines-One-Pass-O(N)-and-space-O(1)-with-explain
 public:
     int maxTurbulenceSize(vector<int>& arr) {
         char lastc = '=';
