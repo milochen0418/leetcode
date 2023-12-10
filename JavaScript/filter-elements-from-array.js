2d03d9954d1688e19e743562ed5f9c22c9cea4c1
milochen0418 <milo_chen@mail2000.com.tw>
2023-12-10 23:06:19 +0800

    JavaScript/filter-elements-from-array.js finished by Array.filter

---
+++ JavaScript/filter-elements-from-array.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+//https://leetcode.com/problems/filter-elements-from-array
+var filter = function(arr, fn) {
+    return arr.filter(fn)
+};
