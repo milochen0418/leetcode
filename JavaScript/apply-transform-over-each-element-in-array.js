55e9a61b6bb5be96362eda05c8e04644391af7f8
milochen0418 <milo_chen@mail2000.com.tw>
2023-12-10 23:05:02 +0800

    JavaScript/apply-transform-over-each-element-in-array.js finished by map

---
+++ JavaScript/apply-transform-over-each-element-in-array.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+//https://leetcode.com/problems/apply-transform-over-each-element-in-array
+var map = function(arr, fn) {
+    return arr.map(fn)  
+};
