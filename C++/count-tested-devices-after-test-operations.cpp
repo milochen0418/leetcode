dd51d167e693623d4e9106188f403c9746090a90
milochen0418 <milo_chen@mail2000.com.tw>
2023-12-10 15:02:00 +0800

    C++/count-tested-devices-after-test-operations.cpp add article

--- C++/count-tested-devices-after-test-operations.cpp
+++ C++/count-tested-devices-after-test-operations.cpp
@@ -1,5 +1,6 @@
 class Solution {
     //https://leetcode.com/problems/count-tested-devices-after-test-operations
+    //article https://leetcode.com/problems/count-tested-devices-after-test-operations/discuss/4385215/C%2B%2B-or-simple-3-lines-O(N)
 public:
     int countTestedDevices(vector<int>& batteryPercentages) {
         int ans = 0;
