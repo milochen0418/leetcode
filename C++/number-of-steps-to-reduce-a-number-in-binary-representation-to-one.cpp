690883dee3951d395e44a3629d8ff5558ae73a34
milochen0418 <milo_chen@mail2000.com.tw>
2023-12-10 23:54:31 +0800

    C++/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp finished by simulate

---
+++ C++/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
@@ -0,0 +1,31 @@
+class Solution {
+    //https://leetcode.com/problems/number-of-steps-to-reduce-a-number-in-binary-representation-to-one/
+public:
+    int numSteps(string s) {
+        s = "0"+s; 
+        int ans = 0;
+        
+        while(  //while (s != "01" && s != "1")
+            (!(s.length()==1 && s[0]=='1'))  &&
+            (!(s.length()==2 && s[0]=='0' && s[1]=='1'))
+         ) {
+            if(s.back()=='0') {
+                s.pop_back();
+                ans++;
+            } else {
+                int n = s.length();
+                int i = n-1;
+                for(; i>=0; i--) {
+                    if(s[i]=='0') {
+                        s[i]='1';
+                        ans=ans+1;
+                        break;
+                    } else {
+                        s[i]='0';
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
