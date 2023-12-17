f3d0c2c71307ea0ebac974f00ac809bf505fd3f3
milochen0418 <milo_chen@mail2000.com.tw>
2023-12-17 14:24:16 +0800

    C++/clumsy-factorial.cpp finished by recursive approach

---
+++ C++/clumsy-factorial.cpp
@@ -0,0 +1,27 @@
+
+class Solution {
+    //https://leetcode.com/problems/clumsy-factorial
+public:
+    int clumsy(int N) {
+        /*
+        10 * 9 / 8 + 7 - 6 * 5 / 4 + 3 - 2 * 1.
+        (10*9/8) + 7 -1*(6 * 5 / 4)+3 -1*(2 * 1); 
+        */
+        function<double(double)> sol = [&](double n) -> double {
+            if(n<=1.1f) return 0.0f;
+            double val = n;
+            if(abs(val-(double)N) > 0.5f) val = val*-1;
+            val = val * (n-1>0.9f?n-1:1.0f);
+            val = val / (n-2>0.9f?n-2:1.0f);
+            val = val + (n-3>0.9f?n-3:0.0f);
+            //printf("%f\n", val);
+            val = val + round(sol(n-4));
+            return val;
+        };
+        double ans = sol((double)N);
+        //printf("sol(N=%d) = %f\n",N,ans);
+        ans = round(ans);
+        return (int)ans;
+    }
+};
+
