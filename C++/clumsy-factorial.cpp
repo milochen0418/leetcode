df98e6c495555c0402503b1e773c0c58ede33480
milochen0418 <milo_chen@mail2000.com.tw>
2023-12-17 23:14:15 +0800

    C++/clumsy-factorial.cpp finished by recursive (fix bug)

--- C++/clumsy-factorial.cpp
+++ C++/clumsy-factorial.cpp
@@ -1,28 +1,23 @@
-
 class Solution {
-    //https://leetcode.com/problems/clumsy-factorial
-    //article https://leetcode.com/problems/clumsy-factorial/discuss/4415320/C%2B%2B-or-Recursive-Approach-O(N)-with-explain
+    https://leetcode.com/problems/clumsy-factorial///
 public:
     int clumsy(int N) {
         /*
         10 * 9 / 8 + 7 - 6 * 5 / 4 + 3 - 2 * 1.
         (10*9/8) + 7 -1*(6 * 5 / 4)+3 -1*(2 * 1); 
         */
-        function<double(double)> sol = [&](double n) -> double {
-            if(n<=1.1f) return 0.0f;
-            double val = n;
-            if(abs(val-(double)N) > 0.5f) val = val*-1;
-            val = val * (n-1>0.9f?n-1:1.0f);
-            val = val / (n-2>0.9f?n-2:1.0f);
-            val = val + (n-3>0.9f?n-3:0.0f);
-            //printf("%f\n", val);
-            val = val + round(sol(n-4));
+        function<int(int)> sol = [&](int n)-> int {
+            if(n<1) return 0;
+            int val = n;
+            if(N!=n) val = val * -1;
+            int b = (n-1>=1)?n-1:1;
+            int c = (n-2>=1)?n-2:1;
+            int d = (n-3>=1)?n-3:0;
+            val = ((val * b)/c)+d;
+            val = val + sol(n-4);
             return val;
         };
-        double ans = sol((double)N);
-        //printf("sol(N=%d) = %f\n",N,ans);
-        ans = round(ans);
-        return (int)ans;
+        return sol(N);        
     }
-};
 
+};
