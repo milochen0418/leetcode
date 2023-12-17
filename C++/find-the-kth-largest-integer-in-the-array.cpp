c0745cc2947b21a897201fd5d0d7c31b4fc17f81
milochen0418 <milo_chen@mail2000.com.tw>
2023-12-18 01:56:27 +0800

    C++/find-the-kth-largest-integer-in-the-array.cpp refactor code

--- C++/find-the-kth-largest-integer-in-the-array.cpp
+++ C++/find-the-kth-largest-integer-in-the-array.cpp
@@ -1,6 +1,20 @@
 class Solution {
     //https://leetcode.com/problems/find-the-kth-largest-integer-in-the-array
 public:
+    //maintain an k number of Min priority_queue
+    //keep pop and push new element when size of queue >=k
+    string kthLargestNumber(vector<string>& nums, int k) {
+        auto comp = [](const std::string& a, const std::string& b) {
+            return a.length() == b.length() ? a > b : a.length() > b.length();
+        };        
+        priority_queue<string, vector<string>, decltype(comp)> minQ;
+        for(auto &s: nums) {
+            minQ.push(s);
+            if(minQ.size()>k) minQ.pop();
+        }
+        return minQ.top();
+    }
+
     struct comp_num_str{
         bool operator()(const std::string& a, const std::string& b) const {
             if(a.length()==b.length()) {
@@ -11,9 +25,7 @@ public:
         }
     };  
     
-    string kthLargestNumber(vector<string>& nums, int k) {
-        //maintain an k number of Min priority_queue
-        //keep pop and push new element when size of queue >=k
+    string kthLargestNumber_v01(vector<string>& nums, int k) {
         priority_queue<string, vector<string>, comp_num_str> minQ;
         for(auto &s: nums) {
             minQ.push(s);
