0e41e12fad430ec50e17559d422e70de3d688b77
milochen0418 <milo_chen@mail2000.com.tw>
2023-12-19 09:18:59 +0800

    C++/find-the-minimum-possible-sum-of-a-beautiful-array.cpp meet the issue. Cannot process /2 for mod 1e9+7 correctly

--- C++/find-the-minimum-possible-sum-of-a-beautiful-array.cpp
+++ C++/find-the-minimum-possible-sum-of-a-beautiful-array.cpp
@@ -2,6 +2,36 @@ class Solution {
     //https://leetcode.com/problems/find-the-minimum-possible-sum-of-a-beautiful-array
 public:
     int minimumPossibleSum(int n, int target) {
+        //7,15
+        //1 ,2 ,3 ,4 ,5, 6, 7
+        //14,13,12,11,10,9, 8
+        //ans = 28
+
+        //8,15
+        //1 ,2 ,3 ,4 ,5, 6, 7
+        //14,13,12,11,10,9, 8
+        //ans = 28+15=43
+        
+        //9,15
+        //1 ,2 ,3 ,4 ,5, 6, 7
+        //14,13,12,11,10,9, 8
+        //ans = 43+16=59
+        //28 + ((15+16)*2)/2
+
+        //9,15
+        //1,2,3,4,5,6,7
+        
+        
+        //7,16
+        //1 ,2 ,3 ,4 ,5, 6, 7, "_"
+        //15,14,13,12,11,10,9,
+        //ans is 28
+
+        //8,16
+        //1 ,2 ,3 ,4 ,5, 6, 7, "8"
+        //15,14,13,12,11,10,9,
+        //ans is 36
+        
         typedef long long ll;
         const ll MOD = 1e9+7;
         ll ans = 0;
@@ -12,13 +42,32 @@ public:
             Lcnt++;
             upwidth += 1;
         }
+       
         if(n<=Lcnt) {
+            downwidth = 1;
+            upwidth = n;
+            ll high = n; 
             ll updown = ((upwidth%MOD) + (downwidth%MOD)) % MOD;
-            ll twoarea = ((updown%MOD) * (Lcnt%MOD)) % MOD;
+            ll twoarea = ((updown%MOD) * (high%MOD)) % MOD;
             return twoarea/2;
+        } else {
+            downwidth = 1;
+            upwidth = Lcnt;
+            ll high = Lcnt; 
+            ll updown = ((upwidth%MOD) + (downwidth%MOD)) % MOD;
+            ll twoarea = ((updown%MOD) * (high%MOD)) % MOD;
+            n-=Lcnt;
+            ans = twoarea; 
         }
-        return 0;
 
+        downwidth = target;
+        upwidth = target -1+ n;
+        ll newhigh = n;
+        ll newupdown = ((upwidth%MOD) + (downwidth%MOD)) % MOD;
+        ll newtwoarea = ((newupdown%MOD) * (newhigh%MOD)) % MOD;
+        ans = ((ans%MOD) + (newtwoarea%MOD)) %MOD;
+        return ans/2;
+        
     }
     int minimumPossibleSum_v02(int n, int target) {
         //7,15
